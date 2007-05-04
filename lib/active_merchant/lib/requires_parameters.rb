a1795700ce4314211e728039dfbde1cf8e2bfdc3
codyfauser <codyfauser@6513ea26-6c20-0410-8a68-89cd7235086d>
2007-05-04 13:56:50 +0000

    Improve the error message generated by requires
    git-svn-id: https://activemerchant.googlecode.com/svn/trunk/active_merchant@322 6513ea26-6c20-0410-8a68-89cd7235086d

--- lib/active_merchant/lib/requires_parameters.rb
+++ lib/active_merchant/lib/requires_parameters.rb
@@ -4,10 +4,10 @@ module ActiveMerchant #:nodoc:
       keys = hash.keys
       params.each do |param| 
         if param.is_a?(Array)
-          raise ArgumentError.new("Missing required parameter: #{param}") unless keys.include?(param.first) 
+          raise ArgumentError.new("Missing required parameter: #{param.first}") unless keys.include?(param.first) 
 
           valid_options = param[1..-1]
-          raise ArgumentError.new("Parameter :#{param.first} must be one of #{valid_options.inspect}") unless valid_options.include?(hash[param.first])
+          raise ArgumentError.new("Parameter: #{param.first} must be one of #{valid_options.to_sentence(:connector => 'or')}") unless valid_options.include?(hash[param.first])
         else
           raise ArgumentError.new("Missing required parameter: #{param}") unless keys.include?(param) 
         end
