5e09474cc4fc90df8049a363836722fd59affe92
codyfauser <codyfauser@6513ea26-6c20-0410-8a68-89cd7235086d>
2007-06-05 16:25:48 +0000

    Add metadata to securepay gateway
    git-svn-id: https://activemerchant.googlecode.com/svn/trunk/active_merchant@363 6513ea26-6c20-0410-8a68-89cd7235086d

--- lib/active_merchant/billing/gateways/secure_pay.rb
+++ lib/active_merchant/billing/gateways/secure_pay.rb
@@ -3,6 +3,9 @@ module ActiveMerchant #:nodoc:
     class SecurePayGateway < AuthorizeNetGateway
       self.live_url = self.test_url = 'https://www.securepay.com/AuthSpayAdapter/process.aspx'
       
+      self.homepage_url = 'http://www.securepay.com/'
+      self.display_name = 'SecurePay'
+      
       private
       def split(response)
         response.split('%')
