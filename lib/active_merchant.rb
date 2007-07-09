d3650a7b94e7c359aaf6ee712c3e3b106f49c65d
codyfauser <codyfauser@6513ea26-6c20-0410-8a68-89cd7235086d>
2007-07-09 20:10:21 +0000

    Minor documentation shuffling
    git-svn-id: https://activemerchant.googlecode.com/svn/trunk/active_merchant@394 6513ea26-6c20-0410-8a68-89cd7235086d

--- lib/active_merchant.rb
+++ lib/active_merchant.rb
@@ -1,5 +1,5 @@
 #--
-# Copyright (c) 2005 Tobias Luetke
+# Copyright (c) 2005-2007 Tobias Luetke
 #
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
