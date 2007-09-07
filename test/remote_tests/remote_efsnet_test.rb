6af756b77e1031d3bedf7edf7a5c312d1971a311
codyfauser <codyfauser@6513ea26-6c20-0410-8a68-89cd7235086d>
2007-09-07 16:44:35 +0000

    Add support for Concord Efsnet payment gateway
    git-svn-id: https://activemerchant.googlecode.com/svn/trunk/active_merchant@418 6513ea26-6c20-0410-8a68-89cd7235086d

---
+++ test/remote_tests/remote_efsnet_test.rb
@@ -0,0 +1,100 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class RemoteEfsnetTest < Test::Unit::TestCase
+  
+  LOGIN    = 'LOGIN'
+  PASSWORD = 'PASSWORD'
+  
+  AMOUNT = 100
+  DECLINED_AMOUNT = 156
+
+  def setup
+    ActiveMerchant::Billing::Base.gateway_mode = :test
+
+    @gateway = EfsnetGateway.new(
+      :login => LOGIN,
+      :password => PASSWORD
+    )
+
+    @creditcard = credit_card('4000100011112224')
+
+    @options = { :order_id => 0, 
+                 :address => { :address1 => '1234 Shady Brook Lane',
+                               :zip => '90210'
+                             }
+               }
+  end
+  
+  def test_successful_purchase
+    @options[:order_id] += 1
+    assert response = @gateway.purchase(AMOUNT, @creditcard, @options)
+    assert_equal 'Approved', response.message
+    assert response.success?
+    assert response.test?
+  end
+
+  def test_successful_force
+    @options[:order_id] += 1
+    assert response = @gateway.force(AMOUNT, '123456', @creditcard, @options)
+    assert_equal 'Approved', response.message
+    assert response.success?
+  end
+
+  def test_successful_voice_authorize
+    @options[:order_id] += 1
+    assert response = @gateway.voice_authorize(AMOUNT, '123456', @creditcard, @options)
+    assert_equal 'Accepted', response.message
+    assert response.success?
+  end
+
+  def test_unsuccessful_purchase
+    @options[:order_id] += 1
+    assert response = @gateway.purchase(DECLINED_AMOUNT, @creditcard, @options)
+    assert_equal 'Declined', response.message
+    assert !response.success?
+  end
+
+  def test_authorize_and_capture
+    @options[:order_id] += 1
+    
+    amount = AMOUNT
+    assert auth = @gateway.authorize(amount, @creditcard, @options)
+    assert auth.success?    
+    assert_equal 'Approved', auth.message
+    assert auth.authorization
+    
+    @options[:order_id] += 1
+    assert capture = @gateway.capture(amount, auth.authorization, @options)
+    assert capture.success?
+  end
+
+  def test_purchase_and_void
+    @options[:order_id] += 1
+    amount = AMOUNT
+    assert purchase = @gateway.purchase(amount, @creditcard, @options)
+    assert purchase.success?
+    assert_equal 'Approved', purchase.message
+    assert purchase.authorization
+    @options[:order_id] += 1
+    assert void = @gateway.void(purchase.authorization, @options)
+    assert void.success?
+  end
+
+  def test_failed_capture
+    @options[:order_id] += 1
+    assert response = @gateway.capture(AMOUNT, '1;1', @options)
+    assert !response.success?
+    assert_equal 'Bad original transaction', response.message
+  end
+
+  def test_invalid_login
+    @options[:order_id] += 1
+    gateway = EfsnetGateway.new(
+      :login => '',
+      :password => ''
+    )
+    assert response = gateway.purchase(AMOUNT, @creditcard, @options)
+    assert_equal 'Invalid credentials', response.message
+    assert !response.success?
+  end
+end
---
+++ test/unit/gateways/efsnet_test.rb
@@ -0,0 +1,70 @@
+require File.dirname(__FILE__) + '/../../test_helper'
+
+class EfsnetTest < Test::Unit::TestCase
+  AMOUNT = 100
+
+  def setup
+    @gateway = EfsnetGateway.new(
+      :login => 'LOGIN',
+      :password => 'PASSWORD'
+    )
+
+    @creditcard = credit_card('4242424242424242')
+
+    @address = { :address1 => '1234 My Street',
+                 :address2 => 'Apt 1',
+                 :company => 'Widgets Inc',
+                 :city => 'Ottawa',
+                 :state => 'ON',
+                 :zip => 'K1C2N6',
+                 :country => 'Canada',
+                 :phone => '(555)555-5555'
+               }
+   @options = {:order_id => 1}
+  end
+  
+  def test_successful_request
+    @creditcard.number = 1
+    assert response = @gateway.purchase(AMOUNT, @creditcard, @options)
+    assert response.success?
+    assert_equal '5555', response.authorization
+    assert response.test?
+  end
+
+  def test_unsuccessful_request
+    @creditcard.number = 2
+    assert response = @gateway.purchase(AMOUNT, @creditcard, @options)
+    assert !response.success?
+    assert response.test?
+  end
+
+  def test_request_error
+    @creditcard.number = 3
+    assert_raise(Error){ @gateway.purchase(AMOUNT, @creditcard, @options) }
+  end
+
+  def test_authorize_is_valid_xml
+   params = {
+     :order_id => "order1",
+     :transaction_amount => "1.01",
+     :account_number => "4242424242424242",
+     :expiration_month => "12",
+     :expiration_year => "2029",
+   }
+
+   assert data = @gateway.send(:post_data, :credit_card_authorize, params)
+   assert REXML::Document.new(data)
+  end
+
+  def test_settle_is_valid_xml
+   params = {
+     :order_id => "order1",
+     :transaction_amount => "1.01",
+     :original_transaction_amount => "1.01",
+     :original_transaction_id => "1",
+   }
+
+   assert data = @gateway.send(:post_data, :credit_card_settle, params)
+   assert REXML::Document.new(data)
+  end
+end
