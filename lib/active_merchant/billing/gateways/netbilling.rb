1cdf1010065e36d134889ee990f7a81955ea9478
codyfauser <codyfauser@6513ea26-6c20-0410-8a68-89cd7235086d>
2007-10-10 15:23:47 +0000

    fix jcb card type in netbilling gateway
    git-svn-id: https://activemerchant.googlecode.com/svn/trunk/active_merchant@449 6513ea26-6c20-0410-8a68-89cd7235086d

--- lib/active_merchant/billing/gateways/netbilling.rb
+++ lib/active_merchant/billing/gateways/netbilling.rb
@@ -23,7 +23,7 @@ module ActiveMerchant #:nodoc:
       self.display_name = 'NETbilling'
       self.homepage_url = 'http://www.netbilling.com'
       self.supported_countries = ['US']
-      self.supported_cardtypes = [:visa, :master, :american_express, :discover, :jsb, :diners_club]
+      self.supported_cardtypes = [:visa, :master, :american_express, :discover, :jcb, :diners_club]
       
       def initialize(options = {})
         requires!(options, :login)
