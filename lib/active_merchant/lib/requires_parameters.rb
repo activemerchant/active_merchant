92a052becea04bff66cb839b15950eeebbfa09a9
codyfauser <codyfauser@6513ea26-6c20-0410-8a68-89cd7235086d>
2007-10-30 19:32:33 +0000

    Update RequiresParameters to support HashWithIndifferentAccess
    git-svn-id: https://activemerchant.googlecode.com/svn/trunk/active_merchant@459 6513ea26-6c20-0410-8a68-89cd7235086d

--- lib/active_merchant/lib/requires_parameters.rb
+++ lib/active_merchant/lib/requires_parameters.rb
@@ -1,15 +1,14 @@
 module ActiveMerchant #:nodoc:
   module RequiresParameters #:nodoc:
     def requires!(hash, *params)
-      keys = hash.keys
       params.each do |param| 
         if param.is_a?(Array)
-          raise ArgumentError.new("Missing required parameter: #{param.first}") unless keys.include?(param.first) 
+          raise ArgumentError.new("Missing required parameter: #{param.first}") unless hash.has_key?(param.first) 
 
           valid_options = param[1..-1]
           raise ArgumentError.new("Parameter: #{param.first} must be one of #{valid_options.to_sentence(:connector => 'or')}") unless valid_options.include?(hash[param.first])
         else
-          raise ArgumentError.new("Missing required parameter: #{param}") unless keys.include?(param) 
+          raise ArgumentError.new("Missing required parameter: #{param}") unless hash.has_key?(param) 
         end
       end
     end    
