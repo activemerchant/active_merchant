940778a9ad066a59a87b34f24ed8a01f1405b4f8
codyfauser <codyfauser@6513ea26-6c20-0410-8a68-89cd7235086d>
2008-01-03 17:13:49 +0000

    handle return from 2checkout
    git-svn-id: https://activemerchant.googlecode.com/svn/trunk/active_merchant@512 6513ea26-6c20-0410-8a68-89cd7235086d

--- lib/active_merchant/billing/integrations/two_checkout/return.rb
+++ lib/active_merchant/billing/integrations/two_checkout/return.rb
@@ -3,6 +3,13 @@ module ActiveMerchant #:nodoc:
     module Integrations #:nodoc:
       module TwoCheckout
         class Return < ActiveMerchant::Billing::Integrations::Return
+          def success?
+            params['credit_card_processed'] == 'Y'
+          end
+          
+          def message
+            
+          end
 	      end
       end
     end
