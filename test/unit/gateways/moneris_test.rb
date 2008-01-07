bb7cedcd68c2ae546ae83c9e9058f730fb396635
codyfauser <codyfauser@6513ea26-6c20-0410-8a68-89cd7235086d>
2008-01-07 04:33:32 +0000

    Update name of Moneris test suite
    git-svn-id: https://activemerchant.googlecode.com/svn/trunk/active_merchant@542 6513ea26-6c20-0410-8a68-89cd7235086d

--- test/unit/gateways/moneris_test.rb
+++ test/unit/gateways/moneris_test.rb
@@ -1,6 +1,6 @@
 require File.dirname(__FILE__) + '/../../test_helper'
 
-class MonerisResponseTest < Test::Unit::TestCase
+class MonerisTest < Test::Unit::TestCase
   def setup
     Base.mode = :test
     
