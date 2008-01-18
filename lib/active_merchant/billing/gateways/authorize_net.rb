2e6b704dad799d891c753744864332846ad10010
codyfauser <codyfauser@6513ea26-6c20-0410-8a68-89cd7235086d>
2008-01-18 00:44:10 +0000

    Improve Authorize.net documentation based on the DataCashGateway docs
    git-svn-id: https://activemerchant.googlecode.com/svn/trunk/active_merchant@559 6513ea26-6c20-0410-8a68-89cd7235086d

--- lib/active_merchant/billing/gateways/authorize_net.rb
+++ lib/active_merchant/billing/gateways/authorize_net.rb
@@ -1,14 +1,28 @@
 module ActiveMerchant #:nodoc:
   module Billing #:nodoc:
-        
+
+    # For more information on the Authorize.Net Gateway please visit their {Integration Center}[http://developer.authorize.net/]
+    #
+    # The login and password are not the username and password you use to 
+    # login to the Authorize.Net Merchant Interface. Instead, you will 
+    # use the API Login ID as the login and Transaction Key as the 
+    # password.
+    # 
+    # ==== How to Get Your API Login ID and Transaction Key
+    #
+    # 1. Log into the Merchant Interface
+    # 2. Select Settings from the Main Menu
+    # 3. Click on API Login ID and Transaction Key in the Security section
+    # 4. Type in the answer to the secret question configured on setup
+    # 5. Click Submit
     class AuthorizeNetGateway < Gateway
       API_VERSION = '3.1'
-      
+
       class_inheritable_accessor :test_url, :live_url
-    
+
       self.test_url = "https://test.authorize.net/gateway/transact.dll"
       self.live_url = "https://secure.authorize.net/gateway/transact.dll"
-    
+
       APPROVED, DECLINED, ERROR, FRAUD_REVIEW = 1, 2, 3, 4
 
       RESPONSE_CODE, RESPONSE_REASON_CODE, RESPONSE_REASON_TEXT = 0, 2, 3
@@ -18,50 +32,100 @@ module ActiveMerchant #:nodoc:
       self.supported_cardtypes = [:visa, :master, :american_express, :discover]
       self.homepage_url = 'http://www.authorize.net/'
       self.display_name = 'Authorize.net'
-      
+
       CARD_CODE_ERRORS = %w( N S )
       AVS_ERRORS = %w( A E N R W Z )
 
+      # Creates a new AuthorizeNetGateway
+      # 
+      # The gateway requires that a valid login and password be passed
+      # in the +options+ hash.
+      #
+      # ==== Options
+      #
+      # * <tt>:login</tt> -- The Authorize.Net API Login ID (REQUIRED)
+      # * <tt>:password</tt> -- The Authorize.Net Transaction Key. (REQUIRED)
+      # * <tt>:test</tt> -- +true+ or +false+. If true, perform transactions against the test server. Otherwise, perform against the production server.
       def initialize(options = {})
         requires!(options, :login, :password)
         @options = options
         super
-      end  
-      
+      end
+
+      # Performs an authorization, which reserves the funds on the customer's credit card, but does not
+      # charge the card.
+      #
+      # ==== Parameters
+      #
+      # * <tt>money</tt> -- The amount to be authorized. Either an Integer value in cents or a Money object.
+      # * <tt>credit_card</tt> -- The CreditCard details for the transaction.
+      # * <tt>options</tt> -- A hash of optional parameters.
       def authorize(money, creditcard, options = {})
         post = {}
         add_invoice(post, options)
-        add_creditcard(post, creditcard)        
-        add_address(post, options)        
+        add_creditcard(post, creditcard)
+        add_address(post, options)
         add_customer_data(post, options)
-        
+
         commit('AUTH_ONLY', money, post)
       end
-      
+
+      # Perform a purchase, which is essentially an authorization and capture in a single operation.
+      #
+      # ==== Parameters
+      #
+      # * <tt>money</tt> -- The amount to be purchased. Either an Integer value in cents or a Money object.
+      # * <tt>credit_card</tt> -- The CreditCard details for the transaction.
+      # * <tt>options</tt> -- A hash of optional parameters.
       def purchase(money, creditcard, options = {})
         post = {}
         add_invoice(post, options)
-        add_creditcard(post, creditcard)        
-        add_address(post, options)   
+        add_creditcard(post, creditcard)
+        add_address(post, options)
         add_customer_data(post, options)
-             
+
         commit('AUTH_CAPTURE', money, post)
-      end                       
-    
+      end
+
+      # Captures the funds from an authorized transaction.
+      #
+      # ==== Parameters
+      #
+      # * <tt>money</tt> -- The amount to be captured.  Either an Integer value in cents or a Money object.
+      # * <tt>authorization</tt> -- The authorization returned from the previous authorize request.
       def capture(money, authorization, options = {})
         post = {:trans_id => authorization}
         add_customer_data(post, options)
         commit('PRIOR_AUTH_CAPTURE', money, post)
       end
 
+      # Void a previous transaction
+      #
+      # ==== Parameters
+      #
+      # * <tt>authorization</tt> - The authorization returned from the previous authorize request.
       def void(authorization, options = {})
         post = {:trans_id => authorization}
         commit('VOID', nil, post)
       end
-      
+
+      # Credit an account. 
+      # 
+      # This transaction is also referred to as a Refund and indicates to the gateway that 
+      # money should flow from the merchant to the customer.
+      #
+      # ==== Parameters
+      #
+      # * <tt>money</tt> -- The amount to be credited to the customer. Either an Integer value in cents or a Money object.
+      # * <tt>identification</tt> -- The ID of the original transaction against which the credit is being issued
+      # * <tt>options</tt> -- A hash of optional parameters.
+      #
+      # ==== Options
+      #
+      # * <tt>:card_number</tt> -- The credit card number the credit is being issued to. (REQUIRED)
       def credit(money, identification, options = {})
         requires!(options, :card_number)
-        
+
         post = { :trans_id => identification,
                  :card_num => options[:card_number]
                }
@@ -69,70 +133,70 @@ module ActiveMerchant #:nodoc:
 
         commit('CREDIT', money, post)
       end
-       
-      private                       
+
+      private
       def commit(action, money, parameters)
         parameters[:amount] = amount(money) unless action == 'VOID'
-        
+
         # Only activate the test_request when the :test option is passed in
-        parameters[:test_request] = @options[:test] ? 'TRUE' : 'FALSE'                                  
-        
+        parameters[:test_request] = @options[:test] ? 'TRUE' : 'FALSE'
+
         url = test? ? self.test_url : self.live_url
         data = ssl_post url, post_data(action, parameters)
 
         @response = parse(data)
-  
+
         message = message_from(@response)
 
-        # Return the response. The authorization can be taken out of the transaction_id 
+        # Return the response. The authorization can be taken out of the transaction_id
         # Test Mode on/off is something we have to parse from the response text.
         # It usually looks something like this
         #
         #   (TESTMODE) Successful Sale
         test_mode = test? || message =~ /TESTMODE/
-        
+
         Response.new(success?(@response), message, @response, 
           :test => test_mode, 
           :authorization => @response[:transaction_id],
           :fraud_review => fraud_review?(@response),
           :avs_result => { :code => @response[:avs_result_code] },
           :cvv_result => @response[:card_code]
-        )        
+        )
       end
-      
+
       def success?(response)
         response[:response_code] == APPROVED
       end
-      
+
       def fraud_review?(response)
         response[:response_code] == FRAUD_REVIEW
       end
-                                               
+
       def parse(body)
         fields = split(body)
-                
-        results = {         
+
+        results = {
           :response_code => fields[RESPONSE_CODE].to_i,
           :response_reason_code => fields[RESPONSE_REASON_CODE], 
           :response_reason_text => fields[RESPONSE_REASON_TEXT],
           :avs_result_code => fields[AVS_RESULT_CODE],
           :transaction_id => fields[TRANSACTION_ID],
-          :card_code => fields[CARD_CODE_RESPONSE_CODE]          
-        }      
+          :card_code => fields[CARD_CODE_RESPONSE_CODE]
+        }
         results
-      end     
+      end
 
       def post_data(action, parameters = {})
         post = {}
 
-        post[:version]    = API_VERSION
-        post[:login]      = @options[:login]
-        post[:tran_key]   = @options[:password]
+        post[:version]        = API_VERSION
+        post[:login]          = @options[:login]
+        post[:tran_key]       = @options[:password]
         post[:relay_response] = "FALSE"
-        post[:type]       = action
-        post[:delim_data] = "TRUE"
-        post[:delim_char] = ","
-        post[:encap_char] = "$"
+        post[:type]           = action
+        post[:delim_data]     = "TRUE"
+        post[:delim_char]     = ","
+        post[:encap_char]     = "$"
 
         request = post.merge(parameters).collect { |key, value| "x_#{key}=#{CGI.escape(value.to_s)}" }.join("&")
         request
@@ -142,43 +206,43 @@ module ActiveMerchant #:nodoc:
         post[:invoice_num] = options[:order_id]
         post[:description] = options[:description]
       end
-      
-      def add_creditcard(post, creditcard)      
-        post[:card_num]  = creditcard.number
-        post[:card_code] = creditcard.verification_value if creditcard.verification_value?
-        post[:exp_date]  = expdate(creditcard)
+
+      def add_creditcard(post, creditcard)
+        post[:card_num]   = creditcard.number
+        post[:card_code]  = creditcard.verification_value if creditcard.verification_value?
+        post[:exp_date]   = expdate(creditcard)
         post[:first_name] = creditcard.first_name
         post[:last_name]  = creditcard.last_name
       end
-      
+
       def add_customer_data(post, options)
         if options.has_key? :email
           post[:email] = options[:email]
           post[:email_customer] = false
         end
-        
+
         if options.has_key? :customer
           post[:cust_id] = options[:customer]
         end
-        
+
         if options.has_key? :ip
           post[:customer_ip] = options[:ip]
-        end        
+        end
       end
 
-      def add_address(post, options)      
+      def add_address(post, options)
 
         if address = options[:billing_address] || options[:address]
-          post[:address]    = address[:address1].to_s
-          post[:company]    = address[:company].to_s
-          post[:phone]      = address[:phone].to_s
-          post[:zip]        = address[:zip].to_s       
-          post[:city]       = address[:city].to_s
-          post[:country]    = address[:country].to_s
-          post[:state]      = address[:state].blank?  ? 'n/a' : address[:state]
-        end        
+          post[:address] = address[:address1].to_s
+          post[:company] = address[:company].to_s
+          post[:phone]   = address[:phone].to_s
+          post[:zip]     = address[:zip].to_s
+          post[:city]    = address[:city].to_s
+          post[:country] = address[:country].to_s
+          post[:state]   = address[:state].blank?  ? 'n/a' : address[:state]
+        end
       end
-    
+
       # Make a ruby type out of the response string
       def normalize(field)
         case field
@@ -187,30 +251,30 @@ module ActiveMerchant #:nodoc:
         when ""       then nil
         when "null"   then nil
         else field
-        end        
-      end          
-      
+        end
+      end
+
       def message_from(results)  
         if results[:response_code] == DECLINED
           return CVVResult.messages[ results[:card_code] ] if CARD_CODE_ERRORS.include?(results[:card_code])
           return AVSResult.messages[ results[:avs_result_code] ] if AVS_ERRORS.include?(results[:avs_result_code])
         end
-        
+
         return results[:response_reason_text].nil? ? '' : results[:response_reason_text][0..-2]
       end
-        
+
       def expdate(creditcard)
         year  = sprintf("%.4i", creditcard.year)
         month = sprintf("%.2i", creditcard.month)
 
         "#{month}#{year[-2..-1]}"
       end
-      
+
       def split(response)
         response[1..-2].split(/\$,\$/)
       end
     end
-    
+
     AuthorizedNetGateway = AuthorizeNetGateway
   end
 end
