39a7ce0bf76aad28d6628932218e2c301cfd1b77
Cody Fauser <codyfauser@gmail.com>
2009-05-03 23:38:35 -0400

    Update Instapay gateway to support capture and add address, order, and invoice fields. Add support for CVV and AVS response

--- test/remote/gateways/remote_instapay_test.rb
+++ test/remote/gateways/remote_instapay_test.rb
@@ -1,19 +1,18 @@
 require File.dirname(__FILE__) + '/../../test_helper'
 
 class RemoteInstapayTest < Test::Unit::TestCase
-
-
+  
   def setup
     @gateway = InstapayGateway.new(fixtures(:instapay))
 
     @amount = 100
     @credit_card = credit_card('5454545454545454')
     @declined_card = credit_card('4000300011112220')
-       @authorization = '92888036'
 
     @options = {
-      :order_id => '1',
+      :order_id => generate_unique_id,
       :billing_address => address,
+      :shipping_address => address,
       :description => 'Store Purchase'
     }
   end
@@ -21,24 +20,42 @@ class RemoteInstapayTest < Test::Unit::TestCase
   def test_successful_purchase
     assert response = @gateway.purchase(@amount, @credit_card, @options)
     assert_success response
-    assert_equal 'Accepted', response.message
+    assert_equal InstapayGateway::SUCCESS_MESSAGE, response.message
   end
 
   def test_failed_purchase
     assert response = @gateway.purchase(@amount,  @declined_card, @options)
     assert_failure response
-    assert_equal  'Declined', response.message
   end
 
-  def test_succesful_auth
+  def test_succesful_authorization
     assert response = @gateway.authorize(@amount,  @credit_card, @options)
     assert_success response
-    assert_equal  'Accepted', response.message
+    assert_equal InstapayGateway::SUCCESS_MESSAGE, response.message
   end
 
-  def test_failed_auth
+  def test_failed_authorization
     assert response = @gateway.authorize(@amount,  @declined_card, @options)
     assert_failure response
-    assert_equal  'Declined', response.message
+  end
+  
+  def test_authorization_and_capture
+    assert authorization = @gateway.authorize(@amount, @credit_card, @options)
+    assert_success authorization
+  
+    assert capture = @gateway.capture(@amount, authorization.authorization)
+    assert_success capture
+    assert_equal InstapayGateway::SUCCESS_MESSAGE, capture.message
+  end
+  
+  def test_invalid_login
+    gateway = InstapayGateway.new(
+      :login => 'X',
+      :password => 'Y'
+    )
+    
+    assert response = gateway.purchase(@amount, @credit_card)
+    assert_failure response
+    assert_equal "Invalid merchant", response.message
   end
 end
--- test/unit/gateways/instapay_test.rb
+++ test/unit/gateways/instapay_test.rb
@@ -2,10 +2,7 @@ require File.dirname(__FILE__) + '/../../test_helper'
 
 class InstapayTest < Test::Unit::TestCase
   def setup
-    @gateway = InstapayGateway.new(
-                 :acctid => 'TEST0'
-               )
-
+    @gateway = InstapayGateway.new(:login => 'TEST0')
     @credit_card = credit_card
     @amount = 100
   end
@@ -16,14 +13,16 @@ class InstapayTest < Test::Unit::TestCase
     assert response = @gateway.purchase(@amount, @credit_card)
     assert_instance_of  Response, response
     assert_success response
+    assert_equal "118583850", response.authorization
   end
 
   def test_unsuccessful_purchase
     @gateway.expects(:ssl_post).returns(failed_purchase_response)
 
     assert response = @gateway.purchase(@amount, @credit_card)
-       assert_instance_of Response, response
+    assert_instance_of Response, response
     assert_failure response
+    assert_nil response.authorization
   end
 
   def test_successful_auth
@@ -32,90 +31,72 @@ class InstapayTest < Test::Unit::TestCase
     assert response = @gateway.authorize(@amount, @credit_card)
     assert_instance_of  Response, response
     assert_success response
+    assert_equal "118583850", response.authorization
   end
 
   def test_unsuccessful_auth
     @gateway.expects(:ssl_post).returns(failed_purchase_response)
 
     assert response = @gateway.authorize(@amount, @credit_card)
-       assert_instance_of Response, response
+    assert_instance_of Response, response
     assert_failure response
+    assert_nil response.authorization
+  end
+  
+  def test_avs_result
+    @gateway.expects(:ssl_post).returns(successful_purchase_response)
+    
+    response = @gateway.purchase(@amount, @credit_card)
+    assert_equal 'X', response.avs_result['code']
+  end
+  
+  def test_cvv_result
+    @gateway.expects(:ssl_post).returns(successful_purchase_response)
+    
+    response = @gateway.purchase(@amount, @credit_card)
+    assert_equal 'M', response.cvv_result['code']
+  end
+  
+  def test_successful_capture
+    @gateway.expects(:ssl_post).returns(successful_capture_response)
+    
+    response = @gateway.capture(100, "123456")
+    assert_equal InstapayGateway::SUCCESS_MESSAGE, response.message
+  end
+  
+  def test_failed_capture
+    @gateway.expects(:ssl_post).returns(failed_capture_response)
+    
+    response = @gateway.capture(100, "123456")
+    assert_equal "Post amount exceeds Auth amount", response.message
   end
 
   private
 
   # Place raw successful response from gateway here
   def successful_purchase_response
-       '<html><body><plaintext>
-Accepted=SALE:TEST:::118583850:::
-historyid=118583850
-orderid=92886714
-Accepted=SALE:TEST:::118583850:::
-ACCOUNTNUMBER=************5454
-authcode=TEST
-AuthNo=SALE:TEST:::118583850:::
-historyid=118583850
-orderid=92886714
-recurid=0
-refcode=118583850-TEST
-result=1
-Status=Accepted
-transid=0
-'
+    "<html><body><plaintext>\r\nAccepted=SALE:TEST:::118583850:X::M\r\nhistoryid=118583850\r\norderid=92886714\r\nAccepted=SALE:TEST:::118583850:::\r\nACCOUNTNUMBER=************5454\r\nauthcode=TEST\r\nAuthNo=SALE:TEST:::118583850:::\r\nhistoryid=118583850\r\norderid=92886714\r\nrecurid=0\r\nrefcode=118583850-TEST\r\nresult=1\r\nStatus=Accepted\r\ntransid=0\r\n"
   end
 
   # Place raw failed response from gateway here
   def failed_purchase_response
-  '<html><body><plaintext>
-Declined=DECLINED:0720930009:CVV2 MISMATCH:N7
-historyid=118583848
-orderid=92886713
-ACCOUNTNUMBER=************2220
-Declined=DECLINED:0720930009:CVV2 MISMATCH:N7
-historyid=118583848
-orderid=92886713
-rcode=0720930009
-Reason=DECLINED:0720930009:CVV2 MISMATCH:N7
-recurid=0
-result=0
-Status=Declined
-transid=80410586
-'
+    "<html><body><plaintext>\r\nDeclined=DECLINED:0720930009:CVV2 MISMATCH:N7\r\nhistoryid=118583848\r\norderid=92886713\r\nACCOUNTNUMBER=************2220\r\nDeclined=DECLINED:0720930009:CVV2 MISMATCH:N7\r\nhistoryid=118583848\r\norderid=92886713\r\nrcode=0720930009\r\nReason=DECLINED:0720930009:CVV2 MISMATCH:N7\r\nrecurid=0\r\nresult=0\r\nStatus=Declined\r\ntransid=80410586\r\n"
   end
+  
   def successful_auth_response
-   '<html><body><plaintext>
-Accepted=AUTH:TEST:::118585994:::
-historyid=118585994
-orderid=92888143
-Accepted=AUTH:TEST:::118585994:::
-ACCOUNTNUMBER=************5454
-authcode=TEST
-AuthNo=AUTH:TEST:::118585994:::
-historyid=118585994
-orderid=92888143
-recurid=0
-refcode=118585994-TEST
-result=1
-Status=Accepted
-transid=0
-'
+    "<html><body><plaintext>\r\nAccepted=AUTH:TEST:::118585994:::\r\nhistoryid=118585994\r\norderid=92888143\r\nAccepted=AUTH:TEST:::118585994:::\r\nACCOUNTNUMBER=************5454\r\nauthcode=TEST\r\nAuthNo=AUTH:TEST:::118585994:::\r\nhistoryid=118585994\r\norderid=92888143\r\nrecurid=0\r\nrefcode=118585994-TEST\r\nresult=1\r\nStatus=Accepted\r\ntransid=0\r\n"
   end
 
   def failed_auth_response
-  '<html><body><plaintext>
-Declined=DECLINED:0720930009:CVV2 MISMATCH:N7
-historyid=118585991
-orderid=92888142
-ACCOUNTNUMBER=************2220
-Declined=DECLINED:0720930009:CVV2 MISMATCH:N7
-historyid=118585991
-orderid=92888142
-rcode=0720930009
-Reason=DECLINED:0720930009:CVV2 MISMATCH:N7
-recurid=0
-result=0
-Status=Declined
-transid=80412271
-'
+    "<html><body><plaintext>\r\nDeclined=DECLINED:0720930009:CVV2 MISMATCH:N7\r\nhistoryid=118585991\r\norderid=92888142\r\nACCOUNTNUMBER=************2220\r\nDeclined=DECLINED:0720930009:CVV2 MISMATCH:N7\r\nhistoryid=118585991\r\norderid=92888142\r\nrcode=0720930009\r\nReason=DECLINED:0720930009:CVV2 MISMATCH:N7\r\nrecurid=0\r\nresult=0\r\nStatus=Declined\r\ntransid=80412271\r\n"
+  end
+  
+  def successful_capture_response
+    "<html><body><plaintext>\r\nAccepted=AVSAUTH:TEST:::121609962::::DUPLICATE\r\nhistoryid=121609962\r\norderid=95009583\r\nAccepted=AVSAUTH:TEST:::121609962::::DUPLICATE\r\nACCOUNTNUMBER=************5454\r\nauthcode=TEST\r\nAuthNo=AVSAUTH:TEST:::121609962::::DUPLICATE\r\nDUPLICATE=1\r\nhistoryid=121609962\r\norderid=95009583\r\nrecurid=0\r\nrefcode=121609962-TEST\r\nresult=1\r\nStatus=Accepted\r\ntransid=0\r\n"
+  end
+  
+  def failed_capture_response
+    "<html><body><plaintext>\r\nDeclined=DECLINED:1101450002:Post amount exceeds Auth amount:\r\nhistoryid=\r\norderid=\r\nDeclined=DECLINED:1101450002:Post amount exceeds Auth amount:\r\nrcode=1101450002\r\nReason=DECLINED:1101450002:Post amount exceeds Auth amount:\r\nresult=0\r\nStatus=Declined\r\ntransid=0\r\n"
   end
 end
+
