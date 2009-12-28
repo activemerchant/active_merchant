7293bda24d23d468aca11ed7cdbacb50ebad0db4
pjhyett <pjhyett@gmail.com>
2009-12-27 17:04:29 -0700

    Fix jcb regex to include wider range of card numbers.

--- lib/active_merchant/billing/credit_card_methods.rb
+++ lib/active_merchant/billing/credit_card_methods.rb
@@ -8,7 +8,7 @@ module ActiveMerchant #:nodoc:
         'discover'           => /^(6011|65\d{2})\d{12}$/,
         'american_express'   => /^3[47]\d{13}$/,
         'diners_club'        => /^3(0[0-5]|[68]\d)\d{11}$/,
-        'jcb'                => /^3528\d{12}$/,
+        'jcb'                => /^35(28|29|[3-8]\d)\d{12}$/,
         'switch'             => /^6759\d{12}(\d{2,3})?$/,  
         'solo'               => /^6767\d{12}(\d{2,3})?$/,
         'dankort'            => /^5019\d{12}$/,
