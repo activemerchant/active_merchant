503b6e20d5c04a588621ec21b84f2813b8e14b6a
Dennis Theisen <dennis@jadedpixel.com>
2010-01-12 16:58:28 +0100

    Updated SecurePay gateway to use a comma delimiter when parsing response messages instead of the old percentage sign. Also updated tests.

--- test/unit/gateways/secure_pay_test.rb
+++ test/unit/gateways/secure_pay_test.rb
@@ -1,13 +1,13 @@
 require 'test_helper'
-
+ 
 class SecurePayTest < Test::Unit::TestCase
-
+ 
   def setup
     @gateway = SecurePayGateway.new(
       :login => 'X',
       :password => 'Y'
     )
-
+ 
     @credit_card = credit_card
     
     @options = {
@@ -18,7 +18,7 @@ class SecurePayTest < Test::Unit::TestCase
     
     @amount = 100
   end
-
+ 
   def test_failed_purchase
     @gateway.stubs(:ssl_post).returns(failure_response)
     
@@ -27,6 +27,16 @@ class SecurePayTest < Test::Unit::TestCase
     assert response.test?
     assert_equal 'This transaction has been declined', response.message
     assert_equal '3377475', response.authorization
+  end
+  
+  def test_failed_production_purchase
+    @gateway.stubs(:ssl_post).returns(failed_production_response)
+    
+    assert response = @gateway.purchase(@amount, @credit_card, @options)
+    assert_failure response
+    assert response.test?
+    assert_equal 'This transaction has been declined', response.message
+    assert_equal '7416654', response.authorization
   end
   
   def test_successful_purchase
@@ -74,14 +84,18 @@ class SecurePayTest < Test::Unit::TestCase
   private
   
   def successful_purchase_response
-    '1%%1%This transaction has been approved.%100721%X%3377575%f6af895031c07d88399ed9fdb48c8476%Store+purchase%0.01%%AUTH_CAPTURE%%Cody%Fauser%%100+Example+St.%Ottawa%ON%K2A5P7%Canada%%%%%%%%%%%%%%%%%%%'
+    '1,,1,This transaction has been approved.,100721,X,3377575,f6af895031c07d88399ed9fdb48c8476,Store+purchase,0.01,,AUTH_CAPTURE,,Cody,Fauser,,100+Example+St.%Ottawa,ON,K2A5P7,Canada,,,,,,,,,,,,,,,,,,,'
   end
-
+ 
   def failure_response
-    '2%%2%This transaction has been declined.%NOT APPROVED%U%3377475%55adbbaed13aa7e2526846d672fdb594%Store+purchase%1.00%%AUTH_CAPTURE%%Longbob%Longsen%%1234+Test+St.%Ottawa%ON%K1N5P8%Canada%%%%%%%%%%%%%%%%%%%'
+    '2,,2,This transaction has been declined.,NOT APPROVED,U,3377475,55adbbaed13aa7e2526846d672fdb594,Store+purchase,1.00,,AUTH_CAPTURE,,Longbob,Longsen,,1234+Test+St.,Ottawa,ON,K1N5P8,Canada,,,,,,,,,,,,,,,,,,,'
   end
   
   def failed_capture_response
-    '3%%6%The credit card number is invalid.%%%%%%0.01%%PRIOR_AUTH_CAPTURE%%%%%%%%%%%%%%%%%%%%%%%%%%%%'
+    '3,,6,The credit card number is invalid.,,,,,,0.01,,PRIOR_AUTH_CAPTURE,,,,,,,,,,,,,,,,,,,,,,,,,,,,'
+  end
+  
+  def failed_production_response
+    '2,,2,This transaction has been declined.,NOT APPROVED,U,7416654,%231014.11,,7.95,,AUTH_CAPTURE,321hunter%40comcast.net,David,Hunter,,260+Windsor+Ave,Haddonfield,NJ,08033,US,856+795+7941,,321hunter%40comcast.net,,,,260+Windsor+Ave,Haddonfield,Haddonfield,NJ,08033,,,,,,,,'
   end
 end
