72e5a0a660f2f2e51219c6f1ccdaebea8a8361c9
Kamal Fariz Mahyuddin <kamal.fariz@gmail.com>
2010-05-25 14:06:07 +0800

    Added support for iPay88 integration

---
+++ lib/active_merchant/billing/integrations/ipay88/helper.rb
@@ -0,0 +1,120 @@
+require "digest/sha1"
+
+module ActiveMerchant #:nodoc:
+  module Billing #:nodoc:
+    module Integrations #:nodoc:
+      module Ipay88
+        class Helper < ActiveMerchant::Billing::Integrations::Helper
+          include RequiresParameters
+
+          # Currencies supported
+          #   MYR (Malaysian Ringgit - for all payment methods except China Union Pay and PayPal)
+          #   USD (US Dollar - only for PayPal)
+          #   CNY (Yuan Renminbi - only for China Union Pay)
+          SUPPORTED_CURRENCIES = %w[MYR USD CNY]
+
+          # Languages supported
+          #   ISO-8859-1 (English)
+          #   UTF-8      (Unicode)
+          #   GB2312     (Chinese Simplified)
+          #   GD18030    (Chinese Simplified)
+          #   BIG5       (Chinese Traditional)
+          SUPPORTED_LANGS      = %w[ISO-8859-1 UTF-8 GB2312 GD18030 BIG5]
+
+          # Payment methods supported
+          #   8  (Alliance Online Transfer)
+          #   10 (AmBank)
+          #   21 (China Union Pay)
+          #   20 (CIMB Click)
+          #   2  (Credit Card MYR)
+          #   16 (FPX)
+          #   15 (Hong Leong Bank Transfer)
+          #   6  (Maybank2u.com)
+          #   23 (MEPS Cash)
+          #   17 (Mobile Money)
+          #   33 (PayPal)
+          #   14 (RHB)
+          PAYMENT_METHODS      = %w[8 10 21 20 2 16 15 6 23 17 33 14]
+
+          attr_reader :amount_in_cents
+
+          def initialize(order, account, options = {})
+            requires!(options, :amount, :currency)
+            @amount_in_cents = options[:amount]
+            super
+            add_field mappings[:signature], signature
+          end
+
+          def amount=(money)
+            cents = money.respond_to?(:cents) ? money.cents : money
+            if money.is_a?(String) or cents.to_i < 0
+              raise ArgumentError, "money amount must be either a Money object or a positive integer in cents."
+            end
+            add_field mappings[:amount], sprintf("%.2f", cents.to_f/100)
+          end
+
+          def currency(symbol)
+            raise ArgumentError, "unsupported currency" unless SUPPORTED_CURRENCIES.include?(symbol)
+            add_field mappings[:currency], symbol
+          end
+
+          def language(lang)
+            raise ArgumentError, "unsupported language" unless SUPPORTED_LANGS.include?(lang)
+            add_field mappings[:language], lang
+          end
+
+          def payment(pay_method)
+            raise ArgumentError, "unsupported payment method" unless PAYMENT_METHODS.include?(pay_method.to_s)
+            add_field mappings[:payment], pay_method
+          end
+
+          # Generate a Base64-encoded SHA1 given a string
+          def self.sign(str)
+            [Digest::SHA1.digest(str)].pack("m").chomp
+          end
+
+          def signature
+            self.class.sign(self.sig_components)
+          end
+
+          # MerchantCode (required)
+          mapping :account,     "MerchantCode"
+          # Amount (required)
+          mapping :amount,      "Amount"
+          # Currency (required)
+          mapping :currency,    "Currency"
+          # RefNo (required)
+          mapping :order,       "RefNo"
+          # ProdDesc (required)
+          mapping :description, "ProdDesc"
+          # UserName (required)
+          # UserEmail (required)
+          # UserContact (required)
+          mapping :customer, :name  => "UserName",
+                             :email => "UserEmail",
+                             :phone => "UserContact"
+          # Remark
+          mapping :remark,      "Remark"
+          # Lang
+          mapping :language,    "Lang"
+          # PaymentId
+          mapping :payment,     "PaymentId"
+          # ResponseURL
+          mapping :return_url,  "ResponseURL"
+          # Signature
+          mapping :signature,   "Signature"
+
+          protected
+          def sig_components #:nodoc:
+            components  = [Ipay88.merchant_key]
+            components << fields[mappings[:account]]
+            components << fields[mappings[:order]]
+            components << amount_in_cents
+            components << fields[mappings[:currency]]
+            components.join
+          end
+        end
+      end
+    end
+  end
+end
---
+++ test/unit/integrations/returns/ipay88_return_test.rb
@@ -0,0 +1,103 @@
+require "test_helper"
+
+class Ipay88ReturnTest < Test::Unit::TestCase
+  include ActiveMerchant::Billing::Integrations
+
+  def setup
+    Ipay88.merchant_key = "apple"
+    @ipay88 = Ipay88::Return.new(http_raw_data)
+  end
+
+  def test_accessors
+    assert_equal "ipay88merchcode",              @ipay88.account
+    assert_equal 6,                              @ipay88.payment
+    assert_equal "order-500",                    @ipay88.order
+    assert_equal "5.00",                         @ipay88.amount
+    assert_equal "MYR",                          @ipay88.currency
+    assert_equal "Remarkable",                   @ipay88.remark
+    assert_equal "12345",                        @ipay88.transaction
+    assert_equal "auth123",                      @ipay88.auth_code
+    assert_equal "1",                            @ipay88.status
+    assert_equal "Invalid merchant",             @ipay88.error
+    assert_equal "bPlMszCBwxlfGX9ZkgmSfT+OeLQ=", @ipay88.signature
+  end
+
+  def test_secure_request
+    assert @ipay88.secure?
+  end
+
+  def test_insecure_request
+    assert !Ipay88::Return.new(http_raw_data(:invalid_sig)).secure?
+  end
+
+  def test_successful_return
+    params = parameterize(payload)
+    Ipay88::Return.any_instance.expects(:ssl_post).with(Ipay88.service_url, params,
+      { "Content-Length" => params.size.to_s, "User-Agent" => "Active Merchant -- http://activemerchant.org" }
+    ).returns("00")
+
+    assert @ipay88.success?
+  end
+
+  def test_unsuccessful_return_due_to_signature
+    ipay = Ipay88::Return.new(http_raw_data(:invalid_sig))
+    assert !ipay.success?
+  end
+
+  def test_unsuccessful_return_due_to_requery
+    params = parameterize(payload)
+    Ipay88::Return.any_instance.expects(:ssl_post).with(Ipay88.service_url, params,
+      { "Content-Length" => params.size.to_s, "User-Agent" => "Active Merchant -- http://activemerchant.org" }
+    ).returns("Invalid parameters")
+    assert !@ipay88.success?
+  end
+
+  def test_unsuccessful_return_due_to_payment_failed
+    params = parameterize(payload)
+    Ipay88::Return.any_instance.expects(:ssl_post).with(Ipay88.service_url, params,
+      { "Content-Length" => params.size.to_s, "User-Agent" => "Active Merchant -- http://activemerchant.org" }
+    ).returns("00")
+    ipay = Ipay88::Return.new(http_raw_data(:payment_failed))
+    assert !ipay.success?
+  end
+
+  def test_unsuccessful_return_due_to_missing_amount
+    ipay = Ipay88::Return.new(http_raw_data(:missing_amount))
+    assert !ipay.success?
+  end
+
+  private
+  def http_raw_data(mode=:success)
+    base = { "MerchantCode" => "ipay88merchcode",
+             "PaymentId"    =>  6,
+             "RefNo"        =>  "order-500",
+             "Amount"       =>  "5.00",
+             "Currency"     =>  "MYR",
+             "Remark"       =>  "Remarkable",
+             "TransId"      =>  "12345",
+             "AuthCode"     =>  "auth123",
+             "Status"       =>  1,
+             "ErrDesc"      =>  "Invalid merchant" }
+
+    case mode
+    when :success
+      parameterize(base.merge("Signature" => "bPlMszCBwxlfGX9ZkgmSfT+OeLQ="))
+    when :invalid_sig
+      parameterize(base.merge("Signature" => "hacked"))
+    when :payment_failed
+      parameterize(base.merge("Status" => 0, "Signature" => "p8nXYcl/wytpNMzsf31O/iu/2EU="))
+    when :missing_amount
+      parameterize(base.except("Amount"))
+    else
+      ""
+    end
+  end
+
+  def payload
+    { "MerchantCode" => "ipay88merchcode", "RefNo" => "order-500", "Amount" => "5.00" }
+  end
+
+  def parameterize(params)
+    params.reject{|k, v| v.blank?}.keys.sort.collect { |key| "#{key}=#{CGI.escape(params[key].to_s)}" }.join("&")
+  end
+end
