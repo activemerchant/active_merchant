9346608e4ab242fa27f5d6c930db67f05a0b893f
Jan Riethmayer <jan@riethmayer.de>
2010-09-13 15:46:34 -0400

    add activemerchant compliant behavior

--- lib/active_merchant/billing/gateways/moneybookers.rb
+++ lib/active_merchant/billing/gateways/moneybookers.rb
@@ -1,105 +1,25 @@
-require 'net/http'
-require 'uri'
-
 module ActiveMerchant #:nodoc:
   module Billing #:nodoc:
     class MoneybookersResponse
-      def initialize response, options
-        @params   = response.header.to_hash
-        @body     = response.body
-        @response = response
-        @options  = options
+      def initialize response_body
+        @response = response_body
       end
 
       def token
-        cookie = @params["set-cookie"]
-        if cookie && cookie[0]
-          cookie[0].match(/SESSION_ID=(.+?);/)[1]
-        else
-          nil
-        end
-      end
-
-      def body
-        @body
+        return result = if(success?)
+                          @response
+                        else
+                          nil
+                        end
       end
 
       def success?
-        @response && @response.header && @response.header.message == "OK" || false
-      end
-
-      def test?
-        @options['prepare_only'] == "1"
-      end
-    end
-
-    class MoneybookersRequest
-      attr_reader :result
-
-      def initialize options
-        @options  = setup_options(options)
-      end
-
-      def commit
-        @result = MoneybookersResponse.
-          new(Net::HTTP.post_form(URI.parse(endpoint_url),@options), @options)
-      end
-
-      def endpoint_url
-        if @options['prepare_only'] == "1"
-          MoneybookersGateway::TEST_URL
-        else
-          MoneybookersGateway::LIVE_URL
-        end
-      end
-
-      def redirect_url
-        "#{endpoint_url}?sid=#{@result.token}"
-      end
-
-      #      private
-      def setup_options options
-        {
-          'status_url'        => options[:return_url],
-          'status_url2'       => options[:notification_url],
-          'language'          => two_letter_code(options[:language]),
-          'confirmation_note' => options[:confirmation_note],
-          'logo_url'          => options[:logo_url],
-          'prepare_only'      => prepare_only?(options),
-          'merchant_fields'   => merchant_fields(options)
-        }
-      end
-
-      def two_letter_code language
-        language
-      end
-
-      # API expects 1 or 0.
-      def prepare_only? options
-        options[:test] ? "1" : "0"
-      end
-
-      # API takes only the first 5 paramenters into account
-      def merchant_fields options
-        if options[:merchant_fields]
-          fields = options[:merchant_fields].split(",").map(&:strip)
-          warn_for_excessive_custom_parameters if fields.size > 5
-          fields.first(5)
-        else
-          nil
-        end
-      end
-
-      def warn_for_excessive_custom_parameters
-        logger.warn("Moneybookers API takes only 5 custom fields into account")
+        @response =~ /^\w{30,255}$/
       end
     end
 
     class MoneybookersGateway < Gateway
-      # TODO support https post request
-      TEST_URL = 'http://www.moneybookers.com/app/test_payment.pl'
-      LIVE_URL = 'http://www.moneybookers.com/app/payment.pl'
-      API_URL  = 'http://www.moneybookers.com/app/query.pl'
+      PAYMENT_URL = 'https://www.moneybookers.com/app/payment.pl'
 
       # Moneybookers API version
       # September 03, 2009
@@ -107,35 +27,119 @@ module ActiveMerchant #:nodoc:
 
       class << self
         # TODO check additional supported countries
-        supported_countries = ['DE']
+        # supported_countries = ['DE']
         # TODO check additional supported card types
-        supported_cardtypes = [:visa, :master, :american_express]
-        homepage_url        = LIVE_URL # Gateway homepage URL
-        test_redirect_url   = TEST_URL # Gateway test URL
-        money_format        = :cents   # 100 is 1.00 Euro
+        # supported_cardtypes = [:visa, :master, :american_express]
+        homepage_url        = PAYMENT_URL
+        test_redirect_url   = PAYMENT_URL
+        money_format        = :cents # 100 is 1.00 Euro
         default_currency    = 'EUR'
         display_name        = 'Moneybookers Payment Gateway'
       end
 
-      attr_reader :request, :response
-
-      # The email address of the Merchant's moneybookers.com account
-      # is mandatory. You won't need anything else.
       def initialize(options = {})
-        requires!(options, :pay_to_email)
+        requires!(options,
+                  :pay_to_email,        # merchant email
+                  :return_url,          # client purchases
+                  :cancel_url,          # client cancels
+                  :language,            # displayed moneybookers language
+                  :detail1_description, # e.g. "Product ID:"
+                  :detail1_text        # part of clients history payments
+                  )
         @options = options
-        @request = {}
         super
       end
 
       def setup_purchase(amount)
-        requires!(@options, :return_url, :cancel_url)
-        @request = MoneybookersRequest.new(@options)
-        @response  = request.commit
+        data = post_data(amount,{:prepare_only => "1"})
+        @response = MoneybookersResponse.new(ssl_post(PAYMENT_URL, data))
       end
 
       def checkout_url
-        @request && @response && @request.redirect_url
+        @response.success? && redirect_url || nil
+      end
+
+      private
+      def redirect_url
+        "#{PAYMENT_URL}?sid=#{@response.token}"
+      end
+
+      def currency
+        @options[:currency] || "EUR"
+      end
+
+      def post_data(amount, parameters = {})
+        post = {}
+        # mandatory fields
+        post[:pay_to_email]        = @options[:pay_to_email]
+        post[:return_url]          = @options[:return_url]
+        post[:return_url_text]     = @options[:return_url_text]
+        post[:cancel_url]          = @options[:cancel_url]
+        post[:detail1_description] = @options[:detail1_description]
+        post[:detail1_text]        = @options[:detail1_text]
+        post[:amount]              = amount
+        post[:language]            = @options[:language]
+        post[:currency]            = currency
+
+        # billing_address
+        if @options[:billing_address]
+          post[:pay_from_email]        = @options[:email]
+          post[:title]                 = @options[:billing_address][:title]
+          post[:firstname]             = @options[:billing_address][:firstname]
+          post[:lastname]              = @options[:billing_address][:lastname]
+          post[:date_of_birth]         = @options[:billing_address][:date_of_birth]
+          post[:address]               = @options[:billing_address][:address]
+          post[:address2]              = @options[:billing_address][:address2]
+          post[:phone_number]          = @options[:billing_address][:phone_number]
+          post[:postal_code]           = @options[:billing_address][:postal_code]
+          post[:city]                  = @options[:billing_address][:city]
+          post[:state]                 = @options[:billing_address][:state]
+          post[:country]               = @options[:billing_address][:country]
+        end
+
+        # Merchant may specify a detailed calculation for the total
+        # amount payable. Please note that moneybookers does not check
+        # the validity of these data - they are only displayed in the
+        # details section of Step 2 of the payment process.
+        [:amount2_description,  # e.g. "Product Price:"
+         :amount2,              # e.g. "29.90"
+         :amount3_description,  # e.g. "Handling Fees:"
+         :amount3,              # e.g. "3.10"
+         :amount4_description,  # e.g. "VAT (20%):"
+         :amount4,              # e.g. "6.60"
+
+         # customer
+         :dynamic_descriptor,   # merchant name
+         :confirmation_note,    # thank you note
+         :merchant_fields,      # fields which will be sent back to you
+
+         # product details (up to 5 fields)
+         # Merchant may show up to 5 details about the product or
+         # transfer in the 'Payment Details' section of Step 2 of the
+         # process. The detail1_descritpion is shown on the left side.
+         :detail2_description,
+         :detail2_text,
+         :detail3_description,
+         :detail3_text,
+         :detail4_description,
+         :detail4_text,
+         :detail5_description,
+         :detail5_text,
+
+         # recurring billing
+         :rec_period,
+         :rec_grace_period,
+         :rec_cycle,
+         :ondemand_max_currency
+        ].each do |f|
+          post[f] = @options[f] if @options[f]
+        end
+
+        request = post.merge(parameters).collect do |key, value|
+          "#{key}=#{CGI.escape(value.to_s)}"
+        end.join("&")
+
+        request
       end
     end
   end
--- test/unit/gateways/moneybookers_test.rb
+++ test/unit/gateways/moneybookers_test.rb
@@ -1,51 +1,299 @@
-require "test_helper"
-
+# -*- coding: utf-8 -*-
+require File.dirname(__FILE__) + '/../../test_helper'
 class MoneybookersTest < Test::Unit::TestCase
   def setup
     @amount = 100
     @options = {
-      :pay_to_email => 'test@urbanvention.com',
-      :order_id        => '1',
+      :pay_to_email => 'seller_test@urbanvention.com',
       :billing_address => address,
       :return_url      => 'localhost:3000/payment_confirmed',
       :cancel_url      => 'localhost:3000/payment_canceled',
-      :test            => true
+      :language        => "DE",
+      :detail1_description => "Bestellnummer: ",
+      :detail1_text    => "FooBar123"
     }
     @gateway = MoneybookersGateway.new(@options)
   end
 
-  def test_successful_purchase
-    # TODO stop hitting the API here. This should go into the remote testing
-    # @gateway.expects(:ssl_post).returns(successful_purchase_response)
+  def test_currency_defaults_to_euro
+    assert_equal "EUR", @gateway.instance_eval {currency}
+  end
 
+  def test_billing_address_present
+    assert @options[:billing_address]
+  end
+
+  def test_successful_purchase_setup
+    @gateway.expects(:ssl_post).returns(successful_purchase_response)
     assert response = @gateway.setup_purchase(@amount)
-    assert request  = @gateway.request
-    assert response = @gateway.response
-    assert_instance_of MoneybookersResponse, response
-    assert_success response
-
-    # Replace with authorization number from the successful response
+    assert response.success?
     assert response.token =~ /\w{32}/
-    assert @gateway.checkout_url =~ /\?sid=\w{32}/
-    assert response.test?
+  end
+
+  def test_successful_purchase_setup_provides_checkout_url
+    @gateway.expects(:ssl_post).returns(successful_purchase_response)
+    assert response = @gateway.setup_purchase(@amount)
+    assert_false @gateway.checkout_url.nil?
   end
 
   def test_unsuccessful_request
     @gateway.expects(:ssl_post).returns(failed_purchase_response)
     assert response = @gateway.setup_purchase(@amount)
     assert_failure response
-    assert response.test?
   end
 
   private
 
-  # Place raw successful response from gateway here
-  # TODO add mock object for gateway response
   def successful_purchase_response
+    "b30507717219e01892e5c238c559a7f7"
   end
 
   # Place raw failed response from gateway here
-  # TODO add mock object for gateway response
+  # this one happens with a wrong currency
   def failed_purchase_response
+    <<-RESPONSE
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+
+<script type="text/javascript">
+    function LoaderDispatch ( show, force ) {
+        if ( ! force ) return;
+        var arr = [ 'loader_back_id', 'loader_popup_id' ];
+        for ( var i in arr ) {
+            var obj = document.getElementById( arr[i] );
+            if ( obj ) obj.style.display = show ? 'block' : 'none';
+        }
+        return true;
+    }
+</script>
+<noscript></noscript>
+
+<style type="text/css" media="screen">
+    html, body { padding:0px !important; margin:0px !important; }
+    body { height:100% !important; }
+
+    .loader_cover {
+        display:none; z-index:1000;
+        position:fixed; _position:absolute;
+        top:0px; left:0px; width:100%; height:100%;
+    }
+    .loader_back {
+        display:none; z-index:2000;
+        background-color:#fff;
+    }
+    .loader_popup {
+        display:none; z-index:3000;
+    }
+    .loader_popup_position {
+        position:absolute;
+        top:0%; left:50%;
+        width:0px; height:0px;
+    }
+    .loader_popup_message {
+        position:relative;
+        height:100px; width:200px; padding-top:25px;
+        top:0px; left:-100px;
+        line-height:40px !important;
+        color:#bbb; background-color:#fff; text-align:center;
+        font:bold 18px Arial,Helvetica,sans-serif;
+    }
+
+    #loader_wrapper_id { height: 100%; }
+    #loader_clear_id { clear: both; height: 0px; overflow: hidden; }
+</style>
+
+<script type="text/javascript">
+    document.write('\
+<div id="loader_back_id" class="loader_cover loader_back"></div>\
+<div id="loader_popup_id" class="loader_cover loader_popup">\
+    <div class="loader_popup_position">\
+        <div class="loader_popup_message">\
+            Bitte warten ...<br /><img src="/images/loader/gw-loading.gif" alt="" />\
+        </div>\
+    </div>\
+</div>\
+    ');
+</script>
+<noscript></noscript>
+
+<script type="text/javascript"> LoaderDispatch(true,true); </script>
+<noscript></noscript>
+
+<title>moneybookers.com</title>
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+
+<meta name="Author" content="Moneybookers ltd.">
+<meta name="Publisher" content="Moneybookers ltd.">
+<meta name="Copyright" content="Moneybookers ltd.">
+<meta name="robots" content="all,index,follow" />
+<meta name="distribution" content="global" />
+<meta name="rating" content="general" />
+
+
+    <link rel="icon" href="https://www.moneybookers.com/favicon.ico" type="image/vnd.microsoft.icon" />
+<link rel="styleSheet" href="https://www.moneybookers.com/css/style_de.css" type="text/css" media="screen">
+
+
+
+    <link rel="styleSheet" href="https://www.moneybookers.com/css/onboarding.css" type="text/css" media="screen">
+
+
+    <link rel="styleSheet" href="https://www.moneybookers.com/css/gw/default/style.css" type="text/css" media="screen" />
+    <link rel="styleSheet" href="https://www.moneybookers.com/css/gw/default/btn.css" type="text/css" media="screen" />
+    
+    <script type="text/javascript">
+function ServerDate() {
+    //return server date as javascript Date object
+    return new Date(2010, 9 - 1, 13);
+}
+</script>
+<script type="text/javascript" src="https://www.moneybookers.com/main.js"></script>
+<script type="text/javascript" src="https://www.moneybookers.com/ourlib.js"></script>
+<script type="text/javascript" src="https://www.moneybookers.com/regexp.js"></script>
+<script type="text/javascript" src="https://www.moneybookers.com/swfobject.js"></script>    <script type="text/javascript" src="https://www.moneybookers.com/js/jquery.js"></script>
+
+<script type="text/javascript" src="https://www.moneybookers.com/js/jqplugins/qtip/mb.jquery.qtip.min.js"></script>
+<script type="text/javascript">
+    
+    // define variables that can be used from any script for specific reason
+    var hint_anchors = new Array();
+</script>
+
+
+
+
+
+    <script language="Javascript" src="https://www.moneybookers.com/js/gw.js"></script> 
+
+    <script>
+        var mbcookie = getCookie("SESSION_ID");
+        if (mbcookie==null||mbcookie.length==0||navigator.cookieEnabled==0){
+            $(document).ready(function() {
+                $("body").html($("#mbcookie").html());
+            });
+        }
+    </script>
+</head>
+<body >
+
+<!-- START: loader -->
+<div id="loader_wrapper_id">
+
+    <!-- START: gateway content -->
+    <div id="gw_content_wrapper">
+
+<script language="Javascript">
+    function get_turing_info() {
+        return "<b>Was hat es mit der Sicherheitsnummer auf sich?</b><br/>Die Eingabe der Sicherheitsnummer während der Anmeldung soll automatisierte, unbefugte Anmeldungen bei Ihrem Konto verhindern. Zum Fortfahren muss eine als Bild angezeigte Zufallszahl korrekt eingegeben werden.";
+    }
+</script>
+
+<script language="Javascript" src="https://www.moneybookers.com/ourlib.js"></script>
+<div id="ourDiv" width="1" style="position:absolute; visibility:hidden; z-index:2;"></div>
+
+
+<div id="preloader" class="preloader">    <img src="https://www.moneybookers.com/images/default/btn_right_disabled.gif" height="1" width="1" />
+    <img src="https://www.moneybookers.com/images/default/btn_right2.gif" height="1" width="1" />
+    <img src="https://www.moneybookers.com/images/default/btn_right.gif" height="1" width="1" />
+    <img src="https://www.moneybookers.com/images/default/btn_left_disabled.gif" height="1" width="1" />
+    <img src="https://www.moneybookers.com/images/default/btn_left2.gif" height="1" width="1" />
+    <img src="https://www.moneybookers.com/images/default/btn_left.gif" height="1" width="1" /></div>
+<div id="logo_area">        <div class="logo_spacer"/>           
+</div>
+
+<form name="changeamount" action="payment.pl">    
+<div id="payment_info_expanded"  style="display:none" >    <div class="pay_to" title="Zahlung an seller_test@urbanvention.com" >Zahlung an seller_test@urbanvention.com</div>            <dl class="row">
+                <dd class="col1">Bestellnummer:</dd>
+                <dd class="col2">FooBar123</dd>
+                <dd class="col3">&nbsp;</dd>
+                <dd class="col4">&nbsp;</dd>
+            </dl>            <div>
+                <dl class="total" title="ZAHLUNGSSUMME">
+                    <dd>ZAHLUNGSSUMME                :</dd><dd class="price">100.00&nbsp;       </dd>                     </dl>
+            </div>        <div class="collapse_area">
+            <a href="javascript:more_info(0);" class="collapse" title="Für weniger Informationen – Feld verkleinern"><span class="less_info">Weniger Informationen</span><strong>Für weniger Informationen – Feld verkleinern</strong></a>
+        </div>
+</div>            
+<div id="payment_info_folded" style="">  <div class="pay_to" title="Zahlung an seller_test@urbanvention.com">Zahlung an seller_test@urbanvention.com</div>  <dl class="total" title="ZAHLUNGSSUMME">
+        <dd>ZAHLUNGSSUMME                :</dd><dd class="price">100.00&nbsp;       </dd>      </dl>  <div class="expand_area"><a href="javascript:more_info(1);" class="expand" title="Für mehr Informationen – Feld vergrößern"><span class="more_info">Mehr Informationen</span><strong>Für mehr Informationen – Feld vergrößern</strong></a></div>
+</div>        
+
+</form>            
+
+<script language="JavaScript">
+    function more_info (show) {                 
+        if ( $("#payment_info_folded") != null && $("#payment_info_expanded") != null) {
+            if ( show == 1) {            
+                $("#payment_info_folded").slideUp('fast',function (){
+                   $("#payment_info_expanded").slideDown('slow');
+                });
+            } else {                           
+                $("#payment_info_expanded").slideUp('slow',function (){
+                    $("#payment_info_folded").slideDown('fast');
+                });
+            }        
+        }
+    }
+    
+    var init_rec_amount = '';
+    function calculate_total_amount( new_send_amount ){
+        var total_amount_rec = getObject('total_amount_rec');
+        if ( total_amount_rec != null && typeof(total_amount_rec) != 'undefined' ) {
+            var new_total_amount = init_rec_amount*1 + new_send_amount*1;
+            lwr(to_money(new_total_amount) + ' ', 'total_amount_rec');
+        }
+    } </script>
+ 
+
+<script language="Javascript">
+    function get_turing_info() {
+        return "<b>Was hat es mit der Sicherheitsnummer auf sich?</b><br/>Die Eingabe der Sicherheitsnummer während der Anmeldung soll automatisierte, unbefugte Anmeldungen bei Ihrem Konto verhindern. Zum Fortfahren muss eine als Bild angezeigte Zufallszahl korrekt eingegeben werden.";
+    }
+</script>
+
+<script language="Javascript" src="https://www.moneybookers.com/ourlib.js"></script>
+<div id="ourDiv" width="1" style="position:absolute; visibility:hidden; z-index:2;"></div>
+
+    <h1 class="title" style=>Transaktion nicht erlaubt      </h1>    <div class="gateway_content">                <dl>
+            <dd>Die vom Händler gewählte Währung für diese Zahlung wird von Moneybookers nicht unterstüzt. Bitte kontaktieren Sie ihren Händler um Unterstützung zu erhalten.</dd> 
+            <dd><div class="separator" id="separator"></div>
+<dl class="buttons">    <dd ><button type="button" 
+        class="submit_btn"
+        value="Abbrechen"            onclick="top.location='localhost:3000/payment_canceled';">
+<span class="msg">ABBRECHEN</span></button>    </dd></dl>                                              </dd>
+        </dl>    </div><div id="mbcookie" style="display:none">    <div class="gateway_content">    <dl>          <dd>Bitte aktivieren Sie Ihre Cookies, damit Sie Moneybookers in vollem Umfang nutzen können.
+
+Um Ihre Bezahlung an  abzuschließen, müssen Sie Cookies von Moneybookers in den Einstellungen Ihres Browsers akzeptieren. Falls Sie Ihre Zugriffsrechte über die Software eines Drittherstellers verwalten, erlauben Sie dort bitte Cookies von Moneybookers um Ihre Transaktion erfolgreich durchzuführen.</dd>    </dl>        
+    <dl>
+        <dd><div class="separator" id="separator"></div>
+<dl class="buttons">    <dd ><button type="button" 
+        class="submit_btn"
+        value="Weiter"            onclick="location.replace('https://www.moneybookers.com/app/payment.pl?sid=eb9e98f3dc5e70dda4e03834f35aa72b');">
+<span class="msg">WEITER</span></button>    </dd></dl>                                             </dd>
+       </dl>    </div></div>
+
+<script language="JavaScript">                        repositionHints();//attach hints (if any) to anchors elements
+
+</script>
+
+<!-- basename: payment, template_name: error -->
+<script type="text/javascript" src="/js/tracker/init.js">/**/</script>
+<script type="text/javascript"> customTracker( { _upage: '/de/payment/error' } ); </script>
+
+</div>
+<!-- END: gateway content -->
+
+<!-- END: loader -->
+    <div id="loader_clear_id"></div>
+</div>
+
+<script type="text/javascript"> setTimeout( function () { LoaderDispatch(false,true); }, 200 ); </script>
+<noscript></noscript>
+
+</body>
+</html>
+RESPONSE
   end
 end
