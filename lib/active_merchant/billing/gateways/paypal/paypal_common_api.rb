69949b9b3948780268c86e05685dcc13b9ca3600
Chris Gaudreau <chris@klickflow.com>
2011-01-03 06:23:20 -0800

    fixed my fix

--- lib/active_merchant/billing/gateways/paypal/paypal_common_api.rb
+++ lib/active_merchant/billing/gateways/paypal/paypal_common_api.rb
@@ -132,7 +132,11 @@ module ActiveMerchant #:nodoc:
             xml.tag! 'n2:Version', API_VERSION
             xml.tag! 'AuthorizationID', authorization
             xml.tag! 'Amount', amount(money), 'currencyID' => options[:currency] || currency(money)
-            xml.tag! 'CompleteType', options[:completetype] unless options[:completetype].blank? else "Complete"
+          if options[:complettype].blank?
+            xml.tag! 'CompleteType', 'Complete'
+          else
+            xml.tag! 'CompleteType', options[:completetype]
+          end
             xml.tag! 'InvoiceID', options[:order_id] unless options[:order_id].blank?
             xml.tag! 'Note', options[:description]
           end
