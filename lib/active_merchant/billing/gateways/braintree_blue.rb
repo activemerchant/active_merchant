42381a1b7c7dacc15a0ac9e11baef31dd233d59b
Jonathan Rudenberg <jonathan@shopify.com>
2011-03-25 11:40:25 -0400

    Update BraintreeBlue#refund to have consistent method signature

--- lib/active_merchant/billing/gateways/braintree_blue.rb
+++ lib/active_merchant/billing/gateways/braintree_blue.rb
@@ -46,9 +46,14 @@ module ActiveMerchant #:nodoc:
         create_transaction(:credit, money, credit_card_or_vault_id, options)
       end
 
-      def refund(transaction_id, options = {})
+      def refund(*args)
+        # legacy signature: #refund(transaction_id, options = {})
+        # new signature: #refund(money, transaction_id, options = {})
+        money, transaction_id, options = extract_refund_args(args)
+        money = amount(money).to_s if money
+
         commit do
-          result = Braintree::Transaction.find(transaction_id).refund
+          result = Braintree::Transaction.refund(transaction_id, money)
           Response.new(result.success?, message_from_result(result))
         end
       end
@@ -202,6 +207,19 @@ module ActiveMerchant #:nodoc:
           response.instance_variable_set("@avs_result", avs_result)
           response.instance_variable_set("@cvv_result", cvv_result)
           response
+        end
+      end
+
+      def extract_refund_args(args)
+        options = args.extract_options!
+
+        # money, transaction_id, options
+        if args.length == 1 # legacy signature
+          return nil, args[0], options
+        elsif args.length == 2
+          return args[0], args[1], options
+        else
+          raise ArgumentError, "wrong number of arguments (#{args.length} for 2)"
         end
       end
     end
--- test/unit/gateways/braintree_blue_test.rb
+++ test/unit/gateways/braintree_blue_test.rb
@@ -1,9 +1,22 @@
 require 'test_helper'
 
 class BraintreeBlueTest < Test::Unit::TestCase
-  def test_initialize_does_not_raise_an_error
-    assert_nothing_raised do
-      BraintreeBlueGateway.new(:merchant_id => "test", :public_key => "test", :private_key => "test")
-    end
+
+  def setup
+    @gateway = BraintreeBlueGateway.new(
+      :merchant_id => 'test',
+      :public_key => 'test',
+      :private_key => 'test'
+    )
+  end
+
+  def test_refund_legacy_method_signature
+    Braintree::Transaction.expects(:refund).with('transaction_id', nil).returns(Response.new(true, 'test', {}))
+    @gateway.refund('transaction_id', :test => true)
+  end
+
+  def test_refund_method_signature
+    Braintree::Transaction.expects(:refund).with('transaction_id', '10.00').returns(Response.new(true, 'test', {}))
+    @gateway.refund(1000, 'transaction_id', :test => true)
   end
 end
