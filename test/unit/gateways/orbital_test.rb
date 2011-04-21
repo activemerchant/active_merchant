b71fb6f8c3698887f533662aece9ab4cad4bb278
Nathaniel Talbott <nathaniel@talbott.ws>
2011-04-21 11:51:37 -0400

    Missed the unit test.

--- test/unit/gateways/orbital_test.rb
+++ test/unit/gateways/orbital_test.rb
@@ -15,7 +15,7 @@ class OrbitalGatewayTest < Test::Unit::TestCase
     assert response = @gateway.purchase(50, credit_card, :order_id => '1')
     assert_instance_of Response, response
     assert_success response
-    assert_equal '4A5398CF9B87744GG84A1D30F2F2321C66249416', response.authorization
+    assert_equal '4A5398CF9B87744GG84A1D30F2F2321C66249416;1', response.authorization
   end
     
   def test_unauthenticated_response
