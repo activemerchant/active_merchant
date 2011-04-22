0c4f8e4e69fda060907c25f4efb1c2dc1b5c504b
Nathaniel Talbott <nathaniel@talbott.ws>
2011-04-22 11:08:28 -0400

    Add authorize/capture/credit/void to RealEx.

--- test/unit/gateways/realex_test.rb
+++ test/unit/gateways/realex_test.rb
@@ -3,14 +3,21 @@ require 'digest/sha1'
 
 class RealexTest < Test::Unit::TestCase
   
+  class ActiveMerchant::Billing::RealexGateway
+    # For the purposes of testing, lets redefine some protected methods as public.
+    public :build_purchase_or_authorization_request, :build_credit_request, :build_void_request, :build_capture_request, :stringify_values, :avs_input_code
+  end
+  
   def setup
     @login = 'your_merchant_id'
     @password = 'your_secret'
+    @account = 'your_account'
+    @rebate_secret = 'your_rebate_secret'
   
     @gateway = RealexGateway.new(
-      :login => @merchant_id,
-      :password => @secret,
-      :account => ''
+      :login => @login,
+      :password => @password,
+      :account => @account
     )
 
     @gateway_with_account = RealexGateway.new(
@@ -30,6 +37,15 @@ class RealexTest < Test::Unit::TestCase
     
     @options = {
       :order_id => '1'
+    }
+    
+    @address = {
+      :name => 'Longbob Longsen',
+      :address1 => '123 Fake Street',
+      :city => 'Belfast',
+      :state => 'Antrim',
+      :country => 'Northern Ireland',
+      :zip => 'BT2 8XX'
     }
     
     @amount = 100
@@ -67,6 +83,16 @@ class RealexTest < Test::Unit::TestCase
     assert response.test?
   end
   
+  def test_successful_credit
+    @gateway.expects(:ssl_post).returns(successful_credit_response)    
+    assert_success @gateway.credit(@amount, '1234;1234;1234')
+  end
+  
+  def test_unsuccessful_credit
+    @gateway.expects(:ssl_post).returns(unsuccessful_credit_response)
+    assert_failure @gateway.credit(@amount, '1234;1234;1234')
+  end
+  
   def test_supported_countries
     assert_equal ['IE', 'GB'], RealexGateway.supported_countries
   end
@@ -89,6 +115,181 @@ class RealexTest < Test::Unit::TestCase
     assert_equal 'M', response.cvv_result['code']
   end
   
+  def test_capture_xml
+    @gateway.expects(:new_timestamp).returns('20090824160201')
+    
+    valid_capture_xml = <<-SRC
+<request timestamp="20090824160201" type="settle">
+  <merchantid>your_merchant_id</merchantid>
+  <account>your_account</account>
+  <orderid>1</orderid>
+  <pasref>4321</pasref>
+  <authcode>1234</authcode>
+  <sha1hash>4132600f1dc70333b943fc292bd0ca7d8e722f6e</sha1hash>
+</request>
+SRC
+    
+    assert_equal valid_capture_xml, @gateway.build_capture_request('1;4321;1234', {})
+  end
+  
+  def test_purchase_xml
+    options = {
+      :order_id => '1'
+    }
+
+    @gateway.expects(:new_timestamp).returns('20090824160201')
+
+    valid_purchase_request_xml = <<-SRC
+<request timestamp="20090824160201" type="auth">
+  <merchantid>your_merchant_id</merchantid>
+  <account>your_account</account>
+  <orderid>1</orderid>
+  <amount currency="EUR">100</amount>
+  <card>
+    <number>4263971921001307</number>
+    <expdate>0808</expdate>
+    <chname>Longbob Longsen</chname>
+    <type>VISA</type>
+    <issueno></issueno>
+    <cvn>
+      <number></number>
+      <presind></presind>
+    </cvn>
+  </card>
+  <autosettle flag="1"/>
+  <sha1hash>3499d7bc8dbacdcfba2286bd74916d026bae630f</sha1hash>
+</request>
+SRC
+
+    assert_equal valid_purchase_request_xml, @gateway.build_purchase_or_authorization_request(:purchase, @amount, @credit_card, options)
+  end
+  
+  def test_void_xml
+    @gateway.expects(:new_timestamp).returns('20090824160201')
+
+    valid_void_request_xml = <<-SRC
+<request timestamp="20090824160201" type="void">
+  <merchantid>your_merchant_id</merchantid>
+  <account>your_account</account>
+  <orderid>1</orderid>
+  <pasref>4321</pasref>
+  <authcode>1234</authcode>
+  <sha1hash>4132600f1dc70333b943fc292bd0ca7d8e722f6e</sha1hash>
+</request>
+SRC
+
+    assert_equal valid_void_request_xml, @gateway.build_void_request('1;4321;1234', {})
+  end
+  
+  def test_auth_xml
+    options = {
+      :order_id => '1'
+    }
+
+    @gateway.expects(:new_timestamp).returns('20090824160201')
+
+    valid_auth_request_xml = <<-SRC
+<request timestamp="20090824160201" type="auth">
+  <merchantid>your_merchant_id</merchantid>
+  <account>your_account</account>
+  <orderid>1</orderid>
+  <amount currency=\"EUR\">100</amount>
+  <card>
+    <number>4263971921001307</number>
+    <expdate>0808</expdate>
+    <chname>Longbob Longsen</chname>
+    <type>VISA</type>
+    <issueno></issueno>
+    <cvn>
+      <number></number>
+      <presind></presind>
+    </cvn>
+  </card>
+  <autosettle flag="0"/>
+  <sha1hash>3499d7bc8dbacdcfba2286bd74916d026bae630f</sha1hash>
+</request>
+SRC
+
+    assert_equal valid_auth_request_xml, @gateway.build_purchase_or_authorization_request(:authorization, @amount, @credit_card, options)
+  end
+  
+  def test_credit_xml
+    @gateway.expects(:new_timestamp).returns('20090824160201')
+
+    valid_credit_request_xml = <<-SRC
+<request timestamp="20090824160201" type="rebate">
+  <merchantid>your_merchant_id</merchantid>
+  <account>your_account</account>
+  <orderid>1</orderid>
+  <pasref>4321</pasref>
+  <authcode>1234</authcode>
+  <amount currency="EUR">100</amount>
+  <autosettle flag="1"/>
+  <sha1hash>ef0a6c485452f3f94aff336fa90c6c62993056ca</sha1hash>
+</request>
+SRC
+
+    assert_equal valid_credit_request_xml, @gateway.build_credit_request(@amount, '1;4321;1234', {})
+
+  end
+  
+  def test_credit_with_rebate_secret_xml
+    gateway = RealexGateway.new(:login => @login, :password => @password, :account => @account, :rebate_secret => @rebate_secret)
+    
+    gateway.expects(:new_timestamp).returns('20090824160201')
+
+    valid_credit_request_xml = <<-SRC
+<request timestamp="20090824160201" type="rebate">
+  <merchantid>your_merchant_id</merchantid>
+  <account>your_account</account>
+  <orderid>1</orderid>
+  <pasref>4321</pasref>
+  <authcode>1234</authcode>
+  <amount currency="EUR">100</amount>
+  <refundhash>f94ff2a7c125a8ad87e5683114ba1e384889240e</refundhash>
+  <autosettle flag="1"/>
+  <sha1hash>ef0a6c485452f3f94aff336fa90c6c62993056ca</sha1hash>
+</request>
+SRC
+
+    assert_equal valid_credit_request_xml, gateway.build_credit_request(@amount, '1;4321;1234', {})
+
+  end
+  
+  def test_stringify_values
+    assert_equal "timestamp.merchantid.orderid.ammount.currency.creditcard", 
+      @gateway.stringify_values(["timestamp","merchantid", "orderid", "ammount", "currency", "creditcard"])
+    
+    assert_equal "timestamp.merchantid.orderid.ammount.currency.", 
+      @gateway.stringify_values(["timestamp","merchantid", "orderid", "ammount", "currency"])
+    
+    assert_equal "timestamp.merchantid.orderid...", 
+      @gateway.stringify_values(["timestamp","merchantid", "orderid"])
+  end
+  
+  def test_should_extract_avs_input
+    address = {:address1 => "123 Fake Street", :zip => 'BT1 0HX'}
+    assert_equal "10|123", @gateway.avs_input_code(address)
+  end
+
+  def test_auth_with_address
+    @gateway.expects(:ssl_post).returns(successful_purchase_response)
+    
+    options = {
+      :order_id => '1',
+      :billing_address => @address,
+      :shipping_address => @address
+    }
+
+    @gateway.expects(:new_timestamp).returns('20090824160201')
+    
+    response = @gateway.authorize(@amount, @credit_card, options)
+    assert_instance_of Response, response
+    assert_success response
+    assert response.test?
+    
+  end
+
   private
   
   def successful_purchase_response
@@ -143,6 +344,42 @@ class RealexTest < Test::Unit::TestCase
     <check id="1000">9</check>
     <check id="1001">9</check>
   </tss>
+  <sha1hash>7384ae67....ac7d7d</sha1hash>
+  <md5hash>34e7....a77d</md5hash>
+</response>"
+    RESPONSE
+  end
+  
+  def successful_credit_response
+    <<-RESPONSE
+<response timestamp='20010427043422'>
+  <merchantid>your merchant id</merchantid>
+  <account>account to use</account>
+  <orderid>order id from request</orderid>
+  <authcode>authcode received</authcode>
+  <result>00</result>
+  <message>[ test system ] message returned from system</message>
+  <pasref> realex payments reference</pasref>
+  <cvnresult>M</cvnresult>
+  <batchid>batch id for this transaction (if any)</batchid>
+  <sha1hash>7384ae67....ac7d7d</sha1hash>
+  <md5hash>34e7....a77d</md5hash>
+</response>"
+    RESPONSE
+  end
+
+  def unsuccessful_credit_response
+    <<-RESPONSE
+<response timestamp='20010427043422'>
+  <merchantid>your merchant id</merchantid>
+  <account>account to use</account>
+  <orderid>order id from request</orderid>
+  <authcode>authcode received</authcode>
+  <result>508</result>
+  <message>[ test system ] You may only rebate up to 115% of the original amount.</message>
+  <pasref> realex payments reference</pasref>
+  <cvnresult>M</cvnresult>
+  <batchid>batch id for this transaction (if any)</batchid>
   <sha1hash>7384ae67....ac7d7d</sha1hash>
   <md5hash>34e7....a77d</md5hash>
 </response>"
