12e7a8cdbc680993a85d8094b5db278d92ef6271
Nikos Dimitrakopoulos <nospam@nikosd.com>
2011-05-03 09:58:25 +0300

    First take on Eurobank ProxyPay implementation.
    Implemented all supported gateway services (authorize, capture, refund, void).

--- lib/active_merchant/billing/gateways/eurobank.rb
+++ lib/active_merchant/billing/gateways/eurobank.rb
@@ -1,75 +1,265 @@
+require 'rexml/document'
 module ActiveMerchant #:nodoc:
   module Billing #:nodoc:
+    # Eurobank ProxyPay integration.
+    #
+    # Ported from https://github.com/akDeveloper/Aktive-Merchant
+    #
+    # @author : Nikos Dimitrakopoulos
+    # @copyright : Fraudpointer.com
     class EurobankGateway < Gateway
-      TEST_URL = 'https://example.com/test'
-      LIVE_URL = 'https://example.com/live'
+      TEST_URL = 'https://eptest.eurocommerce.gr/proxypay/apacsonline'
+      LIVE_URL = 'https://ep.eurocommerce.gr/proxypay/apacsonline'
       
       # The countries the gateway supports merchants from as 2 digit ISO country codes
-      self.supported_countries = ['US']
+      self.supported_countries = ['GR']
       
       # The card types supported by the payment gateway
       self.supported_cardtypes = [:visa, :master, :american_express, :discover]
       
       # The homepage URL of the gateway
-      self.homepage_url = 'http://www.example.net/'
+      self.homepage_url = 'http://www.eurobank.gr/online/home/generic.aspx?id=79&mid=635'
       
       # The name of the gateway
-      self.display_name = 'New Gateway'
-      
+      self.display_name = 'Eurobank Euro-Commerce'
+
+      # The money format of the gateway
+      self.money_format = :cents
+
+      # The default currency of the gateway
+      self.default_currency = 'EUR'
+
+      ACTIONS = {
+        :authorize => "PreAuth",
+        :capture   => "Capture",
+        :credit    => "Refund",
+        :void      => "Cancel"
+      }
+
+      CURRENCY_MAPPINGS = {
+        'USD' => 840,
+        'GRD' => 300,
+        'EUR' => 978
+      }
+
+      ERROR_CODE_DESCRIPTIONS = {
+        '0' => 'Transaction completed successfully',
+        '30' => 'Payment Failed',
+        '40' => 'Data received from server but details could not be parsed from XML message',
+        '50' => 'Malformed XML message, JProxyPayLink element missing',
+        '51' => 'Malformed XML message, message element missing',
+        '52' => 'Malformed XML message, type element missing',
+        '53' => 'Error in XML parsing',
+        '54' => 'Malformed XML message, OrderInfo element missing',
+        '55' => 'Malformed XML message, amount element missing',
+        '56' => 'Malformed XML message, merchantRef element missing',
+        '57' => 'Malformed XML message, merchantDesc element missing',
+        '58' => 'Malformed XML message, Currency element missing',
+        '59' => 'Malformed XML message, CustomerEmail element missing',
+        '60' => 'Malformed XML message, Var1 element missing',
+        '61' => 'Malformed XML message, Var2 element missing',
+        '62' => 'Malformed XML message, Var3 element missing',
+        '63' => 'Malformed XML message, Var4 element missing',
+        '64' => 'Malformed XML message, Var5 element missing',
+        '65' => 'Malformed XML message, Var6 element missing',
+        '66' => 'Malformed XML message, Var7 element missing',
+        '67' => 'Malformed XML message, Var8 element missing',
+        '68' => 'Malformed XML message, Var9 element missing',
+        '69' => 'Malformed XML message, PaymentInfo element missing',
+        '70' => 'Malformed XML message, CreditCard Number element missing',
+        '71' => 'Malformed XML message, Expiry Date element missing',
+        '72' => 'Malformed XML message, CVCCVV element missing',
+        '73' => 'Malformed XML message, InstallmentOffset element missing',
+        '74' => 'Malformed XML message, InstallmentPeriod element missing',
+        '75' => 'Malformed XML message, Amount Invalid contents',
+        '76' => 'Malformed XML message, CCN Invalid Length',
+        '77' => 'Malformed XML message, CCN Invalid Contents',
+        '78' => 'Malformed XML message, Expiry Date Invalid Length',
+        '79' => 'Malformed XML message, Expiry Date Invalid Contents',
+        '80' => 'Malformed XML message, element missing authentication',
+        '81' => 'Malformed XML message, element missing merchant id',
+        '82' => 'Malformed XML message, CVCCVV / Currency Invalid length',
+        '83' => 'Malformed XML message, CVCCVV / Currency Invalid Contents',
+        '84' => 'Malformed XML message, Invalid Instalment Period',
+        '85' => 'Malformed XML message, Invalid Offset Period',
+        '86' => 'Malformed XML message, Invalid Instalment/Offset Combo',
+        '100' => 'Unknown merchant',
+        '101' => 'Password not matching',
+        '102' => 'Sequence number not in sync',
+        '500' => 'Malformed XML message, merchantid elementmissing',
+        '501' => 'Malformed XML message, password element missing',
+        '502' => 'Malformed XML message, sequence number elementmissing',
+        '503' => 'Malformed XML message, type element missing',
+        '504' => 'Malformed XML message, reference element missing',
+        '505' => 'Malformed XML message, amount element missing',
+        '506' => 'Malformed XML message, currency element missing',
+        '550' => 'Malformed XML message, syntax error',
+        '1000' => 'No connection could be made with server',
+        '1001' => 'Transaction with this reference not known',
+        '1002' => 'Amount requested does not match amount in database',
+        '1003' => 'Currency requested does not match currency in DB',
+        '1004' => 'Invalid Amount',
+        '1005' => 'Invalid Transaction State',
+        '1100' => 'To many attempts, online transaction interface has been shut down for this merchant',
+        '2000' => 'Transaction rejected by host (with errorcode 0-999). Errorcode – 2000 = host-errorcode',
+        '10000' => 'Unspecified failure, contact bank',
+      }
+
+      # Creates a new +EurobankGateway+
+      #
+      # The gateway requires that a valid login, password, and name be passed
+      # in the +options+ hash.
+      #
+      # ==== Parameters
+      #
+      # * <tt>options</tt>
+      #   * <tt>:login</tt> - The merchant id.
+      #   * <tt>:password</tt> - The encrypted password.
       def initialize(options = {})
-        #requires!(options, :login, :password)
+        requires!(options, :login, :password)
         @options = options
         super
-      end  
-      
+      end
+
+      # Authorize a credit card for a given amount.
+      #
+      # ==== Parameters
+      #
+      # * <tt>money</tt> - The amount to be authorized as an Integer value in cents.
+      # * <tt>credit_card</tt> - The CreditCard details for the transaction.
+      # * <tt>options</tt>
+      #   * <tt>:order_id</tt> - A unique reference for this order (required).
+      #   * <tt>:email</tt> - The customer email (optional).
+      #   * <tt>:description</tt> - A description for this order (optional).
+      #   * <tt>:variables</tt> - An +Array+ of additional data that will be sent to the getaway in 'VarX' elements. Maximum length of +Array+ is 9.
       def authorize(money, creditcard, options = {})
-        post = {}
-        add_invoice(post, options)
-        add_creditcard(post, creditcard)        
-        add_address(post, creditcard, options)        
-        add_customer_data(post, options)
-        
-        commit('authonly', money, post)
+        requires!(options, :order_id)
+        commit(:authorize, {:money => money,
+                          :creditcard => creditcard}.merge!(options))
       end
-      
-      def purchase(money, creditcard, options = {})
-        post = {}
-        add_invoice(post, options)
-        add_creditcard(post, creditcard)        
-        add_address(post, creditcard, options)   
-        add_customer_data(post, options)
-             
-        commit('sale', money, post)
-      end                       
-    
+
+      # Capture a previously authorized amount.
+      #
+      # ==== Parameters
+      #
+      # * <tt>money</tt> - The amount that was previously authorized.
+      # * <tt>authorization</tt> - The original order_id that was given to the authorization request.
+      # * <tt>options</tt>
       def capture(money, authorization, options = {})
-        commit('capture', money, post)
-      end
-    
-      private                       
-      
-      def add_customer_data(post, options)
+        commit(:capture, {:money => money,
+                          :order_id => authorization}.merge!(options))
       end
 
-      def add_address(post, creditcard, options)      
+      # Credit/Refund an amount.
+      #
+      # ==== Parameters
+      #
+      # * <tt>money</tt> - The amount of the transaction.
+      # * <tt>identification</tt> -
+      # * <tt>options</tt>
+      def credit(money, identification, options = {})
+        commit(:credit, {:money => money,
+                         :order_id => identification}.merge!(options))
       end
 
-      def add_invoice(post, options)
+      # Cancel/Void a transaction.
+      #
+      # ==== Parameters
+      #
+      # * <tt>identification</tt> - The original order_id that was given to the authorization request.
+      # * <tt>options</tt>
+      def void(identification, options = {})
+        commit(:void, {:money => 0, :order_id => identification}.merge!(options))
       end
-      
-      def add_creditcard(post, creditcard)      
+
+      #######
+      private
+      #######
+
+      def commit(action, parameters)
+        response = parse(ssl_post(test? ? TEST_URL : LIVE_URL, 'APACScommand=NewRequest&Data=' + build_xml(action, parameters)))
+        Response.new(success?(response),
+                     message_from(response),
+                     response,
+                     :authorization => authorization_from(response),
+                     :test => test?)
       end
-      
-      def parse(body)
-      end     
-      
-      def commit(action, money, parameters)
+
+      def success?(response)
+        response[:errorcode] == "0"
       end
 
       def message_from(response)
+        response[:errormessage] || ERROR_CODE_DESCRIPTIONS[response[:errorcode]]
       end
-      
-      def post_data(action, parameters = {})
+
+      def authorization_from(response)
+        response[:reference]
+      end
+
+      def build_xml(action, parameters)
+        xml = <<XML
+<?xml version="1.0" encoding="UTF-8"?>
+  <JProxyPayLink>
+    <Message>
+      <Type>#{ACTIONS[action]}</Type>
+      <Authentication>
+        <MerchantID>#{@options[:login]}</MerchantID>
+        <Password>#{@options[:password]}</Password>
+      </Authentication>
+      <OrderInfo>
+        <Amount>#{amount(parameters[:money])}</Amount>
+        <MerchantRef>#{parameters[:order_id]}</MerchantRef>
+        <MerchantDesc>#{parameters[:description]}</MerchantDesc>
+        <Currency>#{CURRENCY_MAPPINGS[currency(parameters[:money])]}</Currency>
+        <CustomerEmail>#{parameters[:email]}</CustomerEmail>
+XML
+
+        parameters[:variables] ||= []
+        parameters[:variables].each_with_index do |v, i|
+          xml << <<XML
+        <Var#{i+1}>#{v}</Var#{i+1}>
+XML
+        end
+
+        for i in (parameters[:variables].length + 1)..9
+          xml << <<XML
+        <Var#{i} />
+XML
+        end
+
+        xml << <<XML
+      </OrderInfo>
+XML
+        if parameters[:creditcard]
+          xml << <<XML
+      <PaymentInfo>
+        <CCN>#{parameters[:creditcard].number}</CCN>
+        <Expdate>#{format(parameters[:creditcard].month, :two_digits)}#{format(parameters[:creditcard].year, :two_digits)}</Expdate>
+        <CVCCVV>#{parameters[:creditcard].verification_value}</CVCCVV>
+        <InstallmentOffset>0</InstallmentOffset>
+        <InstallmentPeriod>0</InstallmentPeriod>
+      </PaymentInfo>
+XML
+        end
+
+        xml << <<XML
+    </Message>
+  </JProxyPayLink>
+XML
+
+        puts xml
+
+        xml
+      end
+
+      def parse(xml)
+        response = {}
+        xml = REXML::Document.new(xml)
+        xml.root.elements.each do |node|
+          response[node.name.underscore.to_sym] = node.text
+        end unless xml.root.nil?
+        response
       end
     end
   end
--- test/unit/gateways/eurobank_test.rb
+++ test/unit/gateways/eurobank_test.rb
@@ -7,43 +7,236 @@ class EurobankTest < Test::Unit::TestCase
                  :password => 'password'
                )
 
-    @credit_card = credit_card
+    @credit_card = ActiveMerchant::Billing::CreditCard.new(
+      :number     => '4111111111111111',
+      :month      => '8',
+      :year       => '2011',
+      :first_name => 'Tobias',
+      :last_name  => 'Luetke',
+      :verification_value  => '123'
+    )
+
     @amount = 100
     
     @options = { 
-      :order_id => '1',
-      :billing_address => address,
-      :description => 'Store Purchase'
+      :order_id => 'Some order id',
+      :description => 'Description'
     }
   end
-  
-  def test_successful_purchase
-    @gateway.expects(:ssl_post).returns(successful_purchase_response)
+
+  def test_authorize_xml
+    expected_authorize_request_xml = <<XML
+<?xml version="1.0" encoding="UTF-8"?>
+  <JProxyPayLink>
+    <Message>
+      <Type>PreAuth</Type>
+      <Authentication>
+        <MerchantID>login</MerchantID>
+        <Password>password</Password>
+      </Authentication>
+      <OrderInfo>
+        <Amount>100</Amount>
+        <MerchantRef>Some order id</MerchantRef>
+        <MerchantDesc>Description</MerchantDesc>
+        <Currency>978</Currency>
+        <CustomerEmail></CustomerEmail>
+        <Var1>Variable 1</Var1>
+        <Var2>Variable 2</Var2>
+        <Var3 />
+        <Var4 />
+        <Var5 />
+        <Var6 />
+        <Var7 />
+        <Var8 />
+        <Var9 />
+      </OrderInfo>
+      <PaymentInfo>
+        <CCN>4111111111111111</CCN>
+        <Expdate>0811</Expdate>
+        <CVCCVV>123</CVCCVV>
+        <InstallmentOffset>0</InstallmentOffset>
+        <InstallmentPeriod>0</InstallmentPeriod>
+      </PaymentInfo>
+    </Message>
+  </JProxyPayLink>
+XML
+
+    params = {:money => @amount,
+              :order_id => @options[:order_id],
+              :description => @options[:description],
+              :variables => ["Variable 1", "Variable 2"],
+              :creditcard => @credit_card}
+    actual_authorize_request_xml = @gateway.send(:build_xml, :authorize, params)
+
+    assert_equal expected_authorize_request_xml, actual_authorize_request_xml
+  end
+
+  def test_successful_authorize_request
+    @gateway.expects(:ssl_post).returns(successful_authorize_response)
     
-    assert response = @gateway.purchase(@amount, @credit_card, @options)
-    assert_instance_of 
+    assert response = @gateway.authorize(@amount, @credit_card, @options)
     assert_success response
     
-    # Replace with authorization number from the successful response
-    assert_equal '', response.authorization
+    assert_equal '10186301', response.authorization
     assert response.test?
   end
 
-  def test_unsuccessful_request
-    @gateway.expects(:ssl_post).returns(failed_purchase_response)
+  def test_unsuccessful_authorize_request
+    @gateway.expects(:ssl_post).returns(failed_authorize_response)
     
-    assert response = @gateway.purchase(@amount, @credit_card, @options)
+    assert response = @gateway.authorize(@amount, @credit_card, @options)
     assert_failure response
+    assert_equal '0', response.authorization
     assert response.test?
   end
 
+  def test_capture_xml
+    expected_capture_request_xml = <<XML
+<?xml version="1.0" encoding="UTF-8"?>
+  <JProxyPayLink>
+    <Message>
+      <Type>Capture</Type>
+      <Authentication>
+        <MerchantID>login</MerchantID>
+        <Password>password</Password>
+      </Authentication>
+      <OrderInfo>
+        <Amount>100</Amount>
+        <MerchantRef>Some order id</MerchantRef>
+        <MerchantDesc>Description</MerchantDesc>
+        <Currency>978</Currency>
+        <CustomerEmail></CustomerEmail>
+        <Var1>Variable 1</Var1>
+        <Var2>Variable 2</Var2>
+        <Var3 />
+        <Var4 />
+        <Var5 />
+        <Var6 />
+        <Var7 />
+        <Var8 />
+        <Var9 />
+      </OrderInfo>
+    </Message>
+  </JProxyPayLink>
+XML
+
+    params = {:money => @amount,
+              :order_id => @options[:order_id],
+              :description => @options[:description],
+              :variables => ["Variable 1", "Variable 2"]}
+    actual_capture_request_xml = @gateway.send(:build_xml, :capture, params)
+
+    assert_equal expected_capture_request_xml, actual_capture_request_xml
+  end
+
+  def test_successful_capture_request
+    @gateway.expects(:ssl_post).returns(successful_capture_response)
+
+    assert response = @gateway.capture(@amount, @credit_card, @options)
+    assert_success response
+
+    assert_equal '0', response.authorization
+    assert response.test?
+  end
+
+    def test_credit_xml
+    expected_credit_request_xml = <<XML
+<?xml version="1.0" encoding="UTF-8"?>
+  <JProxyPayLink>
+    <Message>
+      <Type>Refund</Type>
+      <Authentication>
+        <MerchantID>login</MerchantID>
+        <Password>password</Password>
+      </Authentication>
+      <OrderInfo>
+        <Amount>100</Amount>
+        <MerchantRef>Some order id</MerchantRef>
+        <MerchantDesc>Description</MerchantDesc>
+        <Currency>978</Currency>
+        <CustomerEmail></CustomerEmail>
+        <Var1>Variable 1</Var1>
+        <Var2>Variable 2</Var2>
+        <Var3 />
+        <Var4 />
+        <Var5 />
+        <Var6 />
+        <Var7 />
+        <Var8 />
+        <Var9 />
+      </OrderInfo>
+    </Message>
+  </JProxyPayLink>
+XML
+
+    params = {:money => @amount,
+              :order_id => @options[:order_id],
+              :description => @options[:description],
+              :variables => ["Variable 1", "Variable 2"]}
+    actual_credit_request_xml = @gateway.send(:build_xml, :credit, params)
+
+    assert_equal expected_credit_request_xml, actual_credit_request_xml
+  end
+
+  def test_void_xml
+    expected_void_request_xml = <<XML
+<?xml version="1.0" encoding="UTF-8"?>
+  <JProxyPayLink>
+    <Message>
+      <Type>Cancel</Type>
+      <Authentication>
+        <MerchantID>login</MerchantID>
+        <Password>password</Password>
+      </Authentication>
+      <OrderInfo>
+        <Amount>0</Amount>
+        <MerchantRef>Some order id</MerchantRef>
+        <MerchantDesc>Description</MerchantDesc>
+        <Currency>978</Currency>
+        <CustomerEmail></CustomerEmail>
+        <Var1>Variable 1</Var1>
+        <Var2>Variable 2</Var2>
+        <Var3 />
+        <Var4 />
+        <Var5 />
+        <Var6 />
+        <Var7 />
+        <Var8 />
+        <Var9 />
+      </OrderInfo>
+    </Message>
+  </JProxyPayLink>
+XML
+
+    params = {:money => 0,
+              :order_id => @options[:order_id],
+              :description => @options[:description],
+              :variables => ["Variable 1", "Variable 2"]}
+    actual_void_request_xml = @gateway.send(:build_xml, :void, params)
+
+    assert_equal expected_void_request_xml, actual_void_request_xml
+  end
+
+  #######
   private
-  
-  # Place raw successful response from gateway here
-  def successful_purchase_response
+  #######
+
+  def successful_authorize_response
+    <<XML
+<?xml version="1.0" encoding="UTF-8"?><RESPONSE> <ERRORCODE>0</ERRORCODE> <ERRORMESSAGE> </ERRORMESSAGE> <REFERENCE>Some order id</REFERENCE> <PROXYPAYREF>10186301</PROXYPAYREF> <SEQUENCE>0</SEQUENCE></RESPONSE>
+XML
   end
   
-  # Place raw failed response from gateway here
-  def failed_purcahse_response
+  def failed_authorize_response
+    <<XML
+<?xml version="1.0" encoding="UTF-8"?><RESPONSE> <ERRORCODE>30</ERRORCODE> <ERRORMESSAGE> Could not send payment request to acquirer</ERRORMESSAGE> <REFERENCE>Some order id</REFERENCE> <PROXYPAYREF>0</PROXYPAYREF> <SEQUENCE>0</SEQUENCE></RESPONSE>
+XML
   end
+
+  def successful_capture_response
+    <<XML
+<?xml version="1.0" encoding="UTF-8"?><RESPONSE> <ERRORCODE>0</ERRORCODE> <ERRORMESSAGE> </ERRORMESSAGE> <REFERENCE>Some order id</REFERENCE> <PROXYPAYREF>0</PROXYPAYREF> <SEQUENCE>0</SEQUENCE></RESPONSE>
+XML
+  end
+
 end
