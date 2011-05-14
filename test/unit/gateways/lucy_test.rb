628e3c3b79ee8e681195584e24454fecb84a9572
Justin Weidmann <justin@twinsoftech.com>
2011-05-14 16:44:21 -0600

    lucy unit test remove old comments

--- test/unit/gateways/lucy_test.rb
+++ test/unit/gateways/lucy_test.rb
@@ -32,7 +32,6 @@ class LucyTest < Test::Unit::TestCase
     assert_instance_of Response, response
     assert_success response
     
-    # Replace with authorization number from the successful response
     assert_equal '13266', response.authorization
   end
 
@@ -59,12 +58,10 @@ class LucyTest < Test::Unit::TestCase
 
   private
   
-  # Place raw successful response from gateway here
   def successful_purchase_response
     "<?xml version=\"1.0\" encoding=\"utf-8\"?>\r\n<Response xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns=\"SmartPayments\">\r\n  <Result>0</Result>\r\n  <RespMSG>Approved</RespMSG>\r\n  <Message>APPROVED</Message>\r\n  <AuthCode>095331</AuthCode>\r\n  <PNRef>13266</PNRef>\r\n  <HostCode>00000000</HostCode>\r\n  <GetCommercialCard>False</GetCommercialCard>\r\n  <ExtData>CardType=VISA,BatchNum=000000&lt;BatchNum&gt;000000&lt;/BatchNum&gt;</ExtData>\r\n</Response>"
   end
   
-  # Place raw failed response from gateway here
   def failed_purchase_response
     "<?xml version=\"1.0\" encoding=\"utf-8\"?>\r\n<Response xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns=\"SmartPayments\">\r\n  <Result>12</Result>\r\n  <RespMSG>Decline</RespMSG>\r\n  <Message>DECLINED</Message>\r\n  <AuthCode>AUTH  DECLINED  200</AuthCode>\r\n  <PNRef>13315</PNRef>\r\n  <HostCode>00000000</HostCode>\r\n  <GetCommercialCard>False</GetCommercialCard>\r\n  <ExtData>CardType=VISA,BatchNum=000000&lt;BatchNum&gt;000000&lt;/BatchNum&gt;</ExtData>\r\n</Response>"
   end
