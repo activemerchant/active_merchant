c9753c8070de6376243d4c6d97c78bccde3417f3
Anders Lemke <mail@anderslemke.dk>
2011-05-24 17:28:35 +0200

    Updated Quickpay to use version 4

--- lib/active_merchant/billing/gateways/quickpay.rb
+++ lib/active_merchant/billing/gateways/quickpay.rb
@@ -13,23 +13,24 @@ module ActiveMerchant #:nodoc:
       self.homepage_url = 'http://quickpay.dk/'
       self.display_name = 'Quickpay'
       
-      PROTOCOL = 3
+      PROTOCOL = 4
       
       MD5_CHECK_FIELDS = {
-        :authorize => [:protocol, :msgtype, :merchant, :ordernumber, :amount, :currency, :autocapture, :cardnumber, :expirationdate, :cvd, :cardtypelock],
-        :capture   => [:protocol, :msgtype, :merchant, :amount, :transaction],
-        :cancel    => [:protocol, :msgtype, :merchant, :transaction],
-        :refund    => [:protocol, :msgtype, :merchant, :amount, :transaction],
-        :subscribe => [:protocol, :msgtype, :merchant, :ordernumber, :cardnumber, :expirationdate, :cvd, :cardtypelock, :description],
-        :recurring => [:protocol, :msgtype, :merchant, :ordernumber, :amount, :currency, :autocapture, :transaction],
-        :status    => [:protocol, :msgtype, :merchant, :transaction],
-        :chstatus  => [:protocol, :msgtype, :merchant],
+        :authorize => [:protocol, :msgtype, :merchant, :ordernumber, :amount, :currency, :autocapture, :cardnumber, :expirationdate, :cvd, :cardtypelock, :fraud_remote_addr, :fraud_http_accept, :fraud_http_accept_language, :fraud_http_accept_encoding, :fraud_http_accept_charset, :fraud_http_referer, :fraud_http_user_agent, :apikey],
+        :capture   => [:protocol, :msgtype, :merchant, :amount, :transaction, :fraud_remote_addr, :fraud_http_accept, :fraud_http_accept_language, :fraud_http_accept_encoding, :fraud_http_accept_charset, :fraud_http_referer, :fraud_http_user_agent, :apikey],
+        :cancel    => [:protocol, :msgtype, :merchant, :transaction, :fraud_remote_addr, :fraud_http_accept, :fraud_http_accept_language, :fraud_http_accept_encoding, :fraud_http_accept_charset, :fraud_http_referer, :fraud_http_user_agent, :apikey],
+        :refund    => [:protocol, :msgtype, :merchant, :amount, :transaction, :fraud_remote_addr, :fraud_http_accept, :fraud_http_accept_language, :fraud_http_accept_encoding, :fraud_http_accept_charset, :fraud_http_referer, :fraud_http_user_agent, :apikey],
+        :subscribe => [:protocol, :msgtype, :merchant, :ordernumber, :cardnumber, :expirationdate, :cvd, :cardtypelock, :description, :fraud_remote_addr, :fraud_http_accept, :fraud_http_accept_language, :fraud_http_accept_encoding, :fraud_http_accept_charset, :fraud_http_referer, :fraud_http_user_agent, :apikey],
+        :recurring => [:protocol, :msgtype, :merchant, :ordernumber, :amount, :currency, :autocapture, :transaction, :fraud_remote_addr, :fraud_http_accept, :fraud_http_accept_language, :fraud_http_accept_encoding, :fraud_http_accept_charset, :fraud_http_referer, :fraud_http_user_agent, :apikey],
+        :status    => [:protocol, :msgtype, :merchant, :transaction, :fraud_remote_addr, :fraud_http_accept, :fraud_http_accept_language, :fraud_http_accept_encoding, :fraud_http_accept_charset, :fraud_http_referer, :fraud_http_user_agent, :apikey],
+        :chstatus  => [:protocol, :msgtype, :merchant, :fraud_remote_addr, :fraud_http_accept, :fraud_http_accept_language, :fraud_http_accept_encoding, :fraud_http_accept_charset, :fraud_http_referer, :fraud_http_user_agent, :apikey],
       }
       
       APPROVED = '000'
       
       # The login is the QuickpayId
-      # The password is the md5checkword from the Quickpay admin interface
+      # The password is the md5checkword from the Quickpay manager
+      # To use the API-key from the Quickpay manager, specify :api-key
       def initialize(options = {})
         requires!(options, :login, :password)
         @options = options
@@ -43,6 +44,7 @@ module ActiveMerchant #:nodoc:
         add_invoice(post, options)
         add_creditcard_or_reference(post, credit_card_or_reference, options)
         add_autocapture(post, false)
+        add_fraud_parameters(post, options)
 
         commit(recurring_or_authorize(credit_card_or_reference), post)
       end
@@ -54,7 +56,8 @@ module ActiveMerchant #:nodoc:
         add_creditcard_or_reference(post, credit_card_or_reference, options)
         add_invoice(post, options)
         add_autocapture(post, true)
-
+        add_fraud_parameters(post, options)
+        
         commit(recurring_or_authorize(credit_card_or_reference), post)
       end
       
@@ -63,6 +66,7 @@ module ActiveMerchant #:nodoc:
         
         add_reference(post, authorization)
         add_amount_without_currency(post, money)
+        add_fraud_parameters(post, options)
         
         commit(:capture, post)
       end
@@ -71,6 +75,7 @@ module ActiveMerchant #:nodoc:
         post = {}
         
         add_reference(post, identification)
+        add_fraud_parameters(post, options)
         
         commit(:cancel, post)
       end
@@ -80,6 +85,7 @@ module ActiveMerchant #:nodoc:
 
         add_amount_without_currency(post, money)
         add_reference(post, identification)
+        add_fraud_parameters(post, options)
 
         commit(:refund, post)
       end
@@ -95,6 +101,7 @@ module ActiveMerchant #:nodoc:
         add_creditcard(post, creditcard, options)
         add_invoice(post, options)
         add_description(post, options)
+        add_fraud_parameters(post, options)
 
         commit(:subscribe, post)
       end
@@ -123,6 +130,10 @@ module ActiveMerchant #:nodoc:
       
       def add_reference(post, identification)
         post[:transaction] = identification
+      end
+      
+      def add_fraud_parameters(post, options)
+        post[:fraud_http_referer] = options[:fraud_http_referer]
       end
       
       def add_creditcard_or_reference(post, credit_card_or_reference, options)
@@ -184,6 +195,7 @@ module ActiveMerchant #:nodoc:
         params[:protocol] = PROTOCOL
         params[:msgtype]  = action.to_s
         params[:merchant] = @options[:login]
+        params[:apikey] = @options[:apikey] if @options[:apikey]
         #params[:testmode] = '1' if test?
         params[:md5check] = generate_check_hash(action, params)
         
