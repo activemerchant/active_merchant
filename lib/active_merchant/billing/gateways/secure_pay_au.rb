b8b360458b2c0c57c9ae87dbc0c83fc856462f64
James Healy <jimmy@deefa.com>
2011-05-25 12:15:31 +1000

    add card storage and triggered payment support to SecurePayAu gateway

--- lib/active_merchant/billing/gateways/secure_pay_au.rb
+++ lib/active_merchant/billing/gateways/secure_pay_au.rb
@@ -4,9 +4,12 @@ module ActiveMerchant #:nodoc:
   module Billing #:nodoc:
     class SecurePayAuGateway < Gateway
       API_VERSION = 'xml-4.2'
+      PERIODIC_API_VERSION = "spxml-3.0"
       
       TEST_URL = 'https://www.securepay.com.au/test/payment'
       LIVE_URL = 'https://www.securepay.com.au/xmlapi/payment'
+      TEST_PERIODIC_URL = "https://test.securepay.com.au/xmlapi/periodic"
+      LIVE_PERIODIC_URL = "https://api.securepay.com.au/xmlapi/periodic"
       
       self.supported_countries = ['AU']
       self.supported_cardtypes = [:visa, :master, :american_express, :diners_club, :jcb]
@@ -35,6 +38,18 @@ module ActiveMerchant #:nodoc:
         :void => 6,
         :refund => 4
       }
+
+      PERIODIC_ACTIONS = {
+        :add_triggered    => "add",
+        :remove_triggered => "delete",
+        :trigger          => "trigger"
+      }
+
+      PERIODIC_TYPES = {
+        :add_triggered    => 4,
+        :remove_triggered => nil,
+        :trigger          => nil
+      }
       
       SUCCESS_CODES = [ '00', '08', '11', '16', '77' ]
       
@@ -48,9 +63,16 @@ module ActiveMerchant #:nodoc:
         @options[:test] || super
       end
       
-      def purchase(money, credit_card, options = {})
-        requires!(options, :order_id)
-        commit :purchase, build_purchase_request(money, credit_card, options)
+      def purchase(money, payment_source, options = {})
+        if credit_card.is_a?(ActiveMerchant::Billing::CreditCard)
+          requires!(options, :order_id)
+          commit :purchase, build_purchase_request(money, credit_card, options)
+        elsif credit_card.to_s.size > 0
+          options[:billingid] = credit_card.to_s
+          commit_periodic(build_periodic_item(:trigger, money, nil, options))
+        else
+          raise ArgumentError, "credit_card must be a ActiveMerchant::Billing::CreditCard or string with size > 0"
+        end
       end                       
     
       def authorize(money, credit_card, options = {})
@@ -73,6 +95,24 @@ module ActiveMerchant #:nodoc:
 
       def void(reference, options = {})
         commit :void, build_reference_request(nil, reference)
+      end
+
+      def store(creditcard, options = {})
+        unless options[:billingid].to_s.size > 0
+          raise ArgumentError, ":billingid options must be provided"
+        end
+
+        commit_periodic(build_periodic_item(:add_triggered, 100, creditcard, options))
+      end
+
+      def unstore(billingid, options = {})
+        if billingid.to_s.size > 0
+          options[:billingid] = billingid
+        else
+          raise ArgumentError, "billingid must be provided"
+        end
+
+        commit_periodic(build_periodic_item(:remove_triggered, 100, nil, options))
       end
       
       private
@@ -142,6 +182,63 @@ module ActiveMerchant #:nodoc:
         
         Response.new(success?(response), message_from(response), response, 
           :test => test?, 
+          :authorization => authorization_from(response)
+        )
+      end
+
+      def build_periodic_item(action, money, credit_card, options)
+        xml = Builder::XmlMarkup.new
+
+        xml.tag! 'actionType', PERIODIC_ACTIONS[action]
+        xml.tag! 'clientID', options[:billingid].to_s
+
+        if credit_card
+          xml.tag! 'CreditCardInfo' do
+            xml.tag! 'cardNumber', credit_card.number
+            xml.tag! 'expiryDate', expdate(credit_card)
+            xml.tag! 'cvv', credit_card.verification_value if credit_card.verification_value?
+          end
+        end
+        xml.tag! 'amount', amount(money)
+        xml.tag! 'periodicType', PERIODIC_TYPES[action] if PERIODIC_TYPES[action]
+
+        xml.target!
+      end
+
+      def build_periodic_request(body)
+        xml = Builder::XmlMarkup.new
+        xml.instruct!
+        xml.tag! 'SecurePayMessage' do
+          xml.tag! 'MessageInfo' do
+            xml.tag! 'messageID', ActiveMerchant::Utils.generate_unique_id.slice(0, 30)
+            xml.tag! 'messageTimestamp', generate_timestamp
+            xml.tag! 'timeoutValue', request_timeout
+            xml.tag! 'apiVersion', PERIODIC_API_VERSION
+          end
+
+          xml.tag! 'MerchantInfo' do
+            xml.tag! 'merchantID', @options[:login]
+            xml.tag! 'password', @options[:password]
+          end
+
+          xml.tag! 'RequestType', 'Periodic'
+          xml.tag! 'Periodic' do
+            xml.tag! 'PeriodicList', "count" => 1 do
+              xml.tag! 'PeriodicItem', "ID" => 1 do
+                xml << body
+              end
+            end
+          end
+        end
+
+        xml.target!
+      end
+
+      def commit_periodic(request)
+        response = parse(ssl_post(test? ? TEST_PERIODIC_URL : LIVE_PERIODIC_URL, build_periodic_request(request)))
+
+        Response.new(success?(response), message_from(response), response,
+          :test => test?,
           :authorization => authorization_from(response)
         )
       end
