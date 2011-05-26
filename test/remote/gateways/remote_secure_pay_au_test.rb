3ea9e6ef8b89b2ed9005ded6290efd2ea5f42f8b
Tom Meier <tom@venombytes.com>
2011-05-26 21:17:32 +1000

    SecurePay AU gateway modifications   - Integrated github.com/yob commits on triggered payments   - Added more unit tests   - Added test account to fixtures.yml, allows remote tests to work   - Added complete coverage for remote tests

--- test/remote/gateways/remote_secure_pay_au_test.rb
+++ test/remote/gateways/remote_secure_pay_au_test.rb
@@ -1,20 +1,20 @@
 require 'test_helper'
 
 class RemoteSecurePayAuTest < Test::Unit::TestCase
-  
+
   def setup
     @gateway = SecurePayAuGateway.new(fixtures(:secure_pay_au))
-    
-    @amount = 100
-    @credit_card = credit_card('4444333322221111')
 
-    @options = { 
+    @amount = 100
+    @credit_card = credit_card('4444333322221111', {:month => 9, :year => 15})
+
+    @options = {
       :order_id => '1',
       :billing_address => address,
       :description => 'Store Purchase'
     }
   end
-  
+
   def test_successful_purchase
     assert response = @gateway.purchase(@amount, @credit_card, @options)
     assert_success response
@@ -36,7 +36,7 @@ class RemoteSecurePayAuTest < Test::Unit::TestCase
     assert capture = @gateway.capture(@amount, auth.authorization)
     assert_success capture
   end
-  
+
   def test_failed_authorize
     @amount = 151
     assert auth = @gateway.authorize(@amount, @credit_card, @options)
@@ -62,7 +62,7 @@ class RemoteSecurePayAuTest < Test::Unit::TestCase
     assert_success response
     assert_equal 'Approved', response.message
   end
-  
+
   def test_failed_refund
     assert response = @gateway.purchase(@amount, @credit_card, @options)
     assert_success response
@@ -72,7 +72,7 @@ class RemoteSecurePayAuTest < Test::Unit::TestCase
     assert_failure response
     assert_equal 'Only $1.0 available for refund', response.message
   end
-  
+
   def test_successful_void
     assert response = @gateway.purchase(@amount, @credit_card, @options)
     assert_success response
@@ -91,6 +91,61 @@ class RemoteSecurePayAuTest < Test::Unit::TestCase
     assert response = @gateway.void(authorization+'1')
     assert_failure response
     assert_equal 'Unable to retrieve original FDR txn', response.message
+  end
+
+  def test_successful_unstore
+    @gateway.store(@credit_card, {:billing_id => 'test1234', :amount => 15000}) rescue nil
+
+    assert response = @gateway.unstore('test1234')
+    assert_success response
+
+    assert_equal 'Successful', response.message
+  end
+
+  def test_failed_unstore
+    @gateway.unstore('test1234') rescue nil #Ensure it is already missing
+
+    assert response = @gateway.unstore('test1234')
+    assert_failure response
+
+    assert_equal 'Normal', response.message
+  end
+
+  def test_successful_store
+    @gateway.unstore('test1234') rescue nil
+
+    assert response = @gateway.store(@credit_card, {:billing_id => 'test1234', :amount => 15000})
+    assert_success response
+
+    assert_equal 'Successful', response.message
+  end
+
+  def test_failed_store
+    @gateway.store(@credit_card, {:billing_id => 'test1234', :amount => 15000}) rescue nil #Ensure it already exists
+
+    assert response = @gateway.store(@credit_card, {:billing_id => 'test1234', :amount => 15000})
+    assert_failure response
+
+    assert_equal 'Duplicate Client ID Found', response.message
+  end
+
+  def test_successful_triggered_payment
+    @gateway.store(@credit_card, {:billing_id => 'test1234', :amount => 15000}) rescue nil #Ensure it already exists
+
+    assert response = @gateway.purchase(12300, 'test1234', @options)
+    assert_success response
+    assert_equal response.params['amount'], '12300'
+
+    assert_equal 'Approved', response.message
+  end
+
+  def test_failure_triggered_payment
+    @gateway.unstore('test1234') rescue nil #Ensure its no longer there
+
+    assert response = @gateway.purchase(12300, 'test1234', @options)
+    assert_failure response
+
+    assert_equal 'Payment not found', response.message
   end
 
   def test_invalid_login
