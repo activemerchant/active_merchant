54b60ea2c17dc18b90862c1f15c823aabb5dc47c
Greg Brockman <gdb@gregbrockman.com>
2011-06-26 16:03:01 -0700

    Stripe: Add separate auth/capture and partial linked refunds

--- test/unit/gateways/stripe_test.rb
+++ test/unit/gateways/stripe_test.rb
@@ -8,7 +8,8 @@ class StripeTest < Test::Unit::TestCase
     @gateway = StripeGateway.new(:login => 'login')
 
     @credit_card = credit_card()
-    @amount = 100
+    @amount = 400
+    @refund_amount = 200
 
     @options = {
       :billing_address => address(),
@@ -28,10 +29,48 @@ class StripeTest < Test::Unit::TestCase
     assert response.test?
   end
 
+  def test_successful_authorize
+    @gateway.expects(:ssl_request).returns(successful_authorize_response)
+
+    assert response = @gateway.authorize(@amount, @credit_card, @options)
+    assert_instance_of Response, response
+    assert_success response
+    assert response
+
+    # Replace with authorization number from the successful response
+    assert_equal 'ch_test_charge', response.authorization
+    assert response.test?
+  end
+
+  def test_successful_capture
+    @gateway.expects(:ssl_request).returns(successful_purchase_response)
+
+    assert response = @gateway.capture(nil, 'ch_test_charge')
+    assert_instance_of Response, response
+    assert_success response
+    assert response
+
+    # Replace with authorization number from the successful response
+    assert_equal 'ch_test_charge', response.authorization
+    assert response.test?
+  end
+
   def test_successful_void
     @gateway.expects(:ssl_request).returns(successful_purchase_response(true))
 
     assert response = @gateway.void('ch_test_charge')
+    assert_instance_of Response, response
+    assert_success response
+
+    # Replace with authorization number from the successful response
+    assert_equal 'ch_test_charge', response.authorization
+    assert response.test?
+  end
+
+  def test_successful_refund
+    @gateway.expects(:ssl_request).returns(successful_partially_refunded_response)
+
+    assert response = @gateway.refund(@refund_amount, 'ch_test_charge')
     assert_instance_of Response, response
     assert_success response
 
@@ -54,14 +93,6 @@ class StripeTest < Test::Unit::TestCase
     assert response = @gateway.purchase(@amount, @credit_card, @options)
     assert_failure response
     assert_match /^Invalid response received from the Stripe API/, response.message
-  end
-
-  def test_ensure_does_not_respond_to_authorize
-    assert !@gateway.respond_to?(:authorize)
-  end
-
-  def test_ensure_does_not_respond_to_capture
-    assert !@gateway.respond_to?(:capture) || @gateway.method(:capture).owner != @gateway.class
   end
 
   def test_add_customer
@@ -92,6 +123,10 @@ class StripeTest < Test::Unit::TestCase
     assert_equal @options[:billing_address][:country], post[:card][:address_country]
   end
 
+  def test_ensure_does_not_respond_to_credit
+    assert !@gateway.respond_to?(:credit)
+  end
+
   def test_gateway_without_credentials
     assert_raises ArgumentError do
       StripeGateway.new
@@ -109,27 +144,75 @@ class StripeTest < Test::Unit::TestCase
   # Place raw successful response from gateway here
   def successful_purchase_response(refunded=false)
     <<-RESPONSE
-    {
-      "attempted": true,
-      "refunded": #{refunded},
-      "paid": true,
-      "amount": 400,
-      "card": {
-        "type": "Visa",
-        "country": "US",
-        "exp_month": 9,
-        "last4": "4242",
-        "exp_year": #{Time.now.year + 1},
-        "object": "card",
-        "id": "cc_test_card"
-      },
-      "id": "ch_test_charge",
-      "livemode": false,
-      "description": "Test Purchase",
-      "currency": "usd",
-      "object": "charge",
-      "created": 1307309607
-    }
+{
+  "amount": 400,
+  "created": 1309131571,
+  "currency": "usd",
+  "description": "Test Purchase",
+  "id": "ch_test_charge",
+  "livemode": false,
+  "object": "charge",
+  "paid": true,
+  "refunded": #{refunded},
+  "card": {
+    "country": "US",
+    "exp_month": 9,
+    "exp_year": #{Time.now.year + 1},
+    "last4": "4242",
+    "object": "card",
+    "type": "Visa"
+  }
+}
+    RESPONSE
+  end
+
+  def successful_partially_refunded_response
+    <<-RESPONSE
+{
+  "amount": 400,
+  "amount_refunded": 200,
+  "created": 1309131571,
+  "currency": "usd",
+  "description": "Test Purchase",
+  "id": "ch_test_charge",
+  "livemode": false,
+  "object": "charge",
+  "paid": true,
+  "refunded": true,
+  "card": {
+    "country": "US",
+    "exp_month": 9,
+    "exp_year": #{Time.now.year + 1},
+    "last4": "4242",
+    "object": "card",
+    "type": "Visa"
+  }
+}
+    RESPONSE
+  end
+
+  def successful_authorize_response
+    <<-RESPONSE
+{
+  "amount": 400,
+  "created": 1309131571,
+  "currency": "usd",
+  "description": "Test Purchase",
+  "id": "ch_test_charge",
+  "livemode": false,
+  "object": "charge",
+  "paid": true,
+  "refunded": true,
+  "uncaptured": true,
+  "card": {
+    "country": "US",
+    "exp_month": 9,
+    "exp_year": #{Time.now.year + 1},
+    "last4": "4242",
+    "object": "card",
+    "type": "Visa"
+  }
+}
     RESPONSE
   end
 
