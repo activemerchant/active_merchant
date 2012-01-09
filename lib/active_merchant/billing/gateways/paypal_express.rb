c9853425dc6d2eb2e3afa25f2454b94e66aa580f
Olek Janiszewski <olek.janiszewski@gmail.com>
2012-01-09 19:40:08 +0100

    Paypal Express: Support specifying :brand_name

--- lib/active_merchant/billing/gateways/paypal_express.rb
+++ lib/active_merchant/billing/gateways/paypal_express.rb
@@ -106,6 +106,7 @@ module ActiveMerchant #:nodoc:
               xml.tag! 'n2:NoShipping', options[:no_shipping] ? '1' : '0'
               xml.tag! 'n2:AddressOverride', options[:address_override] ? '1' : '0'
               xml.tag! 'n2:LocaleCode', options[:locale] unless options[:locale].blank?
+              xml.tag! 'n2:BrandName', options[:brand_name] unless options[:brand_name].blank?
               # Customization of the payment page
               xml.tag! 'n2:PageStyle', options[:page_style] unless options[:page_style].blank?
               xml.tag! 'n2:cpp-header-image', options[:header_image] unless options[:header_image].blank?
