9e7a79931a1496e5572bd5f837fca244b6203d67
Przemysław Ciąćka <przemyslaw.ciacka@gmail.com>
2012-03-25 22:26:58 +0200

    Dotpay integration support.

---
+++ lib/active_merchant/billing/integrations/dotpay/return.rb
@@ -0,0 +1,11 @@
+module ActiveMerchant #:nodoc:
+  module Billing #:nodoc:
+    module Integrations #:nodoc:
+      module Dotpay
+        class Return < ActiveMerchant::Billing::Integrations::Return
+        end
+      end
+    end
+  end
+end
+
