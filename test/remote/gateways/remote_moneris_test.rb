426d026a8d086c1df297d0a2adad73ab203824eb
Kenzie Campbell <kenzie@route19.com>
2012-06-04 22:21:51 -0300

    Moneris gateway: Add support for vault
    Adds #store, #unstore, and #update, and support for them to #authorize
    and #capture.
    
    Closes #369.

--- test/remote/gateways/remote_moneris_test.rb
+++ test/remote/gateways/remote_moneris_test.rb
@@ -9,8 +9,7 @@ class MonerisRemoteTest < Test::Unit::TestCase
     @credit_card = credit_card('4242424242424242')
     @options = { 
       :order_id => generate_unique_id,
-      :billing_address => address,
-      :description => 'Store Purchase'
+      :customer => generate_unique_id
     }
   end
   
@@ -63,7 +62,7 @@ class MonerisRemoteTest < Test::Unit::TestCase
   def test_failed_purchase_and_void
     purchase = @gateway.purchase(101, @credit_card, @options)
     assert_failure purchase
-    
+
     void = @gateway.void(purchase.authorization)
     assert_failure void
   end
@@ -81,4 +80,50 @@ class MonerisRemoteTest < Test::Unit::TestCase
     assert_failure response
     assert_equal 'Declined', response.message
   end
+
+  def test_successful_store
+    assert response = @gateway.store(@credit_card)
+    assert_success response
+    assert_equal "Successfully registered cc details", response.message
+    assert response.params["data_key"].present?
+    @data_key = response.params["data_key"]
+  end
+
+  def test_successful_unstore
+    test_successful_store
+    assert response = @gateway.unstore(@data_key)
+    assert_success response
+    assert_equal "Successfully deleted cc details", response.message
+    assert response.params["data_key"].present?
+  end
+
+  def test_update
+    test_successful_store
+    assert response = @gateway.update(@data_key, @credit_card)
+    assert_success response
+    assert_equal "Successfully updated cc details", response.message
+    assert response.params["data_key"].present?
+  end
+
+  def test_successful_purchase_with_vault
+    test_successful_store
+    assert response = @gateway.purchase(@amount, @data_key, @options)
+    assert_success response
+    assert_equal "Approved", response.message
+    assert_false response.authorization.blank?
+  end
+
+  def test_successful_authorization_with_vault
+    test_successful_store
+    assert response = @gateway.authorize(@amount, @data_key, @options)
+    assert_success response
+    assert_false response.authorization.blank?
+  end
+
+  def test_failed_authorization_with_vault
+    test_successful_store
+    response = @gateway.authorize(105, @data_key, @options)
+    assert_failure response
+  end
+
 end
