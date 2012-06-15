7adad78e8d32bfca55b592173a7a56acd4272b4c
Matthew Savage <matt@amasses.net>
2012-06-15 16:20:37 +1000

    Added code for Fat Zebra gateway with the following functionality:
     * Purchase
     * Store card
     * Purchase with token
     * Refund
    
    Included remote and unit tests.

--- lib/active_merchant/billing/gateways/fat_zebra.rb
+++ lib/active_merchant/billing/gateways/fat_zebra.rb
@@ -5,7 +5,6 @@ module ActiveMerchant #:nodoc:
     class FatZebraGateway < Gateway
       LIVE_URL    = "https://gateway.fatzebra.com.au/v1.0"
       SANDBOX_URL = "https://gateway.sandbox.fatzebra.com.au/v1.0"
-      LOCAL_URL   = "http://fatapi.dev/v1.0"
 
       self.supported_countries = ['AU']
       self.default_currency = 'AUD'
@@ -40,18 +39,112 @@ module ActiveMerchant #:nodoc:
 
         add_amount(post, money, options)
         add_creditcard(post, creditcard, options)
-        add_customer(post, options)
-        post[:description] = options[:description] || options[:email]
-        add_flags(post, options)
+        post[:reference] = options[:order_id]
+        post[:customer_ip] = options[:ip]
 
-        meta = generate_meta(options)
+        commit(:post, 'purchases', post)
+      end
 
-        raise ArgumentError.new("Customer or Credit Card required.") if !post[:card] && !post[:customer]
+      # Refund a transaction
+      #
+      # amount - Integer - the amount to refund
+      # txn_id - String - the original transaction to be refunded
+      # reference - String - your transaction reference
+      def refund(money, txn_id, reference)
+        post = {}
 
-        commit(:post, 'charges', post, meta)
+        post[:amount] = money
+        post[:transaction_id] = txn_id
+        post[:reference] = reference
+
+        commit(:post, "refunds", post)
+      end
+
+      # Tokenize a credit card
+      def store(creditcard)
+        post = {}
+        add_creditcard(post, creditcard)
+
+        commit(:post, "credit_cards", post)
       end
 
       private
+      def add_amount(post, money, options)
+        post[:amount] = money
+      end
+
+      def add_creditcard(post, creditcard, options = {})
+        if creditcard.respond_to?(:number)
+          post[:card_number] = creditcard.number
+          post[:card_expiry] = "#{creditcard.month}/#{creditcard.year}"
+          post[:cvv] = creditcard.verification_value if creditcard.verification_value?
+          post[:card_holder] = creditcard.name if creditcard.name
+        else
+            post[:card_token] = creditcard[:token]
+            post[:cvv] = creditcard[:cvv]
+        end
+      end
+
+      # Post the data to the gateway
+      def commit(method, uri, parameters=nil)
+        raw_response = response = nil
+        success = false
+        begin
+          raw_response = ssl_request(method, get_url(uri), parameters.to_json, headers)
+          response = parse(raw_response)
+          success = response["successful"] && (response["response"]["successful"] || response["response"]["token"])
+        rescue ResponseError => e
+          if e.response.code == "401"
+            return Response.new(false, "Invalid Login")
+          end
+
+          raw_response = e.response.body
+          response = response_error(raw_response)
+        rescue JSON::ParserError
+          response = json_error(raw_response)
+        end
+
+        message = response["response"]["message"]
+        unless response["successful"]
+          # There is an error, so we will show that instead
+          message = response["errors"].empty? ? "Unknown Error" : response["errors"].join(", ")
+        end
+
+        Response.new(success,
+          message,
+          response,
+          :test => response.has_key?("test") ? response["test"] : false,
+          :authorization => response["response"]["authorization"],
+          :id => response["response"]["id"])
+      end
+
+      def response_error(data)
+        puts data.inspect
+        {}
+      end
+
+      # Parse the returned JSON
+      def parse(response)
+        begin
+          JSON.parse(response)
+        rescue JSON::ParserError
+          msg = 'Invalid JSON response received from Fat Zebra. Please contact support@fatzebra.com.au if you continue to receive this message.'
+          msg += "  (The raw response returned by the API was #{response.inspect})"
+          {
+            "successful" => false,
+            "response" => {},
+            "errors" => [msg]
+          }
+        end
+      end
+
+      # Build the URL based on the AM mode and the URI
+      def get_url(uri)
+        base = test? ? SANDBOX_URL : LIVE_URL
+        base + "/" + uri
+      end
+
+      # Builds the auth and U-A headers for the request
       def headers
         {
           "Authorization" => "Basic " + Base64.encode64(@username.to_s + ":" + @token.to_s).strip,
