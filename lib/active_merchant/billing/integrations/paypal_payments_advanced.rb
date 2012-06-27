ce5dec854770e349c9059fe0febfc1913a40d355
csaunders <chris@christophersaunders.ca>
2012-06-27 20:26:16 +0000

    Fix payflow link

--- lib/active_merchant/billing/integrations/paypal_payments_advanced.rb
+++ lib/active_merchant/billing/integrations/paypal_payments_advanced.rb
@@ -3,13 +3,12 @@ module ActiveMerchant
     module Integrations
       module PaypalPaymentsAdvanced
         autoload :Helper, 'active_merchant/billing/integrations/paypal_payments_advanced/helper.rb'
-        autoload :Notification, 'active_merchant/billing/integrations/payflow_link/notification.rb'
 
         mattr_accessor :service_url
         self.service_url = 'https://payflowlink.paypal.com'
 
         def self.notification(post, options = {})
-          Notification.new(post)
+          PayflowLink::Notification.new(post)
         end
 
         def self.return(query_string, options = {})
