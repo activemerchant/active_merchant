9d454508f3ad511e307022ae0606c2272c9bd07a
csaunders <chris@christophersaunders.ca>
2012-07-16 21:13:59 +0000

    Plug'n Pay gateway: Add tests for partial capture
    Partial captures were not being completed through Plug'n Pay, and
    initially it looked like a code change was needed to fix it (using
    reauth). However, it turned out to be a configuration problem with the
    account on the Plug'n Pay side, so this commit simply adds tests
    specifically for partial capture.
    
    Closes #410.

--- test/remote/gateways/remote_plugnpay_test.rb
+++ test/remote/gateways/remote_plugnpay_test.rb
@@ -10,62 +10,72 @@ class PlugnpayTest < Test::Unit::TestCase
       :description => 'Store purchaes'
     }
     @amount = 100
-    
   end
-  
+
   def test_bad_credit_card
     assert response = @gateway.authorize(@amount, @bad_credit_card, @options)
     assert_failure response
     assert_equal 'Invalid Credit Card No.', response.message
   end
-  
+
   def test_good_credit_card
     assert response = @gateway.authorize(@amount, @good_credit_card, @options)
     assert_success response
     assert !response.authorization.blank?
     assert_equal 'Success', response.message
   end
-  
+
   def test_purchase_transaction
-    assert response = @gateway.purchase(@amount, @good_credit_card, @options)        
-    assert_success response 
+    assert response = @gateway.purchase(@amount, @good_credit_card, @options)
+    assert_success response
     assert !response.authorization.blank?
     assert_equal 'Success', response.message
   end
-  
+
   # Capture, and Void require that you Whitelist your IP address.
-  # In the gateway admin tool, you must add your IP address to the allowed addresses and uncheck "Remote client" under the 
+  # In the gateway admin tool, you must add your IP address to the allowed addresses and uncheck "Remote client" under the
   # "Auth Transactions" section of the "Security Requirements" area in the test account Security Administration Area.
   def test_authorization_and_capture
     assert authorization = @gateway.authorize(@amount, @good_credit_card, @options)
     assert_success authorization
-    
+
     assert capture = @gateway.capture(@amount, authorization.authorization)
     assert_success capture
+    assert capture.params['aux_msg'].include? "has been successfully marked for settlement."
     assert_equal 'Success', capture.message
   end
-  
+
+  def test_authorization_and_partial_capture
+    assert authorization = @gateway.authorize(@amount, @good_credit_card, @options)
+    assert_success authorization
+
+    assert capture = @gateway.capture(@amount - 1, authorization.authorization)
+    assert_success capture
+    assert capture.params['aux_msg'].include? "has been successfully reauthed for usd 0.99"
+    assert_equal 'Success', capture.message
+  end
+
   def test_authorization_and_void
     assert authorization = @gateway.authorize(@amount, @good_credit_card, @options)
     assert_success authorization
-    
+
     assert void = @gateway.void(authorization.authorization)
     assert_success void
     assert_equal 'Success', void.message
   end
-  
+
   def test_purchase_and_credit
     assert purchase = @gateway.purchase(@amount, @good_credit_card, @options)
     assert_success purchase
-    
+
     assert credit = @gateway.credit(@amount, purchase.authorization)
     assert_success credit
     assert_equal 'Success', credit.message
   end
-  
+
   def test_credit_with_no_previous_transaction
     assert credit = @gateway.credit(@amount, @good_credit_card, @options)
-    
+
     assert_success credit
     assert_equal 'Success', credit.message
   end
