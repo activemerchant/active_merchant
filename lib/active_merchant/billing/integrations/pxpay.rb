349b900f51226c52b9f7c986fb10181a5865b517
Tom Burns <tom.burns@jadedpixel.com>
2012-07-24 14:34:18 -0400

    add options to return & notification as we need merchant credentials to process response :(

--- lib/active_merchant/billing/integrations/pxpay.rb
+++ lib/active_merchant/billing/integrations/pxpay.rb
@@ -4,7 +4,8 @@ module ActiveMerchant #:nodoc:
       module Pxpay
         autoload :Helper, 'active_merchant/billing/integrations/pxpay/helper.rb'
         autoload :Notification, 'active_merchant/billing/integrations/pxpay/notification.rb'
-        
+        autoload :Return, 'active_merchant/billing/integrations/pxpay/return.rb'
+
         TOKEN_URL = 'https://sec.paymentexpress.com/pxpay/pxaccess.aspx'
 
         LIVE_URL = 'https://sec.paymentexpress.com/pxpay/pxpay.aspx'
@@ -17,12 +18,12 @@ module ActiveMerchant #:nodoc:
           LIVE_URL
         end
 
-        def self.notification(post)
-          Notification.new(post)
+        def self.notification(post, options={})
+          Notification.new(post, options)
         end
         
-        def self.return(query_string)
-          Return.new(query_string)
+        def self.return(query_string, options={})
+          Return.new(query_string, options)
         end
       end
     end
