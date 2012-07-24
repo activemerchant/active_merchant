5b468d5d9ecf21bdf539c38476e7dfa5f12327db
Tom Burns <tom.burns@jadedpixel.com>
2012-07-24 14:35:18 -0400

    tests - remote test full transaction, unit test for helper

--- test/remote/integrations/remote_pxpay_integration_test.rb
+++ test/remote/integrations/remote_pxpay_integration_test.rb
@@ -11,7 +11,6 @@ class RemotePxpayIntegrationTest < Test::Unit::TestCase
     @options = fixtures(:pxpay)
 
     @helper = Pxpay::Helper.new('50', @options[:login], :amount => "120.99", :currency => 'USD', :credential2 => @options[:password])
-    @notification = Pxpay::Notification.new('')
   end
 
   def test_valid_credentials_returns_secure_token
@@ -36,7 +35,55 @@ class RemotePxpayIntegrationTest < Test::Unit::TestCase
     assert_equal Pxpay.service_url, "#{url.scheme}://#{url.host}#{url.path}"
   end
 
-  def test_redirect_accepts_form_post
+  def test_entire_payment
+
+    # generate form to redirect customer to pxpay gateway
+    generate_valid_redirect_form
+
+    puts @output_buffer
+
+    # submit generated form and ensure we're redirected to the CC info page
+    agent = Mechanize.new { |a|
+      a.user_agent_alias = 'Mac Safari'
+    }
+
+    page = Mechanize::Page.new(nil,{'content-type'=>'text/html'}, @output_buffer, nil, agent)
+
+    gateway_page = agent.submit page.forms.first
+
+    puts gateway_page.body
+
+    # entire valid test CC credentials and submit form
+
+    assert gateway_page.forms.size > 0
+
+    confirm_page = gateway_page.form_with(:name => 'PmtEnt') do |form|
+      form.CardNum = '4111111111111111'
+      form.ExMnth = '12'
+      form.ExYr = '20'
+      form.NmeCard = 'Firstname Lastname'
+      form.Cvc2 = '123'
+    end.submit
+
+    puts confirm_page.body
+
+    # pull out redirected URL params
+    return_url = confirm_page.link_with(:text => 'Click Here to Proceed to the Next step').href
+
+    assert !return_url.empty?
+
+    param_string = return_url.sub(/.*\?/, "")
+
+    puts "\n\nparam_string = #{param_string}\n"
+
+    return_handler = Pxpay.return(param_string, :account => @options[:login], :credential2 => @options[:password])
+
+    assert return_handler
+  end
+
+  private
+
+  def generate_valid_redirect_form
     payment_service_for('1', @options[:login], :service => :pxpay,  :amount => "157.0") do |service|
             
       # You must set :credential2 to your pxpay key
@@ -68,27 +115,7 @@ class RemotePxpayIntegrationTest < Test::Unit::TestCase
       service.cancel_return_url "http://t/pxpay/payment_cancelled"
       service.payment_header 'My store name'
       service.currency 'USD'
-
     end
-
-    puts @output_buffer
-
-    # submit generated form and ensure we're redirected to the CC info page
-    agent = Mechanize.new { |a|
-      a.user_agent_alias = 'Mac Safari'
-    }
-
-    page = Mechanize::Page.new(nil,{'content-type'=>'text/html'}, @output_buffer, nil, agent)
-    page.forms.first.delete_field!('utf8')
-    page.forms.first.method = "GET"
-
-    res = agent.submit page.forms.first
-
-    puts res.body
-
-    assert res.forms.size > 0
   end
-
-
 end
 
--- test/unit/integrations/pxpay_module_test.rb
+++ test/unit/integrations/pxpay_module_test.rb
@@ -1,4 +1,6 @@
 require 'test_helper'
+require 'nokogiri'
+require 'mechanize'
 
 class PxpayModuleTest < Test::Unit::TestCase
   include CommStub
@@ -7,16 +9,19 @@ class PxpayModuleTest < Test::Unit::TestCase
 
   def setup
     super
-    @username = "ShopifyHPP_Dev"
-    @key = "b1853e58edccc1cc13bb24b9bede3acd41cdeaa8942ad330b27ab04d209003c0"
+    @options = fixtures(:pxpay)
+    @username = @options[:login]
+    @key = @options[:password]
   end
 
   def test_notification_method
-    assert_instance_of Pxpay::Notification, Pxpay.notification('name=cody')
+    Pxpay::Notification.any_instance.stubs(:decrypt_transaction_result)
+
+    assert_instance_of Pxpay::Notification, Pxpay.notification('name=cody&result=token', :account => '', :credential2 => '')
   end
 
   def test_should_round_numbers
-    Pxpay::Helper.any_instance.stubs(:request_secure_token)
+    Pxpay::Helper.any_instance.stubs(:form_fields).returns({})
 
     request = ""
     payment_service_for('44',@username, :service => :pxpay,  :amount => "157.003"){ |service| request = service.generate_request}
@@ -27,7 +32,7 @@ class PxpayModuleTest < Test::Unit::TestCase
   end
 
   def test_amount_has_cent_precision
-    Pxpay::Helper.any_instance.stubs(:request_secure_token)
+    Pxpay::Helper.any_instance.stubs(:form_fields).returns({})
 
     request = ""
     payment_service_for('44',@username, :service => :pxpay,  :amount => "157"){ |service| request = service.generate_request}
@@ -35,44 +40,38 @@ class PxpayModuleTest < Test::Unit::TestCase
   end
 
   def test_all_fields
-    Pxpay::Helper.any_instance.stubs(:request_secure_token)
+    Pxpay::Helper.any_instance.stubs(:form_fields).returns({})
 
     request = ""
-      payment_service_for('44',@username, :service => :pxpay,  :amount => 157.0){|service|
+    payment_service_for('44',@username, :service => :pxpay,  :amount => 157.0){|service|
 
-        service.customer_id 8
-        service.customer :first_name => 'g',
-                         :last_name => 'g',
-                         :email => 'g@g.com',
-                         :phone => '3'
+      service.customer_id 8
+      service.customer :first_name => 'g',
+                       :last_name => 'g',
+                       :email => 'g@g.com',
+                       :phone => '3'
 
-        service.billing_address :zip => 'g',
-                         :country => 'United States of America',
-                         :address1 => 'g'
+      service.return_url "http://example.com/pxpay/return_url"
+      service.cancel_return_url "http://example.com/pxpay/cancel_url"
 
-        service.ship_to_address :first_name => 'g',
-                                :last_name => 'g',
-                                :city => '',
-                                :address1 => 'g',
-                                :address2 => '',
-                                :state => 'ut',
-                                :country => 'United States of America',
-                                :zip => 'g'
+      service.credential2 @key
 
-        service.return_url "http://t/pxpay/return_url"
-        service.cancel_return_url "http://t/pxpay/cancel_url"
+      request = service.generate_request
+    }
 
-        request = service.generate_request
-      }
-
-    # <GenerateRequest><TxnId>44</TxnId><PxPayUserId>PxPayUser</PxPayUserId><TxnType>Purchase</TxnType><AmountInput>157.0</AmountInput>
-    # <EnableAddBillCard>0</EnableAddBillCard><EmailAddress>g@g.com</EmailAddress><TxnData1>g</TxnData1>
-    # <TxnData3>United States of America</TxnData3><UrlSuccess>http://t/pxpay/return_url</UrlSuccess>
-    # <UrlFail>http://t/pxpay/cancel_url</UrlFail></GenerateRequest>
+    assert_match /<TxnId>44</, request
+    assert_match /<PxPayUserId>#{@username}</, request
+    assert_match /<PxPayKey>#{@key}</, request
+    assert_match /<TxnType>Purchase</, request
+    assert_match /<AmountInput>157.00</, request
+    assert_match /<EnableAddBillCard>0</, request
+    assert_match /<EmailAddress>g@g.com</, request
+    assert_match /<UrlSuccess>http:\/\/example.com\/pxpay\/return_url</, request
+    assert_match /<UrlFail>http:\/\/example.com\/pxpay\/cancel_url</, request
   end
 
-  def test_xml_escaping_all_fields
-    Pxpay::Helper.any_instance.stubs(:request_secure_token)
+  def test_xml_escaping_fields
+    Pxpay::Helper.any_instance.stubs(:form_fields).returns({})
 
     request = ""
 
@@ -97,7 +96,7 @@ class PxpayModuleTest < Test::Unit::TestCase
                               :country => '>United States of America',
                               :zip => '>g'
 
-      service.credential2 = @key
+      service.credential2 @key
 
       service.return_url "http://t/pxpay/return_url?&"
       service.cancel_return_url "http://t/pxpay/cancel_url?&"
@@ -107,11 +106,25 @@ class PxpayModuleTest < Test::Unit::TestCase
     assert_nothing_raised do
       doc = Nokogiri::XML(request) { |config| config.options = Nokogiri::XML::ParseOptions::STRICT }
     end
+  end
 
-    # <GenerateRequest><TxnId>44</TxnId><PxPayUserId>PxPayUser</PxPayUserId><TxnType>Purchase</TxnType><AmountInput>157.0</AmountInput>
-    # <EnableAddBillCard>0</EnableAddBillCard><EmailAddress>g@g.com</EmailAddress><TxnData1>g</TxnData1>
-    # <TxnData3>United States of America</TxnData3><UrlSuccess>http://t/pxpay/return_url</UrlSuccess>
-    # <UrlFail>http://t/pxpay/cancel_url</UrlFail></GenerateRequest>
+  def test_created_form_is_valid()
+    Pxpay::Helper.any_instance.stubs(:ssl_post).returns('<Request valid="1"><URI>https://sec.paymentexpress.com/pxpay/pxpay.aspx?userid=ShopifyHPP_Dev&amp;request=REQUEST_TOKEN</URI></Request>')
+
+    payment_service_for('44',@username, :service => :pxpay, :amount => 157.0){|service|
+       service.credential2 @key
+       service.return_url "http://store.shopify.com/done"
+       service.cancel_return_url "http://store.shopify.com/cancel"
+    }
+
+    agent = Mechanize.new { |a| a.user_agent_alias = 'Mac Safari' }
+
+    page = Mechanize::Page.new(nil, {'content-type'=>'text/html'}, @output_buffer, nil, agent)
+
+    form = page.forms.first
+
+    assert_match /GET/i, form.method
+
   end
 
   def test_payment_key_required()
@@ -120,48 +133,11 @@ class PxpayModuleTest < Test::Unit::TestCase
   def test_currency_required()
   end
 
-  def test_urlsuccess_required()
-  end
-
-  def test_urlfailure_required()
-  end
-
-  def test_ 
-  end
-
   def check_inclusion(these_lines)
     for line in these_lines do
       assert @output_buffer.include?(line), ['unable to find ', line, ' ', 'in \n', @output_buffer].join(' ')
     end
   end
 
-  def test_normal_fields
-    Pxpay::Helper.any_instance.stubs(:request_secure_token)
-
-    payment_service_for('44','8wd65QS', :service => :pxpay,  :amount => 157.0){|service|
-
-      service.setup_hash :transaction_key => '8CP6zJ7uD875J6tY',
-          :order_timestamp => 1206836763
-      service.customer_id 8
-      service.customer :first_name => 'Cody',
-                         :last_name => 'Fauser',
-                         :phone => '(555)555-5555',
-                         :email => 'g@g.com'
-
-      service.billing_address :city => 'city1',
-                                :address1 => 'g',
-                                :address2 => '',
-                                :state => 'UT',
-                                :country => 'United States of America',
-                                :zip => '90210'
-       service.invoice '#1000'
-       service.shipping '30.00'
-       service.tax '31.00'
-       service.test_request 'true'
-
-    }
-  end
-
- 
 
 end
