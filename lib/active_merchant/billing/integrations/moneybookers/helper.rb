13725804ee75bad0e9563ab2c5598978e6169606
tamaras <tamara.solomon@jadedpixel.com>
2012-10-05 09:59:21 -0400

    Passing shop language to Moneybookers if supported

--- lib/active_merchant/billing/integrations/moneybookers/helper.rb
+++ lib/active_merchant/billing/integrations/moneybookers/helper.rb
@@ -26,6 +26,24 @@ module ActiveMerchant #:nodoc:
           mapping :return_url, 'return_url'
           mapping :cancel_return_url, 'cancel_url'
           mapping :description, 'detail1_text'
+
+          MAPPED_LOCALE_CODES = {
+            'SE' => 'SV', #Sweden to Swedish
+            'DK' => 'DA', #Denmark to Danish
+            'FI' => 'FI', #The rest are the same... do they need the mapping?
+            'DE' => 'DE', 
+            'ES' => 'ES', 
+            'FR' => 'FR', 
+            'IT' => 'IT',
+            'PL' => 'PL', 
+            'GR' => 'GR', 
+            'RO' => 'RO', 
+            'RU' => 'RU', 
+            'TR' => 'TR', 
+            'CN' => 'CN', 
+            'CZ' => 'CZ', 
+            'NL' => 'NL'
+          }
           
           def initialize(order, account, options = {})
             super
@@ -33,7 +51,6 @@ module ActiveMerchant #:nodoc:
             add_default_parameters
             add_seller_details(options)
           end
-
 
           private
           
@@ -51,6 +68,11 @@ module ActiveMerchant #:nodoc:
           def add_seller_details(options)
             add_field('recipient_description', options[:account_name]) if options[:account_name]
             add_field('country', lookup_country_code(options[:country], :alpha3)) if options[:country]
+            add_field('language', locale_code(options[:country])) if options[:country]
+          end
+
+          def locale_code(country_code)
+            MAPPED_LOCALE_CODES[country_code] || 'EN'
           end
         end
       end
