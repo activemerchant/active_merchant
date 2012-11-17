14a30333e690cbafa631053722a4ef479141e2d2
Forest Carlisle <forestcarlisle@gmail.com>
2012-11-16 16:52:06 -0800

    Added support for passing a credit card OR token to the authorize and purchase methods.   * Note: a token is not a reference (authorization <litleTxnId>) to a previous transaction, but a token representing a credit card. A token is created by the gateway (if configured for tokenization) and returned in either explicit or implicit register token requests.    * Added certification tests defined by the LitleXML Reference Guide section 2.4.4    * Started refactoring the LitleGateway class to match the patterns used by other gateways.

--- test/remote/gateways/remote_litle_certification_test.rb
+++ test/remote/gateways/remote_litle_certification_test.rb
@@ -36,8 +36,8 @@ class RemoteLitleCertification < Test::Unit::TestCase
   end
 
   def test2
-    credit_card = CreditCard.new(:number => '5112010000000003', :month => '02', 
-                                 :year => '2012', :brand => 'master', 
+    credit_card = CreditCard.new(:number => '5112010000000003', :month => '02',
+                                 :year => '2012', :brand => 'master',
                                  :verification_value => '261')
 
     options = {
@@ -117,8 +117,8 @@ class RemoteLitleCertification < Test::Unit::TestCase
   end
 
   def test6
-    credit_card = CreditCard.new(:number => '4457010100000008', :month => '06', 
-                                 :year => '2012', :brand => 'visa', 
+    credit_card = CreditCard.new(:number => '4457010100000008', :month => '06',
+                                 :year => '2012', :brand => 'visa',
                                  :verification_value => '992')
 
     options = {
@@ -153,8 +153,8 @@ class RemoteLitleCertification < Test::Unit::TestCase
   end
 
   def test7
-    credit_card = CreditCard.new(:number => '5112010100000002', :month => '07', 
-                                 :year => '2012', :brand => 'master', 
+    credit_card = CreditCard.new(:number => '5112010100000002', :month => '07',
+                                 :year => '2012', :brand => 'master',
                                  :verification_value => '251')
 
     options = {
@@ -188,8 +188,8 @@ class RemoteLitleCertification < Test::Unit::TestCase
   end
 
   def test8
-    credit_card = CreditCard.new(:number => '6011010100000002', :month => '08', 
-                                 :year => '2012', :brand => 'discover', 
+    credit_card = CreditCard.new(:number => '6011010100000002', :month => '08',
+                                 :year => '2012', :brand => 'discover',
                                  :verification_value => '184')
 
     options = {
@@ -223,8 +223,8 @@ class RemoteLitleCertification < Test::Unit::TestCase
   end
 
   def test9
-    credit_card = CreditCard.new(:number => '375001010000003', :month => '09', 
-                                 :year => '2012', :brand => 'american_express', 
+    credit_card = CreditCard.new(:number => '375001010000003', :month => '09',
+                                 :year => '2012', :brand => 'american_express',
                                  :verification_value => '0421')
 
     options = {
@@ -255,7 +255,122 @@ class RemoteLitleCertification < Test::Unit::TestCase
     assert_equal "I", response.avs_result["code"]
   end
 
+  # Implicit Token Registration Tests
+  def test55
+    credit_card = CreditCard.new(:number             => '5435101234510196',
+                                 :month              => '11',
+                                 :year               => '2014',
+                                 :brand              => 'master',
+                                 :verification_value => '987')
+    options     = {
+        :order_id => '55'
+    }
+
+    # authorize
+    assert response = @gateway.authorize(15000, credit_card, options)
+    #"tokenResponse" => { "litleToken"        => "1712000118270196",
+    #                     "tokenResponseCode" => "802",
+    #                     "tokenMessage"      => "Account number was previously registered",
+    #                     "type"              => "MC",
+    #                     "bin"               => "543510" }
+    assert_success response
+    assert_equal 'Approved', response.message
+    assert_equal '0196', response.params['litleOnlineResponse']['authorizationResponse']['tokenResponse']['litleToken'][-4,4]
+    assert %w(801 802).include? response.params['litleOnlineResponse']['authorizationResponse']['tokenResponse']['tokenResponseCode']
+    assert_equal 'MC', response.params['litleOnlineResponse']['authorizationResponse']['tokenResponse']['type']
+    assert_equal '543510', response.params['litleOnlineResponse']['authorizationResponse']['tokenResponse']['bin']
+  end
+
+  def test56
+    credit_card = CreditCard.new(:number             => '5435109999999999',
+                                 :month              => '11',
+                                 :year               => '2014',
+                                 :brand              => 'master',
+                                 :verification_value => '987')
+    options     = {
+        :order_id => '56'
+    }
+
+    # authorize
+    assert response = @gateway.authorize(15000, credit_card, options)
+
+    assert_failure response
+    assert_equal '301', response.params['litleOnlineResponse']['authorizationResponse']['response']
+  end
+
+  def test57
+    credit_card = CreditCard.new(:number             => '5435101234510196',
+                                 :month              => '11',
+                                 :year               => '2014',
+                                 :brand              => 'master',
+                                 :verification_value => '987')
+    options     = {
+        :order_id => '57'
+    }
+
+    # authorize
+    assert response = @gateway.authorize(15000, credit_card, options)
+
+    assert_success response
+    assert_equal 'Approved', response.message
+    assert_equal '0196', response.params['litleOnlineResponse']['authorizationResponse']['tokenResponse']['litleToken'][-4,4]
+    assert %w(801 802).include? response.params['litleOnlineResponse']['authorizationResponse']['tokenResponse']['tokenResponseCode']
+    assert_equal 'MC', response.params['litleOnlineResponse']['authorizationResponse']['tokenResponse']['type']
+    assert_equal '543510', response.params['litleOnlineResponse']['authorizationResponse']['tokenResponse']['bin']
+  end
+
+  def test59
+      token = '1712990000040196'
+
+      options = {
+          :order_id => '59'
+      }
+
+      # authorize
+      assert response = @gateway.authorize(15000, token, options)
+
+    assert_failure response
+    assert_equal '822', response.params['litleOnlineResponse']['authorizationResponse']['response']
+  end
+
+  def test60
+    token = '171299999999999'
+
+    options = {
+        :order_id => '59'
+    }
+
+    # authorize
+    assert response = @gateway.authorize(15000, token, options)
+
+    assert_failure response
+    assert_equal '823', response.params['litleOnlineResponse']['authorizationResponse']['response']
+  end
+
+  def test_authorize_and_purchase_with_token
+    credit_card = CreditCard.new(:number             => '5435101234510196',
+                                 :month              => '11',
+                                 :year               => '2014',
+                                 :brand              => 'master',
+                                 :verification_value => '987')
+
+    # authorize
+    assert auth_response = @gateway.authorize(0, credit_card, :order_id => '12345678')
+
+    assert_success auth_response
+    assert_equal 'Approved', auth_response.message
+    token = auth_response.params['litleOnlineResponse']['authorizationResponse']['tokenResponse']['litleToken']
+    assert_equal '0196', token[-4, 4]
+    assert %w(801 802).include? auth_response.params['litleOnlineResponse']['authorizationResponse']['tokenResponse']['tokenResponseCode']
+
+    # purchase
+    assert purchase_response = @gateway.authorize(0, token, :order_id => '12345679')
+    assert_success purchase_response
+    assert_equal 'Approved', purchase_response.message
+  end
+
   private
+
   def auth_assertions(amount, card, options, assertions)
     # 1: authorize
     assert response = @gateway.authorize(amount, card, options)
