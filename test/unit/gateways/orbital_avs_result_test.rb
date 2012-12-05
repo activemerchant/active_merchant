activemerchant/active_merchant

commit 8cf277a826a268df8c4e2c15ae90bf217df0807d

Orbital gateway: handle custom AVS response codes
Orbital has their own custom set of AVS response codes. This introduces
a custom AVS class for Orbital that translates those codes into what
ActiveMerchant expects.

Also required some refactoring of Billing::Response to allow a custom
AVS response class to be used. Which, in turn, required some fixes in
the BluePay gateway.

Closes #465.

lib/active_merchant/billing/response.rb
@@ -1,6 +1,5 @@
 module ActiveMerchant #:nodoc:
   module Billing #:nodoc:
-
     class Error < ActiveMerchantError #:nodoc:
     end
 
@@ -24,8 +23,20 @@ module ActiveMerchant #:nodoc:
         @test = options[:test] || false
         @authorization = options[:authorization]
         @fraud_review = options[:fraud_review]
-        @avs_result = AVSResult.new(options[:avs_result]).to_hash
-        @cvv_result = CVVResult.new(options[:cvv_result]).to_hash
+
+        avs_result_builder = if options[:avs_result].kind_of?(Hash)
+          AVSResult.new(options[:avs_result])
+        else
+          options[:avs_result] || {}
+        end
+        @avs_result = avs_result_builder.to_hash
+
+        cvv_result_builder = if options[:cvv_result].kind_of?(String)
+          CVVResult.new(options[:cvv_result])
+        else
+          options[:cvv_result] || {}
+        end
+        @cvv_result = cvv_result_builder.to_hash
       end
     end
 

test/unit/gateways/orbital_avs_result_test.rb
@@ -1,0 +1,41 @@
+require 'test_helper'
+
+class OrbitalAVSResultTest < Test::Unit::TestCase
+  def test_no_match
+    check_match_results('G', 'N', 'N')
+  end
+
+  def test_only_street_match
+    check_match_results('F', 'Y', 'N')
+  end
+
+  def test_only_postal_match
+    check_match_results('A', 'N', 'Y')
+  end
+
+  def test_nil_data
+    result = ActiveMerchant::Billing::OrbitalGateway::AVSResult.new(nil)
+    assert_nil result.code
+    assert_nil result.message
+  end
+
+  def test_empty_data
+    result = ActiveMerchant::Billing::OrbitalGateway::AVSResult.new('')
+    assert_nil result.code
+    assert_nil result.message
+  end
+
+  # Helper functions
+
+  def check_match_results(code, street_match, postal_match)
+    result = ActiveMerchant::Billing::OrbitalGateway::AVSResult.new(code)
+    assert_equal code, result.code
+    assert_equal street_match, result.street_match
+    assert_equal postal_match, result.postal_match
+    assert_equal ActiveMerchant::Billing::OrbitalGateway::AVSResult.messages[code], result.message
+
+    avs_data = result.to_hash
+    assert_equal code, avs_data['code']
+    assert_equal ActiveMerchant::Billing::OrbitalGateway::AVSResult.messages[code], avs_data['message']
+  end
+end

lib/active_merchant/billing/gateways.rb
@@ -1,18 +1,17 @@
 module ActiveMerchant
   module Billing
     autoload :Gateway, 'active_merchant/billing/gateway'
-        
-    Dir[File.dirname(__FILE__) + '/gateways/**/*.rb'].each do |f|      
-      
-      # Get camelized class name 
+
+    Dir[File.dirname(__FILE__) + '/gateways/**/*.rb'].each do |f|
+      # Get camelized class name
       filename = File.basename(f, '.rb')
       # Add _gateway suffix
       gateway_name = filename + '_gateway'
       # Camelize the string to get the class name
-      gateway_class = gateway_name.camelize      
-      
+      gateway_class = gateway_name.camelize
+
       # Register for autoloading
-      autoload gateway_class, f      
+      autoload gateway_class, f
     end
   end
 end

lib/active_merchant/billing/gateways/orbital.rb
@@ -1,4 +1,5 @@
-require File.dirname(__FILE__) + '/orbital/orbital_soft_descriptors.rb'
+require File.dirname(__FILE__) + '/orbital/orbital_soft_descriptors'
+require File.dirname(__FILE__) + '/orbital/avs_result'
 require "rexml/document"
 
 module ActiveMerchant #:nodoc:
@@ -301,7 +302,7 @@ module ActiveMerchant #:nodoc:
           {
              :authorization => authorization_string(response[:tx_ref_num], response[:order_id]),
              :test => self.test?,
-             :avs_result => {:code => response[:avs_resp_code]},
+             :avs_result => OrbitalGateway::AVSResult.new(response[:avs_resp_code]),
              :cvv_result => response[:cvv2_resp_code]
           }
         )

lib/active_merchant/billing/gateways/orbital/avs_result.rb
@@ -1,0 +1,93 @@
+module ActiveMerchant #:nodoc:
+  module Billing #:nodoc:
+    class OrbitalGateway < Gateway
+      # Unfortunately, Orbital uses their own special codes for AVS responses
+      # that are different than the standard codes defined in
+      # <tt>ActiveMerchant::Billing::AVSResult</tt>.
+      #
+      # This class encapsulates the response codes shown on page 240 of their spec:
+      # http://download.chasepaymentech.com/docs/orbital/orbital_gateway_xml_specification.pdf
+      #
+      class AVSResult < ActiveMerchant::Billing::AVSResult
+        CODES = {
+            '1'  => 'No address supplied',
+            '2'  => 'Bill-to address did not pass Auth Host edit checks',
+            '3'  => 'AVS not performed',
+            '4'  => 'Issuer does not participate in AVS',
+            '5'  => 'Edit-error - AVS data is invalid',
+            '6'  => 'System unavailable or time-out',
+            '7'  => 'Address information unavailable',
+            '8'  => 'Transaction Ineligible for AVS',
+            '9'  => 'Zip Match/Zip 4 Match/Locale match',
+            'A'  => 'Zip Match/Zip 4 Match/Locale no match',
+            'B'  => 'Zip Match/Zip 4 no Match/Locale match',
+            'C'  => 'Zip Match/Zip 4 no Match/Locale no match',
+            'D'  => 'Zip No Match/Zip 4 Match/Locale match',
+            'E'  => 'Zip No Match/Zip 4 Match/Locale no match',
+            'F'  => 'Zip No Match/Zip 4 No Match/Locale match',
+            'G'  => 'No match at all',
+            'H'  => 'Zip Match/Locale match',
+            'J'  => 'Issuer does not participate in Global AVS',
+            'JA' => 'International street address and postal match',
+            'JB' => 'International street address match. Postal code not verified',
+            'JC' => 'International street address and postal code not verified',
+            'JD' => 'International postal code match. Street address not verified',
+            'M1' => 'Merchant Override Decline',
+            'M2' => 'Cardholder name, billing address, and postal code matches',
+            'M3' => 'Cardholder name and billing code matches',
+            'M4' => 'Cardholder name and billing address matches',
+            'M5' => 'Cardholder name incorrect, billing address and postal code match',
+            'M6' => 'Cardholder name incorrect, billing address matches',
+            'M7' => 'Cardholder name incorrect, billing address matches',
+            'M8' => 'Cardholder name, billing address and postal code are all incorrect',
+            'N3' => 'Address matches, ZIP not verified',
+            'N4' => 'Address and ZIP code not verified due to incompatible formats',
+            'N5' => 'Address and ZIP code match (International only)',
+            'N6' => 'Address not verified (International only)',
+            'N7' => 'ZIP matches, address not verified',
+            'N8' => 'Address and ZIP code match (International only)',
+            'N9' => 'Address and ZIP code match (UK only)',
+            'R'  => 'Issuer does not participate in AVS',
+            'UX' => 'Unknown',
+            'X'  => 'Zip Match/Zip 4 Match/Address Match',
+            'Z'  => 'Zip Match/Locale no match',
+        }
+
+        # Map vendor's AVS result code to a postal match code
+        ORBITAL_POSTAL_MATCH_CODE = {
+            'Y' => %w( 9 A B C H JA JD M2 M3 M5 N5 N8 N9 X Z ),
+            'N' => %w( D E F G M8 ),
+            'X' => %w( 4 J R ),
+            nil => %w( 1 2 3 5 6 7 8 JB JC M1 M4 M6 M7 N3 N4 N6 N7 UX )
+        }.inject({}) do |map, (type, codes)|
+          codes.each { |code| map[code] = type }
+          map
+        end
+
+        # Map vendor's AVS result code to a street match code
+        ORBITAL_STREET_MATCH_CODE = {
+            'Y' => %w( 9 B D F H JA JB M2 M4 M5 M6 M7 N3 N5 N7 N8 N9 X ),
+            'N' => %w( A C E G M8 Z ),
+            'X' => %w( 4 J R ),
+            nil => %w( 1 2 3 5 6 7 8 JC JD M1 M3 N4 N6 UX )
+        }.inject({}) do |map, (type, codes)|
+          codes.each { |code| map[code] = type }
+          map
+        end
+
+        def self.messages
+          CODES
+        end
+
+        def initialize(code)
+          @code = code.to_s.strip.upcase unless code.blank?
+          if @code
+            @message      = CODES[@code]
+            @postal_match = ORBITAL_POSTAL_MATCH_CODE[@code]
+            @street_match = ORBITAL_STREET_MATCH_CODE[@code]
+          end
+        end
+      end
+    end
+  end
+end

test/unit/gateways/orbital_test.rb
@@ -132,6 +132,8 @@ class OrbitalGatewayTest < Test::Unit::TestCase
       assert_match(/<AVScountryCode>CA/, data)
     end.respond_with(successful_purchase_response)
     assert_success response
+    assert_equal 'Y', response.avs_result['street_match']
+    assert_equal 'Y', response.avs_result['postal_match']
   end
 
   def test_dont_send_address_details_for_germany
@@ -231,5 +233,4 @@ class OrbitalGatewayTest < Test::Unit::TestCase
   def successful_profile_response
     %q{<?xml version="1.0" encoding="UTF-8"?><Response><ProfileResp><CustomerBin>000001</CustomerBin><CustomerMerchantID>700000000000</CustomerMerchantID><CustomerName>Longbob Longsen</CustomerName><CustomerRefNum>ABC</CustomerRefNum><CustomerProfileAction>CREATE</CustomerProfileAction><ProfileProcStatus>0</ProfileProcStatus><CustomerProfileMessage>Profile Request Processed</CustomerProfileMessage><CustomerAccountType>CC</CustomerAccountType><Status>A</Status><CCAccountNum>4111111111111111</CCAccountNum><RespTime/></ProfileResp></Response>}
   end
-
 end

commit 0905662674899cdd85c8d90fc75edebf5c3a562b

AVS response needs to have the keys for the object in the hash returned, even if they are nil
8cf277a broke this expectation, now there are tests to cover the case.

A MultiResponse test needed to be fixed because of this change as well,
it shouldn't affect the interface or how AVSResult/CVVResult are
returned.

lib/active_merchant/billing/response.rb
@@ -24,19 +24,17 @@ module ActiveMerchant #:nodoc:
         @authorization = options[:authorization]
         @fraud_review = options[:fraud_review]
 
-        avs_result_builder = if options[:avs_result].kind_of?(Hash)
-          AVSResult.new(options[:avs_result])
+        @avs_result = if options[:avs_result].kind_of?(AVSResult)
+          options[:avs_result].to_hash
         else
-          options[:avs_result] || {}
+          AVSResult.new(options[:avs_result]).to_hash
         end
-        @avs_result = avs_result_builder.to_hash
 
-        cvv_result_builder = if options[:cvv_result].kind_of?(String)
-          CVVResult.new(options[:cvv_result])
+        @cvv_result = if options[:cvv_result].kind_of?(CVVResult)
+          options[:cvv_result].to_hash
         else
-          options[:cvv_result] || {}
+          CVVResult.new(options[:cvv_result]).to_hash
         end
-        @cvv_result = cvv_result_builder.to_hash
       end
     end
 

test/unit/gateways/orbital_avs_result_test.rb
@@ -25,6 +25,18 @@ class OrbitalAVSResultTest < Test::Unit::TestCase
     assert_nil result.message
   end
 
+  def test_response_with_orbital_avs
+    response = Response.new(true, 'message', {}, :avs_result => OrbitalGateway::AVSResult.new('A'))
+
+    assert_equal 'A', response.avs_result['code']
+  end
+
+  def test_response_with_orbital_avs_nil
+    response = Response.new(true, 'message', {}, :avs_result => OrbitalGateway::AVSResult.new(nil))
+
+    assert response.avs_result.has_key?('code')
+  end
+
   # Helper functions
 
   def check_match_results(code, street_match, postal_match)

test/unit/multi_response_test.rb
@@ -38,7 +38,7 @@ class MultiResponseTest < Test::Unit::TestCase
       :test => true,
       :authorization => "auth1",
       :avs_result => {:code => "AVS1"},
-      :cvv_result => {"code" => "CVV1"},
+      :cvv_result => "CVV1",
       :fraud_review => true
     )
     m.process{r1}
@@ -58,7 +58,7 @@ class MultiResponseTest < Test::Unit::TestCase
       :test => false,
       :authorization => "auth2",
       :avs_result => {:code => "AVS2"},
-      :cvv_result => {"code" => "CVV2"},
+      :cvv_result => "CVV2",
       :fraud_review => false
     )
     m.process{r2}

test/unit/response_test.rb
@@ -6,6 +6,16 @@ class ResponseTest < Test::Unit::TestCase
     assert !Response.new(false, 'message', :param => 'value').success?
   end
 
+  def test_response_without_avs
+    response = Response.new(true, 'message', :param => 'value')
+    assert response.avs_result.has_key?('code')
+  end
+
+  def test_response_without_cvv
+    response = Response.new(true, 'message', :param => 'value')
+    assert response.cvv_result.has_key?('code')
+  end
+
   def test_get_params
     response = Response.new(true, 'message', :param => 'value')
 