bb3d573f12899c01b22327f198eeffec70d52887
Sam Lown <me@samlown.com>
2012-12-27 19:27:06 +0100

    Basics working, starting to refine options and remote tests

---
+++ lib/active_merchant/billing/gateways/netpay.rb
@@ -0,0 +1,179 @@
+module ActiveMerchant #:nodoc:
+  module Billing #:nodoc:
+
+    #
+    # NETPAY Gateway
+    #
+    # Support for NETPAY's HTTP Connector payment gateway in Mexico.
+    #
+    # The gateway sends requests as HTTP POST and receives the response details
+    # in the HTTP header, making the process really rather simple.
+    #
+    #
+    class NetpayGateway < Gateway
+      self.test_url = 'http://200.57.87.243:8855'
+      self.live_url = 'https://example.com/live'
+
+      # The countries the gateway supports merchants from as 2 digit ISO country codes
+      self.supported_countries = ['MX']
+
+      self.default_currency = 'MXN'
+
+      # The card types supported by the payment gateway
+      self.supported_cardtypes = [:visa, :master, :american_express, :discover]
+
+      # The homepage URL of the gateway
+      self.homepage_url = 'http://www.netpay.com.mx'
+
+      # The name of the gateway
+      self.display_name = 'NETPAY Gateway'
+
+      CURRENCY_CODES = {
+        "MXN" => '484'
+      }
+
+      RESPONSE_KEYS = ['ResponseMsg', 'ResponseText', 'ResponseCode', 'TimeIn', 'TimeOut', 'AuthCode', 'OrderId', 'CardTypeName', 'MerchantId', 'IssuerAuthDate']
+
+      def initialize(options = {})
+        requires!(options, :store_id, :login, :password)
+        super
+      end
+
+      def authorize(money, creditcard, options = {})
+        post = {}
+        add_invoice(post, options)
+        add_creditcard(post, creditcard)
+        add_customer_data(post, options)
+        add_amount(post, money, options)
+
+        commit('PreAuth', post)
+      end
+
+      def cancel(authorization, options = {})
+        post = {}
+        add_order_id(post, authorization)
+
+        commit('Void', post)
+      end
+
+      def capture(money, authorization, options = {})
+        post = {}
+        add_order_id(post, authorization)
+        add_amount(post, money, options)
+
+        commit('PostAuth', post)
+      end
+
+      def purchase(money, creditcard, options = {})
+        post = {}
+        add_invoice(post, options)
+        add_creditcard(post, creditcard)
+        add_customer_data(post, options)
+        add_amount(post, money, options)
+
+        commit('Auth', post)
+      end
+
+      def refund(money, authorization, options = {})
+        post = {}
+
+        commit('Credit', post)
+      end
+
+
+      private
+
+      def add_login_data(post)
+        post['StoreId']     = @options[:store_id]
+        post['UserName']    = @options[:login]
+        post['Password']    = @options[:password]
+      end
+
+      def add_action(post, action)
+        post['ResourceName'] = action
+        post['ContentType']  = 'Transaction'
+        post['Mode']         = 'P'
+        #post['TerminalId']   = '00000001'
+      end
+
+      def add_order_id(post, order_id)
+        post['OrderId'] = order_id
+      end
+
+      def add_amount(post, money, options)
+        post['Total'] = amount(money)
+        post['CurrencyCode'] = currency_code(options[:currency] || currency(money))
+      end
+
+      def add_customer_data(post, options)
+        post['IPAddress'] = options[:ip] unless options[:ip].blank?
+      end
+
+      def add_invoice(post, options)
+        post['Comments'] = options[:description] if options[:description]
+      end
+
+      def add_creditcard(post, creditcard)
+        post['CardNumber']   = creditcard.number
+        post['ExpDate']      = expdate(creditcard)
+        post['CustomerName'] = creditcard.name
+        post['CVV2']         = creditcard.verification_value unless creditcard.verification_value.nil?
+      end
+
+      def expdate(credit_card)
+        year  = sprintf("%.4i", credit_card.year)
+        month = sprintf("%.2i", credit_card.month)
+
+        "#{month}/#{year[-2..-1]}"
+      end
+
+      def url
+        test? ? test_url : live_url
+      end
+
+      def parse(response)
+        params = params_from_response(response)
+
+        success = (params['ResponseCode'] == '00')
+        message = params['ResponseText'] || params['ResponseMsg']
+        options = @options.merge(:test => test?, :authorization => params['OrderId'])
+
+        Response.new(success, message, params, options)
+      end
+
+      def commit(action, parameters)
+        add_login_data(parameters)
+        add_action(parameters, action)
+
+        post = parameters.collect{|key, value| "#{key}=#{CGI.escape(value.to_s)}" }.join("&")
+        parse ssl_post(url, post)
+      end
+
+      # Override the regular handle response so we can access the headers
+      def handle_response(response)
+        case response.code.to_i
+        when 200...300
+          response
+        else
+          raise ResponseError.new(response)
+        end
+      end
+
+      # Return a hash containing all the useful, or informative values from netpay
+      def params_from_response(response)
+        params = {}
+        RESPONSE_KEYS.each do |k|
+          params[k] = response[k] unless response[k].to_s.empty?
+        end
+        params
+      end
+
+      def currency_code(currency)
+        return currency if currency =~ /^\d+$/
+        CURRENCY_CODES[currency]
+      end
+
+    end
+  end
+end
+
---
+++ test/remote/gateways/remote_netpay_test.rb
@@ -0,0 +1,69 @@
+require 'test_helper'
+
+class RemoteNetpayTest < Test::Unit::TestCase
+
+
+  def setup
+    @gateway = NetpayGateway.new(fixtures(:netpay))
+
+    NetpayGateway.wiredump_device = File.open(File.join('/tmp', "netpay.log"), "a+")
+    NetpayGateway.wiredump_device.sync = true
+
+    @amount = 2000
+    @credit_card = credit_card('5454545454545454')
+    @declined_card = credit_card('4000300011112220')
+
+    @options = {
+      :description => 'Store Purchase'
+    }
+  end
+
+  def test_successful_purchase
+    assert response = @gateway.purchase(@amount, @credit_card, @options)
+    assert_success response
+    assert_equal 'Aprobada', response.message
+  end
+
+  #def test_unsuccessful_purchase
+  #  assert response = @gateway.purchase(@amount, @declined_card, @options)
+  #  assert_failure response
+  #  assert_equal 'REPLACE WITH FAILED PURCHASE MESSAGE', response.message
+  #end
+
+  def test_authorize_and_capture
+    amount = @amount
+    assert auth = @gateway.authorize(amount, @credit_card, @options)
+    assert_success auth
+    assert_equal 'Aprobada', auth.message
+    assert auth.authorization
+    assert capture = @gateway.capture(amount, auth.authorization)
+    assert_success capture
+    assert_equal 'Aprobada', capture.message
+  end
+
+  def test_authorize_and_cancel
+    amount = @amount
+    assert auth = @gateway.authorize(amount, @credit_card, @options)
+    assert_success auth
+    assert_equal 'Aprobada', auth.message
+    assert cancel = @gateway.cancel(auth.authorization)
+    assert_success cancel
+    assert_equal 'Aprobada', cancel.message
+  end
+
+  #def test_failed_capture
+  #  assert response = @gateway.capture(@amount, '')
+  #  assert_failure response
+  #  assert_equal 'REPLACE WITH GATEWAY FAILURE MESSAGE', response.message
+  #end
+
+  #def test_invalid_login
+  #  gateway = NetpayGateway.new(
+  #              :login => '',
+  #              :password => ''
+  #            )
+  #  assert response = gateway.purchase(@amount, @credit_card, @options)
+  #  assert_failure response
+  #  assert_equal 'REPLACE WITH FAILURE MESSAGE', response.message
+  #end
+end
