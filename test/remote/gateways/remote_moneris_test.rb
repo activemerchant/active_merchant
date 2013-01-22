d9a10567587efb6652aece2da9a74ec7be007711
Nathaniel Talbott <nathaniel@talbott.ws>
2013-01-22 18:35:53 -0500

    Moneris: Use a capture of $0 for void
    Moneris uses a capture of $0 to accomplish the logical equivalent of
    what ActiveMerchant calls a void. This just delegates
    `void(authorization)` to `capture(0, authorization)`.
    
    Fixes #516.

--- test/remote/gateways/remote_moneris_test.rb
+++ test/remote/gateways/remote_moneris_test.rb
@@ -7,19 +7,19 @@ class MonerisRemoteTest < Test::Unit::TestCase
     @gateway = MonerisGateway.new(fixtures(:moneris))
     @amount = 100
     @credit_card = credit_card('4242424242424242')
-    @options = { 
+    @options = {
       :order_id => generate_unique_id,
       :customer => generate_unique_id
     }
   end
-  
+
   def test_successful_purchase
     assert response = @gateway.purchase(@amount, @credit_card, @options)
     assert_success response
     assert_equal 'Approved', response.message
-    assert_false response.authorization.blank? 
+    assert_false response.authorization.blank?
   end
-  
+
   def test_successful_authorization
     response = @gateway.authorize(@amount, @credit_card, @options)
     assert_success response
@@ -39,38 +39,20 @@ class MonerisRemoteTest < Test::Unit::TestCase
     response = @gateway.capture(@amount, response.authorization)
     assert_success response
   end
-  
+
   def test_successful_authorization_and_void
     response = @gateway.authorize(@amount, @credit_card, @options)
     assert_success response
     assert response.authorization
-    
-    # Moneris cannot void a preauthorization
-    # You must capture the auth transaction with an amount of $0.00
-    void = @gateway.capture(0, response.authorization)
-    assert_success void
-  end
-  
-  def test_successful_purchase_and_void
-    purchase = @gateway.purchase(@amount, @credit_card, @options)
-    assert_success purchase
-    
-    void = @gateway.void(purchase.authorization)
-    assert_success void
-  end
-  
-  def test_failed_purchase_and_void
-    purchase = @gateway.purchase(101, @credit_card, @options)
-    assert_failure purchase
 
-    void = @gateway.void(purchase.authorization)
-    assert_failure void
+    void = @gateway.void(response.authorization)
+    assert_success void
   end
-  
+
   def test_successful_purchase_and_credit
     purchase = @gateway.purchase(@amount, @credit_card, @options)
     assert_success purchase
-    
+
     credit = @gateway.credit(@amount, purchase.authorization)
     assert_success credit
   end
