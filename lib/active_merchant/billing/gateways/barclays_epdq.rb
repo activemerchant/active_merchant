4cc5efa364ba49b713c7f8c4a6a0f76817288a72
Alexander Profeit <aprofeit@me.com>
2013-02-19 17:30:54 -0500

    always encode to UTF-8 from ISO-8859-1

--- lib/active_merchant/billing/gateways/barclays_epdq.rb
+++ lib/active_merchant/billing/gateways/barclays_epdq.rb
@@ -134,10 +134,7 @@ module ActiveMerchant #:nodoc:
         end
 
         def parse
-          doc = begin REXML::Document.new(@response)
-          rescue REXML::ParseException
-            REXML::Document.new(@response.encode("UTF-8", "ISO-8859-1"))
-          end
+          doc = REXML::Document.new(@response.encode("UTF-8", "ISO-8859-1"))
 
           auth_type = find(doc, "//Transaction/Type").to_s
 
