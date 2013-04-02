1f94f2118f963ddd570712a31b4cd70d3bc54472
Caleb Simpson <caleb.simpson@jadedpixel.com>
2013-04-02 15:14:34 -0400

    Refactor of my last PR to remove warnings for ruby >= 1.9

--- test/remote/gateways/remote_finansbank_test.rb
+++ test/remote/gateways/remote_finansbank_test.rb
@@ -2,8 +2,8 @@ require 'test_helper'
 
 class RemoteFinansbankTest < Test::Unit::TestCase
   def setup
-    @original_kcode = $KCODE
     if RUBY_VERSION < '1.9' && $KCODE == "NONE"
+      @original_kcode = $KCODE
       $KCODE = 'u'
     end
 
@@ -23,7 +23,7 @@ class RemoteFinansbankTest < Test::Unit::TestCase
   end
 
   def teardown
-    $KCODE = @original_kcode
+    $KCODE = @original_kcode if @original_kcode
   end
 
   def test_successful_purchase
