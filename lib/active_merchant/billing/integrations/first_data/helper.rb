1e476466d3c6f4554e22605f84cd1bb6c0e18062
sanemat <o.gata.ken@gmail.com>
2013-05-11 20:37:54 +0900

    Require unless activesupport v4
    active_support/core_ext/float/rounding.rb is removed in rails4.
    That patch about float for ruby1.8.

--- lib/active_merchant/billing/integrations/first_data/helper.rb
+++ lib/active_merchant/billing/integrations/first_data/helper.rb
@@ -1,4 +1,4 @@
-require 'active_support/core_ext/float/rounding.rb' # Float#round(precision)
+require 'active_support/core_ext/float/rounding.rb' unless ActiveSupport::VERSION::MAJOR == 4 # Float#round(precision)
 
 module ActiveMerchant #:nodoc:
   module Billing #:nodoc:
