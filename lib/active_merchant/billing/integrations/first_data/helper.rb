b3ed7cfed8a5f2edac4d20c9624bc1bd4881832e
sanemat <o.gata.ken@gmail.com>
2013-05-11 22:16:45 +0900

    Require version of activesupport for ActiveSupport v2.3

--- lib/active_merchant/billing/integrations/first_data/helper.rb
+++ lib/active_merchant/billing/integrations/first_data/helper.rb
@@ -1,3 +1,4 @@
+require 'active_support/version' # for ActiveSupport2.3
 require 'active_support/core_ext/float/rounding.rb' unless ActiveSupport::VERSION::MAJOR == 4 # Float#round(precision)
 
 module ActiveMerchant #:nodoc:
