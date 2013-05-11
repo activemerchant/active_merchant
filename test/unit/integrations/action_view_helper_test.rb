a5ea74bed607297f4433a442fd5afb0576e6e850
sanemat <o.gata.ken@gmail.com>
2013-05-11 21:22:28 +0900

    Add config.secret_key_base for rails4
    This key is generated on my rake secret :)

--- test/unit/integrations/action_view_helper_test.rb
+++ test/unit/integrations/action_view_helper_test.rb
@@ -50,6 +50,9 @@ if "".respond_to? :html_safe?
 
       class MerchantApp < Rails::Application; end
       PaymentServiceController.send :include, Rails.application.routes.url_helpers
+      if Rails.version.start_with? '4'
+        Rails.application.config.secret_key_base = 'dad95720ad4ac592311874defcac8dd586795da07a5c87e51810c5a84012f2f2bf474b352fa76b1a0852cc14cf451b19d82abafa97dfdb1d14298843904c9b9b'
+      end
     end
 
     def test_html_safety
