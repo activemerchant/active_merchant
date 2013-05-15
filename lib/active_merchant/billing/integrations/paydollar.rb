a306896b0ce96bdd029883c2408c556c118d4bbf
Sachin Shintre <sachin@joshsoftware.com>
2013-05-15 15:12:31 +0530

    Removed unused currencies hash

--- lib/active_merchant/billing/integrations/paydollar.rb
+++ lib/active_merchant/billing/integrations/paydollar.rb
@@ -17,31 +17,6 @@ module ActiveMerchant #:nodoc:
 	  service_urls(ActiveMerchant::Billing::Base.integration_mode)
 	end
      
-	def self.currencies
-          {
-            '344' => 'HKD',
-            '840' => 'USD',
-            '702' => 'SGD',
-            '156' => 'CNY', # (RMB)
-            '392' => 'JPY',
-            '901' => 'TWD',
-            '036' => 'AUD',
-            '978' => 'EUR',
-            '826' => 'GBP',
-            '124' => 'CAD',
-            '446' => 'MOP',
-            '608' => 'PHP',
-            '764' => 'THB',
-            '458' => 'MYR',
-            '360' => 'IDR',
-            '410' => 'KRW',
-            '682' => 'SAR',
-            '554' => 'NZD',
-            '784' => 'AED',
-            '096' => 'BND',
-          }
-        end
-
 	private
 
  	def self.service_urls(mode)
