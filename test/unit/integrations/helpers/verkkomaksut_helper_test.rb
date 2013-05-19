77fc8316f3759e0e8d9d6988775a9b4e8c4b5811
Petteri Kääpä <petteri.kaapa@gmail.com>
2013-05-19 19:08:34 +0300

    Whitespace cleanup

--- test/unit/integrations/helpers/verkkomaksut_helper_test.rb
+++ test/unit/integrations/helpers/verkkomaksut_helper_test.rb
@@ -2,17 +2,17 @@ require 'test_helper'
 
 class VerkkomaksutHelperTest < Test::Unit::TestCase
   include ActiveMerchant::Billing::Integrations
-  
+
   def setup
     @helper = Verkkomaksut::Helper.new('2','13466', :amount => 500, :currency => 'EUR', :credential2 => "6pKF4jkv97zmqBJ3ZL8gUw5DfT2NMQ")
   end
- 
+
   def test_basic_helper_fields
     assert_field 'MERCHANT_ID', '13466'
     assert_field 'ORDER_NUMBER', '2'
     assert_field 'CURRENCY', 'EUR'
   end
-  
+
   def test_customer_fields
     @helper.customer :first_name => 'Antti', :last_name => 'Akonniemi', :email => 'antti@example.com', :phone => "0401234556", :tellno => "0401234557", :company => "Kisko Labs"
     assert_field 'CONTACT_FIRSTNAME', 'Antti'
@@ -30,13 +30,13 @@ class VerkkomaksutHelperTest < Test::Unit::TestCase
                             :state => '-',
                             :zip => '00180',
                             :country  => 'Finland'
-   
+
     assert_field 'CONTACT_ADDR_STREET', '1 My Street'
     assert_field 'CONTACT_ADDR_CITY', 'Helsinki'
     assert_field 'CONTACT_ADDR_ZIP', '00180'
     assert_field 'CONTACT_ADDR_COUNTRY', 'FI'
   end
-  
+
   def test_authcode_generation
     @helper.customer :first_name => 'Antti', :last_name => 'Akonniemi', :email => 'antti@example.com', :phone => "0401234556", :tellno => "0401234557", :company => "Kisko Labs"
     @helper.billing_address :address1 => '1 My Street',
@@ -71,7 +71,7 @@ class VerkkomaksutHelperTest < Test::Unit::TestCase
       @helper.company_address :address => '500 Dwemthy Fox Road'
     end
   end
-  
+
   def test_setting_invalid_address_field
     fields = @helper.fields.dup
     @helper.billing_address :street => 'My Street'
