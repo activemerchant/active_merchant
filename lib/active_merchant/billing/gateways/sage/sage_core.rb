76842003285a747c10441af5d5ba62ca9317c4fb
Caleb Simpson <caleb.simpson@jadedpixel.com>
2013-05-22 11:44:19 -0400

    Don't include customer_number unless it is numeric.

--- lib/active_merchant/billing/gateways/sage/sage_core.rb
+++ lib/active_merchant/billing/gateways/sage/sage_core.rb
@@ -45,7 +45,7 @@ module ActiveMerchant #:nodoc:
       end
 
       def add_customer_data(post, options)
-        post[:T_customer_number] = options[:customer]
+        post[:T_customer_number] = options[:customer] if options[:customer].is_a?(Fixnum)
       end
 
       def add_addresses(post, options)
