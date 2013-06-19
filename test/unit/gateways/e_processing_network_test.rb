f6f8f43cf380f6b2b22b65b81fd2a529c3244b12
Colin Mattson <colinm@pixelcloud.us>
2013-06-18 19:31:41 -0700

    Added sample responses to unit test. Updated assertions to expect sample authorization number.

--- test/unit/gateways/e_processing_network_test.rb
+++ test/unit/gateways/e_processing_network_test.rb
@@ -8,7 +8,8 @@ class EProcessingNetworkTest < Test::Unit::TestCase
                )
 
     @credit_card = credit_card
-    @amount = 100
+    @pass_amount = 100
+    @fail_amount = 101
 
     @options = {
       :order_id => '1',
@@ -20,19 +21,19 @@ class EProcessingNetworkTest < Test::Unit::TestCase
   def test_successful_purchase
     @gateway.expects(:ssl_post).returns(successful_purchase_response)
 
-    assert response = @gateway.purchase(@amount, @credit_card, @options)
+    assert response = @gateway.purchase(@pass_amount, @credit_card, @options)
     assert_instance_of Response, response
     assert_success response
 
     # Replace with authorization number from the successful response
-    assert_equal '', response.authorization
+    assert_equal '20130618211654-080880-151234', response.authorization
     assert response.test?
   end
 
   def test_unsuccessful_request
     @gateway.expects(:ssl_post).returns(failed_purchase_response)
 
-    assert response = @gateway.purchase(@amount, @credit_card, @options)
+    assert response = @gateway.purchase(@fail_amount, @credit_card, @options)
     assert_failure response
     assert response.test?
   end
@@ -41,9 +42,11 @@ class EProcessingNetworkTest < Test::Unit::TestCase
 
   # Place raw successful response from gateway here
   def successful_purchase_response
+    '"YAPPROVED 535624","AVS Match 9 Digit Zip and Address (X)","CVV2 Match (M)","151234","20130618211654-080880-151234"'
   end
 
   # Place raw failed response from gateway here
   def failed_purchase_response
+    '"NDECLINED","AVS Match 9 Digit Zip and Address (X)","CVV2 Match (M)","151235","20130618211737-080880-151235-0"'
   end
 end
