ab1a0910864755e390f97eef542a25c83c222213
Lyndon Maydwell <lyndon@sordina.net>
2013-06-21 11:54:54 +1000

    Renaming all references to Paychoice

--- test/unit/gateways/paychoice_test.rb
+++ test/unit/gateways/paychoice_test.rb
@@ -1,9 +1,9 @@
 require 'test_helper'
 
-class PayChoiceTest < Test::Unit::TestCase
+class PaychoiceTest < Test::Unit::TestCase
 
   def setup
-    @gateway = PayChoiceGateway.new(
+    @gateway = PaychoiceGateway.new(
       :login    => 'LOGIN',
       :password => 'PASSWORD'
     )
@@ -14,23 +14,23 @@ class PayChoiceTest < Test::Unit::TestCase
   end
 
   def test_new_with_login_password_creates_paychoice
-    assert_instance_of PayChoiceGateway, @gateway
+    assert_instance_of PaychoiceGateway, @gateway
   end
 
   def test_should_have_display_name_of_just_paychoice
-    assert_equal "PayChoice", PayChoiceGateway.display_name
+    assert_equal "Paychoice", PaychoiceGateway.display_name
   end
 
   def test_should_have_homepage_url
-    assert_equal "http://www.paychoice.com.au/", PayChoiceGateway.homepage_url
+    assert_equal "http://www.paychoice.com.au/", PaychoiceGateway.homepage_url
   end
 
   def test_should_have_supported_credit_card_types
-    assert_equal [:visa, :master, :american_express, :discover], PayChoiceGateway.supported_cardtypes
+    assert_equal [:visa, :master, :american_express, :discover], PaychoiceGateway.supported_cardtypes
   end
 
   def test_should_have_supported_countries
-    assert_equal ['AU'], PayChoiceGateway.supported_countries
+    assert_equal ['AU'], PaychoiceGateway.supported_countries
   end
 
   def test_successful_purchase
@@ -38,7 +38,7 @@ class PayChoiceTest < Test::Unit::TestCase
             .returns(successful_purchase_response)
 
     assert response = @gateway.purchase(@amount, @credit_card, @options)
-    assert_instance_of PayChoiceGateway::PayChoiceGatewayPurchase, response
+    assert_instance_of PaychoiceGateway::PaychoiceGatewayPurchase, response
     assert_success response
 
     assert_equal '769582d7-8f24-4b84-b56f-3110651f3674', response.authorization
@@ -48,20 +48,20 @@ class PayChoiceTest < Test::Unit::TestCase
   def test_unsuccessful_request
     @gateway.expects(:purchase).raises(failed_purchase_response)
 
-    assert_raise PayChoiceException do
+    assert_raise PaychoiceException do
       @gateway.purchase(@amount, @credit_card, @options)
     end
   end
 
   def test_should_have_default_currency
-    assert_equal "AUD", PayChoiceGateway.default_currency
+    assert_equal "AUD", PaychoiceGateway.default_currency
   end
 
   private
 
   # Place raw successful response from gateway here
   def successful_purchase_response
-    PayChoiceGateway::PayChoiceGatewayPurchase.create @gateway, {"charge"=>
+    PaychoiceGateway::PaychoiceGatewayPurchase.create @gateway, {"charge"=>
       {"amount"=>12.0,
        "created"=>"2013-06-04T04:16:34.95775Z",
        "error"=>"Transaction Approved",
@@ -77,6 +77,6 @@ class PayChoiceTest < Test::Unit::TestCase
   end
 
   def failed_purchase_response
-    PayChoiceException.new("Invalid Charge")
+    PaychoiceException.new("Invalid Charge")
   end
 end
