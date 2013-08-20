509cdf2ec456df4f46bb6cf9f246fa2e0676d0aa
sandeep.kumar <sandeep.kumar@payu.in>
2013-08-21 00:22:01 +0530

    test cases passed.

--- test/unit/integrations/payu_in_module_test.rb
+++ test/unit/integrations/payu_in_module_test.rb
@@ -5,8 +5,13 @@ class PayuInModuleTest < Test::Unit::TestCase
 
   def setup
     ActiveMerchant::Billing::Base.integration_mode = :test
-    @merchant_id = 'C0Dr8m'
-    @secret_key = '3sf0jURk'
+    ActiveMerchant::Billing::Integrations::PayuIn.merchant_id = 'C0Dr8m'
+    ActiveMerchant::Billing::Integrations::PayuIn.secret_key = '3sf0jURk'
+  end
+
+  def test_merchant_id_and_secret_key    
+    assert_equal 'C0Dr8m', PayuIn.merchant_id    
+    assert_equal '3sf0jURk', PayuIn.secret_key
   end
 
   def test_service_url_method
@@ -27,6 +32,6 @@ class PayuInModuleTest < Test::Unit::TestCase
 
   def test_checksum_method
     payu_load = "4ba4afe87f7e73468f2a|10.00|Product Info|Payu-Admin|test@example.com||||||||||"
-    assert_equal "cd324f64891b07d95492a2fd80ae469092e302faa3d3df5ba1b829936fd7497b6e89c3e48fd70e2a131cdd4f17d14bc20f292e9408650c085bc3bedb32f44266", PayuIn.checksum(@merchant_id, @secret_key, payu_load)
+    assert_equal "cd324f64891b07d95492a2fd80ae469092e302faa3d3df5ba1b829936fd7497b6e89c3e48fd70e2a131cdd4f17d14bc20f292e9408650c085bc3bedb32f44266", PayuIn.checksum(PayuIn.merchant_id, PayuIn.secret_key, payu_load)
   end
 end
