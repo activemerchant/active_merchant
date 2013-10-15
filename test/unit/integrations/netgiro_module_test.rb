acf2447b803d463d8fdd45adb58c626997727b89
Sævar Öfjörð Magnússon <saevar@overcast.io>
2013-10-15 16:15:02 +0000

    More testing

--- test/unit/integrations/netgiro_module_test.rb
+++ test/unit/integrations/netgiro_module_test.rb
@@ -3,32 +3,24 @@ require 'test_helper'
 class NetgiroModuleTest < Test::Unit::TestCase
   include ActiveMerchant::Billing::Integrations
   
-  # def test_notification_method
-  #   notification = Netgiro.notification('Land=USA', :credential2 => 'password')
-  #   assert_instance_of Valitor::Notification, notification
-  #   assert_equal 'password', notification.instance_eval{@options}[:credential2]
-  #   assert_equal 'USA', notification.customer_country
-  #   assert notification.test?
+  def test_notification_method
+    notification = Netgiro.notification('orderid=WEB-123', :credential2 => 'password')
+    assert_instance_of Netgiro::Notification, notification
+    assert_equal 'password', notification.instance_eval{@options}[:credential2]
+    assert_equal 'WEB-123', notification.item_id
+  ensure
+    ActiveMerchant::Billing::Base.integration_mode = :test
+  end
 
-  #   ActiveMerchant::Billing::Base.integration_mode = :production
-  #   assert !Valitor.notification('Land=USA', :credential2 => 'password').test?
-  # ensure
-  #   ActiveMerchant::Billing::Base.integration_mode = :test
-  # end
+  def test_return_method
+    ret = Netgiro::Return.new('orderid=WEB-123', :credential2 => 'password')
+    assert_instance_of Netgiro::Return, ret
+    assert_equal 'password', ret.instance_eval{@options}[:credential2]
+    assert_equal 'WEB-123', ret.item_id
+  ensure
+    ActiveMerchant::Billing::Base.integration_mode = :test
+  end
 
-  # def test_return_method
-  #   ret = Valitor.return('Land=USA', :credential2 => 'password')
-  #   assert_instance_of Valitor::Return, ret
-  #   assert_equal 'password', ret.instance_eval{@options}[:credential2]
-  #   assert_equal 'USA', ret.customer_country
-  #   assert ret.test?
-
-  #   ActiveMerchant::Billing::Base.integration_mode = :production
-  #   assert !Valitor.return('Land=USA', :credential2 => 'password').test?
-  # ensure
-  #   ActiveMerchant::Billing::Base.integration_mode = :test
-  # end
-  
   def test_service_url
     assert_equal "http://test.netgiro.is/user/securepay", Netgiro.service_url
     ActiveMerchant::Billing::Base.integration_mode = :production
