6e6c61f04bdda968e0f3ef0ef62276315eb28974
Alexander Balashov <divineforest@gmail.com>
2013-11-12 18:09:35 +0400

    Use autoload instead of require for helper.rb and notification.rb in integration generator.
    Look like a more common practice nowadays.

--- generators/integration/templates/integration.rb
+++ generators/integration/templates/integration.rb
@@ -1,10 +1,9 @@
-require File.dirname(__FILE__) + '/<%= identifier %>/helper.rb'
-require File.dirname(__FILE__) + '/<%= identifier %>/notification.rb'
-
 module ActiveMerchant #:nodoc:
   module Billing #:nodoc:
     module Integrations #:nodoc:
       module <%= class_name %>
+        autoload :Helper, File.dirname(__FILE__) + '/<%= identifier %>/helper.rb'
+        autoload :Notification, File.dirname(__FILE__) + '/<%= identifier %>/notification.rb'
 
         mattr_accessor :service_url
         self.service_url = 'https://www.example.com'
