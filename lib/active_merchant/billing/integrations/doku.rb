68453b073e8fcf2a7a6d70626987e50c4a476442
Arbab Ahmed <arbab.ahmed@shopify.com>
2013-12-11 18:00:45 -0500

    remove test_url vs production_url

--- lib/active_merchant/billing/integrations/doku.rb
+++ lib/active_merchant/billing/integrations/doku.rb
@@ -5,22 +5,9 @@ module ActiveMerchant #:nodoc:
   module Billing #:nodoc:
     module Integrations #:nodoc:
       module Doku
-        mattr_accessor :test_url
-        self.test_url       = 'https://apps.myshortcart.com/payment/request-payment/'
-
-        mattr_accessor :production_url
-        self.production_url = 'https://apps.myshortcart.com/payment/request-payment/'
 
         def self.service_url
-          mode = ActiveMerchant::Billing::Base.integration_mode
-          case mode
-          when :production
-            self.production_url
-          when :test
-            self.test_url
-          else
-            raise StandardError, "Integration mode set to an invalid value: #{mode}"
-          end
+          'https://apps.myshortcart.com/payment/request-payment/'
         end
 
         def self.notification(post)
