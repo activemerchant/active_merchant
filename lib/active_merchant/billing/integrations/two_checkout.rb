ae06f939b173ebf8e66afc3dc27bc2f13938693b
Arbab Ahmed <arbab.ahmed@shopify.com>
2014-01-12 23:37:40 -0500

    tweaked notifications to handle return params

--- lib/active_merchant/billing/integrations/two_checkout.rb
+++ lib/active_merchant/billing/integrations/two_checkout.rb
@@ -1,25 +1,25 @@
 module ActiveMerchant #:nodoc:
   module Billing #:nodoc:
     module Integrations #:nodoc:
-      module TwoCheckout 
+      module TwoCheckout
         autoload 'Helper', File.dirname(__FILE__) + '/two_checkout/helper'
         autoload 'Return', File.dirname(__FILE__) + '/two_checkout/return'
         autoload 'Notification', File.dirname(__FILE__) + '/two_checkout/notification'
-       
+
         mattr_accessor :payment_routine
         self.payment_routine = :single_page
-        
+
         def self.service_url
           case self.payment_routine
           when :multi_page
-            'https://www.2checkout.com/checkout/purchase'  
+            'https://www.2checkout.com/checkout/purchase'
           when :single_page
             'https://www.2checkout.com/checkout/spurchase'
           else
             raise StandardError, "Integration payment routine set to an invalid value: #{self.payment_routine}"
           end
         end
-        
+
         def self.service_url=(service_url)
           # Note: do not use this method, it is here for backward compatibility
           # Use the payment_routine method to change service_url
@@ -29,14 +29,14 @@ module ActiveMerchant #:nodoc:
             self.payment_routine = :multi_page
           end
         end
-        
-        
+
+
         def self.notification(post, options = {})
-          Notification.new(post)
-        end  
-        
+          Notification.new(post, options)
+        end
+
         def self.return(query_string, options = {})
-          Return.new(query_string)
+          Return.new(query_string, options)
         end
       end
     end
