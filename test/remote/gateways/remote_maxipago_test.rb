4002d2f924e2a42414e86ed72d90298d27b850e3
alexandremcosta <alexandremcost@gmail.com>
2014-01-14 18:07:16 -0200

    Add maxiPago gateway
    http://www.maxipago.com/
    
    Closes #996.

---
+++ test/remote/gateways/remote_maxipago_test.rb
@@ -0,0 +1,61 @@
+require 'test_helper'
+
+class RemoteMaxipagoTest < Test::Unit::TestCase
+  def setup
+    @gateway = MaxipagoGateway.new(fixtures(:maxipago))
+
+    @amount = 1000
+    @invalid_amount = 2009
+    @credit_card = credit_card('4111111111111111')
+    @invalid_card = credit_card('4111111111111111', year: Time.now.year - 1)
+
+    @options = {
+      order_id: '12345',
+      billing_address: address,
+      description: 'Store Purchase',
+    }
+  end
+
+  def test_successful_authorize
+    assert response = @gateway.authorize(@amount, @credit_card, @options)
+    assert_success response
+  end
+
+  def test_failed_authorize
+    assert response = @gateway.authorize(@amount, @invalid_card, @options)
+    assert_failure response
+  end
+
+  def test_authorize_and_capture
+    amount = @amount
+    authorize = @gateway.authorize(amount, @credit_card, @options)
+    assert_success authorize
+
+    capture = @gateway.capture(amount, authorize.authorization, @options)
+    assert_success capture
+  end
+
+  def test_successful_purchase
+    assert response = @gateway.purchase(@amount, @credit_card, @options)
+    assert_success response
+  end
+
+  def test_failed_purchase
+    assert response = @gateway.purchase(@invalid_amount, @credit_card, @options)
+    assert_failure response
+  end
+
+  def test_failed_capture
+    assert response = @gateway.capture(@amount, 'bogus')
+    assert_failure response
+  end
+
+  def test_invalid_login
+    gateway = MaxipagoGateway.new(
+      login: '',
+      password: ''
+    )
+    response = gateway.purchase(@amount, @credit_card, @options)
+    assert_failure response
+  end
+end
---
+++ test/unit/gateways/maxipago_test.rb
@@ -0,0 +1,189 @@
+require 'test_helper'
+
+class MaxipagoTest < Test::Unit::TestCase
+  def setup
+    @gateway = MaxipagoGateway.new(
+      :login => 'login',
+      :password => 'password'
+    )
+
+    @credit_card = credit_card
+    @amount = 100
+
+    @options = {
+      :order_id => '1',
+      :billing_address => address,
+      :description => 'Store Purchase'
+    }
+  end
+
+  def test_successful_purchase
+    @gateway.expects(:ssl_post).returns(successful_purchase_response)
+
+    response = @gateway.purchase(@amount, @credit_card, @options)
+    assert_success response
+
+    assert_equal '123456789', response.authorization
+  end
+
+  def test_failed_purchase
+    @gateway.expects(:ssl_post).returns(failed_purchase_response)
+
+    response = @gateway.purchase(@amount, @credit_card, @options)
+    assert_failure response
+  end
+
+  def test_successful_authorize
+    @gateway.expects(:ssl_post).returns(successful_authorize_response)
+
+    response = @gateway.authorize(@amount, @credit_card, @options)
+    assert_success response
+
+    assert_equal 'C0A8013F:014455FCC857:91A0:01A7243E', response.authorization
+    assert response.test?
+  end
+
+  def test_failed_authorize
+    @gateway.expects(:ssl_post).returns(failed_authorize_response)
+
+    response = @gateway.authorize(@amount, @credit_card, @options)
+    assert_failure response
+  end
+
+  def test_successful_capture
+    @gateway.expects(:ssl_post).returns(successful_capture_response)
+
+    response = @gateway.capture(nil, "authorization", @options)
+    assert_success response
+  end
+
+  def test_failed_capture
+    @gateway.expects(:ssl_post).returns(failed_capture_response)
+
+    response = @gateway.capture(nil, "bogus", @options)
+    assert_failure response
+  end
+
+  private
+
+  def successful_purchase_response
+    %(
+      <transaction-response>
+        <authCode>555555</authCode>
+        <orderID>123456789</orderID>
+        <referenceNum>123456789</referenceNum>
+        <transactionID>123456789</transactionID>
+        <transactionTimestamp>123456789</transactionTimestamp>
+        <responseCode>0</responseCode>
+        <responseMessage>CAPTURED</responseMessage>
+        <avsResponseCode/>
+        <cvvResponseCode/>
+        <processorCode>0</processorCode>
+        <processorMessage>APPROVED</processorMessage>
+        <errorMessage/>
+        <processorTransactionID>123456789</processorTransactionID>
+        <processorReferenceNumber>123456789</processorReferenceNumber>
+        <fraudScore>29</fraudScore>
+      </transaction-response>
+    )
+  end
+
+  def failed_purchase_response
+    %(
+      <transaction-response>
+        <authCode/>
+        <orderID>123456789</orderID>
+        <referenceNum>123456789</referenceNum>
+        <transactionID>123456789</transactionID>
+        <transactionTimestamp>123456789</transactionTimestamp>
+        <responseCode>1</responseCode>
+        <responseMessage>DECLINED</responseMessage>
+        <avsResponseCode>NNN</avsResponseCode>
+        <cvvResponseCode>N</cvvResponseCode>
+        <processorCode>D</processorCode>
+        <processorMessage>DECLINED</processorMessage>
+        <errorMessage/>
+      </transaction-response>
+    )
+  end
+
+  def successful_authorize_response
+    %(
+      <?xml version="1.0" encoding="UTF-8"?>
+      <transaction-response>
+        <authCode>123456</authCode>
+        <orderID>C0A8013F:014455FCC857:91A0:01A7243E</orderID>
+        <referenceNum>12345</referenceNum>
+        <transactionID>663921</transactionID>
+        <transactionTimestamp>1393012206</transactionTimestamp>
+        <responseCode>0</responseCode>
+        <responseMessage>AUTHORIZED</responseMessage>
+        <avsResponseCode>YYY</avsResponseCode>
+        <cvvResponseCode>M</cvvResponseCode>
+        <processorCode>A</processorCode>
+        <processorMessage>APPROVED</processorMessage>
+        <errorMessage/>
+      </transaction-response>
+    )
+  end
+
+  def failed_authorize_response
+    %(
+      <?xml version="1.0" encoding="UTF-8"?>
+      <transaction-response>
+        <authCode/>
+        <orderID/>
+        <referenceNum/>
+        <transactionID/>
+        <transactionTimestamp>1393012170003</transactionTimestamp>
+        <responseCode>1024</responseCode>
+        <responseMessage>INVALID REQUEST</responseMessage>
+        <avsResponseCode/>
+        <cvvResponseCode/>
+        <processorCode/>
+        <processorMessage/>
+        <errorMessage>The transaction has an expired credit card.</errorMessage>
+      </transaction-response>
+    )
+  end
+
+  def successful_capture_response
+    %(
+      <?xml version="1.0" encoding="UTF-8"?>
+      <transaction-response>
+        <authCode/>
+        <orderID>C0A8013F:014455FF974D:82CA:01C7717B</orderID>
+        <referenceNum>12345</referenceNum>
+        <transactionID>663924</transactionID>
+        <transactionTimestamp>1393012391</transactionTimestamp>
+        <responseCode>0</responseCode>
+        <responseMessage>CAPTURED</responseMessage>
+        <avsResponseCode/>
+        <cvvResponseCode/>
+        <processorCode>A</processorCode>
+        <processorMessage>APPROVED</processorMessage>
+        <errorMessage/>
+      </transaction-response>
+    )
+  end
+
+  def failed_capture_response
+    %(
+      <?xml version="1.0" encoding="UTF-8"?>
+      <transaction-response>
+        <authCode/>
+        <orderID/>
+        <referenceNum/>
+        <transactionID/>
+        <transactionTimestamp>1393012277035</transactionTimestamp>
+        <responseCode>1024</responseCode>
+        <responseMessage>INVALID REQUEST</responseMessage>
+        <avsResponseCode/>
+        <cvvResponseCode/>
+        <processorCode/>
+        <processorMessage/>
+        <errorMessage>Reference Number is a required field.</errorMessage>
+      </transaction-response>
+    )
+  end
+end
