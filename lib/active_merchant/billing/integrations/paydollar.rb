b9700d1e295db437971d24e1c50bfc7ea18acab1
Boris Slobodin <boris.slobodin@jadedpixel.com>
2014-02-18 20:44:57 +0000

    Remove obsolete URLs

--- lib/active_merchant/billing/integrations/paydollar.rb
+++ lib/active_merchant/billing/integrations/paydollar.rb
@@ -30,29 +30,12 @@ module ActiveMerchant #:nodoc:
             'USD' => '840',
         }
 
-        mattr_accessor :production_url, :test_url, :api_production_url, :api_test_url
-        self.production_url = 'https://www.paydollar.com/b2c2/eng/payment/payForm.jsp'
-        self.test_url = 'https://test.paydollar.com/b2cDemo/eng/payment/payForm.jsp'
-        self.api_production_url = 'https://www.paydollar.com/b2c2/eng/merchant/api/orderApi.jsp'
-        self.api_test_url = 'https://test.paydollar.com/b2cDemo/eng/merchant/api/orderApi.jsp'
-
         def self.service_url
           case ActiveMerchant::Billing::Base.integration_mode
           when :production
-            self.production_url
+            'https://www.paydollar.com/b2c2/eng/payment/payForm.jsp'
           when :test
-            self.test_url
-          else
-            raise StandardError, "Integration mode set to an invalid value: #{mode}"
-          end
-        end
-
-        def self.api_url
-          case ActiveMerchant::Billing::Base.integration_mode
-          when :production
-            self.api_production_url
-          when :test
-            self.api_test_url
+            'https://test.paydollar.com/b2cDemo/eng/payment/payForm.jsp'
           else
             raise StandardError, "Integration mode set to an invalid value: #{mode}"
           end
