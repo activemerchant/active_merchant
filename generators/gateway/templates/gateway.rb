aff305e7b23d8b4c367d1b67d31d4b47a49e6663
Nathaniel Talbott <nathaniel@talbott.ws>
2014-02-21 15:55:46 -0500

    Improve gateway generator
    This brings the generated code and tests into better alignment with
    current best practice.

--- generators/gateway/templates/gateway.rb
+++ generators/gateway/templates/gateway.rb
@@ -4,45 +4,48 @@ module ActiveMerchant #:nodoc:
       self.test_url = 'https://example.com/test'
       self.live_url = 'https://example.com/live'
 
-      # The countries the gateway supports merchants from as 2 digit ISO country codes
       self.supported_countries = ['US']
-
-      # The card types supported by the payment gateway
+      self.default_currency = 'USD'
       self.supported_cardtypes = [:visa, :master, :american_express, :discover]
 
-      # The homepage URL of the gateway
       self.homepage_url = 'http://www.example.net/'
-
-      # The name of the gateway
       self.display_name = 'New Gateway'
 
-      def initialize(options = {})
-        #requires!(options, :login, :password)
+      def initialize(options={})
+        requires!(options, :some_credential, :another_credential)
         super
       end
 
-      def authorize(money, creditcard, options = {})
+      def purchase(money, payment, options={})
         post = {}
-        add_invoice(post, options)
-        add_creditcard(post, creditcard)
-        add_address(post, creditcard, options)
+        add_invoice(post, money, options)
+        add_payment(post, payment)
+        add_address(post, payment, options)
         add_customer_data(post, options)
 
-        commit('authonly', money, post)
+        commit('sale', post)
       end
 
-      def purchase(money, creditcard, options = {})
+      def authorize(money, payment, options={})
         post = {}
-        add_invoice(post, options)
-        add_creditcard(post, creditcard)
-        add_address(post, creditcard, options)
+        add_invoice(post, money, options)
+        add_payment(post, payment)
+        add_address(post, payment, options)
         add_customer_data(post, options)
 
-        commit('sale', money, post)
+        commit('authonly', post)
       end
 
-      def capture(money, authorization, options = {})
-        commit('capture', money, post)
+      def capture(money, authorization, options={})
+        commit('capture', post)
+      end
+
+      def refund(money, authorization, options={})
+        commit('refund', post)
+      end
+
+      def void(authorization, options={})
+        commit('void', post)
       end
 
       private
@@ -53,19 +56,38 @@ module ActiveMerchant #:nodoc:
       def add_address(post, creditcard, options)
       end
 
-      def add_invoice(post, options)
+      def add_invoice(post, money, options)
+        post[:amount] = amount(money)
+        post[:currency] = (options[:currency] || currency(money))
       end
 
-      def add_creditcard(post, creditcard)
+      def add_payment(post, payment)
       end
 
       def parse(body)
+        {}
       end
 
-      def commit(action, money, parameters)
+      def commit(action, parameters)
+        url = (test? ? test_url : live_url)
+        response = parse(ssl_post(url, post_data(action, parameters)))
+
+        Response.new(
+          success_from(response),
+          message_from(response),
+          response,
+          authorization: authorization_from(response),
+          test: test?
+        )
+      end
+
+      def success_from(response)
       end
 
       def message_from(response)
+      end
+
+      def authorization_from(response)
       end
 
       def post_data(action, parameters = {})
@@ -73,4 +95,3 @@ module ActiveMerchant #:nodoc:
     end
   end
 end
-
