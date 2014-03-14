9f65d1da1e828b278730318cfe5fb30500d9bfc7
Denis Odorcic <denis.odorcic@jadedpixel.com>
2014-03-14 12:40:58 -0400

    Bitpay: Raise in helper on failed invoice creation

--- lib/active_merchant/billing/integrations/bit_pay/helper.rb
+++ lib/active_merchant/billing/integrations/bit_pay/helper.rb
@@ -38,6 +38,8 @@ module ActiveMerchant #:nodoc:
           def form_fields
             invoice = create_invoice
 
+            raise StandardError, "Invalid response while retrieving BitPay Invoice ID. Please try again." unless invoice
+
             {"id" => invoice['id']}
           end
 
