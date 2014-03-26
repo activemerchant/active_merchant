eb43071c31de802a4e8ab3fb6c5cf463903e0ef8
Boris Slobodin <boris.slobodin@jadedpixel.com>
2014-03-26 14:02:43 +0000

    Adding tests
    More tests
    
    More tests and fixes
    
    Added tests

--- test/unit/integrations/universal_module_test.rb
+++ test/unit/integrations/universal_module_test.rb
@@ -3,7 +3,21 @@ require 'test_helper'
 class UniversalModuleTest < Test::Unit::TestCase
   include ActiveMerchant::Billing::Integrations
 
-  def test_notification_method
-    assert_instance_of Universal::Notification, Universal.notification('name=cody')
+  def test_credential_based_url
+    assert_equal 'zork', Universal.credential_based_url({:forward_url => 'zork'})
   end
+
+  def test_notification
+    assert_instance_of Universal::Notification, Universal.notification('name=zork')
+  end
+
+  def test_return
+    assert_instance_of Universal::Return, Universal.return('name=zork')
+  end
+
+  def test_sign
+    expected = Digest::HMAC.hexdigest('a1b2', 'zork', Digest::SHA256)
+    assert_equal expected, Universal.sign({:b => '2', :a => '1'}, 'zork')
+  end
+
 end
