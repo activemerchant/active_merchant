81d5445b25f53b506f1748a948b981f0aba2f93c
Ryan Daigle <ryan.daigle@gmail.com>
2014-04-01 14:47:54 -0400

    Support store/unstore of cards in Sage (US) gateway.

--- lib/active_merchant/billing/gateways/sage.rb
+++ lib/active_merchant/billing/gateways/sage.rb
@@ -1,5 +1,6 @@
 require File.dirname(__FILE__) + '/sage/sage_bankcard'
 require File.dirname(__FILE__) + '/sage/sage_virtual_check'
+require File.dirname(__FILE__) + '/sage/sage_vault'
 
 module ActiveMerchant #:nodoc:
   module Billing #:nodoc:
@@ -138,7 +139,26 @@ module ActiveMerchant #:nodoc:
         end
       end
 
+      # Stores a credit card in the Sage vault.
+      #
+      # ==== Parameters
+      #
+      # * <tt>credit_card</tt> - The CreditCard object to be stored.
+      def store(credit_card, options = {})
+        vault.store(credit_card, options)
+      end
+
+      # Deletes a stored card from the Sage vault.
+      #
+      # ==== Parameters
+      #
+      # * <tt>identification</tt> - The 'GUID' identifying the stored card.
+      def unstore(identification, options = {})
+        vault.unstore(identification, options)
+      end
+
       private
+
       def bankcard
         @bankcard ||= SageBankcardGateway.new(@options)
       end
@@ -146,7 +166,10 @@ module ActiveMerchant #:nodoc:
       def virtual_check
         @virtual_check ||= SageVirtualCheckGateway.new(@options)
       end
+
+      def vault
+        @vault ||= SageVaultGateway.new(@options)
+      end
     end
   end
 end
-
