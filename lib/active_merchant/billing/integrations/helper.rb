56dd6a7b274879655c23c66178b721da87bea5aa
lulalala <mark@goodlife.tw>
2014-04-15 22:54:57 +0800

    Document integration helper methods.

--- lib/active_merchant/billing/integrations/helper.rb
+++ lib/active_merchant/billing/integrations/helper.rb
@@ -17,6 +17,17 @@ module ActiveMerchant #:nodoc:
           subclass.mappings ||= {}
         end
 
+        # @param order [String] unique id of this order
+        # @param account [String] merchant account id from gateway provider
+        # @param options [Hash] convenience param to set common attributes including:
+        #   * amount
+        #   * currency
+        #   * credential2
+        #   * credential3
+        #   * credential4
+        #   * notify_url
+        #   * return_url
+        #   * redirect_param
         def initialize(order, account, options = {})
           options.assert_valid_keys([:amount, :currency, :test, :credential2, :credential3, :credential4, :country, :account_name, :transaction_type, :authcode, :notify_url, :return_url, :redirect_param, :forward_url])
           @fields             = {}
@@ -34,10 +45,52 @@ module ActiveMerchant #:nodoc:
           self.redirect_param = options[:redirect_param]
         end
 
+        # Add a mapping between attribute name and gateway specific field name.
+        # This mapping can then be used in form helper to assign values to each
+        # attributes. For example, a `mapping :height, "HEIGHT"` means user can
+        # write `f.height 182` in 'payment_service_for' form helper, and an
+        # input tag with the name "HEIGHT" and value of 182 will be generated.
+        # This is an abstraction over the `add_field` method.
+        #
+        # @param attribute [Symbol] attribute name
+        # @param options [String, Array, Hash] input name from gateway's side.
+        #
+        #   String:
+        #   Name of generated input
+        #
+        #   Array:
+        #   Names of generated inputs, for creating synonym inputs.
+        #   Each generated input will have the same value.
+        #
+        #   Hash:
+        #   Keys as attribute name and values as generated input names.
+        #   Used to group related fields together, such as different segments 
+        #   of an address, for example:
+        #
+        #     mapping :address, :region => 'REGION',
+        #                       :city => 'CITY',
+        #                       :address => 'ADD'
         def self.mapping(attribute, options = {})
           self.mappings[attribute] = options
         end
 
+        # Add an input in the form. Useful when gateway requires some uncommon
+        # fields not provided by the gem.
+        #
+        #   # inside `payment_service_for do |service|` block
+        #   service.add_field('c_id','_xclick')
+        #
+        # Gateway implementor can also use this to add default fields in the form:
+        #
+        #   # in a subclass of ActiveMerchant::Billing::Integrations::Helper
+        #   def initialize(order, account, options = {})
+        #     super
+        #     # mandatory fields
+        #     add_field('Rvg2c', 1)
+        #   end
+        #
+        # @param name [String] input name
+        # @param value [String] input value
         def add_field(name, value)
           return if name.blank? || value.blank?
           @fields[name.to_s] = value.to_s
@@ -73,10 +126,15 @@ module ActiveMerchant #:nodoc:
           @fields
         end
 
+        # @return [Boolean] whether in test mode
         def test?
           @test_mode ||= ActiveMerchant::Billing::Base.integration_mode == :test || @test
         end
 
+        # Specifies the HTTP method used in form submmition.
+        # Set to POST which gateway implementor can override if other http method is desired (e.g. PUT).
+        #
+        # @return [String] form submit action, default is "POST".
         def form_method
           "POST"
         end
