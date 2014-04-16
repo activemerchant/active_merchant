3320f6c330bd6a0e265b2f607b289691040a6caf
Edward Ocampo-Gooding <edward@edwardog.net>
2014-04-16 21:20:57 +0000

    Returns from Klarna are always a success

--- lib/active_merchant/billing/integrations/klarna/return.rb
+++ lib/active_merchant/billing/integrations/klarna/return.rb
@@ -3,11 +3,9 @@ module ActiveMerchant #:nodoc:
     module Integrations #:nodoc:
       module Klarna
         class Return < ActiveMerchant::Billing::Integrations::Return
-
           def success?
-            # @params.has_key?('Ref')
+            true
           end
-
         end
       end
     end
