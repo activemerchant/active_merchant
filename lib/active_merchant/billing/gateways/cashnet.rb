3f7794363e7574ee2f14cbcc42a185884dd2451d
kcpavan <mail2kcpavan@gmail.com>
2014-04-21 07:09:08 -0400

    add purchase & refund for cashnet gateway

--- lib/active_merchant/billing/gateways/cashnet.rb
+++ lib/active_merchant/billing/gateways/cashnet.rb
@@ -1,6 +1,9 @@
 module ActiveMerchant #:nodoc:
   module Billing #:nodoc:
-    class Cashnet < Gateway
+    class CashnetGateway < Gateway
+      
+      self.live_url      = 'https://commerce.cashnet.com/'
+
       self.supported_countries = ['US']
       self.supported_cardtypes = [:visa, :master, :american_express, :discover, :diners_club, :jcb]
       self.homepage_url        = 'http://www.higherone.com/'
@@ -26,6 +29,89 @@ module ActiveMerchant #:nodoc:
         super
       end
 
+      def purchase(money, payment_object, fields = {})
+        post = {}
+        add_creditcard(post, payment_object)
+        add_invoice(post, options)
+        add_address(post, options)
+        add_customer_data(post, options)
+        commit('SALE', money, post)
+      end
+
+      def refund(money, identification, fields = {})
+        fields[:origtx]  = identification
+        commit('REFUND', money, fields)
+      end
+
+      private
+
+      def commit(action, money, fields)
+        fields[:amount] = amount(money) 
+        url = live_url + self.options[:gateway_merchant_name]
+        parse(ssl_post(url, post_data(action, fields)))
+      end
+
+      def post_data(action, parameters = {})
+        post = {}
+        post[:command]        = action
+        post[:merchant]       = self.options[:merchant]
+        post[:operator]       = self.options[:operator]
+        post[:station]        = self.options[:station]
+        post[:password]       = self.options[:password]
+        post.merge(parameters).collect { |key, value| "#{key}=#{CGI.escape(value.to_s)}" }.join("&")
+      end
+
+      def add_creditcard(post, creditcard)
+        post[:cardno]          = creditcard.number
+        post[:cid]             = creditcard.verification_value
+        post[:expdate]         = expdate(creditcard)
+        post[:card_name_g]     = creditcard.name
+      end
+
+      def add_invoice(post, options)
+        post[:order_number]    = options[:order_id]
+        post[:itemcode]        = self.options[:itemcode]
+      end
+
+      def add_address(post, options)
+        if address = (options[:shipping_address] || options[:billing_address] || options[:address])
+          post[:addr_g]       = address[:address1]
+          post[:city_g]       = address[:city]
+          post[:state_g]      = address[:state]
+          post[:zip_g]        = address[:zip]
+        end
+      end
+
+      def add_customer_data(post, options)
+        post[:email_g]  = options[:email]
+        post[:custcode] = self.options[:customer_code]
+      end
+
+      def expdate(creditcard)
+        year  = format(creditcard.year, :two_digits)
+        month = format(creditcard.month, :two_digits)
+
+        "#{month}#{year}"
+      end
+
+      def parse(body)
+        response_data = body.match(/<cashnet>(.*)<\/cashnet>/)[1]
+        response_fields = Hash[CGI::parse(response_data).map{|k,v| [k.to_sym,v.first]}]
+
+        # normalize message
+        message = message_from(response_fields)
+        success = response_fields[:result] == '0'
+        authorization = response_fields[:result] == '0' ? response_fields[:txno] : ''
+        Response.new(success, message, response_fields,
+          :test          => test?,
+          :authorization => authorization
+        )
+      end
+
+      def message_from(response_fields)
+        message = response_fields[:respmessage]
+        message
+      end
 
     end
   end
--- test/unit/gateways/cashnet_test.rb
+++ test/unit/gateways/cashnet_test.rb
@@ -1,5 +1,81 @@
 require 'test_helper'
 
 class Cashnet < Test::Unit::TestCase
+  include CommStub
+
+  def setup
+    @gateway = CashnetGateway.new(
+      :gateway_merchant_name => 'X',
+      :station => 'X',
+      :operator => 'X',
+      :password => 'test123',
+      :credit_card_payment_code => 'X',
+      :customer_code => 'X',
+      :item_code => 'X',
+      :site_name => 'X'
+    )
+    @amount = 100
+    @credit_card = credit_card
+  end
+
+  def test_successful_purchase
+    @gateway.expects(:ssl_post).returns(successful_purchase_response)
+    assert response = @gateway.purchase(@amount, @credit_card)
+    assert_instance_of Response, response
+    assert_success response
+    assert_equal '1234', response.authorization
+  end
+
+  def test_failed_purchase
+    @gateway.expects(:ssl_post).returns(failed_purchase_response)
+
+    assert response = @gateway.purchase(@amount, @credit_card)
+    assert_instance_of Response, response
+    assert_failure response
+    assert_equal '', response.authorization
+  end
+
+  def test_successful_refund
+    @gateway.expects(:ssl_post).returns(successful_purchase_response)
+    assert response = @gateway.refund(@amount, @credit_card)
+    assert_instance_of Response, response
+    assert_success response
+    assert_equal '1234', response.authorization
+  end
+
+  def test_failed_refund
+    @gateway.expects(:ssl_post).returns(failed_purchase_response)
+
+    assert response = @gateway.refund(@amount, @credit_card)
+    assert_instance_of Response, response
+    assert_failure response
+    assert_equal '', response.authorization
+  end
+
+  def test_supported_countries
+    assert_equal ['US'], CashnetGateway.supported_countries
+  end
+
+  def test_supported_card_types
+    assert_equal [:visa, :master, :american_express, :discover, :diners_club, :jcb],  CashnetGateway.supported_cardtypes
+  end
+
+  private 
+
+  def successful_refund_response
+    "<cashnet>result=0&respmessage=Success&txno=1234</cashnet>"
+  end
+
+  def failed_refund_response
+    "<cashnet>result=305&respmessage=Refund amounts should be expressed as positive amounts</cashnet>"
+  end
+
+  def successful_purchase_response
+    "<cashnet>result=0&respmessage=Success&txno=1234</cashnet>"
+  end
+
+  def failed_purchase_response
+    "<cashnet>result=7&respmessage= Invalid credit card number, no credit card number provided</cashnet>"
+  end
 
 end
