01ad4581e6f55259a9e30483638d931e2af10ba0
brettv <brettv@onboardingsystems.com>
2014-05-01 13:32:13 -0600

    removing gateway payer id from request
    I hope this works

--- lib/active_merchant/billing/gateways/paylease.rb
+++ lib/active_merchant/billing/gateways/paylease.rb
@@ -123,7 +123,7 @@ module ActiveMerchant #:nodoc:
             xml.tag! 'TransactionAction', CC_TRANSACTION
             xml.tag! 'CreditCardAction', CAPTURE
             xml.tag! 'TransactionId', authorization
-            xml.tag! 'GatewayPayerId', options[:transaction_id]
+            # xml.tag! 'GatewayPayerId', options[:transaction_id]
           end
         end
       end
