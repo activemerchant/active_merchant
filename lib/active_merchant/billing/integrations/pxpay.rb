dbba453cb459a9b06471824a41a8579398f64d76
Denis Odorcic <denis.odorcic@jadedpixel.com>
2014-05-02 21:10:06 +0000

    Revert "Merge pull request #1160 from Shopify/pxpay_credential_based_url"
    This reverts commit 49148eb93c381a4ccc62a1703677814f13582084, reversing
    changes made to ed26b96a65097bddf80c78a8ff2e2b760250bd5f.

--- lib/active_merchant/billing/integrations/pxpay.rb
+++ lib/active_merchant/billing/integrations/pxpay.rb
@@ -8,8 +8,14 @@ module ActiveMerchant #:nodoc:
 
         TOKEN_URL = 'https://sec.paymentexpress.com/pxpay/pxaccess.aspx'
 
+        LIVE_URL = 'https://sec.paymentexpress.com/pxpay/pxpay.aspx'
+
         def self.token_url
           TOKEN_URL
+        end
+
+        def self.service_url
+          LIVE_URL
         end
 
         def self.notification(post, options={})
