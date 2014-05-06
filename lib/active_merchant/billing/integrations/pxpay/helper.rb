65ecc8edf4b5057d34cf5eaf081c0a6f3cd2bb22
Boris Slobodin <boris.slobodin@jadedpixel.com>
2014-05-06 17:19:06 +0000

    Quick cleanup

--- lib/active_merchant/billing/integrations/pxpay/helper.rb
+++ lib/active_merchant/billing/integrations/pxpay/helper.rb
@@ -5,34 +5,6 @@ module ActiveMerchant #:nodoc:
   module Billing #:nodoc:
     module Integrations #:nodoc:
       module Pxpay
-        # An example. Note the username as a parameter and transaction key you
-        # will want to use later. The amount that you pass in will be *rounded*,
-        # so preferably pass in X.2 decimal so that no rounding occurs.  You need
-        # to set :credential2 to your PxPay secret key.
-        #
-        # PxPay accounts have Failproof Notification enabled by default which means
-        # in addition to the user being redirected to your return_url, the return_url will
-        # be accessed by the PxPay servers directly, immediately after transaction success.
-        #
-        #  payment_service_for('order_id', 'pxpay_user_ID', :service => :pxpay,
-        #                       :amount => 157.0, :currency => 'USD', :credential2 => 'pxpay_key') do |service|
-        #
-        #   service.customer :email => 'customer@email.com'
-        #
-        #   service.description 'Order 123 for MyStore'
-        #
-        #   # Must specify both a return_url or PxPay will show an error instead of
-        #   # capturing credit card details.
-        #
-        #   service.return_url "http://t/pxpay/payment_received_notification_sub_step"
-        #
-        #   # These fields will be copied verbatim to the Notification
-        #   service.custom1 'custom text 1'
-        #   service.custom2 ''
-        #   service.custom3 ''
-        #   # See the helper.rb file for various custom fields
-        # end
-
         class Helper < ActiveMerchant::Billing::Integrations::Helper
           include PostsData
 
