4c55a9a424d10f9c45d8b92df130a600440b9021
Jason Lambert <jlambert@globalpersonals.co.uk>
2014-05-20 12:49:55 +0100

    Fix CVV test to use CVV enabled gateway account

--- test/remote/gateways/remote_wirecard_test.rb
+++ test/remote/gateways/remote_wirecard_test.rb
@@ -128,10 +128,9 @@ class RemoteWirecardTest < Test::Unit::TestCase
     assert response.message[/THIS IS A DEMO/]
   end
 
-  # Note: Requires wirecard test account with AVS+CVV responses enabled
   def test_successful_cvv_result
     @credit_card.verification_value = "666" # Magic Value = "Matched (correct) CVC-2"
-    assert response = @gateway.purchase(@amount, @credit_card, @options)
+    assert response = @gateway_avs_cvc_enabled.purchase(@amount, @credit_card, @options)
 
     assert_success response
     assert_equal "M", response.cvv_result["code"]
