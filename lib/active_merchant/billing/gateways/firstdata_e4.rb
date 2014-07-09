562a6c98fd26528915ab4994c573e0463238672a
Richard Wilson <richard@freerunningtech.com>
2014-07-09 16:18:47 -0700

    Add authentication credentials to requests if they're present.

--- lib/active_merchant/billing/gateways/firstdata_e4.rb
+++ lib/active_merchant/billing/gateways/firstdata_e4.rb
@@ -134,6 +134,7 @@ module ActiveMerchant #:nodoc:
 
         add_customer_data(xml, options)
         add_invoice(xml, options)
+        add_card_authentication_data(xml, options)
 
         xml.target!
       end
@@ -144,6 +145,7 @@ module ActiveMerchant #:nodoc:
         add_identification(xml, identification)
         add_amount(xml, money)
         add_customer_data(xml, options)
+        add_card_authentication_data(xml, options)
 
         xml.target!
       end
@@ -198,6 +200,12 @@ module ActiveMerchant #:nodoc:
           xml.tag! "CVD_Presence_Ind", "1"
           xml.tag! "VerificationStr2", credit_card.verification_value
         end
+      end
+
+      def add_card_authentication_data(xml, options)
+        xml.tag! "CAVV", options[:cavv]
+        xml.tag! "XID", options[:xid]
+        xml.tag! "Ecommerce_Flag", options[:eci]
       end
 
       def add_credit_card_token(xml, store_authorization)
