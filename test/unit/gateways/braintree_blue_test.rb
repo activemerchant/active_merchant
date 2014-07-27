6a5f17532643a03340cbc612c5718696423dab63
Nathan Verni <npverni@gmail.com>
2014-07-27 12:50:11 -0400

    Adds application_id functionality to Braintree Blue

--- test/unit/gateways/braintree_blue_test.rb
+++ test/unit/gateways/braintree_blue_test.rb
@@ -506,6 +506,14 @@ class BraintreeBlueTest < Test::Unit::TestCase
     end
   end
 
+  def test_solution_id_is_added_to_create_transaction_parameters
+    assert_nil @gateway.send(:create_transaction_parameters, 100, credit_card("41111111111111111111"),{})[:channel]
+    ActiveMerchant::Billing::BraintreeBlueGateway.application_id = 'ABC123'
+    assert_equal @gateway.send(:create_transaction_parameters, 100, credit_card("41111111111111111111"),{})[:channel], "ABC123"
+  ensure
+    ActiveMerchant::Billing::BraintreeBlueGateway.application_id = nil
+  end
+
   private
 
   def braintree_result(options = {})
