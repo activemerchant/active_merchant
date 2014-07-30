2575655444117d3b0ef3243d700db6cf6e9f85e5
Edward Diehl <EdwDiehl@gmail.com>
2014-07-30 14:27:09 +0300

    Inline constants

--- lib/active_merchant/billing/gateways/hostedpci.rb
+++ lib/active_merchant/billing/gateways/hostedpci.rb
@@ -2,133 +2,18 @@ module ActiveMerchant #:nodoc:
   module Billing #:nodoc:
 
     HPCI_CONST = {
-        #HPCI Actions
-        :PXY_AUTH => '/iSynSApp/paymentAuth.action',
-        :PXY_SALE => '/iSynSApp/paymentSale.action',
-        :PXY_CAPTURE => '/iSynSApp/paymentCapture.action',
-        :PXY_CREDIT  => '/iSynSApp/paymentCredit.action',
-        :PXY_VOID => '/iSynSApp/paymentVoid.action',
-
         #Baseline API Parameters
-        :PXYPARAM_APIVERSION => 'apiVersion',
-        :PXYPARAM_APITYPE => 'apiType',
-        :PXYPARAM_APITYPE_PXYHPCI => 'pxyhpci',
-        :PXYPARAM_USERNAME => 'userName',
-        :PXYPARAM_USERPASSKEY => 'userPassKey',
-
         :PXYPARAM_PXY_TRANSACTION_CUSISO => 'pxyTransaction.txnCurISO',
         :PXYPARAM_PXY_TRANSACTION_AMOUNT => 'pxyTransaction.txnAmount',
-        :PXYPARAM_PXY_TRANSACTION_MER_REFID_NAME => 'pxyTransaction.merchantRefIdName',
-        :PXYPARAM_PXY_TRANSACTION_MER_REFID => 'pxyTransaction.merchantRefId',
         :PXYPARAM_PXY_TRANSACTION_PROCESSOR_REFID => 'pxyTransaction.processorRefId',
 
-        #3D Secure Constants
-        :PXYPARAM_PXY_3DSECAUTH_ACTIONNAME => 'pxyThreeDSecAuth.actionName',
-        :PXYPARAM_PXY_3DSECAUTH_AUTHTXNID => 'pxyThreeDSecAuth.authTxnId',
-        :PXYPARAM_PXY_3DSECAUTH_AUTHSTATUS => 'pxyThreeDSecAuth.authStatus',
-        :PXYPARAM_PXY_3DSECAUTH_SIGNATURESTATUS => 'pxyThreeDSecAuth.signatureStatus',
-        :PXYPARAM_PXY_3DSECAUTH_AUTHCAVV => 'pxyThreeDSecAuth.authCAVV',
-        :PXYPARAM_PXY_3DSECAUTH_AUTHECI => 'pxyThreeDSecAuth.authECI',
-        :PXYPARAM_PXY_3DSECAUTH_AUTHACSURL => 'pxyThreeDSecAuth.authAcsUrl',
-        :PXYPARAM_PXY_3DSECAUTH_PAREQ => 'pxyThreeDSecAuth.paReq',
-        :PXYPARAM_PXY_3DSECAUTH_PARES => 'pxyThreeDSecAuth.paRes',
-        :PXYPARAM_PXY_3DSECAUTH_AUTHSIGN_COMBOLIST => 'pxyThreeDSecAuth.authSignComboList',
-
         :PXYPARAM_PXY_CC_CARDTYPE => 'pxyCreditCard.cardType',
-        :PXYPARAM_PXY_CC_NUMBER => 'pxyCreditCard.creditCardNumber',
-        :PXYPARAM_PXY_CC_EXPMONTH => 'pxyCreditCard.expirationMonth',
-        :PXYPARAM_PXY_CC_EXPYEAR => 'pxyCreditCard.expirationYear',
         :PXYPARAM_PXY_CC_CVV => 'pxyCreditCard.cardCodeVerification',
 
-        :PXYPARAM_PXY_CUSTINFO_CUSTOMERID => 'pxyCustomerInfo.customerId',
-        :PXYPARAM_PXY_CUSTINFO_EMAIL => 'pxyCustomerInfo.email',
-        :PXYPARAM_PXY_CUSTINFO_INSTR => 'pxyCustomerInfo.instructions',
-        :PXYPARAM_PXY_CUSTINFO_CUSTIP => 'pxyCustomerInfo.customerIP',
-
-        :PXYPARAM_PXY_CUSTINFO_BILLADDR_FIRSTNAME => 'pxyCustomerInfo.billingLocation.firstName',
-        :PXYPARAM_PXY_CUSTINFO_BILLADDR_LASTNAME => 'pxyCustomerInfo.billingLocation.lastName',
-        :PXYPARAM_PXY_CUSTINFO_BILLADDR_COMPANYNAME => 'pxyCustomerInfo.billingLocation.companyName',
-        :PXYPARAM_PXY_CUSTINFO_BILLADDR_ADDRESS => 'pxyCustomerInfo.billingLocation.address',
-        :PXYPARAM_PXY_CUSTINFO_BILLADDR_CITY => 'pxyCustomerInfo.billingLocation.city',
-        :PXYPARAM_PXY_CUSTINFO_BILLADDR_STATE => 'pxyCustomerInfo.billingLocation.state',
-        :PXYPARAM_PXY_CUSTINFO_BILLADDR_ZIPCODE => 'pxyCustomerInfo.billingLocation.zipCode',
-        :PXYPARAM_PXY_CUSTINFO_BILLADDR_COUNTRY => 'pxyCustomerInfo.billingLocation.country',
-        :PXYPARAM_PXY_CUSTINFO_BILLADDR_PHONENUMBER => 'pxyCustomerInfo.billingLocation.phoneNumber',
-        :PXYPARAM_PXY_CUSTINFO_BILLADDR_FAX => 'pxyCustomerInfo.billingLocation.fax',
-        :PXYPARAM_PXY_CUSTINFO_BILLADDR_TAX1 => 'pxyCustomerInfo.billingLocation.tax1',
-        :PXYPARAM_PXY_CUSTINFO_BILLADDR_TAX2 => 'pxyCustomerInfo.billingLocation.tax2',
-        :PXYPARAM_PXY_CUSTINFO_BILLADDR_TAX3 => 'pxyCustomerInfo.billingLocation.tax3',
-
-        :PXYPARAM_PXY_CUSTINFO_SHIPADDR_FIRSTNAME => 'pxyCustomerInfo.shippingLocation.firstName',
-        :PXYPARAM_PXY_CUSTINFO_SHIPADDR_LASTNAME => 'pxyCustomerInfo.shippingLocation.lastName',
-        :PXYPARAM_PXY_CUSTINFO_SHIPADDR_COMPANYNAME => 'pxyCustomerInfo.shippingLocation.companyName',
-        :PXYPARAM_PXY_CUSTINFO_SHIPADDR_ADDRESS => 'pxyCustomerInfo.shippingLocation.address',
-        :PXYPARAM_PXY_CUSTINFO_SHIPADDR_CITY => 'pxyCustomerInfo.shippingLocation.city',
-        :PXYPARAM_PXY_CUSTINFO_SHIPADDR_STATE => 'pxyCustomerInfo.shippingLocation.state',
-        :PXYPARAM_PXY_CUSTINFO_SHIPADDR_ZIPCODE => 'pxyCustomerInfo.shippingLocation.zipCode',
-        :PXYPARAM_PXY_CUSTINFO_SHIPADDR_COUNTRY => 'pxyCustomerInfo.shippingLocation.country',
-        :PXYPARAM_PXY_CUSTINFO_SHIPADDR_PHONENUMBER => 'pxyCustomerInfo.shippingLocation.phoneNumber',
-        :PXYPARAM_PXY_CUSTINFO_SHIPADDR_FAX => 'pxyCustomerInfo.shippingLocation.fax',
-        :PXYPARAM_PXY_CUSTINFO_SHIPADDR_TAX1 => 'pxyCustomerInfo.shippingLocation.tax1',
-        :PXYPARAM_PXY_CUSTINFO_SHIPADDR_TAX2 => 'pxyCustomerInfo.shippingLocation.tax2',
-        :PXYPARAM_PXY_CUSTINFO_SHIPADDR_TAX3 => 'pxyCustomerInfo.shippingLocation.tax3',
-
-        :PXYPARAM_PXY_ORDER_INVNUM => 'pxyOrder.invoiceNumber',
-        :PXYPARAM_PXY_ORDER_DESC => 'pxyOrder.description',
         :PXYPARAM_PXY_ORDER_TOTALAMT => 'pxyOrder.totalAmount',
         :PXYPARAM_PXY_ORDER_SHIPPINGAMT => 'pxyOrder.shippingAmount',
 
-        :PXYPARAM_PXY_ORDER_ORDERITEMS => 'pxyOrder.orderItems[',
-        :PXYPARAM_PXY_ORDER_ORDERITEM_ID => '].itemId',
-        :PXYPARAM_PXY_ORDER_ORDERITEM_NAME => '].itemName',
-        :PXYPARAM_PXY_ORDER_ORDERITEM_DESC => '].itemDescription',
-        :PXYPARAM_PXY_ORDER_ORDERITEM_QTY => '].itemQuantity',
-        :PXYPARAM_PXY_ORDER_ORDERITEM_PRICE => '].itemPrice',
-        :PXYPARAM_PXY_ORDER_ORDERITEM_TAXABLE => '].itemTaxable',
-
-        #response parameters
-        :PXYRESP_CALL_STATUS => 'status',
-        :PXYRESP_RESPONSE_STATUS => 'pxyResponse.responseStatus',
-        :PXYRESP_PROCESSOR_REFID => 'pxyResponse.processorRefId',
-        :PXYRESP_RESPSTATUS_NAME => 'pxyResponse.responseStatus.name',
-        :PXYRESP_RESPSTATUS_CODE => 'pxyResponse.responseStatus.code',
-        :PXYRESP_RESPSTATUS_DESCRIPTION => 'pxyResponse.responseStatus.description',
-        :PXYRESP_PROCESSOR_TYPE => 'pxyResponse.processorType',
-        :PXYRESP_FULL_NATIVE_RESP => 'pxyResponse.fullNativeResp',
-        :PXYRESP_FULL_FRAUD_NATIVE_RESP => 'pxyResponse.fullFraudNativeResp',
-
-        #CVV and AVS response parameters
-        :PXYRESP_AVS1 => 'pxyResponse.responseAVS1',
-        :PXYRESP_AVS2 => 'pxyResponse.responseAVS2',
-        :PXYRESP_AVS3 => 'pxyResponse.responseAVS3',
-        :PXYRESP_AVS4 => 'pxyResponse.responseAVS4',
-        :PXYRESP_CVV1 => 'pxyResponse.responseCVV1',
-        :PXYRESP_CVV2 => 'pxyResponse.responseCVV2',
-
-        #3D Secure responses
-        :PXYRESP_3DS_ACS_URL =>  'pxyResponse.threeDSAcsUrl',
-        :PXYRESP_3DS_TRANS_ID => 'pxyResponse.threeDSTransactionId',
-        :PXYRESP_3DS_PA_REQ =>   'pxyResponse.threeDSPARequest',
-
-        #3D Secure Actions
-        :ACTIONNAME_VERIFYENROLL =>  'verifyenroll',
-        :ACTIONNAME_REQUESTPIN =>    'requestpin',
-        :ACTIONNAME_VERIFYRESP =>    'verifyresp',
-
-
         :PXYRESP_CALL_STATUS_SUCCESS => 'success',
-        :PXYRESP_CALL_STATUS_ERROR => 'error',
-
-        :PXYRESP_CALL_ERRID => 'errId',
-        :PXYRESP_CALL_ERRMSG => 'errMsg',
-
-        :PXYRESP_RESPONSE_STATUS_APPROVED => 'approved',
-        :PXYRESP_RESPONSE_STATUS_DECLINED => 'declined',
-        :PXYRESP_RESPONSE_STATUS_ERROR => 'error',
-        :PXYRESP_RESPONSE_STATUS_REVIEW => 'review',
-        :PXYRESP_RESPONSE_STATUS_3DSECURE => '3dsecure',
-
-        :NL => "\n"
     }
 
     class HostedpciGateway < Gateway
@@ -232,10 +117,10 @@ module ActiveMerchant #:nodoc:
 
       def add_api_info(post, options, money)
         ## set basic API parameters
-        post[HPCI_CONST[:PXYPARAM_APIVERSION]] = '1.0.1'
-        post[HPCI_CONST[:PXYPARAM_APITYPE]] = HPCI_CONST[:PXYPARAM_APITYPE_PXYHPCI]
-        post[HPCI_CONST[:PXYPARAM_USERNAME]] = @options[:login]
-        post[HPCI_CONST[:PXYPARAM_USERPASSKEY]] = @options[:password]
+        post['apiVersion'] = '1.0.1'
+        post['apiType'] = 'pxyhpci'
+        post['userName'] = @options[:login]
+        post['userPassKey'] = @options[:password]
 
         ## set currency parameter, need access to money
         if !options[:currency].blank?
@@ -249,55 +134,55 @@ module ActiveMerchant #:nodoc:
 
       def add_customer_data(post, options)
         if options.has_key? :email
-          post[HPCI_CONST[:PXYPARAM_PXY_CUSTINFO_EMAIL]] = options[:email]
+          post['pxyCustomerInfo.email'] = options[:email]
         end
 
         if options.has_key? :customer
-          post[HPCI_CONST[:PXYPARAM_PXY_CUSTINFO_CUSTOMERID]] = options[:customer]
+          post['pxyCustomerInfo.customerId'] = options[:customer]
         end
 
         if options.has_key? :ip
-          post[HPCI_CONST[:PXYPARAM_PXY_CUSTINFO_CUSTIP]] = options[:ip]
+          post['pxyCustomerInfo.customerIP'] = options[:ip]
         end
       end ##END: add_customer_data
 
       def add_address(post, creditcard, options)
         if address = options[:billing_address] || options[:address]
-          post[HPCI_CONST[:PXYPARAM_PXY_CUSTINFO_BILLADDR_FIRSTNAME]] = address[:name].to_s.split[0] if !address[:name].to_s.split[0].blank?
-          post[HPCI_CONST[:PXYPARAM_PXY_CUSTINFO_BILLADDR_LASTNAME]]  = address[:name].to_s.split[1] if !address[:name].to_s.split[1].blank?
-          post[HPCI_CONST[:PXYPARAM_PXY_CUSTINFO_BILLADDR_ADDRESS]]   = address[:address1].to_s
-          post[HPCI_CONST[:PXYPARAM_PXY_CUSTINFO_BILLADDR_ZIPCODE]]   = address[:zip].to_s
-          post[HPCI_CONST[:PXYPARAM_PXY_CUSTINFO_BILLADDR_CITY]]      = address[:city].to_s
-          post[HPCI_CONST[:PXYPARAM_PXY_CUSTINFO_BILLADDR_COUNTRY]]   = address[:country].to_s
-          post[HPCI_CONST[:PXYPARAM_PXY_CUSTINFO_BILLADDR_STATE]]     = address[:state].blank?  ? 'n/a' : address[:state]
+          post['pxyCustomerInfo.billingLocation.firstName'] = address[:name].to_s.split[0] if !address[:name].to_s.split[0].blank?
+          post['pxyCustomerInfo.billingLocation.lastName']  = address[:name].to_s.split[1] if !address[:name].to_s.split[1].blank?
+          post['pxyCustomerInfo.billingLocation.address']   = address[:address1].to_s
+          post['pxyCustomerInfo.billingLocation.zipCode']   = address[:zip].to_s
+          post['pxyCustomerInfo.billingLocation.city']      = address[:city].to_s
+          post['pxyCustomerInfo.billingLocation.country']   = address[:country].to_s
+          post['pxyCustomerInfo.billingLocation.state']     = address[:state].blank?  ? 'n/a' : address[:state]
         end
 
         if address = options[:shipping_address]
-          post[HPCI_CONST[:PXYPARAM_PXY_CUSTINFO_SHIPADDR_FIRSTNAME]] = address[:name].to_s.split[0] if !address[:name].to_s.split[0].blank?
-          post[HPCI_CONST[:PXYPARAM_PXY_CUSTINFO_SHIPADDR_LASTNAME]]  = address[:name].to_s.split[1] if !address[:name].to_s.split[1].blank?
-          post[HPCI_CONST[:PXYPARAM_PXY_CUSTINFO_SHIPADDR_ADDRESS]]   = address[:address1].to_s
-          post[HPCI_CONST[:PXYPARAM_PXY_CUSTINFO_SHIPADDR_ZIPCODE]]   = address[:zip].to_s
-          post[HPCI_CONST[:PXYPARAM_PXY_CUSTINFO_SHIPADDR_CITY]]      = address[:city].to_s
-          post[HPCI_CONST[:PXYPARAM_PXY_CUSTINFO_SHIPADDR_COUNTRY]]   = address[:country].to_s
-          post[HPCI_CONST[:PXYPARAM_PXY_CUSTINFO_SHIPADDR_STATE]]     = address[:state].blank?  ? 'n/a' : address[:state]
+          post['pxyCustomerInfo.shippingLocation.firstName'] = address[:name].to_s.split[0] if !address[:name].to_s.split[0].blank?
+          post['pxyCustomerInfo.shippingLocation.lastName']  = address[:name].to_s.split[1] if !address[:name].to_s.split[1].blank?
+          post['pxyCustomerInfo.shippingLocation.address']   = address[:address1].to_s
+          post['pxyCustomerInfo.shippingLocation.zipCode']   = address[:zip].to_s
+          post['pxyCustomerInfo.shippingLocation.city']      = address[:city].to_s
+          post['pxyCustomerInfo.shippingLocation.country']   = address[:country].to_s
+          post['pxyCustomerInfo.shippingLocation.state']     = address[:state].blank?  ? 'n/a' : address[:state]
         end
       end ##END: add_address
 
       def add_invoice(post, options)
         if options.has_key? :order_id
-          post[HPCI_CONST[:PXYPARAM_PXY_ORDER_INVNUM]] = options[:order_id]
-          post[HPCI_CONST[:PXYPARAM_PXY_TRANSACTION_MER_REFID]] = "merRef:" + options[:order_id]
+          post['pxyOrder.invoiceNumber'] = options[:order_id]
+          post['pxyTransaction.merchantRefId'] = "merRef:" + options[:order_id]
         end
         if options.has_key? :description
-          post[HPCI_CONST[:PXYPARAM_PXY_ORDER_DESC]]   = options[:description]
+          post['pxyOrder.description']   = options[:description]
         end
       end
 
       def add_creditcard(post, creditcard)
-        post[HPCI_CONST[:PXYPARAM_PXY_CC_NUMBER]]   = creditcard.number
-        post[HPCI_CONST[:PXYPARAM_PXY_CC_CVV]]  = creditcard.verification_value if creditcard.verification_value?
-        post[HPCI_CONST[:PXYPARAM_PXY_CC_EXPMONTH]]   = creditcard.month.to_s
-        post[HPCI_CONST[:PXYPARAM_PXY_CC_EXPYEAR]]   = creditcard.year.to_s
+        post['pxyCreditCard.creditCardNumber']   = creditcard.number
+        post['pxyCreditCard.cardCodeVerification']  = creditcard.verification_value if creditcard.verification_value?
+        post['pxyCreditCard.expirationMonth']   = creditcard.month.to_s
+        post['pxyCreditCard.expirationYear']   = creditcard.year.to_s
         if creditcard.brand.nil? || creditcard.brand.empty?
           post[HPCI_CONST[:PXYPARAM_PXY_CC_CARDTYPE]]   = 'any'
         else
@@ -328,15 +213,15 @@ module ActiveMerchant #:nodoc:
 
       def commit(action, money, parameters)
         if action == 'sale'
-          serviceUrl = HPCI_CONST[:PXY_SALE]
+          serviceUrl = '/iSynSApp/paymentSale.action'
         elsif action =='authonly'
-          serviceUrl = HPCI_CONST[:PXY_AUTH]
+          serviceUrl = '/iSynSApp/paymentAuth.action'
         elsif action == 'capture'
-          serviceUrl = HPCI_CONST[:PXY_CAPTURE]
+          serviceUrl = '/iSynSApp/paymentCapture.action'
         elsif action =='void'
-          serviceUrl = HPCI_CONST[:PXY_VOID]
+          serviceUrl = '/iSynSApp/paymentVoid.action'
         elsif action == 'credit'
-          serviceUrl = HPCI_CONST[:PXY_CREDIT]
+          serviceUrl = '/iSynSApp/paymentCredit.action'
         else
           raise Exception, 'Unsupported HPCI payment action: ' + action
         end
@@ -365,7 +250,7 @@ module ActiveMerchant #:nodoc:
 
       def success?(response)
         (response[:call_status] == HPCI_CONST[:PXYRESP_CALL_STATUS_SUCCESS]) &&
-            (response[:payment_status] == HPCI_CONST[:PXYRESP_RESPONSE_STATUS_APPROVED])
+            (response[:payment_status] == 'approved')
       end ##END: success?
 
       def fraud_review?(response)
@@ -382,23 +267,23 @@ module ActiveMerchant #:nodoc:
         end
 
         if !responseHash.empty?
-          put_in_map results, :call_status, responseHash[HPCI_CONST[:PXYRESP_CALL_STATUS]]
-          put_in_map results, :error_id, responseHash[HPCI_CONST[:PXYRESP_CALL_ERRID]]
-          put_in_map results, :error_msg, responseHash[HPCI_CONST[:PXYRESP_CALL_ERRMSG]]
-          put_in_map results, :payment_status, responseHash[HPCI_CONST[:PXYRESP_RESPONSE_STATUS]]
-          put_in_map results, :status_code, responseHash[HPCI_CONST[:PXYRESP_RESPSTATUS_CODE]]
-          put_in_map results, :status_name, responseHash[HPCI_CONST[:PXYRESP_RESPSTATUS_NAME]]
-          put_in_map results, :status_description, responseHash[HPCI_CONST[:PXYRESP_RESPSTATUS_DESCRIPTION]]
-          put_in_map results, :processor_refid, responseHash[HPCI_CONST[:PXYRESP_PROCESSOR_REFID]]
-          put_in_map results, :processor_type, responseHash[HPCI_CONST[:PXYRESP_PROCESSOR_TYPE]]
-          put_in_map results, :processor_native_response, responseHash[HPCI_CONST[:PXYRESP_FULL_NATIVE_RESP]]
-          put_in_map results, :processor_native_fraud_response, responseHash[HPCI_CONST[:PXYRESP_FULL_FRAUD_NATIVE_RESP]]
-          put_in_map results, :avs1_response, responseHash[HPCI_CONST[:PXYRESP_AVS1]]
-          put_in_map results, :avs2_response, responseHash[HPCI_CONST[:PXYRESP_AVS2]]
-          put_in_map results, :avs3_response, responseHash[HPCI_CONST[:PXYRESP_AVS3]]
-          put_in_map results, :avs4_response, responseHash[HPCI_CONST[:PXYRESP_AVS4]]
-          put_in_map results, :cvv1_response, responseHash[HPCI_CONST[:PXYRESP_CVV1]]
-          put_in_map results, :cvv2_response, responseHash[HPCI_CONST[:PXYRESP_CVV2]]
+          put_in_map results, :call_status, responseHash['status']
+          put_in_map results, :error_id, responseHash['errId']
+          put_in_map results, :error_msg, responseHash['errMsg']
+          put_in_map results, :payment_status, responseHash['pxyResponse.responseStatus']
+          put_in_map results, :status_code, responseHash['pxyResponse.responseStatus.code']
+          put_in_map results, :status_name, responseHash['pxyResponse.responseStatus.name']
+          put_in_map results, :status_description, responseHash['pxyResponse.responseStatus.description']
+          put_in_map results, :processor_refid, responseHash['pxyResponse.processorRefId']
+          put_in_map results, :processor_type, responseHash['pxyResponse.processorType']
+          put_in_map results, :processor_native_response, responseHash['pxyResponse.fullNativeResp']
+          put_in_map results, :processor_native_fraud_response, responseHash['pxyResponse.fullFraudNativeResp']
+          put_in_map results, :avs1_response, responseHash['pxyResponse.responseAVS1']
+          put_in_map results, :avs2_response, responseHash['pxyResponse.responseAVS2']
+          put_in_map results, :avs3_response, responseHash['pxyResponse.responseAVS3']
+          put_in_map results, :avs4_response, responseHash['pxyResponse.responseAVS2']
+          put_in_map results, :cvv1_response, responseHash['pxyResponse.responseCVV1']
+          put_in_map results, :cvv2_response, responseHash['pxyResponse.responseCVV2']
         end
 
         results
--- test/remote/gateways/remote_hostedpci_test.rb
+++ test/remote/gateways/remote_hostedpci_test.rb
@@ -1,39 +1,47 @@
 require 'test_helper'
 
 class RemoteHostedpciTest < Test::Unit::TestCase
-
-
   def setup
     @gateway = HostedpciGateway.new(fixtures(:hostedpci))
+    @credit_card_hash = {first_name: 'John',
+                         last_name: 'Smith',
+                         month: 1,
+                         number: '4242000000404242',
+                         verification_value: '200',
+                         year: Time.now.year + 1}
 
     @amount = 100
-    @credit_card = credit_card('4000100011112224')
-    @declined_card = credit_card('4000300011112220')
+    @credit_card = CreditCard.new(@credit_card_hash)
+    @declined_card = CreditCard.new(@declined_credit_card_hash)
 
     @options = {
-      :order_id => '1',
+      :order_id => SecureRandom.hex(16),
+      :ip=>'127.0.0.1',
+      :customer=>'John Smith',
+      :email=>'test@email.com',
       :billing_address => address,
       :description => 'Store Purchase'
     }
   end
 
+  def test_successful_authorize
+
+    assert response = @gateway.authorize(@amount, @credit_card, @options)
+    assert_success response
+    assert_equal 'description: This transaction has been approved.; status_code:1; status_name:APPROVED', response.message
+  end
+
   def test_successful_purchase
     assert response = @gateway.purchase(@amount, @credit_card, @options)
     assert_success response
-    assert_equal 'REPLACE WITH SUCCESS MESSAGE', response.message
-  end
-
-  def test_unsuccessful_purchase
-    assert response = @gateway.purchase(@amount, @declined_card, @options)
-    assert_failure response
-    assert_equal 'REPLACE WITH FAILED PURCHASE MESSAGE', response.message
+    assert_equal 'description: This transaction has been approved.; status_code:1; status_name:APPROVED', response.message
   end
 
   def test_authorize_and_capture
     amount = @amount
     assert auth = @gateway.authorize(amount, @credit_card, @options)
     assert_success auth
-    assert_equal 'Success', auth.message
+    assert_equal 'description: This transaction has been approved.; status_code:1; status_name:APPROVED', auth.message
     assert auth.authorization
     assert capture = @gateway.capture(amount, auth.authorization)
     assert_success capture
@@ -42,16 +50,14 @@ class RemoteHostedpciTest < Test::Unit::TestCase
   def test_failed_capture
     assert response = @gateway.capture(@amount, '')
     assert_failure response
-    assert_equal 'REPLACE WITH GATEWAY FAILURE MESSAGE', response.message
+    assert_equal 'API Call Error, check API Parameters. Error_ID: PPA_ACT_9', response.message
   end
 
   def test_invalid_login
-    gateway = HostedpciGateway.new(
-                :login => '',
-                :password => ''
-              )
+    gateway = HostedpciGateway.new(fixtures(:hostedpci_no_login))
     assert response = gateway.purchase(@amount, @credit_card, @options)
     assert_failure response
-    assert_equal 'REPLACE WITH FAILURE MESSAGE', response.message
+    assert_equal 'API Call Error, check API Parameters. Error_ID: PPA_ACT_1', response.message
   end
+
 end
