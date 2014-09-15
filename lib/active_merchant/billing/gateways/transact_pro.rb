6d38368e5e9756c348bcf5e91892e72fc4873cfa
Piers Chambers <piers@varyonic.com>
2014-09-15 13:53:15 -0400

    Skeleton for Transact Pro gateway.

---
+++ lib/active_merchant/billing/gateways/transact_pro.rb
@@ -0,0 +1,104 @@
+module ActiveMerchant #:nodoc:
+  module Billing #:nodoc:
+    class TransactProGateway < Gateway
+      self.test_url = 'https://example.com/test'
+      self.live_url = 'https://example.com/live'
+
+      self.supported_countries = ['US']
+      self.default_currency = 'USD'
+      self.supported_cardtypes = [:visa, :master, :american_express, :discover]
+
+      self.homepage_url = 'http://www.example.net/'
+      self.display_name = 'New Gateway'
+
+      def initialize(options={})
+        requires!(options, :some_credential, :another_credential)
+        super
+      end
+
+      def purchase(money, payment, options={})
+        post = {}
+        add_invoice(post, money, options)
+        add_payment(post, payment)
+        add_address(post, payment, options)
+        add_customer_data(post, options)
+
+        commit('sale', post)
+      end
+
+      def authorize(money, payment, options={})
+        post = {}
+        add_invoice(post, money, options)
+        add_payment(post, payment)
+        add_address(post, payment, options)
+        add_customer_data(post, options)
+
+        commit('authonly', post)
+      end
+
+      def capture(money, authorization, options={})
+        commit('capture', post)
+      end
+
+      def refund(money, authorization, options={})
+        commit('refund', post)
+      end
+
+      def void(authorization, options={})
+        commit('void', post)
+      end
+
+      def verify(credit_card, options={})
+        MultiResponse.run(:use_first_response) do |r|
+          r.process { authorize(100, credit_card, options) }
+          r.process(:ignore_result) { void(r.authorization, options) }
+        end
+      end
+
+      private
+
+      def add_customer_data(post, options)
+      end
+
+      def add_address(post, creditcard, options)
+      end
+
+      def add_invoice(post, money, options)
+        post[:amount] = amount(money)
+        post[:currency] = (options[:currency] || currency(money))
+      end
+
+      def add_payment(post, payment)
+      end
+
+      def parse(body)
+        {}
+      end
+
+      def commit(action, parameters)
+        url = (test? ? test_url : live_url)
+        response = parse(ssl_post(url, post_data(action, parameters)))
+
+        Response.new(
+          success_from(response),
+          message_from(response),
+          response,
+          authorization: authorization_from(response),
+          test: test?
+        )
+      end
+
+      def success_from(response)
+      end
+
+      def message_from(response)
+      end
+
+      def authorization_from(response)
+      end
+
+      def post_data(action, parameters = {})
+      end
+    end
+  end
+end
