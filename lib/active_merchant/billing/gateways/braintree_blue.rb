9ca566a67151aa44e9f8b2d81cb3464729b9a823
Corey Woodcox <corey.woodcox@gmail.com>
2014-10-08 19:11:47 -0400

    add payment_method_nonce as an option
    this allows us to pass a nonce instead of a credit card number for a
    transaction.

--- lib/active_merchant/billing/gateways/braintree_blue.rb
+++ lib/active_merchant/billing/gateways/braintree_blue.rb
@@ -536,6 +536,9 @@ module ActiveMerchant #:nodoc:
         if credit_card_or_vault_id.is_a?(String) || credit_card_or_vault_id.is_a?(Integer)
           if options[:payment_method_token]
             parameters[:payment_method_token] = credit_card_or_vault_id
+            options.delete(:billing_address)
+          elsif options[:payment_method_nonce]
+            parameters[:payment_method_nonce] = credit_card_or_vault_id
           else
             parameters[:customer_id] = credit_card_or_vault_id
           end
@@ -561,7 +564,7 @@ module ActiveMerchant #:nodoc:
             }
           end
         end
-        parameters[:billing] = map_address(options[:billing_address]) if options[:billing_address] && !options[:payment_method_token]
+        parameters[:billing] = map_address(options[:billing_address]) if options[:billing_address]
         parameters[:shipping] = map_address(options[:shipping_address]) if options[:shipping_address]
         parameters[:channel] = application_id if application_id.present? && application_id != "ActiveMerchant"
 
