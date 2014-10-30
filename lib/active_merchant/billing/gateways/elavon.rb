82f962c495eec941d0e31e50c38c80fdb602f809
Alexander Profeit <alex@profeit.com>
2014-10-30 13:02:41 +0000

    strip non digit characters from zip for elavon, closes #1445

--- lib/active_merchant/billing/gateways/elavon.rb
+++ lib/active_merchant/billing/gateways/elavon.rb
@@ -249,13 +249,13 @@ module ActiveMerchant #:nodoc:
         form[:salestax] = options[:tax] if options[:tax].present?
       end
 
-      def add_address(form,options)
+      def add_address(form, options)
         billing_address = options[:billing_address] || options[:address]
 
         if billing_address
           form[:avs_address]    = billing_address[:address1].to_s.slice(0, 30)
           form[:address2]       = billing_address[:address2].to_s.slice(0, 30)
-          form[:avs_zip]        = billing_address[:zip].to_s.slice(0, 10)
+          form[:avs_zip]        = billing_address[:zip].to_s.gsub(/\D/, '').slice(0, 10)
           form[:city]           = billing_address[:city].to_s.slice(0, 30)
           form[:state]          = billing_address[:state].to_s.slice(0, 10)
           form[:company]        = billing_address[:company].to_s.slice(0, 50)
@@ -273,7 +273,7 @@ module ActiveMerchant #:nodoc:
           form[:ship_to_state]          = shipping_address[:state].to_s.slice(0, 10)
           form[:ship_to_company]        = shipping_address[:company].to_s.slice(0, 50)
           form[:ship_to_country]        = shipping_address[:country].to_s.slice(0, 50)
-          form[:ship_to_zip]            = shipping_address[:zip].to_s.slice(0, 10)
+          form[:ship_to_zip]            = shipping_address[:zip].to_s.gsub(/\D/, '').slice(0, 10)
         end
       end
 
