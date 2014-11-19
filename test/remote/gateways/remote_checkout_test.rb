9aa4b9afde96c1f6243f233c969cb251b6821595
Mark Bennett <mark.bennett.mail@gmail.com>
2014-11-19 13:46:28 -0500

    Checkout: Add support for void, refund, and verify

--- test/remote/gateways/remote_checkout_test.rb
+++ test/remote/gateways/remote_checkout_test.rb
@@ -16,7 +16,8 @@ class RemoteCheckoutTest < Test::Unit::TestCase
       verification_value: '958'
     )
     @options = {
-      order_id: generate_unique_id
+      order_id: generate_unique_id,
+      currency: "CAD"
     }
   end
 
@@ -54,7 +55,17 @@ class RemoteCheckoutTest < Test::Unit::TestCase
     auth = @gateway.authorize(100, @credit_card, @options)
     assert_success auth
 
-    assert capture = @gateway.capture(100, auth.authorization, @options)
+    assert capture = @gateway.capture(100, auth.authorization, {currency: "CAD"})
+    assert_success capture
+    assert_equal 'Successful', capture.message
+  end
+
+  # For backwards compatability with previous version where authorization was just transid.
+  def test_successful_authorize_and_capture_transid_only
+    auth = @gateway.authorize(100, @credit_card, @options)
+    assert_success auth
+
+    assert capture = @gateway.capture(100, auth.authorization.split('|')[0], @options)
     assert_success capture
     assert_equal 'Successful', capture.message
   end
@@ -63,5 +74,52 @@ class RemoteCheckoutTest < Test::Unit::TestCase
     response = @gateway.authorize(100, @declined_card, @options)
     assert_failure response
     assert_equal 'Not Successful', response.message
+  end
+
+  def test_successful_void
+    auth = @gateway.authorize(100, @credit_card, @options)
+    assert_success auth
+
+    assert void = @gateway.void(auth.authorization)
+    assert_success void
+  end
+
+  def test_failed_void
+    auth = @gateway.authorize(100, @credit_card, @options)
+    assert_success auth
+
+    assert void = @gateway.void('||||')
+    assert_failure void
+  end
+
+  def test_successful_refund
+    assert response = @gateway.purchase(100, @credit_card, @options)
+    assert_success response
+
+    assert refund = @gateway.refund(100, response.authorization, {currency: "CAD"})
+    assert_success refund
+    assert_equal "Successful", refund.message
+  end
+
+  def test_failed_refund
+    assert response = @gateway.purchase(100, @credit_card, @options)
+    assert_success response
+
+    assert refund = @gateway.refund(100, '||||', {currency: "CAD"})
+    assert_failure refund
+  end
+
+  def test_successful_verify
+    assert response = @gateway.verify(@credit_card, @options)
+    assert_success response
+    assert_equal "Successful", response.message
+
+    assert_success response.responses.last, "The void should succeed"
+    assert_equal "Successful", response.responses.last.params["result"]
+  end
+
+  def test_failed_verify
+    assert response = @gateway.verify(@declined_card, @options)
+    assert_failure response
   end
 end
