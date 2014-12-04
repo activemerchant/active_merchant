69993512641310abcc40bfe06e7fe4271724eec0
ivanfer <ivan.sanchez@shopify.com>
2014-12-04 15:44:19 +0000

    Add initial QuickbooksGateway adapter

---
+++ lib/active_merchant/billing/gateways/quickbooks.rb
@@ -0,0 +1,171 @@
+module ActiveMerchant #:nodoc:
+  module Billing #:nodoc:
+    class QuickBooksGateway < Gateway
+      self.test_url = 'https://sandbox.api.intuit.com'
+      self.live_url = 'https://api.intuit.com'
+
+      self.supported_countries = ['US']
+      self.default_currency = 'USD'
+      self.supported_cardtypes = [:visa, :master, :american_express, :discover]
+
+      self.homepage_url = 'http://payments.intuit.com'
+      self.display_name = 'QuickBooks Payments'
+      ENDPOINT =  "/quickbooks/v4/payments/charges"
+
+      STANDARD_ERROR_CODE_MAPPING = {}
+
+      def initialize(options={})
+        requires!(options, :credential1, :credential2, :credential3, :credential4, :option1)
+        super
+      end
+
+      def purchase(money, payment, options = {})
+        post = {}
+        charge_data(post: post, options: options)
+        post[:capture] = "true"
+
+        commit(uri: ENDPOINT, body: post)
+      end
+
+      def authorize(money, payment, options={})
+        post = {}
+        charge_data(post: post, options: options)
+        post[:capture] = "false"
+        
+        commit(uri: ENDPOINT, body: post)
+      end
+
+      def capture(money, authorization, options={})
+        capture_uri = "#{ENDPOINT}/#{CGI.escape(authorization)}/capture"
+        post = options[:id]
+        commit(uri: capture_uri, body: post)
+      end
+
+      def refund(money, authorization, options={})
+        post = {}
+        post[:amount] = money.to_s
+        refund_uri = "#{ENDPOINT}/#{CGI.escape(authorization)}/refund"
+        commit(uri: refund_uri, body: post)
+      end
+
+      def void(authorization, options={})
+        MultiResponse.run do |r|
+          amount = r.process { amount_to_void(authorization: authorization) }
+          r.process { refund(amount, authorization, options = {}) }
+        end.responses.last
+      end
+
+      def verify(credit_card, options = {})
+        MultiResponse.run(:use_first_response) do |r|
+          r.process { authorize(1.00, credit_card, options) }
+          r.process(:ignore_result) { void(r.authorization, options) }
+        end
+      end
+
+      private
+
+      def charge_data(post:, payment:, options:)
+        add_payment(post: post, payment: payment, options: options[:card_code])
+        add_address(post: post, payment: payment, options: options)
+        add_context(context: options[:context]) if options[:context]
+      end
+
+      def add_address(post:, options:)
+        return unless post[:card] && post[:card].kind_of?(Hash)
+        card_address = {}
+        if address = options[:billing_address]
+          card_address[:streetAddress] = address[:address1]
+          card_address[:city] = address[:city]
+          card_address[:region] = address[:state]
+          card_address[:country] = address[:country]
+          card_address[:postalCode] = address[:zip] if address[:zip]
+        end
+        post[:card][:address] = card_address
+      end
+
+      def add_amount(post:, money:, options:)
+        currency = options[:currency] || currency(money)
+        post[:amount] = localized_amount(money, currency)
+        post[:currency] = currency.upcase
+        post[:amount]
+      end
+
+      def add_payment(post:, payment:)
+        add_creditcard(post: post, payment: payment)
+      end
+
+      def add_creditcard(post: ,creditcard:)
+        card = {}
+        card[:number] = creditcard.number
+        card[:expMonth] = creditcard.month
+        card[:expYear] = creditcard.year
+        card[:cvc] = creditcard.verification_value if creditcard.verification_value?
+        card[:name] = creditcard.name if creditcard.name
+        card[:commercialCardCode] = creditcard.card_code if creditcard.card_code
+
+        post[:card] = card
+      end
+
+      def add_context(post:, context:)
+        payment_context = {}
+        payment_context[:tax] = context[:tax] if context[:tax]
+        payment_context[:recurring] = context[:recurring] if context[:recurring]
+
+        post[:context] = payment_context
+      end
+
+      def amount_to_void(authorization:)
+        response = parse(ssl_request(:get, "#{gateway_url}#{ENDPOINT}/#{authorization}", post_data, headers))
+        reponse[:amount]
+      end
+
+      def authorization_object_from_endpoint(authorization:)
+
+      end
+        
+      def parse(body:)
+        JSON.parse(body)
+      end
+
+      def commit(uri:, body:)
+        
+        response = parse(ssl_post(gateway_url + uri, post_data(body: body), headers))
+
+        Response.new(
+          success_from(response: response),
+          message_from(response: response),
+          response,
+          authorization: authorization_from(response: response),
+          test: test?,
+          error_code => 'error'
+        )
+      end
+
+      def gateway_url
+        test? ? test_url : live_url
+      end
+
+      def success_from(response:)
+
+      end
+
+      def message_from(response:)
+      end
+
+      def authorization_from(response:)
+      end
+
+      def post_data(data: {})
+        data.to_json
+      end
+
+      def headers
+        {
+          "Content-type"=> "application/json",
+          "Request-Id"=> @request_id.to_s,
+          
+        }
+      end
+    end
+  end
+end
---
+++ test/unit/gateways/quickbooks_test.rb
@@ -0,0 +1,160 @@
+require 'test_helper'
+
+class QuickBooksTest < Test::Unit::TestCase
+  def setup
+    @gateway = QuickBooksGateway.new(
+      some_credential: 'login',
+      another_credential: 'password'
+    )
+
+    @credit_card = credit_card
+    @amount = 100
+
+    @options = {
+      order_id: '1',
+      billing_address: address,
+      description: 'Store Purchase'
+    }
+  end
+
+  def test_successful_purchase
+    @gateway.expects(:ssl_post).returns(successful_purchase_response)
+    response = @gateway.purchase(@amount, @credit_card, @options)
+    assert_success response
+
+    assert_equal 'REPLACE', response.authorization
+    assert response.test?
+  end
+
+  def test_failed_purchase
+    @gateway.expects(:ssl_post).returns(failed_purchase_response)
+
+    response = @gateway.purchase(@amount, @credit_card, @options)
+    assert_failure response
+    assert_equal Gateway::STANDARD_ERROR_CODE[:card_declined], response.error_code
+  end
+
+  def test_successful_authorize
+  end
+
+  def test_failed_authorize
+  end
+
+  def test_successful_capture
+  end
+
+  def test_failed_capture
+  end
+
+  def test_successful_refund
+  end
+
+  def test_failed_refund
+  end
+
+  def test_successful_void
+  end
+
+  def test_failed_void
+  end
+
+  def test_successful_verify
+  end
+
+  def test_successful_verify_with_failed_void
+  end
+
+  def test_failed_verify
+  end
+
+  private
+
+  def successful_purchase_response
+    <<-RESPONSE
+    {
+      "created": "2014-11-27T22:09:01Z",
+      "status": "CAPTURED",
+      "amount": "20.00",
+      "currency": "USD",
+      "card": {
+        "number": "xxxxxxxxxxxx1111",
+        "name": "alicks profit",
+        "address": {
+          "city": "xxxxxxxx",
+          "region": "xx",
+          "country": "xx",
+          "streetAddress": "xxxxxxxxxxxxx",
+          "postalCode": "xxxxx"
+        },
+        "expMonth": "01",
+        "expYear": "2021"
+      },
+      "id": "EF1IQ9GGXS2D",
+      "authCode": "664472",
+      "capture": "true"
+    }
+    RESPONSE
+  end
+
+  def failed_purchase_response
+    <<-RESPONSE
+    {
+      "errors":[{
+        "code": "PMT-4000",
+        "type": "invalid_request",
+        "message": "Amount. is invalid.",
+        "detail": "Amount.",
+        "infoLink": "https://developer.intuit.com/v2/docs?redirectID=PayErrors"
+      }]
+    }
+    RESPONSE
+  end
+
+  def successful_authorize_response
+    <<-RESPONSE
+    {
+      "created": "2014-11-27T22:17:22Z",
+      "status": "AUTHORIZED",
+      "amount": "2000.00",
+      "currency": "USD",
+      "card": {
+        "number": "xxxxxxxxxxxx4242",
+        "name": "alicks profit",
+        "address": {
+          "city": "xxxxxxxx",
+          "region": "xx",
+          "country": "xx",
+          "streetAddress": "xxxxxxxxxxxxx",
+          "postalCode": "xxxxx"
+        },
+        "expMonth": "01",
+        "expYear": "2021"
+      },
+      "capture": false,
+      "id": "ECZ7U0SO423E",
+      "authCode": "279714"
+    }
+    RESPONSE
+  end
+
+  def failed_authorize_response
+  end
+
+  def successful_capture_response
+  end
+
+  def failed_capture_response
+  end
+
+  def successful_refund_response
+  end
+
+  def failed_refund_response
+  end
+
+  def successful_void_response
+  end
+
+  def failed_void_response
+  end
+end
