af805ae1dcfec952a365668f7e2e952d63c6bb1a
Nathaniel Talbott <nathaniel@talbott.ws>
2014-12-19 11:06:18 -0500

    Payflow: Add fraud_review support
    This adds support for the Response#fraud_review? flag in Payflow so
    that merchants can be notified a "successful" transaction has been
    flagged for further review before shipping.
    
    Closes #1496.

--- lib/active_merchant/billing/gateways/payflow/payflow_common_api.rb
+++ lib/active_merchant/billing/gateways/payflow/payflow_common_api.rb
@@ -197,12 +197,23 @@ module ActiveMerchant #:nodoc:
 
         response = parse(ssl_post(test? ? self.test_url : self.live_url, request, headers))
 
-        build_response(response[:result] == "0", response[:message], response,
-          :test => test?,
-          :authorization => response[:pn_ref] || response[:rp_ref],
-          :cvv_result => CVV_CODE[response[:cv_result]],
-          :avs_result => { :code => response[:avs_result] }
+        build_response(
+          success_for(response),
+          response[:message], response,
+          test: test?,
+          authorization: response[:pn_ref] || response[:rp_ref],
+          cvv_result: CVV_CODE[response[:cv_result]],
+          avs_result: { code: response[:avs_result] },
+          fraud_review: under_fraud_review?(response)
         )
+      end
+
+      def success_for(response)
+        %w(0 126).include?(response[:result])
+      end
+
+      def under_fraud_review?(response)
+        (response[:result] == "126")
       end
     end
   end
