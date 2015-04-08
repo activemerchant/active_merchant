3d17a9de1b89068db2772bf3e2fd2b78124e2320
Trevor Grayson <tgrayson@oddz.com>
2015-04-08 16:52:57 -0700

    Adding Central Account Management System gateway  (Credit Card)

---
+++ test/remote/gateways/remote_cams_test.rb
@@ -0,0 +1,138 @@
+require 'test_helper'
+
+class RemoteCamsTest < Test::Unit::TestCase
+  @@id = Time.now.to_i + 100
+  @@amount = (Time.now.to_i % 100000).to_f 
+
+  def setup
+    @gateway = CamsGateway.new(fixtures(:cams))
+
+    @amount = @@amount += 1
+    
+    @credit_card = credit_card('4111111111111111')
+    @declined_card = credit_card('4000300015555555')
+
+    @options = {
+      order_id: @@id,
+      billing_address: address,
+      description: 'Store Purchase'
+    }
+
+    @@id += 1
+  end
+
+  def test_dump_transcript
+    #pending("Transcript scrubbing for this gateway has been tested.")
+
+    # This test will run a purchase transaction on your gateway
+    # and dump a transcript of the HTTP conversation so that
+    # you can use that transcript as a reference while
+    # implementing your scrubbing logic
+    #dump_transcript_and_fail(@gateway, @amount, @credit_card, @options)
+  end
+
+  def test_transcript_scrubbing
+    transcript = capture_transcript(@gateway) do
+      @gateway.purchase(@amount, @credit_card, @options)
+    end
+    transcript = @gateway.scrub(transcript)
+
+    assert_scrubbed(@credit_card.number, transcript)
+    assert_scrubbed(@credit_card.verification_value, transcript)
+    assert_scrubbed(@gateway.options[:password], transcript)
+  end
+
+  def test_successful_purchase
+    response = @gateway.purchase(@amount, @credit_card, @options)
+    assert_success response
+    assert_equal 'SUCCESS', response.message
+  end
+
+  def test_failed_purchase
+    response = @gateway.purchase(@amount, @declined_card, @options)
+    assert_failure response
+    assert !(%r(Invalid Credit Card Number) =~ response.message).nil? 
+  end
+
+  def test_successful_authorize_and_capture
+    auth = @gateway.authorize(@amount, @credit_card, @options)
+    assert_success auth
+
+    assert capture = @gateway.capture(@amount, auth.authorization)
+    assert_success capture
+  end
+
+  def test_failed_authorize
+    response = @gateway.authorize(@amount, @declined_card, @options)
+    assert_failure response
+  end
+
+  def test_partial_capture
+    auth = @gateway.authorize(@amount, @credit_card, @options)
+    assert_success auth
+
+    assert capture = @gateway.capture(@amount-1, auth.authorization)
+    assert_success capture
+  end
+
+  def test_failed_capture
+    response = @gateway.capture(nil, '')
+    assert_failure response
+  end
+
+  def test_successful_refund
+    purchase = @gateway.purchase(@amount, @credit_card, @options)
+    assert_success purchase
+
+    assert refund = @gateway.refund(nil, purchase.authorization)
+    assert_success refund
+  end
+
+  def test_partial_refund
+    purchase = @gateway.purchase(@amount, @credit_card, @options)
+    assert_success purchase
+
+    assert refund = @gateway.refund(@amount-1, purchase.authorization)
+    assert_success refund
+  end
+
+  def test_failed_refund
+    response = @gateway.refund(nil, '')
+    assert_failure response
+  end
+
+  def test_successful_void
+    auth = @gateway.authorize(@amount, @credit_card, @options)
+    assert_success auth
+
+    assert void = @gateway.void(auth.authorization)
+    assert_success void
+  end
+
+  def test_failed_void
+    response = @gateway.void('')
+    assert_failure response
+  end
+
+  def test_successful_verify
+    response = @gateway.verify(@credit_card, @options)
+    assert_success response
+    assert_equal nil, response.message
+  end
+
+  def test_failed_verify
+    response = @gateway.verify(@declined_card, @options)
+    assert_failure response
+    assert_match %r{Invalid Credit Card Number}, response.message
+    assert_equal Gateway::STANDARD_ERROR_CODE[:card_declined], response.error_code
+  end
+
+  def test_invalid_login
+    gateway = CamsGateway.new(
+      username: '',
+      password: ''
+    )
+    response = gateway.purchase(@amount, @credit_card, @options)
+    assert_failure response
+  end
+end
