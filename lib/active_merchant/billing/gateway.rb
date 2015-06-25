2c99fae2510aafed5b5cb4efa3a6d56fce3b90c2
Andrew P <andrew.paliga@shopify.com>
2015-06-25 14:55:04 +0000

    Add config_error code to master list
    Also adds support for it to Authorize.Net.
    
    Closes #1759.

--- lib/active_merchant/billing/gateway.rb
+++ lib/active_merchant/billing/gateway.rb
@@ -91,7 +91,8 @@ module ActiveMerchant #:nodoc:
         :card_declined => 'card_declined',
         :processing_error => 'processing_error',
         :call_issuer => 'call_issuer',
-        :pickup_card => 'pick_up_card'
+        :pickup_card => 'pick_up_card',
+        :config_error => 'config_error'
       }
 
       cattr_reader :implementations
