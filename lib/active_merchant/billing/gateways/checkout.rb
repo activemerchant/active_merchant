1de46d2bda6f6dd85ba9b2241759bf19ebe9352e
Duff OMelia <duff@omelia.org>
2015-06-29 16:44:09 -0400

    Checkout: Add descriptor_name and descriptor_city

--- lib/active_merchant/billing/gateways/checkout.rb
+++ lib/active_merchant/billing/gateways/checkout.rb
@@ -31,12 +31,10 @@ module ActiveMerchant #:nodoc:
       end
 
       def purchase(amount, payment_method, options)
-        requires!(options, :order_id)
-
         commit('purchase', amount, options) do |xml|
           add_credentials(xml, options)
           add_invoice(xml, amount, options)
-          add_track_id(xml, options[:order_id])
+          add_track_id(xml, options[:order_id] || generate_unique_id)
           add_payment_method(xml, payment_method)
           add_billing_info(xml, options)
           add_shipping_info(xml, options)
@@ -46,12 +44,10 @@ module ActiveMerchant #:nodoc:
       end
 
       def authorize(amount, payment_method, options)
-        requires!(options, :order_id)
-
         commit('authorize', amount, options) do |xml|
           add_credentials(xml, options)
           add_invoice(xml, amount, options)
-          add_track_id(xml, options[:order_id])
+          add_track_id(xml, options[:order_id] || generate_unique_id)
           add_payment_method(xml, payment_method)
           add_billing_info(xml, options)
           add_shipping_info(xml, options)
@@ -151,6 +147,8 @@ module ActiveMerchant #:nodoc:
         xml.bill_email_   options[:email]
         xml.bill_customerip_ options[:ip]
         xml.merchantcustomerid_ options[:customer]
+        xml.descriptor_name options[:descriptor_name]
+        xml.descriptor_city options[:descriptor_city]
       end
 
       def add_reference(xml, authorization)
