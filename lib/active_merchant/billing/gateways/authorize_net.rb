f7db42caa5a0691d8b3d29935236047ced51ddde
monkeysuffrage <pguardiario@gmail.com>
2015-07-16 06:51:31 +0800

    Update authorize_net.rb

--- lib/active_merchant/billing/gateways/authorize_net.rb
+++ lib/active_merchant/billing/gateways/authorize_net.rb
@@ -5,8 +5,8 @@ module ActiveMerchant #:nodoc:
     class AuthorizeNetGateway < Gateway
       include Empty
 
-      self.test_url = 'https://apitest.authorize.net/xml/v1/request.api'
-      self.live_url = 'https://api.authorize.net/xml/v1/request.api'
+      self.test_url = 'https://apitest2.authorize.net/xml/v1/request.api'
+      self.live_url = 'https://api2.authorize.net/xml/v1/request.api'
 
       self.supported_countries = %w(AD AT AU BE BG CA CH CY CZ DE DK ES FI FR GB GB GI GR HU IE IT LI LU MC MT NL NO PL PT RO SE SI SK SM TR US VA)
       self.default_currency = 'USD'
