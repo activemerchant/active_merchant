267b39428481da3f02f4808dace06382315898ef
Luke Hutscal <luke.hutscal@shopify.com>
2015-08-14 13:43:30 +0000

    Beanstream: Add Network Tokenization support

--- lib/active_merchant/billing/gateways/beanstream/beanstream_core.rb
+++ lib/active_merchant/billing/gateways/beanstream/beanstream_core.rb
@@ -206,6 +206,11 @@ module ActiveMerchant #:nodoc:
           post[:trnExpMonth] = format(credit_card.month, :two_digits)
           post[:trnExpYear] = format(credit_card.year, :two_digits)
           post[:trnCardCvd] = credit_card.verification_value
+          if credit_card.is_a?(NetworkTokenizationCreditCard)
+            post[:"3DSecureXID"] = credit_card.transaction_id
+            post[:"3DSecureECI"] = credit_card.eci
+            post[:"3DSecureCAVV"] = credit_card.payment_cryptogram
+          end
         end
       end
 
