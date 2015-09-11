ea01ca2902b056500b16e98078f03ff3e5778e42
Piers Chambers <piers@varyonic.com>
2015-09-11 13:53:35 -0400

    World Net gateway.

--- test/remote/gateways/remote_world_net_test.rb
+++ test/remote/gateways/remote_world_net_test.rb
@@ -5,36 +5,52 @@ class RemoteWorldNetTest < Test::Unit::TestCase
     @gateway = WorldNetGateway.new(fixtures(:world_net))
 
     @amount = 100
-    @credit_card = credit_card('4000100011112224')
-    @declined_card = credit_card('4000300011112220')
+    @declined_amount = 101
+    @credit_card = credit_card('3779810000000005')
     @options = {
-      billing_address: address,
-      description: 'Store Purchase'
+      order_id: generate_order_id,
+    }
+    @refund_options = {
+      operator: 'mr.nobody',
+      reason: 'returned'
     }
   end
 
   def test_successful_purchase
     response = @gateway.purchase(@amount, @credit_card, @options)
     assert_success response
-    assert_equal 'REPLACE WITH SUCCESS MESSAGE', response.message
+    assert_equal 'APPROVAL', response.message
   end
 
   def test_successful_purchase_with_more_options
     options = {
-      order_id: '1',
+      order_id: generate_order_id,
+      email: "joe@example.com",
+      billing_address: address,
+      description: 'Store Purchase',
       ip: "127.0.0.1",
-      email: "joe@example.com"
     }
 
     response = @gateway.purchase(@amount, @credit_card, options)
     assert_success response
-    assert_equal 'REPLACE WITH SUCCESS MESSAGE', response.message
+    assert_equal 'APPROVAL', response.message
   end
 
   def test_failed_purchase
-    response = @gateway.purchase(@amount, @declined_card, @options)
+    response = @gateway.purchase(@declined_amount, @credit_card, @options)
     assert_failure response
-    assert_equal 'REPLACE WITH FAILED PURCHASE MESSAGE', response.message
+    assert_equal 'DECLINED', response.message
+    assert_equal Gateway::STANDARD_ERROR_CODE[:card_declined], response.error_code
+
+    response = @gateway.purchase(103, @credit_card, @options)
+    assert_failure response
+    assert_equal 'CVV FAILURE', response.message
+    assert_equal Gateway::STANDARD_ERROR_CODE[:incorrect_cvc], response.error_code
+
+    response = @gateway.purchase(@amount, credit_card('3779810000000005', month: '13'), @options)
+    assert_failure response
+    assert_equal 'Invalid CARDEXPIRY field', response.message
+    assert_equal Gateway::STANDARD_ERROR_CODE[:invalid_expiry_date], response.error_code
   end
 
   def test_successful_authorize_and_capture
@@ -43,13 +59,13 @@ class RemoteWorldNetTest < Test::Unit::TestCase
 
     assert capture = @gateway.capture(@amount, auth.authorization)
     assert_success capture
-    assert_equal 'REPLACE WITH SUCCESS MESSAGE', response.message
+    assert_equal 'APPROVAL', capture.message
   end
 
   def test_failed_authorize
-    response = @gateway.authorize(@amount, @declined_card, @options)
+    response = @gateway.authorize(@declined_amount, @credit_card, @options)
     assert_failure response
-    assert_equal 'REPLACE WITH FAILED AUTHORIZE MESSAGE', response.message
+    assert_equal 'DECLINED', response.message
   end
 
   def test_partial_capture
@@ -63,30 +79,30 @@ class RemoteWorldNetTest < Test::Unit::TestCase
   def test_failed_capture
     response = @gateway.capture(@amount, '')
     assert_failure response
-    assert_equal 'REPLACE WITH FAILED CAPTURE MESSAGE', response.message
+    assert_match %r{not facet-valid with respect to minLength}, response.message
   end
 
   def test_successful_refund
     purchase = @gateway.purchase(@amount, @credit_card, @options)
     assert_success purchase
 
-    assert refund = @gateway.refund(@amount, purchase.authorization)
+    assert refund = @gateway.refund(@amount, purchase.authorization, @refund_options)
     assert_success refund
-    assert_equal 'REPLACE WITH SUCCESSFUL REFUND MESSAGE', response.message
+    assert_equal 'SUCCESS', refund.message
   end
 
   def test_partial_refund
     purchase = @gateway.purchase(@amount, @credit_card, @options)
     assert_success purchase
 
-    assert refund = @gateway.refund(@amount-1, purchase.authorization)
+    assert refund = @gateway.refund(@amount-1, purchase.authorization, @refund_options)
     assert_success refund
   end
 
   def test_failed_refund
-    response = @gateway.refund(@amount, '')
+    response = @gateway.refund(@amount, '', @refund_options)
     assert_failure response
-    assert_equal 'REPLACE WITH FAILED REFUND MESSAGE', response.message
+    assert_match %r{not facet-valid with respect to minLength}, response.message
   end
 
   def test_successful_void
@@ -94,43 +110,23 @@ class RemoteWorldNetTest < Test::Unit::TestCase
     assert_success auth
 
     assert void = @gateway.void(auth.authorization)
-    assert_success void
-    assert_equal 'REPLACE WITH SUCCESSFUL VOID MESSAGE', response.message
+  # UNSUPPORTED
+  #   assert_success void
+  #   assert_equal 'REPLACE WITH SUCCESSFUL VOID MESSAGE', response.message
   end
 
   def test_failed_void
     response = @gateway.void('')
     assert_failure response
-    assert_equal 'REPLACE WITH FAILED VOID MESSAGE', response.message
-  end
-
-  def test_successful_verify
-    response = @gateway.verify(@credit_card, @options)
-    assert_success response
-    assert_match %r{REPLACE WITH SUCCESS MESSAGE}, response.message
-  end
-
-  def test_failed_verify
-    response = @gateway.verify(@declined_card, @options)
-    assert_failure response
-    assert_match %r{REPLACE WITH FAILED PURCHASE MESSAGE}, response.message
+    assert_match %r{Cannot find the declaration of element}, response.message
   end
 
   def test_invalid_login
-    gateway = WorldNetGateway.new(login: '', password: '')
+    gateway = WorldNetGateway.new(terminal_id: '', secret: '')
 
     response = gateway.purchase(@amount, @credit_card, @options)
     assert_failure response
-    assert_match %r{REPLACE WITH FAILED LOGIN MESSAGE}, response.message
-  end
-
-  def test_dump_transcript
-    # This test will run a purchase transaction on your gateway
-    # and dump a transcript of the HTTP conversation so that
-    # you can use that transcript as a reference while
-    # implementing your scrubbing logic.  You can delete
-    # this helper after completing your scrub implementation.
-    dump_transcript_and_fail(@gateway, @amount, @credit_card, @options)
+    assert_match %r{not facet-valid with respect to minLength}, response.message
   end
 
   def test_transcript_scrubbing
@@ -141,7 +137,10 @@ class RemoteWorldNetTest < Test::Unit::TestCase
 
     assert_scrubbed(@credit_card.number, transcript)
     assert_scrubbed(@credit_card.verification_value, transcript)
-    assert_scrubbed(@gateway.options[:password], transcript)
+    assert_scrubbed(@gateway.options[:secret], transcript)
   end
 
+  def generate_order_id
+    (Time.now.to_f * 100).to_i.to_s
+  end
 end
