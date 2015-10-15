46b4207579fc89282745a228d15924261424c3f4
Duff O'Melia <duff@omelia.org>
2015-10-15 12:02:07 -0400

    FirstData_e4: Fix level_2 and level_3
    We weren't using the right XML tags.  Now we are.

--- test/remote/gateways/remote_firstdata_e4_test.rb
+++ test/remote/gateways/remote_firstdata_e4_test.rb
@@ -32,8 +32,16 @@ class RemoteFirstdataE4Test < Test::Unit::TestCase
   end
 
   def test_successful_purchase_with_level_2_and_level_3
-    level_2_xml = "<Tax1Amount>88</Tax1Amount><CustomerRef>TheRef</CustomerRef>"
-    level_3_xml = "<ShipFromZip>22934</ShipFromZip><FreightAmount>7.25</FreightAmount>"
+    level_2_xml = "<TaxAmount>88</TaxAmount><CustomerRef>TheRef</CustomerRef>"
+    level_3_xml = <<-LEVEL3
+        <LineItem>
+          <LineItemTotal>107.20</LineItemTotal>
+          <Quantity>3</Quantity>
+          <Description>The Description</Description>
+          <UnitCost>2.33</UnitCost>
+        </LineItem>
+    LEVEL3
+
     response = @gateway.purchase(500, @credit_card, @options.merge(level_2: level_2_xml, level_3: level_3_xml))
     assert_success response
     assert_equal "Transaction Normal - Approved", response.message
