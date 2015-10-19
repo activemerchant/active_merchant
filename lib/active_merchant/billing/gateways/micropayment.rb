c3467b68073064ce5ecc2e903699e6c79f84007c
Duff O'Melia <duff@omelia.org>
2015-10-19 19:43:41 -0400

    Revert "Micropayment: upgrade to new api"
    This reverts commit e80e9867913db9329df282ba65d76e908daeea04.
    
    I accidentally merged this one.  It's almost ready.  Backing out for
    now.

--- lib/active_merchant/billing/gateways/micropayment.rb
+++ lib/active_merchant/billing/gateways/micropayment.rb
@@ -5,58 +5,55 @@ module ActiveMerchant #:nodoc:
       self.display_name = "micropayment"
       self.homepage_url = "https://www.micropayment.de/"
 
-      self.test_url = self.live_url = "https://sipg.micropayment.de/public/creditcardpsp/v1/nvp/"
+      self.test_url = self.live_url = "https://sipg.micropayment.de/public/creditcard/v1.5.2/nvp/"
 
       self.supported_countries = %w(DE)
       self.default_currency = "EUR"
       self.money_format = :cents
       self.supported_cardtypes = [:visa, :master, :american_express]
 
-
       def initialize(options={})
         requires!(options, :access_key)
-        @am_api_key = "af1fd841af792f4c50131414ff76e004"
         super
       end
 
       def purchase(amount, payment_method, options={})
         post = {}
         add_invoice(post, amount, options)
-        add_payment_method(post, payment_method, options)
+        add_payment_method(post, payment_method)
         add_customer_data(post, options)
-        commit("purchase", post)
+        commit("shortTransactionPurchase", post)
       end
 
       def authorize(amount, payment_method, options={})
         post = {}
         add_invoice(post, amount, options)
-        add_payment_method(post, payment_method, options)
+        add_payment_method(post, payment_method)
         add_customer_data(post, options)
-        commit("authorize", post)
+        commit("shortTransactionAuthorization", post)
       end
 
       def capture(amount, authorization, options={})
         post = {}
         add_reference(post, authorization)
         add_invoice(post, amount, options)
-        commit("capture", post)
+        commit("transactionCapture", post)
       end
 
       def void(authorization, options={})
         post = {}
         add_reference(post, authorization)
-        commit("void", post)
+        commit("transactionReversal", post)
       end
 
       def refund(amount, authorization, options={})
         post = {}
         add_reference(post, authorization)
         add_invoice(post, amount, options)
-        commit("refund", post)
+        commit("transactionRefund", post)
       end
 
       def verify(credit_card, options={})
-
         MultiResponse.run(:use_first_response) do |r|
           r.process { authorize(250, credit_card, options) }
           r.process(:ignore_result) { void(r.authorization, options) }
@@ -84,12 +81,11 @@ module ActiveMerchant #:nodoc:
         post[:project] = options[:project] || "sprdly"
       end
 
-      def add_payment_method(post, payment_method, options={})
+      def add_payment_method(post, payment_method)
         post[:firstname] = payment_method.first_name
         post[:surname] = payment_method.last_name
         post[:number] = payment_method.number
-        post[:recurring] = options[:recurring] == true ? 1 : 0
-        post[:cvc2] = payment_method.verification_value unless post[:recurring] == 1
+        post[:cvc2] = payment_method.verification_value
         post[:expiryYear] = format(payment_method.year, :four_digits)
         post[:expiryMonth] = format(payment_method.month, :two_digits)
       end
@@ -107,9 +103,9 @@ module ActiveMerchant #:nodoc:
       end
 
       def commit(action, params)
+
         params[:testMode] = 1 if test?
         params[:accessKey] = @options[:access_key]
-        params[:apiKey] = @options[:api_key] || @am_api_key
 
         response = parse(ssl_post(url(action), post_data(action, params), headers))
 
@@ -117,7 +113,7 @@ module ActiveMerchant #:nodoc:
           succeeded = success_from(response),
           message_from(succeeded, response),
           response,
-          authorization: authorization_from(response, params),
+          authorization: authorization_from(response),
           avs_result: AVSResult.new(code: response["some_avs_result_key"]),
           cvv_result: CVVResult.new(response["some_cvv_result_key"]),
           test: test?
@@ -163,9 +159,8 @@ module ActiveMerchant #:nodoc:
         authorization.split("|")
       end
 
-      def authorization_from(response, request_params)
-        session_id = response["sessionId"] ? response["sessionId"] : request_params[:sessionId]
-        "#{session_id}|#{response["transactionId"]}"
+      def authorization_from(response)
+        "#{response["sessionId"]}|#{response["transactionId"]}"
       end
     end
   end
