ce7d0dfa52bf1dde8c6270cb8915032ec4d6ab39
rafael <rafaelfmori@gmail.com>
2015-10-29 11:20:00 -0200

    hotfix incontext implementation

--- lib/active_merchant/billing/gateways/paypal_express_common.rb
+++ lib/active_merchant/billing/gateways/paypal_express_common.rb
@@ -17,9 +17,11 @@ module ActiveMerchant
       end
 
       def redirect_url_for(token, options = {})
-        options = {:review => true, :mobile => false, :in_context => :false}.update(options)
+        options = {:review => true, :mobile => false, :in_context => false, :live => false}.update(options)
 
         if options[:in_context] 
+          url = "https://www.sandbox.paypal.com/checkoutnow?token=#{token}"
+        elsif options[:in_context]  and options[:live] 
           url = "https://www.paypal.com/checkoutnow?token=#{token}"
         else
           cmd  = options[:mobile]  ? '_express-checkout-mobile' : '_express-checkout'
