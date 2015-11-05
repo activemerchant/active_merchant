18083e0b6d7a43e1eb170caa09cbc69ba9e96d02
Alex Vaccaro <alex.vaccaro@mycase.com>
2015-11-05 14:31:43 -0800

    av: add comment for LitlePaypageRegistration

--- lib/active_merchant/billing/gateways/litle/paypage_registration.rb
+++ lib/active_merchant/billing/gateways/litle/paypage_registration.rb
@@ -1,5 +1,14 @@
 module ActiveMerchant
   module Billing
+    # Litle provides functionality to make authorization and sale calls
+    # with a Paypage Registration Id instead of a Litle Token.
+    # This class wraps the required data for such a request.
+    #
+    # The first parameter is the paypage_registration_id and is required.
+    #
+    # The second parameter optionally takes a month, year, and verification_value.
+    # These parameters are allowed by the Litle endpoints but not necessary for
+    # a successful request.
     class LitlePaypageRegistration
       attr_reader :paypage_registration_id, :month, :year, :verification_value
 
