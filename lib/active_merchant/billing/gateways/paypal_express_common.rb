7abc000009e68183fe4ee1eb0d7c507c5a90e960
Rafael <rafaelfmori@gmail.com>
2015-11-12 12:24:26 -0800

    refactoring in_context option

--- lib/active_merchant/billing/gateways/paypal_express_common.rb
+++ lib/active_merchant/billing/gateways/paypal_express_common.rb
@@ -4,25 +4,30 @@ module ActiveMerchant
       def self.included(base)
         if base.respond_to?(:class_attribute)
           base.class_attribute :test_redirect_url
+          base.class_attribute :in_context_test_redirect_url
           base.class_attribute :live_redirect_url
         else
           base.class_inheritable_accessor :test_redirect_url
+          base.class_inheritable_accessor :in_context_test_redirect_url
           base.class_inheritable_accessor :live_redirect_url
         end
         base.live_redirect_url = 'https://www.paypal.com/cgi-bin/webscr'
+        base.in_context_live_redirect_url = 'https://www.paypal.com/checkoutnow'
       end
 
-      def redirect_url
-        test? ? test_redirect_url : live_redirect_url
+      def redirect_url(in_context = nil)
+        if in_context
+          test? ? in_context_test_redirect_url : in_context_live_redirect_url
+        else
+          test? ? test_redirect_url : live_redirect_url
+        end
       end
 
       def redirect_url_for(token, options = {})
-        options = {:review => true, :mobile => false, :in_context => false, :live => false}.update(options)
+        options = {:review => true, :mobile => false, :in_context => false}.update(options)
 
-        if options[:in_context]  and options[:live] 
-          url = "https://www.paypal.com/checkoutnow?token=#{token}"
-        elsif options[:in_context] 
-          url = "https://www.sandbox.paypal.com/checkoutnow?token=#{token}"
+        if options[:in_context] 
+          url  = "#{redirect_url(true)}?cmd=#{cmd}&token=#{token}"
         else
           cmd  = options[:mobile]  ? '_express-checkout-mobile' : '_express-checkout'
           url  = "#{redirect_url}?cmd=#{cmd}&token=#{token}"
--- lib/active_merchant/version.rb
+++ lib/active_merchant/version.rb
@@ -1,3 +1,3 @@
 module ActiveMerchant
-  VERSION = "1.54.3"
+  VERSION = "1.54.4"
 end
