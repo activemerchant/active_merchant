6379723759480a68c5583cba19b07a02c2b14f93
WorldNet <namita.kapoor@worldnettps.com>
2016-01-21 16:13:54 +0000

    Initial Commit by Worldnet

--- lib/active_merchant/billing/gateway.rb
+++ lib/active_merchant/billing/gateway.rb
@@ -291,7 +291,7 @@ module ActiveMerchant #:nodoc:
       end
 
       def requires!(hash, *params)
-        params.each do |param|
+          params.each do |param|
           if param.is_a?(Array)
             raise ArgumentError.new("Missing required parameter: #{param.first}") unless hash.has_key?(param.first)
 
