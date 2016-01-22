da5635c33b1d0133857654c3118ad3aed0bed76f
Worldnet <namita.kapoor@worldnettps.com>
2016-01-22 10:18:39 +0000

    Update worldnet_test.rb

--- test/unit/gateways/worldnet_test.rb
+++ test/unit/gateways/worldnet_test.rb
@@ -1,127 +1,73 @@
+require 'active_merchant/billing/gateways/worldnet.rb'
 require 'test_helper'
+require 'active_merchant/billing/response.rb'
 
 class WorldnetTest < Test::Unit::TestCase
   def setup
-    @gateway = WorldnetGateway.new(terminalId: '2921001', sharedSecret: 'secretpass')
-    @credit_card = credit_card
-    @amount = 100
+        @gateway = WorldnetGateway.new(fixtures(:worldnet))
+        @approved_amount = 100.00
+        @refund_amount = 60.00
+        
+        @declined_amount = 100.01
+        @declined_card   = credit_card('1111111111111111')
+        @partial_amount = 100.01
+        @credit_card = credit_card('4000100011112224')
+        @options = {
+          order_id: generate_unique_order_id,
+          billing_address: address,
+          description: 'Store Purchase'
+              
+        }
+            
+      end
+      ###Generate Random OrderID
+      
+        ###Generate Random OrderID
+     def generate_unique_order_id
+           SecureRandom.hex(6)
+         
+     end
+ 
+      def test_successful_purchase 
+         assert response = @gateway.purchase(@approved_amount, @credit_card, @options)
+         assert_success response
+         assert_equal 'APPROVAL', response.message
+            
+    end
+ 
+    def test_successful_authorization
+        assert auth = @gateway.authorize(@approved_amount, @credit_card, @options)
+        assert_success auth
+        assert_equal 'APPROVAL', auth.message
+    end
 
-    @options = {
-      order_id: '1',
-      billing_address: address,
-      description: 'Store Purchase'
-    }
-  end
+    def test_successful_refund
+        purchase = @gateway.purchase(@approved_amount, @credit_card, @options)
+        assert_success purchase
+    
+        assert refund = @gateway.refund(@approved_amount, purchase.authorization)
+        assert_success refund
+      assert_equal 'SUCCESS', refund.message
+   end
 
-  def test_successful_purchase
-    @gateway.expects(:ssl_post).returns(successful_purchase_response)
+   
+   
+   def test_successful_authorize_and_capture
+        auth = @gateway.authorize(@approved_amount, @credit_card, @options)
+        assert_success auth
+        assert capture = @gateway.capture(@approved_amount, auth.authorization)
+        assert_success capture
+        assert_equal 'APPROVAL', capture.message
+   end
+    def test_successful_void
+        auth = @gateway.authorize(@approved_amount, @credit_card, @options)
+        assert_success auth
+    
+        assert void = @gateway.void(@approved_amount, auth.authorization)
+        assert_success void
+        assert_equal 'SUCCESS', void.message
+      end
 
-    response = @gateway.purchase(@amount, @credit_card, @options)
-    assert_success response
 
-    assert_equal 'REPLACE', response.authorization
-    assert response.test?
-  end
 
-  def test_failed_purchase
-    @gateway.expects(:ssl_post).returns(failed_purchase_response)
-
-    response = @gateway.purchase(@amount, @credit_card, @options)
-    assert_failure response
-    assert_equal Gateway::STANDARD_ERROR_CODE[:card_declined], response.error_code
-  end
-
-  def test_successful_authorize
-  end
-
-  def test_failed_authorize
-  end
-
-  def test_successful_capture
-  end
-
-  def test_failed_capture
-  end
-
-  def test_successful_refund
-  end
-
-  def test_failed_refund
-  end
-
-  def test_successful_void
-  end
-
-  def test_failed_void
-  end
-
-  def test_successful_verify
-  end
-
-  def test_successful_verify_with_failed_void
-  end
-
-  def test_failed_verify
-  end
-
-  def test_scrub
-    assert @gateway.supports_scrubbing?
-    assert_equal @gateway.scrub(pre_scrubbed), post_scrubbed
-  end
-
-  private
-
-  def pre_scrubbed
-    %q(
-      Run the remote tests for this gateway, and then put the contents of transcript.log here.
-    )
-  end
-
-  def post_scrubbed
-    %q(
-      Put the scrubbed contents of transcript.log here after implementing your scrubbing function.
-      Things to scrub:
-        - Credit card number
-        - CVV
-        - Sensitive authentication details
-    )
-  end
-
-  def successful_purchase_response
-    %(
-      Easy to capture by setting the DEBUG_ACTIVE_MERCHANT environment variable
-      to "true" when running remote tests:
-
-      $ DEBUG_ACTIVE_MERCHANT=true ruby -Itest \
-        test/remote/gateways/remote_worldnet_test.rb \
-        -n test_successful_purchase
-    )
-  end
-
-  def failed_purchase_response
-  end
-
-  def successful_authorize_response
-  end
-
-  def failed_authorize_response
-  end
-
-  def successful_capture_response
-  end
-
-  def failed_capture_response
-  end
-
-  def successful_refund_response
-  end
-
-  def failed_refund_response
-  end
-
-  def successful_void_response
-  end
-
-  def failed_void_response
-  end
 end
