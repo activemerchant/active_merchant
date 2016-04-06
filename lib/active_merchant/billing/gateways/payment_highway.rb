04d6585b41e33b9b566e581f257cb3d38eed1a5a
Joni Hasanen <joni.hasanen@pieceofcode.net>
2016-04-06 15:04:42 +0300

    Clean class

--- lib/active_merchant/billing/gateways/payment_highway.rb
+++ lib/active_merchant/billing/gateways/payment_highway.rb
@@ -40,61 +40,21 @@ module ActiveMerchant #:nodoc:
             "expiry_month": payment.month.to_s,
             "cvc": payment.verification_value
           }
-        }.to_json)
+        }
+
+        payload = add_ip(payload, options)
+
+        commit("/transaction/#{transactionId}/debit", payload.to_json)
       end
 
-      #def authorize(money, payment, options={})
-        #post = {}
-        #add_invoice(post, money, options)
-        #add_payment(post, payment)
-        #add_address(post, payment, options)
-        #add_customer_data(post, options)
-
-        #commit('authonly', post)
-      #end
-
-      #def capture(money, authorization, options={})
-        #commit('capture', post)
-      #end
-
-      #def refund(money, authorization, options={})
-        #commit('refund', post)
-      #end
-
-      #def void(authorization, options={})
-        #commit('void', post)
-      #end
-
-      #def verify(credit_card, options={})
-        #MultiResponse.run(:use_first_response) do |r|
-          #r.process { authorize(100, credit_card, options) }
-          #r.process(:ignore_result) { void(r.authorization, options) }
-        #end
-      #end
-
-      #def supports_scrubbing?
-        #true
-      #end
-
-      #def scrub(transcript)
-        #transcript
-      #end
-
       private
+      def add_ip(payload, options)
+        return payload unless options.has_key?(:ip)
 
-      #def add_customer_data(post, options)
-      #end
-
-      #def add_address(post, creditcard, options)
-      #end
-
-      #def add_invoice(post, money, options)
-        #post[:amount] = amount(money)
-        #post[:currency] = (options[:currency] || currency(money))
-      #end
-
-      #def add_payment(post, payment)
-      #end
+        new_payload = payload.clone
+        new_payload["customer"] = { "network_address": options.fetch(:ip) }
+        new_payload
+      end
 
       def commit(action, payload)
         url = (test? ? test_url : live_url)
@@ -108,9 +68,6 @@ module ActiveMerchant #:nodoc:
           success_from(response),
           message_from(response),
           response,
-          authorization: authorization_from(response),
-          avs_result: AVSResult.new(code: response["some_avs_response_key"]),
-          cvv_result: CVVResult.new(response["some_cvv_response_key"]),
           test: test?,
           error_code: error_code_from(response)
         )
@@ -148,12 +105,6 @@ module ActiveMerchant #:nodoc:
 
       def message_from(response)
         RESPONSE_CODE_MAPPING[response_code(response)]
-      end
-
-      def authorization_from(response)
-      end
-
-      def post_data(action, parameters = {})
       end
 
       def error_code_from(response)
