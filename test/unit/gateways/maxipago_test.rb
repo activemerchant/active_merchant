daf18bf62e2b2521b98186455fc1f08c513a4ea1
Sanjay Chakrapani <sanjay@spreedly.com>
2016-06-09 21:19:54 +0530

    MaxiPago: Add verify and scrub

--- test/unit/gateways/maxipago_test.rb
+++ test/unit/gateways/maxipago_test.rb
@@ -107,7 +107,76 @@ class MaxipagoTest < Test::Unit::TestCase
     assert_equal "The Return amount is greater than the amount that can be returned.", refund.message
   end
 
+  def test_successful_verify
+    @gateway.expects(:ssl_post).times(2).returns(successful_authorize_response).then.returns(successful_void_response)
+    response = @gateway.verify(@credit_card, @options)
+    assert_success response
+    assert_equal "AUTHORIZED", response.message
+  end
+
+  def test_failed_verify
+    @gateway.expects(:ssl_post).returns(failed_authorize_response)
+    response = @gateway.verify(@credit_card, @options)
+    assert_failure response
+    assert_equal "The transaction has an expired credit card.", response.message
+  end
+
+  def test_scrub
+    assert @gateway.supports_scrubbing?
+    assert_equal @gateway.scrub(pre_scrubbed), post_scrubbed
+  end
+
   private
+
+  def pre_scrubbed
+    %(
+      opening connection to testapi.maxipago.net:443...
+      opened
+      starting SSL for testapi.maxipago.net:443...
+      SSL established
+      <- "POST /UniversalAPI/postXML HTTP/1.1\r\nContent-Type: text/xml\r\nAccept-Encoding: gzip;q=1.0,deflate;q=0.6,identity;q=0.3\r\nAccept: */*\r\nUser-Agent: Ruby\r\nConnection: close\r\nHost: testapi.maxipago.net\r\nContent-Length: 1224\r\n\r\n"
+      <- "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<transaction-request>\n  <version>3.1.1.15</version>\n  <verification>\n    <merchantId>100</merchantId>\n    <merchantKey>21g8u6gh6szw1gywfs165vui</merchantKey>\n  </verification>\n  <order>\n    <sale>\n      <processorID>1</processorID>\n      <fraudCheck>N</fraudCheck>\n      <referenceNum>12345</referenceNum>\n      <transactionDetail>\n        <payType>\n          <creditCard>\n            <number>4111111111111111</number>\n            <expMonth>9</expMonth>\n            <expYear>2017</expYear>\n            <cvvNumber>444</cvvNumber>\n          </creditCard>\n        </payType>\n      </transactionDetail>\n      <payment>\n        <chargeTotal>10.00</chargeTotal>\n        <creditInstallment>\n          <numberOfInstallments>3</numberOfInstallments>\n          <chargeInterest>N</chargeInterest>\n        </creditInstallment>\n      </payment>\n      <billing>\n        <name>Longbob Longsen</name>\n        <address>456 My Street</address>\n        <address2>Apt 1</address2>\n        <city>Ottawa</city>\n        <state>ON</state>\n        <postalcode>K1C2N6</postalcode>\n        <country>CA</country>\n        <phone>(555)555-5555</phone>\n      </billing>\n    </sale>\n  </order>\n</transaction-request>\n"
+      -> "HTTP/1.1 200 OK\r\n"
+      -> "Date: Thu, 09 Jun 2016 14:54:53 GMT\r\n"
+      -> "Server: Apache\r\n"
+      -> "Strict-Transport-Security: max-age=31557600; includeSubDomains\r\n"
+      -> "Content-Length: 628\r\n"
+      -> "X-Frame-Options: SAMEORIGIN\r\n"
+      -> "Connection: close\r\n"
+      -> "Content-Type: text/plain; charset=UTF-8\r\n"
+      -> "\r\n"
+      reading 628 bytes...
+      -> ""
+      -> "<?xml version=\"1.0\" encoding=\"UTF-8\"?><transaction-response>\n<authCode>123456</authCode>\n<orderID>C0A8013F:015535A8A798:D12E:006BB50C</orderID>\n<referenceNum>12345</referenceNum>\n<transactionID>1410203</transactionID>\n<transactionTimestamp>1465484093</transactionTimestamp>\n<responseCode>0</responseCode>\n<responseMessage>CAPTURED</responseMessage>\n<avsResponseCode>YYY</avsResponseCode>\n<cvvResponseCode>M</cvvResponseCode>\n<processorCode>A</processorCode>\n<processorMessage>APPROVED</processorMessage>\n<errorMessage/>\n<creditCardCountry>US</creditCardCountry>\n<creditCardScheme>Visa</creditCardScheme>\n</transaction-response>\n"
+      read 628 bytes
+      Conn close
+    )
+  end
+
+  def post_scrubbed
+    %(
+      opening connection to testapi.maxipago.net:443...
+      opened
+      starting SSL for testapi.maxipago.net:443...
+      SSL established
+      <- "POST /UniversalAPI/postXML HTTP/1.1\r\nContent-Type: text/xml\r\nAccept-Encoding: gzip;q=1.0,deflate;q=0.6,identity;q=0.3\r\nAccept: */*\r\nUser-Agent: Ruby\r\nConnection: close\r\nHost: testapi.maxipago.net\r\nContent-Length: 1224\r\n\r\n"
+      <- "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<transaction-request>\n  <version>3.1.1.15</version>\n  <verification>\n    <merchantId>100</merchantId>\n    <merchantKey>[FILTERED]</merchantKey>\n  </verification>\n  <order>\n    <sale>\n      <processorID>1</processorID>\n      <fraudCheck>N</fraudCheck>\n      <referenceNum>12345</referenceNum>\n      <transactionDetail>\n        <payType>\n          <creditCard>\n            <number>[FILTERED]</number>\n            <expMonth>9</expMonth>\n            <expYear>2017</expYear>\n            <cvvNumber>[FILTERED]</cvvNumber>\n          </creditCard>\n        </payType>\n      </transactionDetail>\n      <payment>\n        <chargeTotal>10.00</chargeTotal>\n        <creditInstallment>\n          <numberOfInstallments>3</numberOfInstallments>\n          <chargeInterest>N</chargeInterest>\n        </creditInstallment>\n      </payment>\n      <billing>\n        <name>Longbob Longsen</name>\n        <address>456 My Street</address>\n        <address2>Apt 1</address2>\n        <city>Ottawa</city>\n        <state>ON</state>\n        <postalcode>K1C2N6</postalcode>\n        <country>CA</country>\n        <phone>(555)555-5555</phone>\n      </billing>\n    </sale>\n  </order>\n</transaction-request>\n"
+      -> "HTTP/1.1 200 OK\r\n"
+      -> "Date: Thu, 09 Jun 2016 14:54:53 GMT\r\n"
+      -> "Server: Apache\r\n"
+      -> "Strict-Transport-Security: max-age=31557600; includeSubDomains\r\n"
+      -> "Content-Length: 628\r\n"
+      -> "X-Frame-Options: SAMEORIGIN\r\n"
+      -> "Connection: close\r\n"
+      -> "Content-Type: text/plain; charset=UTF-8\r\n"
+      -> "\r\n"
+      reading 628 bytes...
+      -> ""
+      -> "<?xml version=\"1.0\" encoding=\"UTF-8\"?><transaction-response>\n<authCode>123456</authCode>\n<orderID>C0A8013F:015535A8A798:D12E:006BB50C</orderID>\n<referenceNum>12345</referenceNum>\n<transactionID>1410203</transactionID>\n<transactionTimestamp>1465484093</transactionTimestamp>\n<responseCode>0</responseCode>\n<responseMessage>CAPTURED</responseMessage>\n<avsResponseCode>YYY</avsResponseCode>\n<cvvResponseCode>M</cvvResponseCode>\n<processorCode>A</processorCode>\n<processorMessage>APPROVED</processorMessage>\n<errorMessage/>\n<creditCardCountry>US</creditCardCountry>\n<creditCardScheme>Visa</creditCardScheme>\n</transaction-response>\n"
+      read 628 bytes
+      Conn close
+    )
+  end
 
   def successful_purchase_response
     %(
