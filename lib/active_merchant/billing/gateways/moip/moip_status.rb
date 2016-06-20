27a53fd34db8c86f9476e922bf62d63d7e515c3b
Mandado <jorgerobertodev@gmail.com>
2016-06-20 08:58:56 -0300

    updated recurring method to receive only plan_code, created create, update and find plan methods

---
+++ lib/active_merchant/billing/gateways/moip/moip_status.rb
@@ -0,0 +1,55 @@
+module ActiveMerchant #:nodoc:
+  module Billing #:nodoc:
+    module MoipStatus #:nodoc:
+      
+      INTERVAL_MAP = {
+        'monthly' => ['MONTH', 1],
+        'quarterly' => ['MONTH', 3],
+        'semesterly' => ['MONTH', 6],
+        'yearly' => ['YEAR', 1]
+      }
+
+      INVOICE_TO_SUBSCRIPTION_STATUS_MAP = {
+        1 => :process,
+        2 => :process,
+        3 => :confirm,
+        4 => :canceled,
+        5 => :process
+      }
+
+      SUBSCRIPTION_STATUS_MAP = {
+        'active'    => :confirm,
+        'suspended' => :cancel,
+        'expired'   => :process,
+        'overdue'   => :process,
+        'canceled'  => :cancel,
+        'trial'     => :no_wait_process
+      }
+
+      INVOICE_STATUS_MAP = {
+        1 => :open,
+        2 => :wait_confirmation,
+        3 => :confirm,
+        4 => :not_pay,
+        5 => :expire
+      }
+
+      PAYMENT_STATUS_MAP = {
+        1 => :authorize,
+        2 => :initiate,
+        3 => :wait_boleto,
+        4 => :confirm,
+        5 => :cancel,
+        6 => :wait_analysis,
+        7 => :reverse,
+        9 => :refund,
+        10 => :wait_boleto
+      }
+
+      PAYMENT_METHOD_MAP = {
+        1 => 'credit_card',
+        2 => 'boleto'
+      }
+    end
+  end
+end
