23e4ddbdbc6126b0f03e7ccb2b73edf96371df97
William Belchior <williamsbelchior@gmail.com>
2016-07-07 13:55:04 -0300

    Add logo and soft_descriptor

--- lib/active_merchant/billing/gateways/pagarme.rb
+++ lib/active_merchant/billing/gateways/pagarme.rb
@@ -13,9 +13,10 @@ module ActiveMerchant #:nodoc:
       self.default_currency = 'BRL'
       self.money_format = :cents
       self.supported_cardtypes = [:visa, :master, :american_express, :discover, :diners_club]
-
       self.homepage_url = 'https://pagar.me/'
+      self.display_fullname = 'Pagar.me'
       self.display_name = 'Pagar.me'
+      self.display_logo = 'https://cdn.edools.com/assets/images/gateways/pagarMe.png'
 
       STANDARD_ERROR_CODE_MAPPING = {
         'refused' => STANDARD_ERROR_CODE[:card_declined],
@@ -35,6 +36,7 @@ module ActiveMerchant #:nodoc:
         post = {}
         add_amount(post, money)
         add_installments(post, options)
+        add_soft_descriptor(post, options)
         add_payment_method(post, payment_method)
         add_metadata(post, options)
 
@@ -45,6 +47,7 @@ module ActiveMerchant #:nodoc:
         post = {}
         add_amount(post, money)
         add_installments(post, options)
+        add_soft_descriptor(post, options)
         add_payment_method(post, payment_method)
         add_metadata(post, options)
 
@@ -121,6 +124,10 @@ module ActiveMerchant #:nodoc:
 
       def add_installments(post, options={})
         post[:installments] = options["credit_card"].try(:[], "installments")
+      end
+
+      def add_soft_descriptor(post, options={})
+        post[:soft_descriptor] = options["extras"].try(:[], "soft_descriptor")
       end
 
       def add_payment_method(post, payment_method)
