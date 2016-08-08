cd0b04ec266d313fb09a8210305bdc8beb9dd532
Duff O'Melia <duff@omelia.org>
2016-08-08 12:50:57 -0400

    SagePay: Ensure we handle truncating huge values
    From:
    https://github.com/activemerchant/active_merchant/commit/172e0a4dac499345bf5f6ac15c20f878b82545fc#commitcomment-18507362

--- lib/active_merchant/billing/gateways/sage_pay.rb
+++ lib/active_merchant/billing/gateways/sage_pay.rb
@@ -178,7 +178,12 @@ module ActiveMerchant #:nodoc:
       def truncate(value, max_size)
         return nil unless value
         return value.to_s if CGI.escape(value.to_s).length <= max_size
-        truncate(value.to_s.chop, max_size)
+
+        if value.size > max_size
+          truncate(super(value, max_size), max_size)
+        else
+          truncate(value.to_s.chop, max_size)
+        end
       end
 
       def add_reference(post, identification)
