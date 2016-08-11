a68d7104eea73702d16fba68517373c5e7c58b4b
William Belchior <williamsbelchior@gmail.com>
2016-08-11 14:09:40 -0300

    fix pagarme card expiration date

--- lib/active_merchant/billing/gateways/pagarme/card_pagarme.rb
+++ lib/active_merchant/billing/gateways/pagarme/card_pagarme.rb
@@ -3,12 +3,13 @@ module ActiveMerchant
     module CardPagarme
       def create_card(credit_card)
         month = "%02d" % credit_card.month
+        year  = credit_card.year.to_s.last(2)
 
         card = PagarMe::Card.new({
           card_number: credit_card.number,
           card_holder_name: credit_card.name,
           card_expiration_month: month,
-          card_expiration_year: credit_card.year,
+          card_expiration_year: year,
           card_cvv: credit_card.verification_value
         })
 
