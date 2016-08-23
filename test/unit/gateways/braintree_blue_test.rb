1a661af9fe8dbe3d89d7a1a94abbcb2e8e717c35
Miki Rezentes <miki@spreedly.com>
2016-08-23 16:50:57 -0400

    Braintree: add support for Android Pay
    Closes #2002.

--- test/unit/gateways/braintree_blue_test.rb
+++ test/unit/gateways/braintree_blue_test.rb
@@ -615,6 +615,36 @@ class BraintreeBlueTest < Test::Unit::TestCase
     assert_equal "transaction_id", response.authorization
   end
 
+  def test_android_pay_card
+    Braintree::TransactionGateway.any_instance.expects(:sale).
+      with(
+        :amount => '1.00',
+        :order_id => '1',
+        :customer => {:id => nil, :email => nil, :first_name => 'Longbob', :last_name => 'Longsen'},
+        :options => {:store_in_vault => false, :submit_for_settlement => nil, :hold_in_escrow => nil},
+        :custom_fields => nil,
+        :android_pay_card => {
+          :number => '4111111111111111',
+          :expiration_month => '09',
+          :expiration_year => (Time.now.year + 1).to_s,
+          :cryptogram => '111111111100cryptogram',
+          :google_transaction_id => '1234567890'
+        }
+      ).
+      returns(braintree_result(:id => "transaction_id"))
+
+    credit_card = network_tokenization_credit_card('4111111111111111',
+      :brand              => 'visa',
+      :transaction_id     => "123",
+      :eci                => "05",
+      :payment_cryptogram => "111111111100cryptogram",
+      :source             => :android_pay
+    )
+
+    response = @gateway.authorize(100, credit_card, :test => true, :order_id => '1', :google_transaction_id => '1234567890')
+    assert_equal "transaction_id", response.authorization
+  end
+
   def test_supports_network_tokenization
     assert_instance_of TrueClass, @gateway.supports_network_tokenization?
   end
