8c2cde61877966cf0ca8f784e5e92da16b0620fc
David Santoso <david@spreedly.com>
2016-09-29 13:52:40 -0400

    Stripe: Update minimum amount during verify

--- lib/active_merchant/billing/gateways/stripe.rb
+++ lib/active_merchant/billing/gateways/stripe.rb
@@ -142,7 +142,7 @@ module ActiveMerchant #:nodoc:
 
       def verify(payment, options = {})
         MultiResponse.run(:use_first_response) do |r|
-          r.process { authorize(50, payment, options) }
+          r.process { authorize(100, payment, options.merge(currency: "USD")) }
           r.process(:ignore_result) { void(r.authorization, options) }
         end
       end
