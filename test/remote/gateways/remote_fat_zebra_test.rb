1b2b13a8a65b846e605fede85dd52691dd93c32b
David Perry <dperry@spreedly.com>
2016-10-06 13:58:51 -0400

    Fat Zebra: Pass 3DS information fields
    This is not 3DS support, it only passes these fields along properly
    if supplied.  All three fields need to be present to succeed.
    
    Closes #2236

--- test/remote/gateways/remote_fat_zebra_test.rb
+++ test/remote/gateways/remote_fat_zebra_test.rb
@@ -127,6 +127,18 @@ class RemoteFatZebraTest < Test::Unit::TestCase
     assert_equal 'Approved', response.message
   end
 
+  def test_successful_purchase_with_3DS_information
+    assert response = @gateway.purchase(@amount, @credit_card, @options.merge(:cavv => 'MDRjN2MxZTAxYjllNTBkNmM2MTA=', :xid => 'MGVmMmNlMzI4NjAyOWU2ZDgwNTQ=', :sli => '05'))
+    assert_success response
+    assert_equal 'Approved', response.message
+  end
+
+  def test_failed_purchase_with_incomplete_3DS_information
+    assert response = @gateway.purchase(@amount, @credit_card, @options.merge(:cavv => 'MDRjN2MxZTAxYjllNTBkNmM2MTA=', :sli => '05'))
+    assert_failure response
+    assert_match %r{Exception caught in application}, response.message
+  end
+
   def test_invalid_login
     gateway = FatZebraGateway.new(
                 :username => 'invalid',
