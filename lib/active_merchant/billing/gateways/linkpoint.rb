fc93208e8bbe9f0b3fbfeca94fe3c47fa3748c9c
Jason Webster <jason.webster@shopify.com>
2017-02-08 13:47:19 -0500

    Linkpoint: Raise ArgumentError when trying to instantiate without `:pem`
    This fixes a regression introduced in https://github.com/activemerchant/active_merchant/commit/df2fc16cd7b1e7791d6073e4bbf712c1795f5b6e
    The addition of the `strip!` call raises NoMethodError if the option
    isn't provided. This restores the previous error w/ a developer friendly
    message.
    
    Closes #2329

--- lib/active_merchant/billing/gateways/linkpoint.rb
+++ lib/active_merchant/billing/gateways/linkpoint.rb
@@ -143,9 +143,9 @@ module ActiveMerchant #:nodoc:
           :pem => LinkpointGateway.pem_file
         }.update(options)
 
-        @options[:pem].strip!
-
         raise ArgumentError, "You need to pass in your pem file using the :pem parameter or set it globally using ActiveMerchant::Billing::LinkpointGateway.pem_file = File.read( File.dirname(__FILE__) + '/../mycert.pem' ) or similar" if @options[:pem].blank?
+
+        @options[:pem].strip!
       end
 
       # Send a purchase request with periodic options
