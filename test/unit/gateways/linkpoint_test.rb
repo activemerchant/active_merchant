activemerchant/active_merchant

commit df2fc16cd7b1e7791d6073e4bbf712c1795f5b6e

Linkpoint: Clean whitespace from PEM
Closes #2212.

lib/active_merchant/billing/gateways/linkpoint.rb
@@ -143,6 +143,8 @@ module ActiveMerchant #:nodoc:
           :pem => LinkpointGateway.pem_file
         }.update(options)
 
+        @options[:pem].strip!
+
         raise ArgumentError, "You need to pass in your pem file using the :pem parameter or set it globally using ActiveMerchant::Billing::LinkpointGateway.pem_file = File.read( File.dirname(__FILE__) + '/../mycert.pem' ) or similar" if @options[:pem].blank?
       end
 

test/remote/gateways/remote_linkpoint_test.rb
@@ -119,6 +119,12 @@ class LinkpointTest < Test::Unit::TestCase
     assert_equal "DECLINED", response.params["approved"]
   end
 
+  def test_cleans_whitespace_from_pem
+    @gateway = LinkpointGateway.new(fixtures(:linkpoint).merge(pem: ' ' + fixtures(:linkpoint)[:pem]))
+    assert response = @gateway.authorize(1000, @credit_card, @options)
+    assert_success response
+  end
+
   def test_transcript_scrubbing
     transcript = capture_transcript(@gateway) do
       @gateway.purchase(@amount, @credit_card, @options)

commit b8b447f3cf21be5aebb2f007434c91e09a5e6df8

Linkpoint: Raise ArgumentError when trying to instantiate without `:pem`
This fixes a regression introduced in https://github.com/activemerchant/active_merchant/commit/df2fc16cd7b1e7791d6073e4bbf712c1795f5b6e
The addition of the `strip!` call raises NoMethodError if the option
isn't provided. This restores the previous error w/ a developer friendly
message.

Closes #2329

lib/active_merchant/billing/gateways/linkpoint.rb
@@ -143,9 +143,9 @@ module ActiveMerchant #:nodoc:
           :pem => LinkpointGateway.pem_file
         }.update(options)
 
-        @options[:pem].strip!
-
         raise ArgumentError, "You need to pass in your pem file using the :pem parameter or set it globally using ActiveMerchant::Billing::LinkpointGateway.pem_file = File.read( File.dirname(__FILE__) + '/../mycert.pem' ) or similar" if @options[:pem].blank?
+
+        @options[:pem].strip!
       end
 
       # Send a purchase request with periodic options

test/unit/gateways/linkpoint_test.rb
@@ -14,6 +14,12 @@ class LinkpointTest < Test::Unit::TestCase
     @options = { :order_id => 1000, :billing_address => address }
   end
 
+  def test_instantiating_without_credential_raises
+    assert_raise ArgumentError do
+      LinkpointGateway.new(login: 123123)
+    end
+  end
+
   def test_credit_card_formatting
     assert_equal '04', @gateway.send(:format_creditcard_expiry_year, 2004)
     assert_equal '04', @gateway.send(:format_creditcard_expiry_year, '2004')