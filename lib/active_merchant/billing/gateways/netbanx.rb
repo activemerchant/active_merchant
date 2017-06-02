413336e656bdfc0692d9a93cb6e0c84a7a54d4ed
Ian Irving <ian@falsepositives.com>
2017-06-02 18:01:09 -0400

    update supported countries and cardtypes

--- lib/active_merchant/billing/gateways/netbanx.rb
+++ lib/active_merchant/billing/gateways/netbanx.rb
@@ -5,9 +5,20 @@ module ActiveMerchant #:nodoc:
       self.test_url = 'https://api.test.netbanx.com/'
       self.live_url = 'https://api.netbanx.com/'
 
-      self.supported_countries = ['CA', 'US', 'GB']
+      self.supported_countries = %w(AF AX AL DZ AS AD AO AI AQ AG AR AM AW AU AT AZ BS BH BD BB BY BE BZ BJ BM BT BO BQ BA BW BV BR IO BN BG BF BI KH CM CA CV KY CF TD CL CN CX CC CO KM CG CD CK CR CI HR CU CW CY CZ DK DJ DM DO EC EG SV GQ ER EE ET FK FO FJ FI FR GF PF TF GA GM GE DE GH GI GR GL GD GP GU GT GG GN GW GY HT HM HN HK HU IS IN ID IR IQ IE IM IL IT JM JP JE JO KZ KE KI KP KR KW KG LA LV LB LS LR LY LI LT LU MO MK MG MW MY MV ML MT MH MQ MR MU YT MX FM MD MC MN ME MS MA MZ MM NA NR NP NC NZ NI NE NG NU NF MP NO OM PK PW PS PA PG PY PE PH PN PL PT PR QA RE RO RU RW BL SH KN LC MF VC WS SM ST SA SN RS SC SL SG SX SK SI SB SO ZA GS SS ES LK PM SD SR SJ SZ SE CH SY TW TJ TZ TH NL TL TG TK TO TT TN TR TM TC TV UG UA AE GB US UM UY UZ VU VA VE VN VG VI WF EH YE ZM ZW)
       self.default_currency = 'CAD'
-      self.supported_cardtypes = [:visa, :master, :american_express, :discover]
+      self.supported_cardtypes = [
+        :american_express,
+        :diners_club,
+        :discover,
+        :jcb,
+        :master,
+        :maestro,
+        :swiff,
+        :visa,
+        :visa_debit,
+        :visa_electron
+      ]
       self.money_format = :cents
 
       self.homepage_url = 'https://processing.paysafe.com/'
@@ -110,9 +121,6 @@ module ActiveMerchant #:nodoc:
       def add_customer_data(post, options)
         post[:merchantCustomerId] = (options[:merchant_customer_id] || SecureRandom.uuid)
         post[:locale] = options[:locale]
-        # if options[:billing_address]
-        #   post[:address]  = map_address(options[:billing_address])
-        # end
       end
 
       def add_credit_card(post, credit_card, options = {})
