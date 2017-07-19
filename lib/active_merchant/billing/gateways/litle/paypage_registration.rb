03d91e7dbd267ec2b36ea0833b7787de55718ac4
Alex Vaccaro <alex.vaccaro@mycase.com>
2017-07-19 10:04:15 -0700

    Add support for passing in name with paypage registration id request

--- lib/active_merchant/billing/gateways/litle/paypage_registration.rb
+++ lib/active_merchant/billing/gateways/litle/paypage_registration.rb
@@ -6,17 +6,21 @@ module ActiveMerchant
     #
     # The first parameter is the paypage_registration_id and is required.
     #
-    # The second parameter optionally takes a month, year, and verification_value.
+    # The second parameter optionally takes a month, year, verification_value, and name.
     # These parameters are allowed by the Litle endpoints but not necessary for
     # a successful request.
+    #
+    # The name parameter is allowed by Vantiv as a member of the billToAddress element.
+    # It is passed in here to be consistent with the rest of the Litle gateway and Activemerchant.
     class LitlePaypageRegistration
-      attr_reader :paypage_registration_id, :month, :year, :verification_value
+      attr_reader :paypage_registration_id, :month, :year, :verification_value, :name
 
       def initialize(paypage_registration_id, options = {})
         @paypage_registration_id = paypage_registration_id
         @month = options[:month]
         @year = options[:year]
         @verification_value = options[:verification_value]
+        @name = options[:name]
       end
     end
   end
