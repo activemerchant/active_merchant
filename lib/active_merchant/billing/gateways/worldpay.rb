48cdef287bf1a3c042295f41905c744d8b10f0c8
Niaja <niaja@spreedly.com>
2018-05-09 17:03:25 -0400

    WorldPay: Update Country List
    Updates the Country list for Worldpay
    
    Loaded suite test/unit/gateways/worldpay_test
    .....................................
    
    37 tests, 210 assertions, 0 failures, 0 errors, 0 pendings, 0 omissions, 0 notifications
    100% passed
    
    Loaded suite test/remote/gateways/remote_worldpay_test
    
    25 tests, 84 assertions, 4 failures, 0 errors, 0 pendings, 0 omissions, 0 notifications
    84% passed

--- lib/active_merchant/billing/gateways/worldpay.rb
+++ lib/active_merchant/billing/gateways/worldpay.rb
@@ -6,7 +6,7 @@ module ActiveMerchant #:nodoc:
 
       self.default_currency = 'GBP'
       self.money_format = :cents
-      self.supported_countries = %w(HK GB AU AD BE CH CY CZ DE DK ES FI FR GI GR HU IE IT LI LU MC MT NL NO NZ PL PT SE SG SI SM TR UM VA)
+      self.supported_countries = %w(HK GB AU AD AR BE BR CA CH CN CO CR CY CZ DE DK ES FI FR GI GR HU IE IN IT JP LI LU MC MT MY MX NL NO NZ PA PE PL PT SE SG SI SM TR UM VA)
       self.supported_cardtypes = [:visa, :master, :american_express, :discover, :jcb, :maestro, :laser, :switch]
       self.currencies_without_fractions = %w(HUF IDR ISK JPY KRW)
       self.currencies_with_three_decimal_places = %w(BHD KWD OMR RSD TND)
