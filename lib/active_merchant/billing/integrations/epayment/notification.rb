f7091d4899dcc3679f46e416dcf58d1659e7913c
Anton <i.fuzzing@gmail.com>
2018-06-08 16:45:03 +0200

    add empty notificator

---
+++ lib/active_merchant/billing/integrations/epayment/notification.rb
@@ -0,0 +1,12 @@
+require 'net/http'
+
+module ActiveMerchant #:nodoc:
+  module Billing #:nodoc:
+    module Integrations #:nodoc:
+      module Epayment
+        class Notification < ActiveMerchant::Billing::Integrations::Notification
+        end
+      end
+    end
+  end
+end
