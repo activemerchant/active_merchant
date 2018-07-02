c1d275051c39c941eb5cb5eca9c360b2bd856ab7
tdurkee <tim.durkee@veracross.com>
2018-07-02 09:19:06 -0400

    Adding first_name and last_name to customer

--- lib/active_merchant/billing/gateways/braintree_blue.rb
+++ lib/active_merchant/billing/gateways/braintree_blue.rb
@@ -533,7 +533,11 @@ module ActiveMerchant #:nodoc:
           :order_id => options[:order_id],
           :customer => {
             :id => options[:store] == true ? "" : options[:store],
-            :email => scrub_email(options[:email])
+            :email => scrub_email(options[:email]),
+            :phone => options[:phone] || (options[:billing_address][:phone] if options[:billing_address] &&
+              options[:billing_address][:phone]),
+            :first_name => options[:billing_address][:name].split(' ')[0], # getting first name
+            :last_name => options[:billing_address][:name][options[:billing_address][:name].split(' ')[0].length + 1...options[:billing_address][:name].length] # getting all names after first
           },
           :options => {
             :store_in_vault => options[:store] ? true : false,
