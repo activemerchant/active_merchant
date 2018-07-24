af70e08ba12f4ba577f56145c192deacbbe515d8
David Perry <dperry@spreedly.com>
2018-07-24 15:00:14 -0400

    Revert "BlueSnap: Update list of supported countries"
    This reverts commit 33fba2fcfe32fb6597d53875ce438e9c85e9bafa.
    
    Whoops, forgot that "supported countries" is actually the countries that
    merchants can be based in, rather than supported billing addresses.

--- lib/active_merchant/billing/gateways/blue_snap.rb
+++ lib/active_merchant/billing/gateways/blue_snap.rb
@@ -5,7 +5,7 @@ module ActiveMerchant
     class BlueSnapGateway < Gateway
       self.test_url = 'https://sandbox.bluesnap.com/services/2'
       self.live_url = 'https://ws.bluesnap.com/services/2'
-      self.supported_countries = %w(AD AE AG AI AL AM AO AQ AR AS AT AU AW AZ BA BB BD BE BF BG BH BI BJ BM BN BO BR BS BT BV BW BY BZ CA CC CD CF CG CH CI CK CL CM CN CO CR CV CW CX CY CZ DE DJ DK DM DO DZ EC EE EG EH ER ES ET FI FJ FK FM FO FR GA GB GD GE GF GG GI GL GM GN GP GQ GR GS GT GU GW GY HK HM HN HR HT HU ID IE IL IM IN IO IS IT JE JM JO JP KE KG KH KI KM KN KR KW KY KZ LA LC LI LK LR LS LT LU LV MA MC MD ME MF MG MH MK ML MN MO MP MQ MR MS MT MU MV MW MX MY MZ NA NC NE NF NG NI NL NO NP NR NU NZ OM PA PE PF PG PH PK PL PM PN PR PT PW PY QA RE RO RS RU RW SA SB SC SE SG SH SI SJ SK SL SM SN SO SR ST SV SX SZ TC TD TF TG TH TJ TK TM TN TO TR TT TV TW TZ UA UG UM US UY UZ VA VC VE VG VI VN VU WF WS YT ZA ZM ZW)
+      self.supported_countries = %w(US CA GB AT BE BG HR CY CZ DK EE FI FR DE GR HU IE IT LV LT LU MT NL PL PT RO SK SI ES SE)
 
       self.default_currency = 'USD'
       self.supported_cardtypes = [:visa, :master, :american_express, :discover, :jcb, :diners_club, :maestro]
