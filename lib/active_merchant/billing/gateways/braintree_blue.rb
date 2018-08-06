93610f84de57e2bb7ca626a4f474a581b4251b2e
Tim <tim.durkee@veracross.com>
2018-08-06 08:37:57 -0400

    Putting extra names with first name instead of last names

--- lib/active_merchant/billing/gateways/braintree_blue.rb
+++ lib/active_merchant/billing/gateways/braintree_blue.rb
@@ -535,8 +535,8 @@ module ActiveMerchant #:nodoc:
             :id => options[:store] == true ? "" : options[:store],
             :email => scrub_email(options[:email]),
             :phone => options[:phone] || options.dig(:billing_address, :phone),
-            :first_name => options[:billing_address][:name].split(/\s+/, 2)[0], # getting first name
-            :last_name => options[:billing_address][:name].split(/\s+/, 2)[1] # getting all names after first
+            :first_name => options[:billing_address][:name].reverse.split(/\s+/, 2).last.reverse, # getting name minus last name (because of reverse the last item is the first name)
+            :last_name => options[:billing_address][:name].reverse.split(/\s+/, 2).first.reverse # getting last name (because of reverse the first item is the last name)
           },
           :options => {
             :store_in_vault => options[:store] ? true : false,
