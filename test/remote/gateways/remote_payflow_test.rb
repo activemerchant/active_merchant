fb2c448e9f2dec84552c3ef9f5db3a93c7fca093
Pierre Nespo <pierre.nespo@gmail.com>
2018-08-13 11:33:32 -0400

    Allow setting CAPTURECOMPLETE on Payflow capture transactions (#2952)
    By default Payflow will consider the first capture the final one, by passing
    `CAPTURECOMPLETE=N` it lets us capture again later on.
    
    [Docs](https://developer.paypal.com/docs/classic/payflow/integration-guide/#paypal-credit-card-transaction-request-parameters)
    
    Remote: (Failures related to ACH and recurring billing)
    32 tests, 137 assertions, 8 failures, 0 errors, 0 pendings, 0 omissions, 0 notifications
    75% passed
    
    Unit:
    46 tests, 208 assertions, 0 failures, 0 errors, 0 pendings, 0 omissions, 0 notifications
    100% passed

--- test/remote/gateways/remote_payflow_test.rb
+++ test/remote/gateways/remote_payflow_test.rb
@@ -135,6 +135,32 @@ class RemotePayflowTest < Test::Unit::TestCase
     assert_success capture
   end
 
+  def test_authorize_and_complete_capture
+    assert auth = @gateway.authorize(100 * 2, @credit_card, @options)
+    assert_success auth
+    assert_equal 'Approved', auth.message
+    assert auth.authorization
+
+    assert capture = @gateway.capture(100, auth.authorization, :capture_complete => 'Y')
+    assert_success capture
+
+    assert capture = @gateway.capture(100, auth.authorization)
+    assert_failure capture
+  end
+
+  def test_authorize_and_uncomplete_capture
+    assert auth = @gateway.authorize(100 * 2, @credit_card, @options)
+    assert_success auth
+    assert_equal 'Approved', auth.message
+    assert auth.authorization
+
+    assert capture = @gateway.capture(100, auth.authorization, :capture_complete => 'N')
+    assert_success capture
+
+    assert capture = @gateway.capture(100, auth.authorization)
+    assert_success capture
+  end
+
   def test_failed_capture
     assert response = @gateway.capture(100, '999')
     assert_failure response
