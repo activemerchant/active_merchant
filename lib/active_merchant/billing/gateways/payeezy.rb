630edd91c87e649f6edd34dd8cbb0092aff70829
molbrown <molly@spreedly.com>
2018-12-05 10:31:24 -0500

    Payeezy: Support $0 for verify transactions
    Payeezy allows $0 authorization. Updating from $1 verify
    to $0 to resolve failures for customer.
    
    ENE-59
    
    Closes #3074
    
    Unit Tests:
    33 tests, 156 assertions, 0 failures, 0 errors, 0 pendings, 0 omissions, 0 notifications
    100% passed
    
    Remote Tests:
    33 tests, 131 assertions, 0 failures, 0 errors, 0 pendings, 0 omissions, 0 notifications
    100% passed

--- lib/active_merchant/billing/gateways/payeezy.rb
+++ lib/active_merchant/billing/gateways/payeezy.rb
@@ -94,7 +94,7 @@ module ActiveMerchant
 
       def verify(credit_card, options={})
         MultiResponse.run(:use_first_response) do |r|
-          r.process { authorize(100, credit_card, options) }
+          r.process { authorize(0, credit_card, options) }
           r.process(:ignore_result) { void(r.authorization, options) }
         end
       end
