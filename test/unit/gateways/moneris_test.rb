069c81854ce8ec03b131245c5f31cbaa8c4400a1
molbrown <molly@spreedly.com>
2019-01-25 09:48:33 -0500

    Rubocop: Style/TrailingCommaInArguments

--- test/unit/gateways/moneris_test.rb
+++ test/unit/gateways/moneris_test.rb
@@ -33,7 +33,7 @@ class MonerisTest < Test::Unit::TestCase
   def test_successful_first_purchase_with_credential_on_file
     gateway = MonerisGateway.new(
       :login => 'store3',
-      :password => 'yesguy',
+      :password => 'yesguy'
     )
     gateway.expects(:ssl_post).returns(successful_first_cof_purchase_response)
     assert response = gateway.purchase(
@@ -54,7 +54,7 @@ class MonerisTest < Test::Unit::TestCase
   def test_successful_subsequent_purchase_with_credential_on_file
     gateway = MonerisGateway.new(
       :login => 'store3',
-      :password => 'yesguy',
+      :password => 'yesguy'
     )
     gateway.expects(:ssl_post).returns(successful_first_cof_authorize_response)
     assert response = gateway.authorize(
