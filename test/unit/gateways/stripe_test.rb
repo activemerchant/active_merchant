activemerchant/active_merchant

commit adc5a88c485e8de8cf30c064233fbe7bc927bf86

Do not consider a refund unsuccessful if only refunding the fee failed
Fixes https://github.com/activemerchant/active_merchant/issues/3116

When specifying the `refund_fee_amount` on a Refund using the Stripe
gateway, Active Merchant makes three different API requests:

1. Issue the actual refund against the charge
2. Fetch the charge to get the application fee identifier
3. Issue the application fee refund against the original application fee

Currently, if either 2 or 3 fail while 1 succeeds, the `#refund`
method's response is a failure, which is not true. This process is not
transactional. The refund had succeeded and is not rolled back, which
isn't possible to do anyway.

lib/active_merchant/billing/gateways/stripe.rb
@@ -150,14 +150,21 @@ module ActiveMerchant #:nodoc:
         post[:reason] = options[:reason] if options[:reason]
         post[:expand] = [:charge]
 
-        MultiResponse.run(:first) do |r|
-          r.process { commit(:post, "charges/#{CGI.escape(identification)}/refunds", post, options) }
+        response = commit(:post, "charges/#{CGI.escape(identification)}/refunds", post, options)
 
-          if options[:refund_fee_amount] && options[:refund_fee_amount].to_s != '0'
-            r.process { fetch_application_fee(identification, options) }
-            r.process { refund_application_fee(options[:refund_fee_amount].to_i, application_fee_from_response(r.responses.last), options) }
+        if options[:refund_fee_amount] && options[:refund_fee_amount].to_s != '0'
+          charge = api_request(:get, "charges/#{CGI.escape(identification)}", nil, options)
+
+          if application_fee = charge['application_fee']
+            fee_refund_options = {
+              currency: options[:currency], # currency isn't used by Stripe here, but we need it for #add_amount
+              key: @fee_refund_api_key
+            }
+            refund_application_fee(options[:refund_fee_amount].to_i, application_fee, fee_refund_options)
           end
         end
+
+        response
       end
 
       def verify(payment, options = {})
@@ -168,21 +175,10 @@ module ActiveMerchant #:nodoc:
         end
       end
 
-      def application_fee_from_response(response)
-        return unless response.success?
-        response.params['application_fee'] unless response.params['application_fee'].empty?
-      end
-
       def refund_application_fee(money, identification, options = {})
-        return Response.new(false, 'Application fee id could not be found') unless identification
-
         post = {}
         add_amount(post, money, options)
-        options[:key] = @fee_refund_api_key if @fee_refund_api_key
-        options.delete(:stripe_account)
-
-        refund_fee = commit(:post, "application_fees/#{CGI.escape(identification)}/refunds", post, options)
-        application_fee_response!(refund_fee, "Application fee could not be refunded: #{refund_fee.message}")
+        commit(:post, "application_fees/#{CGI.escape(identification)}/refunds", post, options)
       end
 
       # Note: creating a new credit card will not change the customer's existing default credit card (use :set_default => true)
@@ -514,17 +510,6 @@ module ActiveMerchant #:nodoc:
       def add_emv_metadata(post, creditcard)
         post[:metadata] ||= {}
         post[:metadata][:card_read_method] = creditcard.read_method if creditcard.respond_to?(:read_method)
-      end
-
-      def fetch_application_fee(identification, options = {})
-        options[:key] = @fee_refund_api_key
-
-        fetch_charge = commit(:get, "charges/#{CGI.escape(identification)}", nil, options)
-        application_fee_response!(fetch_charge, "Application fee id could not be retrieved: #{fetch_charge.message}")
-      end
-
-      def application_fee_response!(response, message)
-        response.success? ? response : Response.new(false, message)
       end
 
       def parse(body)

test/unit/gateways/stripe_test.rb
@@ -686,7 +686,7 @@ class StripeTest < Test::Unit::TestCase
   end
 
   # What is the significance of this??? it's to test that the first response is used as primary. so identical to above with an extra assertion
-  def test_refund_with_fee_response_gives_a_charge_authorization
+  def test_refund_with_fee_response_responds_with_the_refund_authorization
     s = sequence('request')
     @gateway.expects(:ssl_request).returns(successful_partially_refunded_response).in_sequence(s)
     @gateway.expects(:ssl_request).returns(successful_fetch_application_fee_response).in_sequence(s)
@@ -697,24 +697,23 @@ class StripeTest < Test::Unit::TestCase
     assert_equal 're_test_refund', response.authorization
   end
 
-  def test_unsuccessful_refund_with_refund_fee_amount_when_application_fee_id_not_found
+  def test_successful_refund_with_failed_fee_refund_fetch
     s = sequence('request')
     @gateway.expects(:ssl_request).returns(successful_partially_refunded_response).in_sequence(s)
     @gateway.expects(:ssl_request).returns(unsuccessful_fetch_application_fee_response).in_sequence(s)
 
     assert response = @gateway.refund(@refund_amount, 'ch_test_charge', :refund_fee_amount => 100)
-    assert_failure response
-    assert_match(/^Application fee id could not be retrieved/, response.message)
+    assert_success response
   end
 
-  def test_unsuccessful_refund_with_refund_fee_amount_when_refunding_application_fee
+  def test_successful_refund_with_failed_fee_refund
     s = sequence('request')
     @gateway.expects(:ssl_request).returns(successful_partially_refunded_response).in_sequence(s)
     @gateway.expects(:ssl_request).returns(successful_fetch_application_fee_response).in_sequence(s)
     @gateway.expects(:ssl_request).returns(generic_error_response).in_sequence(s)
 
     assert response = @gateway.refund(@refund_amount, 'ch_test_charge', :refund_fee_amount => 100)
-    assert_failure response
+    assert_success response
   end
 
   def test_successful_verify

commit c28cabb5a1efe936327406c5eb439fc7c603ed23

Do not attempt application fee refund if refund was not successful
This was a regression made in adc5a88c485e8de8cf30c064233fbe7bc927bf86
which was trying to make the App Fee refunds not impact the overall
status of the Refund response. That was achieved, but it also made the
unintentional change of always trying to refund the application fee
regardless of the refund succeeding or not. There was previously no test
coverage of this behaviour.

lib/active_merchant/billing/gateways/stripe.rb
@@ -152,7 +152,7 @@ module ActiveMerchant #:nodoc:
 
         response = commit(:post, "charges/#{CGI.escape(identification)}/refunds", post, options)
 
-        if options[:refund_fee_amount] && options[:refund_fee_amount].to_s != '0'
+        if response.success? && options[:refund_fee_amount] && options[:refund_fee_amount].to_s != '0'
           charge = api_request(:get, "charges/#{CGI.escape(identification)}", nil, options)
 
           if application_fee = charge['application_fee']

test/unit/gateways/stripe_test.rb
@@ -685,7 +685,6 @@ class StripeTest < Test::Unit::TestCase
     assert_success response
   end
 
-  # What is the significance of this??? it's to test that the first response is used as primary. so identical to above with an extra assertion
   def test_refund_with_fee_response_responds_with_the_refund_authorization
     s = sequence('request')
     @gateway.expects(:ssl_request).returns(successful_partially_refunded_response).in_sequence(s)
@@ -714,6 +713,14 @@ class StripeTest < Test::Unit::TestCase
 
     assert response = @gateway.refund(@refund_amount, 'ch_test_charge', :refund_fee_amount => 100)
     assert_success response
+  end
+
+  def test_unsuccessful_refund_does_not_refund_fee
+    s = sequence('request')
+    @gateway.expects(:ssl_request).returns(generic_error_response).in_sequence(s)
+
+    assert response = @gateway.refund(@refund_amount, 'ch_test_charge', :refund_fee_amount => 100)
+    assert_failure response
   end
 
   def test_successful_verify