f00890c86174e68e24bdc41bb0b87efbdca8f511
Bruno Mattarollo <bruno.mattarollo@gmail.com>
2019-08-16 09:16:09 +1000

    An attempt to run tests against diff versions
    Modified the shell script and the pipeline to support parallel rails/ruby tests in AM, this tries to reproduce the behaviour seen in the `.travis.yml` and `circle.yml` using our Buildkite setup.
    
    Refs waysact/devops#5286

--- .buildkite/steps/run-tests.sh
+++ .buildkite/steps/run-tests.sh
@@ -2,4 +2,14 @@
 
 set -euo pipefail
 
-docker run -it --rm -v "$PWD":/usr/src -w /usr/src ruby:2.3.8-stretch sh -c "bundle ; bundle exec rake test"
+readonly RAILS_VERSIONS=(41 42 50 51)
+readonly RUBY_VERSIONS=(2.3.8 2.3.8 2.5 2.5)
+readonly CURRENT_JOB_IDX=`expr ${BUILDKITE_PARALLEL_JOB} + 1`
+readonly RAILS_VERSION=${RAILS_VERSIONS[$CURRENT_JOB_IDX]}
+readonly RUBY_VERSION=${RUBY_VERSIONS[$CURRENT_JOB_IDX]}
+
+echo ":llama: Testing with :ruby: ${RUBY_VERSION} | :rails: ${RAILS_VERSION}"
+
+docker run -it --rm -v "$PWD":/usr/src -w /usr/src ruby:${RUBY_VERSION}-stretch sh -c "bundle check --path=vendor/bundle_${RAILS_VERSION} \
+    --gemfile Gemfile.rails${RAILS_VERSION} || bundle install --jobs=4 --retry=3 --gemfile Gemfile.rails${RAILS_VERSION} --path=vendor/bundle_${RAILS_VERSION} ; \
+    BUNDLE_GEMFILE=Gemfile.rails${RAILS_VERSION} bundle exec rake test:units"
