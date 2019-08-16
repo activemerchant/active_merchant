3fbd0285d60cde4cfb56d36abd6e0b910f3b424e
Bruno Mattarollo <bruno.mattarollo@gmail.com>
2019-08-16 10:06:45 +1000

    Backport fix from upstream for CA_File path
    Refs waysact/devops#5286

--- lib/active_merchant/connection.rb
+++ lib/active_merchant/connection.rb
@@ -142,10 +142,8 @@ module ActiveMerchant
 
       if verify_peer
         http.verify_mode = OpenSSL::SSL::VERIFY_PEER
-        # Use OpenSSL's own certificate store instead of ActiveMerchant's
-        # from dxw/active_merchant#2
-        # http.ca_file     = ca_file
-        # http.ca_path     = ca_path
+        http.ca_file     = ca_file
+        http.ca_path     = ca_path
       else
         http.verify_mode = OpenSSL::SSL::VERIFY_NONE
       end
