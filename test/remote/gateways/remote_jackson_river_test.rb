95b1a1f03d44cd540ea209527962eec27a8ffca4
Franco Brusatti <fbrusatti@gmail.com>
2019-09-03 12:29:49 -0300

    Send ACH/EFT details to Jackson River gateway

--- test/remote/gateways/remote_jackson_river_test.rb
+++ test/remote/gateways/remote_jackson_river_test.rb
@@ -4,17 +4,36 @@ class RemoteJacksonRiverTest < Test::Unit::TestCase
   def setup
     @gateway = JacksonRiverGateway.new(fixtures(:jackson_river))
 
-    @amount = 100
+    @amount = 1000
     @credit_card = credit_card('4111111111111111')
+    @check = check({
+      account_type: ['Checking', 'Corporate', 'Corp Savings'].sample
+    })
     @options = {
       first_name: 'Longbob',
       last_name: 'Longsen',
       billing_address: address,
       description: 'Store Purchase',
-      form_id: 34467,
+      form_id: 34641,
       market_source: 'FooBar_MarketSource',
       canvasser_name: 'John Doe Canvasser'
     }
+  end
+
+  def test_successful_debit_purchase
+    response = @gateway.purchase(@amount, @check, @options)
+    assert_success response
+    assert response.test?
+    assert_equal 'Submission successful', response.message
+    assert_match %r(\d+), response.authorization
+  end
+
+  def test_failed_debit_purchase
+    response = @gateway.purchase(@amount, @check.except(:account_type), @options)
+    assert_failure response
+    assert_equal \
+      'accType::An illegal choice has been detected. Please contact the site administrator.', \
+      response.message
   end
 
   def test_successful_purchase
@@ -47,5 +66,4 @@ class RemoteJacksonRiverTest < Test::Unit::TestCase
     assert_scrubbed(@credit_card.number, transcript)
     assert_scrubbed(@gateway.options[:api_key], transcript)
   end
-
 end
