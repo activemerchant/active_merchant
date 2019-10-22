339581062d4b0c5a8f022f41cb0946db78794526
Jason Nappier <jason@spreedly.com>
2019-10-22 15:37:53 -0400

    Add diners_club, jcb, and maestro cards. Add template for other actions in their top-level methods.

--- lib/active_merchant/billing/gateways/ixopay.rb
+++ lib/active_merchant/billing/gateways/ixopay.rb
@@ -6,7 +6,7 @@ module ActiveMerchant #:nodoc:
 
       self.supported_countries = %w(AO AQ AR AS AT AU AW AX AZ BA BB BD BE BF BG BH BI BJ BL BM BN BO BQ BQ BR BS BT BV BW BY BZ CA CC CD CF CG CH CI CK CL CM CN CO CR CU CV CW CX CY CZ DE DJ DK DM DO DZ EC EE EG EH ER ES ET FI FJ FK FM FO FR GA GB GD GE GF GG GH GI GL GM GN GP GQ GR GS GT GU GW GY HK HM HN HR HT HU ID IE IL IM IN IO IQ IR IS IT JE JM JO JP KE KG KH KI KM KN KP KR KW KY KZ LA LB LC LI LK LR LS LT LU LV LY MA MC MD ME MF MG MH MK ML MM MN MO MP MQ MR MS MT MU MV MW MX MY MZ NA NC NE NF NG NI NL NO NP NR NU NZ OM PA PE PF PG PH PK PL PM PN PR PS PT PW PY QA RE RO RS RU RW SA SB SC SD SE SG SH SI SJ SK SL SM SN SO SR SS ST SV SX SY SZ TC TD TF TG TH TJ TK TL TM TN TO TR TT TV TW TZ UA UG UM US UY UZ VA VC VE VG VI VN VU WF WS YE YT ZA ZM ZW)
       self.default_currency = 'EUR'
-      self.supported_cardtypes = [:visa, :master, :american_express, :discover]
+      self.supported_cardtypes = [:visa, :master, :american_express, :discover, :diners_club, :jcb, :maestro]
 
       self.homepage_url = 'https://www.ixopay.com'
       self.display_name = 'Ixopay'
@@ -23,18 +23,22 @@ module ActiveMerchant #:nodoc:
 
       def authorize(money, payment_method, options={})
         # todo
+        # commit('authorize', build_authorize_request(money, payment_method, options), options)
       end
 
       def capture(money, authorization, options={})
         # todo
+        # commit('capture', build_capture_request(money, payment_method, options), options)
       end
 
       def refund(money, authorization, options={})
         # todo
+        # commit('refund', build_refund_request(money, payment_method, options), options)
       end
 
       def void(authorization, options={})
         # todo
+        # commit('void', build_void_request(money, payment_method, options), options)
       end
 
       def verify(credit_card, options={})
