765f0329a0b3510aeeef34e03877e105c76a1c44
Jason Nappier <jason@spreedly.com>
2019-10-22 17:00:14 -0400

    Replace xml tag! calls

--- lib/active_merchant/billing/gateways/ixopay.rb
+++ lib/active_merchant/billing/gateways/ixopay.rb
@@ -107,86 +107,85 @@ module ActiveMerchant #:nodoc:
       def build_purchase_request(money, payment_method, options)
         xml = Builder::XmlMarkup.new(indent: 2)
 
-        xml.instruct! :xml
+        xml.transactionWithCard 'xmlns' => 'http://secure.ixopay.com/Schema/V2/TransactionWithCard' do
+          xml.username @options[:username]
+          xml.password Digest::SHA1.hexdigest(@options[:password])
 
-        xml.tag! 'transactionWithCard', 'xmlns' => 'http://secure.ixopay.com/Schema/V2/TransactionWithCard' do
-          xml.tag! 'username', @options[:username]
-          xml.tag! 'password', Digest::SHA1.hexdigest(@options[:password])
           add_card_data(xml, payment_method)
           add_debit(xml, money, options)
         end
-
-        xml.target!
       end
 
       def add_card_data(xml, payment_method)
-        xml.tag! 'cardData' do
-          xml.tag! 'cardHolder', payment_method.name
-          xml.tag! 'pan', payment_method.number
-          xml.tag! 'cvv', payment_method.verification_value
-          xml.tag! 'expirationMonth', format(payment_method.month, :two_digits)
-          xml.tag! 'expirationYear', format(payment_method.year, :four_digits)
+        xml.cardData do
+          xml.cardHolder      payment_method.name
+          xml.pan             payment_method.number
+          xml.cvv             payment_method.verification_value
+          xml.expirationMonth format(payment_method.month, :two_digits)
+          xml.expirationYear  format(payment_method.year, :four_digits)
         end
       end
 
       def add_debit(xml, money, options)
-        currency = options[:currency] || currency(money)
+        currency    = options[:currency] || currency(money)
         description = options[:description].blank? ? 'Purchase' : options[:description]
 
-        xml.tag! 'debit' do
-          xml.tag! 'transactionId', new_transaction_id
+        xml.debit do
+          xml.transactionId new_transaction_id
+
           add_customer_data(xml, options)
-          xml.tag! 'amount', money
-          xml.tag! 'currency', currency
-          xml.tag! 'description', description
-          xml.tag! 'callbackUrl', options[:callback_url] || 'http://example.com'
+
+          xml.amount      money
+          xml.currency    currency
+          xml.description description
+          xml.callbackUrl(options[:callback_url] || 'http://example.com')
         end
       end
 
       def add_customer_data(xml, options)
         # Ixopay returns an error if the elements are not added in the order used here.
-        xml.tag! 'customer' do
+        xml.customer do
           add_billing_address(xml,  options[:billing_address])  if options[:billing_address]
           add_shipping_address(xml, options[:shipping_address]) if options[:shipping_address]
 
           if options.dig(:billing_address, :company)
-            xml.tag! 'company', options[:billing_address][:company]
+            xml.company options[:billing_address][:company]
           end
 
-          xml.tag! 'email', options[:email]
-          xml.tag! 'ipAddress', options[:ip] || '127.0.0.1'
+          xml.email options[:email]
+          xml.ipAddress(options[:ip] || '127.0.0.1')
         end
       end
 
       def add_billing_address(xml, address)
         if address[:name]
-          xml.tag! 'firstName', split_names(address[:name])[0]
-          xml.tag! 'lastName',  split_names(address[:name])[1]
+          xml.firstName split_names(address[:name])[0]
+          xml.lastName  split_names(address[:name])[1]
         end
 
-        xml.tag! 'billingAddress1', address[:address1]
-        xml.tag! 'billingAddress2', address[:address2]
-        xml.tag! 'billingCity',     address[:city]
-        xml.tag! 'billingPostcode', address[:zip]
-        xml.tag! 'billingState',    address[:state]
-        xml.tag! 'billingCountry',  address[:country]
-        xml.tag! 'billingPhone',    address[:phone]
+        xml.billingAddress1 address[:address1]
+        xml.billingAddress2 address[:address2]
+        xml.billingCity     address[:city]
+        xml.billingPostcode address[:zip]
+        xml.billingState    address[:state]
+        xml.billingCountry  address[:country]
+        xml.billingPhone    address[:phone]
       end
 
       def add_shipping_address(xml, address)
         if address[:name]
-          xml.tag! 'shippingFirstName', split_names(address[:name])[0]
-          xml.tag! 'shippingLastName',  split_names(address[:name])[1]
+          xml.shippingFirstName split_names(address[:name])[0]
+          xml.shippingLastName  split_names(address[:name])[1]
         end
 
-        xml.tag! 'shippingCompany',   address[:company]
-        xml.tag! 'shippingAddress1',  address[:address1]
-        xml.tag! 'shippingAddress2',  address[:address2]
-        xml.tag! 'shippingCity',      address[:city]
-        xml.tag! 'shippingPostcode',  address[:zip]
-        xml.tag! 'shippingState',     address[:state]
-        xml.tag! 'shippingCountry',   address[:country]
-        xml.tag! 'shippingPhone',     address[:phone]
+        xml.shippingCompany   address[:company]
+        xml.shippingAddress1  address[:address1]
+        xml.shippingAddress2  address[:address2]
+        xml.shippingCity      address[:city]
+        xml.shippingPostcode  address[:zip]
+        xml.shippingState     address[:state]
+        xml.shippingCountry   address[:country]
+        xml.shippingPhone     address[:phone]
       end
 
       def new_transaction_id
