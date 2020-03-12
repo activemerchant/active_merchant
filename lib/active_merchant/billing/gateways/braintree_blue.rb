958435cac93df0cee5f072044db9df76e20652c5
Ola Pohorecka Mrożek <43703374+pohor@users.noreply.github.com>
2020-03-12 09:19:54 +0100

    [feature] enable updates for apple pay cards (#94)
    Enables updating apple pay card on Braintree customer to a new one.
    The customer.paypal_accounts.each { |pp| @braintree_gateway.paypal_account.delete(pp.token) } method has not been tested in neither this fork nor master. The method added in this PR is tested via Chargify tests.

--- lib/active_merchant/billing/gateways/braintree_blue.rb
+++ lib/active_merchant/billing/gateways/braintree_blue.rb
@@ -315,6 +315,7 @@ module ActiveMerchant #:nodoc:
 
           # Delete the customers existing payment methods
           customer.paypal_accounts.each { |pp| @braintree_gateway.paypal_account.delete(pp.token) }
+          customer.apple_pay_cards.each { |pp| @braintree_gateway.payment_method.delete(pp.token) }
 
           # Update it with the new nonce
           parameters = {
