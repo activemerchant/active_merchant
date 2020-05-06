52e6401fabccd2b9ec8ca7fee5e565b26fd653df
jacoobb <mikrut.jakub@gmail.com>
2020-05-06 11:58:15 +0200

    add verify method to bogus (#104)
    Why:
    Moneris wrote:
    
    Please note that you CANNOT store CVD value. The example you provided is for adding a cc into the vault.
    
    I am not sure if you are aware, there is a new mandate that is coming into effect on April 2020. It is called Credential on File . What this mandate requires is that merchants who transact using stored cards need to send additional fields ( cof object) to indicate if the card is being stored and whether it is an initial transaction or subsequent transaction. In this scenario, the bank will decline due to the lack of cof fields and initial transaction should have cvv values as well.
    
    The initial transaction should contain CVV value but you will never store the value. So in your subsequent transactions, your will not be providing the CVV value since the card info provided are stored credentials and cvv cannot be stored. The flow is always like this – Initial >Storage> Subsequent
    If you have stored cards in your vault prior to the mandate, they are considered as legacy cards and the process to bring them up to mandate does not require you to provide CVV and your COF object will have the values ( 2, U, null).
    
    What:
    Added Card Verification method for tests.
    
    For CVV Disable process looks the same like for CVV Enabled but without CVD params for Card Verification
    
    For CVV Enabled process:
    
    Card Verification with CVD and Credential on File (like a first transaction)
    "Merchants can use Card Verification with COF AVS and CVD as the first transaction prior to storing the credentials instead of Purchase or Preauthorization." source: developer.moneris
    I send CC details.
    I send CVV and I get CVV acceptance
    I send Credential on File and I get IssuerId
    Vault Add Credit Card source: developer.moneris
    
    Purchase with Vault and Credential on File source developer.moneris

--- lib/active_merchant/billing/gateways/bogus.rb
+++ lib/active_merchant/billing/gateways/bogus.rb
@@ -90,6 +90,17 @@ module ActiveMerchant #:nodoc:
         end
       end
 
+      def verify(paysource, _options = {})
+        case normalize(paysource)
+        when /1$/
+          Response.new(true, SUCCESS_MESSAGE, {:billingid => '1'}, :test => true, :authorization => AUTHORIZATION)
+        when /2$/
+          Response.new(false, FAILURE_MESSAGE, {:billingid => nil, :error => FAILURE_MESSAGE }, :test => true, :error_code => STANDARD_ERROR_CODE[:processing_error])
+        else
+          raise Error, error_message(paysource)
+        end
+      end
+
       def store(paysource, options = {})
         case normalize(paysource)
         when /1$/
