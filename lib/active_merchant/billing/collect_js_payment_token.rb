d8e299bbfb18ae27d6b42de425471c5501f8f35b
David García Martínez <dgarcia@trileucosolutions.com>
2020-06-19 10:51:50 +0200

    Fix rubocop warning

--- lib/active_merchant/billing/collect_js_payment_token.rb
+++ lib/active_merchant/billing/collect_js_payment_token.rb
@@ -2,7 +2,7 @@ module ActiveMerchant
   module Billing
     class CollectJsPaymentToken
       # This is a representation of a token generated with the Collect.js framework as described here:
-      #https://secure.networkmerchants.com/gw/merchants/resources/integration/integration_portal.php#cjs_methodology
+      # https://secure.networkmerchants.com/gw/merchants/resources/integration/integration_portal.php#cjs_methodology
       attr_reader :payment_data
 
       def initialize(payment_token)
