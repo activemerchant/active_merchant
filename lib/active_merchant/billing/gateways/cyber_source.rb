cb4947a8284abe7246970905d956e3c4a552b261
britth <brittany@spreedly.com>
2020-06-23 09:56:35 -0400

    Cybersource: Update supported countries list
    Adds UAE to supported countries list
    
    Remote:
    80 tests, 403 assertions, 5 failures, 0 errors, 0 pendings, 0 omissions, 0 notifications
    93.75% passed (5 known unrelated failures)
    
    Unit:
    86 tests, 402 assertions, 0 failures, 0 errors, 0 pendings, 0 omissions, 0 notifications
    100% passed

--- lib/active_merchant/billing/gateways/cyber_source.rb
+++ lib/active_merchant/billing/gateways/cyber_source.rb
@@ -37,7 +37,7 @@ module ActiveMerchant #:nodoc:
       DEFAULT_COLLECTION_INDICATOR = 2
 
       self.supported_cardtypes = %i[visa master american_express discover diners_club jcb dankort maestro elo]
-      self.supported_countries = %w(US BR CA CN DK FI FR DE IN JP MX NO SE GB SG LB PK)
+      self.supported_countries = %w(US AE BR CA CN DK FI FR DE IN JP MX NO SE GB SG LB PK)
 
       self.default_currency = 'USD'
       self.currencies_without_fractions = %w(JPY)
