fe49b7c9582fab6bf5507780bfcf4310a9db0588
molbrown <molly@spreedly.com>
2020-08-19 10:20:15 -0400

    Openpay: Add Colombia to supported countries
    ECS-1278

--- lib/active_merchant/billing/gateways/openpay.rb
+++ lib/active_merchant/billing/gateways/openpay.rb
@@ -4,7 +4,7 @@ module ActiveMerchant #:nodoc:
       self.live_url = 'https://api.openpay.mx/v1/'
       self.test_url = 'https://sandbox-api.openpay.mx/v1/'
 
-      self.supported_countries = ['MX']
+      self.supported_countries = %w(CO MX)
       self.supported_cardtypes = %i[visa master american_express carnet]
       self.homepage_url = 'http://www.openpay.mx/'
       self.display_name = 'Openpay'
