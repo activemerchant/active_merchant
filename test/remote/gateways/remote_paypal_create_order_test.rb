99700536ce53b34de54036cd30248e46706c101b
Ali Hassan Mirza <ali.hassan.mirzaa@gmail.com>
2020-09-08 00:04:31 -0700

    Updated test case params on sending request to create ORDER

--- test/remote/gateways/remote_paypal_create_order_test.rb
+++ test/remote/gateways/remote_paypal_create_order_test.rb
@@ -18,16 +18,96 @@ class PaypalExpressRestTest < Test::Unit::TestCase
 
     @body = {
         "intent": "CAPTURE",
-        "final_capture": "true",
         "purchase_units": [
             {
+                "reference_id": "camera_shop_seller_#{DateTime.now}",
+                "description": "Camera Shop",
                 "amount": {
                     "currency_code": "USD",
-                    "value": "100.00"
-                }
+                    "value": "12.00",
+                    "breakdown": {
+                        "item_total": {
+                            "currency_code": "USD",
+                            "value": "12.00"
+                        },
+                        "shipping": {
+                            "currency_code": "USD",
+                            "value": "0"
+                        },
+                        "handling": {
+                            "currency_code": "USD",
+                            "value": "0"
+                        },
+                        "tax_total": {
+                            "currency_code": "USD",
+                            "value": "0"
+                        },
+                        "gift_wrap": {
+                            "currency_code": "USD",
+                            "value": "0"
+                        },
+                        "shipping_discount": {
+                            "currency_code": "USD",
+                            "value": "0"
+                        }
+                    }
+                },
+                "payee": {
+                    "email_address": "sb-jnxjj3033194@business.example.com"
+                },
+                "items": [
+                    {
+                        "name": "Levis 501 Selvedge STF",
+                        "sku": "5158936",
+                        "unit_amount": {
+                            "currency_code": "USD",
+                            "value": "12.00"
+                        },
+                        "tax": {
+                            "currency_code": "USD",
+                            "value": "0.00"
+                        },
+                        "quantity": "1",
+                        "category": "PHYSICAL_GOODS"
+                    }
+                ],
+                "shipping": {
+                    "address": {
+                        "address_line_1": "123 Townsend St",
+                        "address_line_2": "Floor 6",
+                        "admin_area_2": "San Francisco",
+                        "admin_area_1": "CA",
+                        "postal_code": "94107",
+                        "country_code": "US"
+                    }
+                },
+                "shipping_method": "United Postal Service",
+                "payment_instruction": {
+                    "disbursement_mode": "INSTANT",
+                    "platform_fees": [
+                        {
+                            "amount": {
+                                "currency_code": "USD",
+                                "value": "2.00"
+                            },
+                            "payee": {
+                                "email_address": "sb-jnxjj3033194@business.example.com"
+                            }
+                        }
+                    ]
+                },
+                "payment_group_id": 1,
+                "custom_id": "custom_value_#{DateTime.now}",
+                "invoice_id": "invoice_number_#{DateTime.now}",
+                "soft_descriptor": "Payment Camera Shop"
             }
-        ]
+        ],
+        "application_context": {
+            "return_url": "https://google.com",
+            "cancel_url": "https://google.com"
+        }
     }
+
     @options = { headers: @headers, body: @body }
   end
 
@@ -35,6 +115,7 @@ class PaypalExpressRestTest < Test::Unit::TestCase
   def test_set_customer_order_creation
     paypal_customer = ActiveMerchant::Billing::PaypalCustomerGateway.new
     response = paypal_customer.create_order(@options)
+    debugger
     assert response.success?
     assert response.parsed_response["status"].eql?("CREATED")
     assert !response.parsed_response["id"].nil?
