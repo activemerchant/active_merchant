c53aa8b8f961a9767f4f2db91a2be283fd90d5bd
Mo Glatz <meagabeth@icloud.com>
2021-01-26 16:52:45 -0500

    Decidir: Improve error mapping
    - Revise the `error_code` to include the numerical `id` associated with the `reason` that was included in the `error_code`, when available
    - Update remote `test_invalid_login` tests based on updated response.message we receive back from Decidir gateway
    
    CE-776
    
    All Local:
    4616 tests, 72969 assertions, 0 failures, 0 errors, 0 pendings, 0 omissions, 0 notifications
    100% passed
    Unit:
    35 tests, 170 assertions, 0 failures, 0 errors, 0 pendings, 0 omissions, 0 notifications
    100% passed
    Remote:
    23 tests, 80 assertions, 0 failures, 0 errors, 0 pendings, 0 omissions, 0 notifications
    100% passed

--- test/unit/gateways/decidir_test.rb
+++ test/unit/gateways/decidir_test.rb
@@ -153,6 +153,14 @@ class DecidirTest < Test::Unit::TestCase
     assert_match 'invalid_request_error | invalid_param | payment_type', response.error_code
   end
 
+  def test_failed_purchase_error_response_with_error_code
+    @gateway_for_purchase.expects(:ssl_request).returns(error_response_with_error_code)
+
+    response = @gateway_for_purchase.purchase(@amount, @credit_card, @options)
+    assert_failure response
+    assert_match '14, invalid_number', response.error_code
+  end
+
   def test_successful_authorize
     @gateway_for_auth.expects(:ssl_request).returns(successful_authorize_response)
 
@@ -534,6 +542,12 @@ class DecidirTest < Test::Unit::TestCase
   def unique_error_response
     %{
       {\"error\":{\"error_type\":\"invalid_request_error\",\"validation_errors\":[{\"code\":\"invalid_param\",\"param\":\"payment_type\"}]}}
+    }
+  end
+
+  def error_response_with_error_code
+    %{
+      {\"error\":{\"type\":\"invalid_number\",\"reason\":{\"id\":14,\"description\":\"TARJETA INVALIDA\",\"additional_description\":\"\"}}}
     }
   end
 end
