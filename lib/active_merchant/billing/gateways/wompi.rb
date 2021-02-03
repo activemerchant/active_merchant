2e9fea269ea77dd553468b5a255bdc4d438c43a3
Franco Brusatti <fbrusatti@gmail.com>
2021-02-03 10:45:31 -0300

    Implementation of Wompi Gateway. Ref waysact/evergiving#6379

---
+++ lib/active_merchant/billing/gateways/wompi.rb
@@ -0,0 +1,246 @@
+module ActiveMerchant #:nodoc:
+  module Billing #:nodoc:
+    class WompiGateway < Gateway
+      self.test_url = 'https://sandbox.wompi.co/v1'
+      self.live_url = 'https://production.wompi.co/v1'
+
+      self.supported_countries = ['CO']
+      self.default_currency = 'COP'
+      self.supported_cardtypes = [:visa, :master, :american_express, :discover]
+
+      self.homepage_url = 'https://wompi.co/'
+      self.display_name = 'Wompi'
+
+      self.money_format = :cents
+
+      PAYMENT_SOURCE_TYPES = %w[ CARD NEQUI ]
+
+      def initialize(options={})
+        requires!(options, :public_key, :private_key)
+
+        super
+      end
+
+      def store(payment_method, options={})
+        post = {}
+
+        post[:number] = payment_method.number
+        post[:cvc] = payment_method.verification_value
+        post[:exp_month] = format(payment_method.month, :two_digits)
+        post[:exp_year] = format(payment_method.year, :two_digits)
+        post[:card_holder] = payment_method.name
+
+        commit(:post, '/tokens/cards', post)
+      end
+
+      def purchase(money, payment, options={})
+        post = {}
+        post[:acceptance_token] = acceptance_token
+
+        add_invoice(post, money, options)
+        add_payment(post, payment, options)
+        add_address(post, payment, options)
+        add_customer_data(post, options)
+
+        commit(:post, '/transactions', post)
+      end
+
+      # Lookup transaction using reference or using transaction id path parans
+      #
+      def query_transaction(reference, options={})
+        if options[:path_params] && reference.present?
+          commit(:get, "/transactions/#{reference}")
+        else
+          commit(:get, "/transactions?reference=#{reference}")
+        end
+      end
+
+      def pse_financial_institutions
+        commit(:get, "/pse/financial_institutions")
+      end
+
+      def supports_scrubbing?
+        true
+      end
+
+      def scrub(transcript)
+        transcript.
+          gsub(/(Authorization: Bearer )([A-Za-z0-9\-\._~\+\/]+=*)/, '\1[FILTERED]').
+          gsub(/(\\?\\?\\?"number\\?\\?\\?":\\?\\?\\?")\d+/, '\1[FILTERED]').
+          gsub(/(\\?\\?\\?"cvc\\?\\?\\?":\\?\\?\\?"?)\d+/, '\1[FILTERED]')
+      end
+
+      private
+
+      def add_customer_data(post, options)
+        post[:customer_data] = {}
+        post[:customer_data][:phone_number] = options[:customer]&.[](:mob_phone)
+        post[:customer_data][:full_name] = options[:customer]&.[](:full_name)
+        post[:customer_email] = options[:customer]&.[](:email)
+        post[:redirect_url] = options[:redirect_url]
+      end
+
+      def add_address(post, creditcard, options)
+        post[:shipping_address] = add_address_data(options)
+      end
+
+      def add_address_data(options)
+        address = {}
+
+        if options[:address].present?
+          address[:address_line_1] = options[:address][:address1]
+          address[:address_line_2] = options[:address][:address2]
+          address[:city] = options[:address][:city]
+          address[:region] = options[:address][:state]
+          address[:name] = options[:customer]&.[](:full_name)
+          address[:phone_number] = options[:customer]&.[](:mob_phone)
+          address[:postal_code] = options[:address][:postal_code]
+          address[:country] = options[:address][:country]
+        end
+
+        address
+      end
+
+      def add_invoice(post, money, options)
+        post[:amount_in_cents] = amount(money).to_i
+        post[:currency] = (options[:currency] || currency(money))
+        post[:reference] = options[:reference]
+      end
+
+      def add_payment(post, payment, options)
+        post[:payment_method] = {}
+        post[:payment_method][:type] = "CARD"
+        post[:payment_method][:token] = options[:token]
+        post[:payment_method][:installments] = options[:installments] || 1
+      end
+
+      def parse(body)
+        JSON.parse(body)
+      end
+
+      def headers(action)
+        key = reference_in_params?(action) ? :private_key : :public_key
+
+        {
+          'accept' => '*/*',
+          'Content-Type' => 'application/json',
+          'Authorization' => "Bearer #{@options.send(:[], key)}"
+        }
+      end
+
+      def reference_in_params?(action)
+        action.match(/\A?reference=[^&]+&*/).present?
+      end
+
+      def commit(verb, action, parameters={})
+        endpoint = url + action
+
+        begin
+          params = post_data(action, parameters)
+          response = parse(ssl_request(verb, endpoint, params, headers(action)))
+        rescue ResponseError => e
+          response = parse(e.response.body)
+        end
+
+        success = success_from(action, response)
+
+        Response.new(
+          success,
+          message_from(success, action, response),
+          response,
+          authorization: authorization_from(action, response),
+          test: test?,
+          error_code: error_code_from(success, response)
+        )
+      end
+
+      def success_from(action, response)
+        case action
+        when '/tokens/cards'
+          response['status'] == 'CREATED'
+
+        when /\/merchants\/pub_(test|prod)_.+/
+          response['data']['presigned_acceptance']['acceptance_token']
+
+        when '/transactions'
+          data = response['data']
+          response_data = data.is_a?(Array) ? data.first : data
+          [ 'PENDING', 'APPROVED' ].include?(response_data&.[]('status'))
+
+        when '/pse/financial_institutions'
+          response['data'].is_a?(Array) && !response['data'].empty?
+        end
+      end
+
+      def message_from(success, endpoint, response)
+        case endpoint
+        when '/tokens/cards'
+          if success
+            response.fetch('status')
+          else
+            response
+              .dig('error', 'messages').map { |k, v| "#{k}: #{v.join}" }
+              .join
+          end
+
+        when /\/merchants\/pub_(test|prod)_\w+/
+          response.dig('data', 'presigned_acceptance', 'acceptance_token')
+
+        when '/pse/financial_institutions'
+          'APPROVED' if response['data'].is_a?(Array) && !response['data'].empty?
+
+        else
+          data = response['data']
+          response_data = data.is_a?(Array) ? data.first : data
+
+          response_data&.[]('status')
+        end
+      end
+
+      def authorization_from(endpoint, response)
+        data = response['data']
+        response_data = data.is_a?(Array) ? data.first : data
+
+        response_data&.[]('id')
+      end
+
+      def post_data(action, parameters = {})
+        parameters.empty? ? nil : parameters.to_json
+      end
+
+      def error_code_from(success, response)
+        unless success
+          error_type = response['error']&.[]('type')
+
+          data = response['data']
+          response_data = data.is_a?(Array) ? data.first : data
+
+          error_type || response_data&.[]('status')
+        end
+      end
+
+      def url
+        test? ? test_url : live_url
+      end
+
+      # This is a previous query before each query needed to get a bearer token
+      # https://docs.wompi.co/docs/en/tokens-de-aceptacion
+      def acceptance_token
+        action = "/merchants/#{@options[:public_key]}"
+        endpoint = url + action
+
+        response = parse(ssl_get(endpoint, headers(action)))
+
+        get_token(success_from(action, response), response)
+      end
+
+      def get_token(success, response)
+        if success
+          response.dig('data', 'presigned_acceptance', 'acceptance_token')
+        else
+          raise "Failed authorization: #{raw_response}"
+        end
+      end
+    end
+  end
+end
---
+++ test/remote/gateways/remote_wompi_test.rb
@@ -0,0 +1,85 @@
+require 'test_helper'
+
+class RemoteWompiTest < Test::Unit::TestCase
+  def setup
+    @gateway = WompiGateway.new(fixtures(:wompi))
+
+    @amount = 1000000
+    @credit_card = credit_card('4242424242424242')
+    @declined_card = credit_card('4111111111111111')
+    @options = {
+      address: address({ country: 'CO' }),
+      description: 'Store Purchase',
+      reference: SecureRandom.uuid,
+      currency: 'COP',
+      customer: {
+        email: "john.smith@test.com",
+        full_name: 'John smith',
+        mob_phone: '08032000001'
+      }
+    }
+  end
+
+  def test_successful_store
+    response = @gateway.store(@credit_card, @options)
+    assert_success response
+    assert_equal 'CREATED', response.message
+    assert_match /tok_test_\d{4}_[a-zA-Z0-9]{32}/, response.authorization
+  end
+
+  def test_fail_store
+    response = @gateway.store(credit_card('4'), @options)
+    assert_failure response
+    assert_equal "number: debe coincidir con el patron \"^\\d{12,19}$\"", response.message
+  end
+
+  def test_pending_purchase
+    tokenizations_response = @gateway.store(@credit_card, @options)
+    options = @options.merge(token: tokenizations_response.authorization)
+    response = @gateway.purchase(@amount, @credit_card, options)
+
+    assert_success response
+    assert_equal 'PENDING', response.message
+  end
+
+  def test_failed_purchase
+    tokenizations_response = @gateway.store(@declined_card, @options)
+    options = @options.merge(token: tokenizations_response.authorization)
+
+    purchase_response = @gateway.purchase(@amount, @declined_card, options)
+    assert_success purchase_response
+
+    response = @gateway.query_transaction(purchase_response.params['data']['reference'])
+    assert_equal 'DECLINED', response.message
+  end
+
+  def test_approved_purchase
+    tokenizations_response = @gateway.store(@credit_card, @options)
+    options = @options.merge(token: tokenizations_response.authorization)
+    purchase_response = @gateway.purchase(@amount, @credit_card, options)
+
+    response = @gateway.query_transaction(purchase_response.params['data']['reference'])
+
+    assert_equal 'APPROVED', response.message
+  end
+
+  def test_financial_institutions
+    response = @gateway.pse_financial_institutions
+
+    assert_equal 'APPROVED', response.message
+  end
+
+  def test_transcript_scrubbing
+    transcript = capture_transcript(@gateway) do
+      tokenizations_response = @gateway.store(@credit_card, @options)
+      options = @options.merge(token: tokenizations_response.authorization)
+      response = @gateway.purchase(@amount, @credit_card, options)
+    end
+
+    transcript = @gateway.scrub(transcript)
+
+    assert_scrubbed(@credit_card.number, transcript)
+    assert_scrubbed(/(\\?\\?\\?"cvv\\?\\?\\?":\\?\\?\\?"?)#{@credit_card.verification_value}+/, transcript)
+    assert_scrubbed(@gateway.options[:private_key], transcript)
+  end
+end
