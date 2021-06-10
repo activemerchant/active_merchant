3f1fc3c5edd3c6315744813d076c5d5aec310ae1
simahero <zoilei7@gmail.com>
2021-06-10 19:00:20 +0200

    Code clean up, documentation. / oneClick implementation.

--- SimplePay/test.rb
+++ SimplePay/test.rb
@@ -3,13 +3,17 @@ require '../lib/active_merchant/billing/gateways/simple_pay.rb'
 
 ActiveMerchant::Billing::Base.mode = :test
 
-gateway = ActiveMerchant::Billing::SimplePayGateway.new(
+gateway = ActiveMerchant::Billing::SimplePayGateway.new({
     :merchantID  => 'PUBLICTESTHUF',
     :merchantKEY => 'FxDa5w314kLlNseq2sKuVwaqZshZT5d6',
     :redirectURL => 'https://127.0.0.1',
     :timeout     => 30,
     :returnRequest => true
-)
+})
+
+puts ActiveMerchant::Billing::SimplePayGateway.allowed_ip
+
+puts ActiveMerchant::Billing::SimplePayGateway.utilbackref('https://sdk.simplepay.hu/back.php?r=eyJyIjowLCJ0Ijo5OTg0NDk0MiwiZSI6IlNVQ0NFU1MiLCJtIjoiUFVCTElDVEVTVEhVRiIsIm8iOiIxMDEwMTA1MTU2ODAyOTI0ODI2MDAifQ%3D%3D&s=El%2Fnvex9TjgjuORI63gEu5I5miGo4CSAD5lmEpKIxp7WuVRq6bBeh1QdyEvVGSsi')
 
 ## Possible URLS
 #:urls => {
@@ -20,8 +24,60 @@ gateway = ActiveMerchant::Billing::SimplePayGateway.new(
 #}
 
 res = gateway.purchase({
+    :orderRef => 'forthequerymethod',
     :amount => 2000,
     :email => 'email@email.hu',
+    :address => {
+        :name =>  'myname',
+        :company => 'company',
+        :country => 'HU',
+        :state => 'Budapest',
+        :city => 'Budapest',
+        :zip => '1111',
+        :address1 => 'Address u.1',
+        :address2 => 'Address u.2',
+        :phone => '06301111111'
+    },
+    :items => [
+        {
+        :ref => "Product ID 2",
+        :title => "Product name 2",
+        :description => "Product description 2",
+        :amount => "2",
+        :price => "5",
+        :tax => "0"
+        }
+    ],
+    :threeDSReqAuthMethod => '01',
+    :maySelectEmail => true,
+    :maySelectInvoice => true,
+    :maySelectDelivery => ["HU","AT","DE"]
+})
+
+# :delivery => [
+#     {
+#     :name => "SimplePay V2 Tester",
+#     :company => "Company name",
+#     :country => "hu",
+#     :state => "Budapest",
+#     :city => "Budapest",
+#     :zip => "1111",
+#     :address => "Delivery address",
+#     :address2 => "",
+#     :phone => "06203164978"
+#     }
+# ],
+
+puts "\n\n"
+puts res.success?
+puts res.message
+puts "\n\n"
+puts "-----------------------------------------"
+
+res = gateway.purchase({
+    :amount => 2000,
+    :email => 'email@email.hu',
+    :cardSecret => 'thesuperdupersecret',
     :address => {
         :name =>  'myname',
         :company => 'company',
@@ -41,7 +97,8 @@ puts res.message
 puts "\n\n"
 puts "-----------------------------------------"
 
-res = gateway.purchase({
+res = gateway.authorize({
+    :orderRef => 'authorderref',
     :amount => 2000,
     :email => 'email@email.hu',
     :address => {
@@ -54,96 +111,83 @@ res = gateway.purchase({
         :address1 => 'Address u.1',
         :address2 => 'Address u.2',
         :phone => '06301111111'
-    },
+    }
 })
 
+puts "\n\n"
+puts res.success?
 puts res.message
+puts "\n\n"
+puts "-----------------------------------------"
 
-# res = gateway.authorize({
-#     :ammount => 2000,
-#     :email => 'email@email.hu',
-#     :threeDSReqAuthMethod => '01', #???
-#     :address => {
-#         :name =>  'myname',
-#         :company => 'company',
-#         :country => 'HU',
-#         :state => 'Budapest',
-#         :city => 'Budapest',
-#         :zip => '1111',
-#         :address1 => 'Address u.1',
-#         :address2 => 'Address u.2',
-#         :phone => '06301111111'
-#     },
-#     :items => [     #optional
-#         {
-#         :ref => "Product ID 2",
-#         :title => "Product name 2",
-#         :description => "Product description 2",
-#         :amount => "2",
-#         :price => "5",
-#         :tax => "0"
-#         }
-#     ]
-# })
-# res = gateway.capture({
-#     :orderRef = 'someRef',
-#     :originalTotal => 2000,
-#     :approveTotal => 1800
-# })
-# res = gateway.refund({
-#     :orderRef = 'someRef',
-#     :refundTotal => 2000
-# })
-# res = gateway.query({
-#     :transactionIds = ['id1', 'id2'],
-#     :detailed = true,   #optional
-#     :refunds = true     #optional
-# })
+res = gateway.capture({
+    :orderRef => 'authorderref',
+    :originalTotal => 2000,
+    :approveTotal => 1800
+})
 
-# credit_card = ActiveMerchant::Billing::CreditCard.new(
-#   :number     => '4908366099900425',
-#   :month      => '10',
-#   :year       => '2021',
-#   :first_name => 'v2 AUTO',
-#   :last_name  => 'Tester',
-#   :verification_value  => '579'
-# )
-
-# res = gateway.auto({
-#     :credit_card => credit_card,
-#     :ammount => 2000,
-#     :email => 'email@email.hu',
-#     :threeDS => {
-#         :threeDSReqAuthMethod => '01', #???
-#         :threeDSReqAuthType => 'CIT', #??? W: CIT WO:MIT rec : REC
-#         :browser => {   #IF CIT
-#             :accept => '',
-#             :agent => '',
-#             :ip => '127.0.01',
-#             :java => 'navigator.javaEnabled()',
-#             :lang => 'navigator.language',
-#             :color => 'screen.colorDepth',
-#             :height => 'screen.height',
-#             :width => 'screen.width',
-#             :tz => ' new Date().getTimezoneOffset()',
-#         },
-#         :address => {
-#             :name =>  'myname',
-#             :company => 'company',
-#             :country => 'HU',
-#             :state => 'Budapest',
-#             :city => 'Budapest',
-#             :zip => '1111',
-#             :address1 => 'Address u.1',
-#             :address2 => 'Address u.2',
-#             :phone => '06301111111'
-#         }
-#     },
-#     :threeDSExternal => {
-#         :xid => "01234567980123456789",
-#         :eci => "01",
-#         :cavv => "ABCDEF"
-#     }
-# })
-
+puts "\n\n"
+puts res.success?
 puts res.message
+puts "\n\n"
+puts "-----------------------------------------"
+
+res = gateway.refund({
+    :orderRef => 'authorderref',
+    :refundTotal => 200
+})
+
+puts "\n\n"
+puts res.success?
+puts res.message
+puts "\n\n"
+puts "-----------------------------------------"
+
+
+## OR orderRefs
+res = gateway.query({
+    :transactionIds => ['501167933'],
+})
+
+puts "\n\n"
+puts res.success?
+puts res.message
+puts "\n\n"
+puts "-----------------------------------------"
+
+credit_card = ActiveMerchant::Billing::CreditCard.new(
+  :number     => '4908366099900425',
+  :month      => '10',
+  :year       => '2021',
+  :first_name => 'v2 AUTO',
+  :last_name  => 'Tester',
+  :verification_value  => '579'
+)
+
+
+res = gateway.auto({
+    :credit_card => credit_card,
+    :amount => 2000,
+    :email => 'email@email.hu',
+    :threeDS => {
+        :threeDSReqAuthMethod => '01', 
+        :threeDSReqAuthType => 'MIT',
+    },
+    :address => {
+        :name =>  'myname',
+        :company => 'company',
+        :country => 'HU',
+        :state => 'Budapest',
+        :city => 'Budapest',
+        :zip => '1111',
+        :address1 => 'Address u.1',
+        :address2 => 'Address u.2',
+        :phone => '06301111111'
+    }
+})
+
+puts "\n\n"
+puts res.success?
+puts res.message
+puts "\n\n"
+puts "-----------------------------------------"
