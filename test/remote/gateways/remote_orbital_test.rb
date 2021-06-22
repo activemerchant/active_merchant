0194e59bc430dc65de1b64b4801b4bfb3f4555ac
Jessi <59324139+jessiagee@users.noreply.github.com>
2021-06-22 18:40:38 -0400

    Orbital: Adding MC SCARecurringPayment field
    Loaded suite test/unit/gateways/orbital_test
    
    122 tests, 716 assertions, 0 failures, 0 errors, 0 pendings, 0 omissions, 0 notifications
    100% passed
    
    Loaded suite test/remote/gateways/remote_orbital_test
    
    69 tests, 320 assertions, 0 failures, 0 errors, 0 pendings, 0 omissions, 0 notifications
    100% passed
    
    bundle exec rake test:local
    
    4778 tests, 73709 assertions, 0 failures, 0 errors, 0 pendings, 0 omissions, 0 notifications
    100% passed
    
    Running RuboCop...
    
    705 files inspected, no offenses detected

--- test/remote/gateways/remote_orbital_test.rb
+++ test/remote/gateways/remote_orbital_test.rb
@@ -197,6 +197,27 @@ class RemoteOrbitalGatewayTest < Test::Unit::TestCase
     assert_false response.authorization.blank?
   end
 
+  def test_successful_purchase_with_sca_recurring_master_card
+    cc = credit_card('5555555555554444', first_name: 'Joe', last_name: 'Smith',
+                     month: '12', year: '2022', brand: 'master', verification_value: '999')
+    options_local = {
+      three_d_secure: {
+        eci: '7',
+        xid: 'TESTXID',
+        cavv: 'AAAEEEDDDSSSAAA2243234',
+        ds_transaction_id: '97267598FAE648F28083C23433990FBC',
+        version: 2
+      },
+      sca_recurring: 'Y'
+    }
+
+    assert response = @three_ds_gateway.purchase(100, cc, @options.merge(options_local))
+
+    assert_success response
+    assert_equal 'Approved', response.message
+    assert_false response.authorization.blank?
+  end
+
   def test_successful_purchase_with_american_express_network_tokenization_credit_card
     network_card = network_tokenization_credit_card('4788250000028291',
       payment_cryptogram: 'BwABB4JRdgAAAAAAiFF2AAAAAAA=',
@@ -693,7 +714,7 @@ class RemoteOrbitalGatewayTest < Test::Unit::TestCase
 
     assert_match(/<AVSname>Jim Smith/, transcript)
     assert_match(/<RespCode>00/, transcript)
-    assert_match(/<StatusMsg>Approved/, transcript)
+    assert_match(/atusMsg>Approved</, transcript)
   end
 
   def test_echeck_purchase_with_no_address_responds_with_name
@@ -705,7 +726,7 @@ class RemoteOrbitalGatewayTest < Test::Unit::TestCase
 
     assert_match(/<AVSname>Test McTest/, transcript)
     assert_match(/<RespCode>00/, transcript)
-    assert_match(/<StatusMsg>Approved/, transcript)
+    assert_match(/atusMsg>Approved</, transcript)
   end
 
   def test_credit_purchase_with_address_responds_with_name
