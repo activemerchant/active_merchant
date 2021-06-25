0e378fa4030d067b77d5c5730aea194fcd6de9e5
Chandan Sinha <chasinha@publicisgroupe.net>
2021-06-25 20:50:37 +0530

    Worldpay 3DS2 support

--- lib/active_merchant/billing/gateways/worldpay.rb
+++ lib/active_merchant/billing/gateways/worldpay.rb
@@ -213,6 +213,9 @@ module ActiveMerchant #:nodoc:
               add_hcg_additional_data(xml, options) if options[:hcg_additional_data]
               add_instalments_data(xml, options) if options[:instalments]
               add_moto_flag(xml, options) if options.dig(:metadata, :manual_entry)
+			  if three_d_secure = options[:three_d_secure]
+              add_three_d_secure(three_d_secure, xml)
+              end
               add_additional_3ds_data(xml, options) if options[:execute_threed] && options[:three_ds_version] && options[:three_ds_version] =~ /^2/
               add_3ds_exemption(xml, options) if options[:exemption_type]
             end
@@ -407,10 +410,6 @@ module ActiveMerchant #:nodoc:
               xml.session 'shopperIPAddress' => options[:ip] if options[:ip]
               xml.session 'id' => options[:session_id] if options[:session_id]
             end
-
-            if three_d_secure = options[:three_d_secure]
-              add_three_d_secure(three_d_secure, xml)
-            end
           end
         end
       end
@@ -418,7 +417,7 @@ module ActiveMerchant #:nodoc:
       def add_three_d_secure(three_d_secure, xml)
         xml.info3DSecure do
           xml.threeDSVersion three_d_secure[:version]
-          if /^2/.match?(three_d_secure[:version])
+          if three_d_secure[:version] && three_d_secure[:ds_transaction_id]
             xml.dsTransactionId three_d_secure[:ds_transaction_id]
           else
             xml.xid three_d_secure[:xid]
