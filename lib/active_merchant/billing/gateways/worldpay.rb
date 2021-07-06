17df199985b779b2c18bc253475443432cdf1d86
Chandan Sinha <chasinha@publicisgroupe.net>
2021-07-06 12:21:38 +0530

    Indentation is fixed

--- lib/active_merchant/billing/gateways/worldpay.rb
+++ lib/active_merchant/billing/gateways/worldpay.rb
@@ -213,7 +213,7 @@ module ActiveMerchant #:nodoc:
               add_hcg_additional_data(xml, options) if options[:hcg_additional_data]
               add_instalments_data(xml, options) if options[:instalments]
               add_moto_flag(xml, options) if options.dig(:metadata, :manual_entry)
-			        if three_d_secure = options[:three_d_secure]
+              if three_d_secure = options[:three_d_secure]
                 add_three_d_secure(three_d_secure, xml)
               end
               add_additional_3ds_data(xml, options) if options[:execute_threed] && options[:three_ds_version] && options[:three_ds_version] =~ /^2/
