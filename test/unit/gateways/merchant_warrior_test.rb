111ba4d2fb9d6c4ac9c15b8f940b6d6236fd4227
Adina Bianchi <adina@spreedly.com>
2021-07-16 15:12:11 -0400

    Merchant Warrior: handle unexpected XML response
    Also updates failure message for remote tests.
    
    ECS-1373
    
    Test Summary
    Local: 4834 tests, 73895 assertions, 0 failures, 0 errors, 0 pendings, 0 omissions, 0 notifications
    Unit: 26 tests, 138 assertions, 0 failures, 0 errors, 0 pendings, 0 omissions, 0 notifications
    Remote: 18 tests, 100 assertions, 0 failures, 0 errors, 0 pendings, 0 omissions, 0 notifications

--- test/unit/gateways/merchant_warrior_test.rb
+++ test/unit/gateways/merchant_warrior_test.rb
@@ -31,6 +31,15 @@ class MerchantWarriorTest < Test::Unit::TestCase
     assert_equal '1336-20be3569-b600-11e6-b9c3-005056b209e0', response.authorization
   end
 
+  def test_failed_authorize
+    @gateway.expects(:ssl_post).returns(nil)
+
+    assert response = @gateway.authorize(@success_amount, @credit_card, @options)
+    assert_failure response
+    assert_equal 'Invalid gateway response', response.message
+    assert response.test?
+  end
+
   def test_successful_purchase
     @gateway.expects(:ssl_post).returns(successful_purchase_response)
 
