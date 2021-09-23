6033a7cd86b4704d978e5330c7a064576b350c41
Harish <85108998+inai-harish@users.noreply.github.com>
2021-09-23 17:48:58 +0530

    [BE-853] Razorpay initial version (#4)
    * Adding razorpay payments
    
    * Adding remote tests
    
    * Made recent API changes
    
    * Refactored razorpay implementation
    
    * Adding new line at EOF
    
    * Error code fix
    
    * Syntax error fix
    
    * Removing unused payment functions
    
    * Modifying tests for asserting latest changes
    
    * Changing error to include reason param
    
    * Changing error to include reason param
    
    * Adding get_payment_by_id API

---
+++ lib/active_merchant/billing/gateways/razorpay.rb
@@ -0,0 +1,168 @@
+module ActiveMerchant #:nodoc:
+    module Billing #:nodoc:
+      class RazorpayGateway < Gateway
+        self.live_url = 'https://api.razorpay.com/v1'
+        self.supported_countries = ['IN']
+        self.default_currency = 'INR'
+        self.supported_cardtypes = [:visa, :mastercard, :maestro, :rupay, :amex, :diners_club, :bajaj]
+  
+        self.homepage_url = 'http://razorpay.com'
+        self.display_name = 'Razorpay'
+        self.money_format = :cents
+  
+        STANDARD_ERROR_CODE_MAPPING = {}
+  
+        def initialize(options={})
+          requires!(options, :key_id, :key_secret)
+          super
+        end
+
+        def create_order(money, options={})
+          requires!(options, :currency)
+          post = {}
+          add_amount(post, money)
+          post[:currency] = (options[:currency] || currency(money))
+          post[:receipt] = options[:order_id] if options[:order_id]
+          post[:notes] = options[:notes] if options[:notes]
+          post[:partial_payment] = options[:partial_payment] if options[:partial_payment]
+          commit(:post, 'order', {}, post)
+        end
+
+        def get_payment(payment_id, options={})
+          return Response.new(false, 'Payment ID is mandatory') if payment_id.empty?
+          parameters = {}
+          parameters[:authorization_id] = payment_id
+          commit(:get, 'fetch', parameters)
+        end
+  
+        def capture(money, payment_id, options={})
+          return Response.new(false, 'Payment ID is mandatory') if payment_id.empty?
+          post = {}
+          parameters = {}
+          add_amount(post, money)
+          post[:currency] = (options[:currency] || currency(money))
+          parameters[:authorization_id] = payment_id
+          commit(:post, 'capture', parameters, post)
+        end
+  
+        def refund(money, payment_id, options={})
+          parameters = {}
+          post = {}
+          parameters[:authorization_id] = payment_id
+          add_amount(post, money)
+          commit(:post, 'refund', parameters, post)
+        end
+  
+        def void(authorization, options={})
+          Response.new(true, 'Razorpay does not support void api')
+        end
+  
+        def supports_scrubbing?
+          false
+        end
+  
+        private
+  
+        def add_amount(post, money)
+          post[:amount] = amount(money)
+        end
+  
+        def parse(body)
+          JSON.parse(body)
+        end
+
+        def endpoint(action, parameters)
+          case action
+          when 'order'
+            'orders'
+          when 'refund'
+            "payments/#{parameters[:authorization_id]}/refund"
+          when 'capture'
+            "payments/#{parameters[:authorization_id]}/capture"
+          when 'fetch'
+            "payments/#{parameters[:authorization_id]}"
+          end
+        end
+
+        def url(action, parameters)
+          "#{live_url}/#{endpoint(action, parameters)}"
+        end
+  
+        def api_request(method, endpoint, body = nil)
+          raw_response = ssl_request(method, endpoint, body, headers)
+          parse(raw_response)
+        rescue ActiveMerchant::ResponseError => e
+          return parse(e.response.body)
+        end
+  
+        def headers
+          {
+            'Content-Type' => 'application/json',
+            'Authorization' => 'Basic ' + Base64.strict_encode64("#{@options[:key_id]}:#{@options[:key_secret]}").strip
+          }
+        end
+  
+        def commit(method, action, parameters, body={})
+          url = url(action, parameters)
+          post = post_data(method, body)
+  
+          response = api_request(method, url, post)
+  
+          Response.new(
+            success_from(response),
+            message_from(response),
+            response,
+            authorization: authorization_from(response),
+            avs_result: nil,
+            cvv_result: nil,
+            test: test?,
+            error_code: error_code_from(response)
+          )
+        end
+  
+        def success_from(response)
+          response['error_code'].nil? && response['error'].nil?
+        end
+  
+        def message_from(response)
+          if success_from(response)
+            'OK'
+          elsif response['error']
+            response['error']['description']
+          elsif response['error_description']
+            response['error_description']
+          end
+        end
+  
+        def authorization_from(response)
+          response['id']
+        end
+
+        def post_data(method, parameters={})
+          if method == :get || method == :delete
+            nil
+          else
+            parameters.to_json
+          end
+        end
+  
+        def error_code_from(response)
+          unless success_from(response)
+            if response['error']
+              if response['error']['reason'] != "NA"
+                response['error']['reason']
+              else
+                response['error']['code']
+              end
+            elsif response["error_reason"]
+              response["error_reason"]
+            elsif response["error_code"]
+              response["error_code"]
+            else
+              'BAD_REQUEST_ERROR'
+            end
+          end
+        end
+      end
+    end
+  end
---
+++ test/remote/gateways/remote_razorpay_test.rb
@@ -0,0 +1,84 @@
+require 'test_helper'
+
+class RemoteRazorpayTest < Test::Unit::TestCase
+  def setup
+    @gateway = RazorpayGateway.new(fixtures(:razorpay))
+
+    @amount = 5000
+    @payment_id = 'pay_Hxh1s5QYjyJQzc'
+    @unauthorized_payment_id = 'pay_HyQdXlAAwUuNxt'
+    @invalid_payment_id = 'pay_HnTllcQFCxVGNU'
+    @options = {
+      billing_address: address,
+      description: 'Store Purchase',
+      currency: 'INR',
+      phone: '917912341123',
+      email: 'user@example.com'
+    }
+    @order_options = {
+      currency: 'INR',
+      order_id: 'order-id'
+    }
+  end
+
+  def test_successful_capture
+    response = @gateway.capture(@amount, @payment_id, @options)
+    assert_success response
+    assert_equal 'OK', response.message
+  end
+
+  def test_successful_order
+    response = @gateway.create_order(@amount, @order_options)
+    assert_success response
+    assert_equal 'OK', response.message
+    assert_match 'created', response.params['status']
+    assert response.params['id'] != nil
+    assert_equal @order_options[:order_id], response.params['receipt'] 
+  end
+
+  def test_failed_capture
+    response = @gateway.capture(@amount, '')
+    assert_failure response
+    assert_equal 'Payment ID is mandatory', response.message
+  end
+
+  def test_successful_refund
+    refund = @gateway.refund(@amount, @payment_id)
+    assert_success refund
+    assert_equal 'OK', refund.message
+  end
+
+  def test_successful_void
+    assert void = @gateway.void(@payment_id, @options)
+    assert_success void
+    assert_equal 'Razorpay does not support void api', void.message
+  end
+
+  def test_invalid_login
+    gateway = RazorpayGateway.new(key_id: '', key_secret: '')
+    response = gateway.capture(@amount, @payment_id, @options)
+    assert_failure response
+    assert_match "The api key provided is invalid", response.message
+  end
+
+  def test_successful_payment_fetch
+    response = @gateway.get_payment(@payment_id)
+    assert_success response
+    assert_equal "payment", response.params["entity"]
+    assert_equal @payment_id, response.authorization
+  end
+
+  def test_successful_payment_fetch_with_failed_payment_id
+    response = @gateway.get_payment(@unauthorized_payment_id)
+    assert_failure response
+    assert_equal "payment", response.params["entity"]
+    assert_equal @unauthorized_payment_id, response.authorization
+    assert_equal "failed", response.params["status"]
+    assert response.params["error_code"] != nil
+    assert response.params["error_description"] != nil
+    assert response.params["error_source"] != nil
+    assert response.params["error_step"] != nil
+    assert response.params["error_reason"] != nil
+  end
+
+end
