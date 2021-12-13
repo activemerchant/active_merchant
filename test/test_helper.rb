d70358df5a0f8f914c56456d1013709a6c798113
David Isaza Arias <disaza@EN2010363.local>
2021-12-13 16:27:32 -0500

    [ADD] add d_local remote test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -22,8 +22,9 @@ ActiveMerchant::Billing::Base.mode = :test
 
 if ENV['DEBUG_ACTIVE_MERCHANT'] == 'true'
   require 'logger'
-  ActiveMerchant::Billing::Gateway.logger = Logger.new(STDOUT)
+  ActiveMerchant::Billing::Gateway.logger = Logger.new(STDOUT) 
   ActiveMerchant::Billing::Gateway.wiredump_device = STDOUT
+  
 end
 
 # Test gateways
