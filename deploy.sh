c6e3b635aabc5b46afcd1192b707ab06275e3f65
Mike <mykhaylo.berdar@vgs.io>
2022-01-25 15:13:10 +0200

    Circleci project setup (#2)
    * Add .circleci/config.yml
    
    * setup CI
    
    * setup CI
    
    * add gemfile.lock
    
    * add semver with meta tag filter
    
    * add semver with meta tag filter
    
    Co-authored-by: Yuriy Yunikov <yyunikov@gmail.com>

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -xe
+
+[ -z "$GEMFURY_PYPI_PUSH_TOKEN" ] && echo "Gemfury token is missing" && exit 1
+[ -z "$PACKAGE_VERSION" ] && echo "Version is missing" && exit 1
+
+gem build activemerchant.gemspec
+
+## Deploy
+curl -F package=@activemerchant-$PACKAGE_VERSION.gem https://$GEMFURY_PYPI_PUSH_TOKEN@push.fury.io/vgs/
+
