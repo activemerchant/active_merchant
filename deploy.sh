06b6eb685fc0fd5fdf21a6cde28353faf156e660
Mike <mykhaylo.berdar@vgs.io>
2022-01-25 16:18:51 +0200

    allow all tags to be releases (#7)

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ set -xe
 [ -z "$GEMFURY_PYPI_PUSH_TOKEN" ] && echo "Gemfury token is missing" && exit 1
 [ -z "$PACKAGE_VERSION" ] && echo "Version is missing" && exit 1
 
-gem build activemerchant.gemspec --output activemerchant-$PACKAGE_VERSION.gem
+gem build activemerchant.gemspec
 
 ## Deploy
 curl -F package=@activemerchant-$PACKAGE_VERSION.gem https://$GEMFURY_PYPI_PUSH_TOKEN@push.fury.io/vgs/
