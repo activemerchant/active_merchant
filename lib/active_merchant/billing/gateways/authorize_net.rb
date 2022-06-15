023f3e8685e763c8758292790e2962dbaffc0c1b
David Perry <dperry@spreedly.com>
2022-06-15 10:45:47 -0400

    Revert "Authorize.net: Custom verify amount for $0 auth"
    This change is causing failures for attempted $0 auths for amex cards
    
    This reverts commit 9ba8f565f4034e7fef7a9369f02055e8e8c901ef.

--- lib/active_merchant/billing/gateways/authorize_net.rb
+++ lib/active_merchant/billing/gateways/authorize_net.rb
@@ -176,39 +176,11 @@ module ActiveMerchant
         end
       end
 
-      def verify(payment_method, options = {})
-        amount = amount_for_verify(payment_method, options)
-        return amount if amount.is_a?(Response)
-
+      def verify(credit_card, options = {})
         MultiResponse.run(:use_first_response) do |r|
-          r.process { authorize(amount, payment_method, options) }
-          r.process(:ignore_result) { void(r.authorization, options) } unless amount == 0
+          r.process { authorize(100, credit_card, options) }
+          r.process(:ignore_result) { void(r.authorization, options) }
         end
-      end
-
-      def amount_for_verify(payment_method, options)
-        if options[:verify_amount].present?
-          if options[:verify_amount].class != Integer || options[:verify_amount] < 0
-            response = Response.new(false, 'verify_amount value must be an integer')
-          elsif options[:verify_amount] > 0
-            response = options[:verify_amount]
-          elsif options[:verify_amount] == 0
-            response = 0
-            response = Response.new(false, 'Billing address including zip code is required for a 0 amount verify') unless
-              validate_billing_address_values?(options)
-          else
-            response = options[:verify_amount]
-          end
-        else
-          response = 100
-          response = 0 if validate_billing_address_values?(options) && payment_method.is_a?(CreditCard) && payment_method.brand != 'jcb'
-        end
-
-        response
-      end
-
-      def validate_billing_address_values?(options)
-        options[:billing_address].present? && (options[:billing_address][:zip].present? && options[:billing_address][:address1].present?)
       end
 
       def store(credit_card, options = {})
