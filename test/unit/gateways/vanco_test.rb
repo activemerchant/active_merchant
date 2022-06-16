b14f986fdbe86eb01e6c733f8b52c6ff7b70271f
Britney Smith <brit.smith7@gmail.com>
2022-06-16 14:28:54 -0400

    Vanco: Purchase with session id from request
    Test Summary
    Local: 5215 tests, 75910 assertions, 0 failures, 0 errors, 0 pendings, 0 omissions, 0 notifications
    100% passed
    
    Unit: 12 tests, 60 assertions, 0 failures, 0 errors, 0 pendings, 0 omissions, 0 notifications
    100% passed
    
    Remote: 15 tests, 34 assertions, 0 failures, 0 errors, 0 pendings, 0 omissions, 0 notifications
    100% passed

--- test/unit/gateways/vanco_test.rb
+++ test/unit/gateways/vanco_test.rb
@@ -3,6 +3,8 @@ require 'test_helper'
 class VancoTest < Test::Unit::TestCase
   include CommStub
 
+  SECONDS_PER_DAY = 3600 * 24
+
   def setup
     @gateway = VancoGateway.new(user_id: 'login', password: 'password', client_id: 'client_id')
     @credit_card = credit_card
@@ -45,6 +47,55 @@ class VancoTest < Test::Unit::TestCase
       assert_match(%r(<CustomerIPAddress>192), data) if data =~ /<RequestType>EFTAdd/
     end.respond_with(successful_login_response, successful_purchase_response)
     assert_success response
+  end
+
+  def test_successful_purchase_with_existing_session_id
+    previous_login_response = @gateway.send(:login)
+
+    response = stub_comms do
+      @gateway.purchase(
+        @amount,
+        @credit_card,
+        @options.merge(
+          session_id:
+            {
+              id: '5d4177ec3c356ec5f7abe0e17e814250',
+              created_at: Time.parse(previous_login_response.params['auth_requesttime'])
+            }
+        )
+      )
+    end.check_request do |_endpoint, data, _headers|
+      assert_match(%r(<SessionID>5d4177ec3c356ec5f7abe0e17e814250<\/SessionID>), data) if data =~ /<RequestType>EFTAddCompleteTransaction/
+    end.respond_with(successful_purchase_with_existing_session_id_response)
+
+    assert_success response
+    assert_equal '14949117|15756594|16136938', response.authorization
+    assert_equal 'Success', response.message
+    assert response.test?
+  end
+
+  def test_successful_purchase_with_expired_session_id
+    two_days_from_now = Time.now - (2 * SECONDS_PER_DAY)
+    response = stub_comms do
+      @gateway.purchase(
+        @amount,
+        @credit_card,
+        @options.merge(
+          session_id:
+            {
+              id: 'f34177ec3c356ec5f7abe0e17e814250',
+              created_at: two_days_from_now
+            }
+        )
+      )
+    end.check_request do |_endpoint, data, _headers|
+      assert_match(%r(<SessionID>5d8b104c9d8265db46bdf35ae9685472f4789dc8<\/SessionID>), data) if data =~ /<RequestType>EFTAddCompleteTransaction/
+    end.respond_with(successful_login_response, successful_purchase_response)
+
+    assert_success response
+    assert_equal '14949117|15756594|16136938', response.authorization
+    assert_equal 'Success', response.message
+    assert response.test?
   end
 
   def test_failed_purchase
@@ -136,6 +187,12 @@ class VancoTest < Test::Unit::TestCase
   def successful_purchase_response
     %(
       <?xml version="1.0" encoding="UTF-8"  ?><VancoWS><Auth><RequestID>ad4cbab9740e909423a02e622689d6</RequestID><RequestTime>2015-05-01 16:08:07 -0400</RequestTime><RequestType>EFTAddCompleteTransaction</RequestType><Signature></Signature><SessionID>5d8b104c9d8265db46bdf35ae9685472f4789dc8</SessionID><Version>2</Version></Auth><Response><StartDate>2015-05-01</StartDate><CustomerRef>14949117</CustomerRef><PaymentMethodRef>15756594</PaymentMethodRef><TransactionRef>16136938</TransactionRef><TransactionFee>3.20</TransactionFee></Response></VancoWS>
+     )
+  end
+
+  def successful_purchase_with_existing_session_id_response
+    %(
+      <?xml version="1.0" encoding="UTF-8"  ?><VancoWS><Auth><RequestID>ad4cbab9740e909423a02e622689d6</RequestID><RequestTime>2015-05-01 16:08:07 -0400</RequestTime><RequestType>EFTAddCompleteTransaction</RequestType><Signature></Signature><SessionID>5d4177ec3c356ec5f7abe0e17e814250</SessionID><Version>2</Version></Auth><Response><StartDate>2015-05-01</StartDate><CustomerRef>14949117</CustomerRef><PaymentMethodRef>15756594</PaymentMethodRef><TransactionRef>16136938</TransactionRef><TransactionFee>3.20</TransactionFee></Response></VancoWS>
      )
   end
 
