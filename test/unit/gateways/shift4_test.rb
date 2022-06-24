28869b37d743429042871910028bfc814d6940f8
ajawadmirza <Abdul.Razzaque@visionetsystems.com>
2022-06-24 16:56:38 +0500

    Shift4: Update card on file request
    Updated card on file transactions as per the `stored_credential` framework along with tests.
    
    SER-94
    
    Remote:
    15 tests, 35 assertions, 0 failures, 0 errors, 0 pendings, 0 omissions, 0 notifications
    100% passed
    
    Unit:
    5230 tests, 75989 assertions, 0 failures, 0 errors, 0 pendings, 0 omissions, 0 notifications
    100% passed
    
    Rubocop:
    746 files inspected, no offenses detected

--- test/unit/gateways/shift4_test.rb
+++ test/unit/gateways/shift4_test.rb
@@ -77,6 +77,26 @@ class Shift4Test < Test::Unit::TestCase
     assert_equal response.message, 'Transaction successful'
   end
 
+  def test_successful_purchase_with_stored_credential
+    stored_credential_options = {
+      inital_transaction: false,
+      reason_type: 'recurring',
+      network_transaction_id: '123abcdefg'
+    }
+    response = stub_comms do
+      @gateway.purchase(@amount, @credit_card, @options.merge({ stored_credential: stored_credential_options }))
+    end.check_request do |_endpoint, data, _headers|
+      request = JSON.parse(data)['transaction']
+      assert_equal request['cardOnFile']['usageIndicator'], '02'
+      assert_equal request['cardOnFile']['indicator'], '01'
+      assert_equal request['cardOnFile']['scheduledIndicator'], '01'
+      assert_equal request['cardOnFile']['transactionId'], stored_credential_options[:network_transaction_id]
+    end.respond_with(successful_purchase_response)
+
+    assert response.success?
+    assert_equal response.message, 'Transaction successful'
+  end
+
   def test_successful_refund
     response = stub_comms do
       @gateway.refund(@amount, '1111g66gw3ryke06', @options.merge!(invoice: '4666309473'))
