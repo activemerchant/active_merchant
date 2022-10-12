c5ab21727dc2370ab530d2dd7888f5af07d506ca
ajawadmirza <Abdul.Razzaque@visionetsystems.com>
2022-10-12 12:40:01 +0500

    Borgun: Add 3ds frictionless tests
    Added test cases to verify implementation for frictionless transcations.
    
    SER-346
    
    Remote:
    23 tests, 46 assertions, 5 failures, 0 errors, 0 pendings, 0 omissions, 0 notifications
    78.2609% passed
    
    Unit:
    5357 tests, 76645 assertions, 0 failures, 0 errors, 0 pendings, 0 omissions, 0 notifications
    100% passed
    
    Rubocop:
    750 files inspected, no offenses detected

--- test/remote/gateways/remote_borgun_test.rb
+++ test/remote/gateways/remote_borgun_test.rb
@@ -9,6 +9,7 @@ class RemoteBorgunTest < Test::Unit::TestCase
 
     @amount = 100
     @credit_card = credit_card('5587402000012011', year: 2027, month: 9, verification_value: 415)
+    @frictionless_3ds_card = credit_card('5455330200000016', verification_value: 415, month: 9, year: 2027)
     @declined_card = credit_card('4155520000000002')
 
     @options = {
@@ -32,6 +33,15 @@ class RemoteBorgunTest < Test::Unit::TestCase
     response = @gateway.purchase(@amount, @credit_card, @options.merge({ merchant_return_url: 'http://localhost/index.html', apply_3d_secure: '1' }))
     assert_success response
     assert_equal 'Succeeded', response.message
+    assert_not_nil response.params['redirecttoacsform']
+  end
+
+  def test_successful_preauth_frictionless_3ds
+    response = @gateway.purchase(@amount, @frictionless_3ds_card, @options.merge({ merchant_return_url: 'http://localhost/index.html', apply_3d_secure: '1' }))
+    assert_success response
+    assert_equal 'Succeeded', response.message
+    assert_nil response.params['redirecttoacsform']
+    assert_equal response.params['threedsfrictionless'], 'A'
   end
 
   def test_successful_purchase_usd
