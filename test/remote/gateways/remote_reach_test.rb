aee3e2950924b33ed5709005322f9a807f284b67
cristian <Heavyblade@users.noreply.github.com>
2022-12-29 15:32:12 -0500

    Revert "Reach: using transaction token as ReferenceId on refunds"
    This reverts commit e40e1ee50e3687f5b123546dcf83141be1440073.

--- test/remote/gateways/remote_reach_test.rb
+++ test/remote/gateways/remote_reach_test.rb
@@ -191,22 +191,11 @@ class RemoteReachTest < Test::Unit::TestCase
     assert_equal 'Not Found', response.message
   end
 
-  def test_successful_refund_with_reference_id
+  def test_successful_refund
     response = @gateway.refund(
       @amount,
       '5cd04b6a-7189-4a71-a335-faea4de9e11d',
       { reference_id: 'REFUND_TAG' }
-    )
-
-    assert_success response
-    assert response.params['response']['RefundId'].present?
-  end
-
-  def test_successful_refund_with_order_id
-    response = @gateway.refund(
-      @amount,
-      '5cd04b6a-7189-4a71-a335-faea4de9e11d',
-      { order_id: 'REFUND_TAG' }
     )
 
     assert_success response
