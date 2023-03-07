2720143243f4656d5b6efaf23e1e89718b877f58
aenand <aenand@spreedly.com>
2023-03-07 11:03:15 -0500

    Paymentez: Add inquire by transaction_id
    Get payment status by Paymentez transaction id
    
    Unit:
    30 tests, 127 assertions, 0 failures, 0 errors, 0 pendings, 0 omissions, 0 notifications
    100% passed
    Remote:
    34 tests, 73 assertions, 9 failures, 0 errors, 0 pendings, 0 omissions, 0 notifications
    73.5294% passed
    ** These failures also existed on the main branch **

--- test/unit/gateways/paymentez_test.rb
+++ test/unit/gateways/paymentez_test.rb
@@ -341,6 +341,18 @@ class PaymentezTest < Test::Unit::TestCase
     assert_equal @gateway.scrub(pre_scrubbed), post_scrubbed
   end
 
+  def test_successful_inquire_with_transaction_id
+    response = stub_comms(@gateway, :ssl_get) do
+      @gateway.inquire('CI-635')
+    end.check_request do |method, _endpoint, _data, _headers|
+      assert_match('https://ccapi-stg.paymentez.com/v2/transaction/CI-635', method)
+    end.respond_with(successful_authorize_response)
+
+    assert_success response
+    assert_equal 'CI-635', response.authorization
+    assert response.test?
+  end
+
   private
 
   def pre_scrubbed
