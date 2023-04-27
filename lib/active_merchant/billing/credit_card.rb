12b7f8aacc7b74763a10fc34fddbc617d7648b6d
Wojciech Zakrzewski <asterdrak@gmail.com>
2023-04-27 03:36:15 -0700

    [internal] update card brands from upstream AM with patch for current tests + fix tests (#203)
    * update_card_brands_from_upstream
    
    * update
    
    * update
    
    * add_maestro_bins and fix tests

--- lib/active_merchant/billing/credit_card.rb
+++ lib/active_merchant/billing/credit_card.rb
@@ -351,7 +351,7 @@ module ActiveMerchant #:nodoc:
         errors = []
 
         if !empty?(brand)
-          errors << [:brand, "is invalid"]  if !CreditCard.card_companies.keys.include?(brand)
+          errors << [:brand, "is invalid"]  if !CreditCard.card_companies.include?(brand)
         end
 
         if empty?(number)
