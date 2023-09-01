672ed06b5d67a2abc2443ecf9903ef6ba413626a
Kunal <kunal@bluebash.co>
2023-09-01 16:41:15 +0530

    added the response type to attr reader

--- lib/active_merchant/billing/response.rb
+++ lib/active_merchant/billing/response.rb
@@ -4,7 +4,7 @@ module ActiveMerchant #:nodoc:
     end
 
     class Response
-      attr_reader :params, :message, :test, :authorization, :avs_result, :cvv_result, :error_code, :emv_authorization, :network_transaction_id
+      attr_reader :params, :message, :test, :authorization, :avs_result, :cvv_result, :error_code, :emv_authorization, :network_transaction_id, :response_type
 
       def success?
         @success
