b62444bd303ba0351b82a4097bbd55120b55fc25
Kunal <kunal@bluebash.co>
2023-10-16 16:39:47 +0530

    fixes for klarna response and code refactoring

--- activemerchant.gemspec
+++ activemerchant.gemspec
@@ -27,7 +27,6 @@ Gem::Specification.new do |s|
   s.add_dependency('i18n', '>= 0.6.9')
   s.add_dependency('nokogiri', '~> 1.4')
   s.add_dependency('rexml', '~> 3.2.5')
-  s.add_dependency 'klarna_proxy'
 
   s.add_development_dependency('mocha', '~> 1')
   s.add_development_dependency('pry')
--- lib/active_merchant/billing/gateways/klarna.rb
+++ lib/active_merchant/billing/gateways/klarna.rb
@@ -24,14 +24,28 @@ module ActiveMerchant
       end
 
       def create_session(options)
-        Klarna.client(:payment).create_session(options)
+        response = Klarna.client(:payment).create_session(options)
+
+        if response.success?
+          message = "Session Created #{response.session_id}"
+          generate_success_response(response, message)
+        else
+          generate_failure_response(response)
+        end
       end
 
       def update_session(session_id, options)
-        Klarna.client(:payment).update_session(session_id, options)
+        response = Klarna.client(:payment).update_session(session_id, options)
+
+        if response.success?
+          message = "Session Updated #{session_id}"
+          generate_success_response(response, message)
+        else
+          generate_failure_response(response)
+        end
       end
 
-      def purchase(amount, authorize_token, options = {})
+      def purchase(amount, authorize_token, options={})
         auth_response = authorize(amount, authorize_token, options)
         if auth_response.success?
           capture(amount, auth_response.params["authorization"], options)
@@ -44,54 +58,32 @@ module ActiveMerchant
         response = Klarna.client(:payment).place_order(authorize_token, options)
 
         if response.success?
-          ActiveMerchant::Billing::Response.new(
-            true,
-            "Placed order #{response.order_id}",
-            response.body,
-            {
-              authorization: response.order_id,
-              fraud_review: response.fraud_status
-            }
-          )
+          message = "Placed order #{response.order_id}"
+          generate_success_response(response, message, response.order_id, response.fraud_status)
         else
-          ActiveMerchant::Billing::Response.new(
-            false,
-            response.error_code.to_s,
-            response.body,
-            {
-              error_code: response.error_code
-            }
-          )
+          generate_failure_response(response)
         end
       end
 
-      def store(authorize_token, options = {})
-        Klarna.client(:payment).customer_token(authorize_token, options)
+      def store(authorize_token, options={})
+        response = Klarna.client(:payment).customer_token(authorize_token, options)
+
+        if response.success?
+          message = "Client token Created"
+          generate_success_response(response, message)
+        else
+          generate_failure_response(response)
+        end
       end
 
       def capture(amount, order_id, options={})
         response = Klarna.client.capture(order_id, {captured_amount: amount, shipping_info: options[:shipping_info]})
 
         if response.success?
-          capture_id = response['Capture-ID']
-
-          ActiveMerchant::Billing::Response.new(
-            true,
-            "Captured order with Klarna id: '#{order_id}' Capture id: '#{capture_id}'",
-            response.body || {},
-            {
-              authorization: order_id
-            }
-          )
+          message = "Captured order with Klarna id: '#{order_id}' Capture id: '#{response['Capture-ID']}'"
+          generate_success_response(response, message, order_id)
         else
-          ActiveMerchant::Billing::Response.new(
-            false,
-            response.error_code.to_s,
-            response.body || {},
-            {
-              error_code: response.error_code
-            }
-          )
+          generate_failure_response(response)
         end
       end
 
@@ -100,21 +92,10 @@ module ActiveMerchant
         response = Klarna.client(:refund).create(order_id, {refunded_amount: amount, order_lines: options[:order_lines]})
 
         if response.success?
-          ActiveMerchant::Billing::Response.new(
-            true,
-            "Refunded order with Klarna id: #{order_id}",
-            response.body || {},
-            {
-              authorization: response['Refund-ID']
-            }
-          )
+          message = "Refunded order with Klarna id: #{order_id}"
+          generate_success_response(response, message, response['Refund-ID'])
         else
-          ActiveMerchant::Billing::Response.new(
-            false,
-            'Klarna Gateway: There was an error refunding this refund.',
-            response.body || {},
-            { error_code: response.error_code }
-          )
+          generate_failure_response(response)
         end
       end
 
@@ -128,20 +109,10 @@ module ActiveMerchant
         response = Klarna.client.acknowledge(order_id)
 
         if response.success?
-          ActiveMerchant::Billing::Response.new(
-            true,
-            "Extended Period for order with Klarna id: #{order_id}",
-            response.body || {}
-          )
+          message = "Extended Period for order with Klarna id: #{order_id}"
+          generate_success_response(response, message)
         else
-          ActiveMerchant::Billing::Response.new(
-            false,
-            'Klarna Gateway: There was an error processing this acknowledge.',
-            response.body || {},
-            {
-              error_code: response.error_code
-            }
-          )
+          generate_failure_response(response)
         end
       end
 
@@ -149,20 +120,10 @@ module ActiveMerchant
         response = Klarna.client.extend(order_id)
 
         if response.success?
-          ActiveMerchant::Billing::Response.new(
-            true,
-            "Extended Period for order with Klarna id: #{order_id}",
-            response.body || {}
-          )
+          message = "Extended Period for order with Klarna id: #{order_id}"
+          generate_success_response(response, message)
         else
-          ActiveMerchant::Billing::Response.new(
-            false,
-            'Klarna Gateway: There was an error processing this period extension.',
-            response.body || {},
-            {
-              error_code: response.error_code
-            }
-          )
+          generate_failure_response(response)
         end
       end
 
@@ -170,23 +131,10 @@ module ActiveMerchant
         response = Klarna.client.release(order_id)
 
         if response.success?
-          ActiveMerchant::Billing::Response.new(
-            true,
-            "Released reamining amount for order with Klarna id: #{order_id}",
-            response.body || {},
-            {
-              authorization: order_id
-            }
-          )
+          message = "Released reamining amount for order with Klarna id: #{order_id}"
+          generate_success_response(response, message, order_id)
         else
-          ActiveMerchant::Billing::Response.new(
-            false,
-            'Klarna Gateway: There was an error processing this release.',
-            response.body || {},
-            {
-              error_code: response.error_code
-            }
-          )
+          generate_failure_response(response)
         end
       end
 
@@ -194,21 +142,10 @@ module ActiveMerchant
         response = Klarna.client.cancel(order_id)
 
         if response.success?
-          ActiveMerchant::Billing::Response.new(
-            true,
-            "Cancelled order with Klarna id: #{order_id}",
-            response.body || {},
-            {
-              authorization: order_id
-            }
-          )
+          message = "Cancelled order with Klarna id: #{order_id}"
+          generate_success_response(response, message, order_id)
         else
-          ActiveMerchant::Billing::Response.new(
-            false,
-            'Klarna Gateway: There was an error cancelling this payment.',
-            response.body || {},
-            { error_code: response.error_code }
-          )
+          generate_failure_response(response)
         end
       end
 
@@ -219,18 +156,10 @@ module ActiveMerchant
           shipping_info
         )
         if response.success?
-          ActiveMerchant::Billing::Response.new(
-            true,
-            "Updated shipment info for order: #{order_id}, capture: #{capture_id}",
-            response.body || {},
-          )
+          message = "Updated shipment info for order: #{order_id}, capture: #{capture_id}"
+          generate_success_response(response, message)
         else
-          ActiveMerchant::Billing::Response.new(
-            false,
-            "Cannot update the shipment info for order: #{order_id} capture: #{capture_id}",
-            response.body || {},
-            { error_code: response.error_code }
-          )
+          generate_failure_response(response)
         end
       end
 
@@ -240,18 +169,56 @@ module ActiveMerchant
           data
         )
         if response.success?
-          ActiveMerchant::Billing::Response.new(
-            true,
-            "Updated customer details for order: #{order_id}",
-            response.body || {},
-          )
+          message = "Updated customer details for order: #{order_id}"
+          generate_success_response(response, message)
         else
-          ActiveMerchant::Billing::Response.new(
-            false,
-            "Cannot update customer details for order: #{order_id}",
-            response.body || {},
-            { error_code: response.error_code }
-          )
+          generate_failure_response(response)
+        end
+      end
+
+      private
+
+      def generate_success_response(response, message, authorization=nil, fraud_status=nil)
+        ActiveMerchant::Billing::Response.new(
+          true,
+          message,
+          response.body || {},
+          {
+            authorization:,
+            fraud_status:,
+            response_http_code: response.http_response&.code,
+            request_endpoint: response.request_endpoint,
+            request_method: response.request_method,
+            request_body: response.request_body,
+            response_type: get_response_type(response)
+          }
+        )
+      end
+
+      def generate_failure_response(response)
+        ActiveMerchant::Billing::Response.new(
+          false,
+          response.error_code.to_s,
+          response.body || {},
+          {
+            error_code: response.error_code,
+            response_http_code: response.http_response&.code,
+            request_endpoint: response.request_endpoint,
+            request_method: response.request_method,
+            request_body: response.request_body,
+            response_type: get_response_type(response)
+          }
+        )
+      end
+
+      def get_response_type(response)
+        code = response.code&.to_i
+        if code == 200
+          0
+        elsif (500..599).include? code
+          1
+        else
+          2
         end
       end
     end
