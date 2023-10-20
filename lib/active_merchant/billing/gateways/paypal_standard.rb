d8a82116a342058fece9bfa813824ab7922946f7
mithlesh kumar <Mithlesh.kumar@bluebash.co>
2023-10-20 11:16:12 +0530

    more changes

--- lib/active_merchant/billing/gateways/paypal_standard.rb
+++ lib/active_merchant/billing/gateways/paypal_standard.rb
@@ -4,6 +4,10 @@ module ActiveMerchant
       include Empty
       self.test_url = 'https://api-m.sandbox.paypal.com'
       self.live_url = 'https://api-m.paypal.com'
+      self.supported_countries = %w[AL DZ AD AO AI AG AR AM AW AU AT AZ BS BH BB BY BE BZ BJ BM BT BO BA BW BR VG BN BG BF BI KH CM CA CV KY TD CL C2 CO KM CG CD CK CR CI HR CY CZ DK DJ DM DO EC EG SV ER EE ET FK FO FJ FI FR GF PF GA GM GE DE GI GR GL GD GP GT GN GW GY HN HK HU IS IN ID IE IL IT JM JP JO KZ KE KI KW KG LA LV LS LI LT LU MK MG MW MY MV ML MT MH MQ MR MU YT MX FM MD MC MN ME MS MA MZ NA NR NP NL NC NZ NI NE NG NU NF NO OM PW PA PG PY PE PH PN PL PT QA RE RO RU RW WS SM ST SA SN RS SC SL SG SK SI SB SO ZA KR ES LK SH KN LC PM VC SR SJ SZ SE CH TW TJ TZ TH TG TO TT TN TM TC TV UG UA AE GB US UY VU VA VE VN WF YE ZM ZW].freeze      self.default_currency = 'USD'
+      self.money_format = :cents
+      self.supported_cardtypes = %i[visa master american_express diners_club maestro discover jcb mada bp_plus]
+      self.display_name = 'PayPal'
 
       ENDPOINTS = {
         generate_token: '/v1/oauth2/token',
@@ -12,17 +16,15 @@ module ActiveMerchant
         refund: '/v2/payments/captures/%{id}/refund'
       }
 
-      SUCCESS_CODE = 100
-      SOFT_DECLINE_CODES = [201, 203, 204, 205, 221, 223, 225, 226, 240].freeze
-
-      self.default_currency = 'USD'
-      self.money_format = :dollars
-      self.display_name = 'PayPal'
+      SOFT_DECLINE_CODES = [].freeze
+      HARD_DECLINE_CODES = %w[CARD_EXPIRED TRANSACTION_BLOCKED_BY_PAYEE PAYER_ACCOUNT_LOCKED_OR_CLOSED]
 
       def initialize(options = {})
         requires!(options, :client_id, :client_secret)
         @client_id = options[:client_id]
         @client_secret = options[:client_secret]
+        @response_http_code = nil
+
         super
         @access_token = setup_access_token
       end
@@ -39,6 +41,7 @@ module ActiveMerchant
 
       def capture(amount, authorization, options = {})
         post = {}
+
         commit(:capture_order, post, options[:order_id])
       end
 
@@ -47,7 +50,8 @@ module ActiveMerchant
 
         add_refund_amount(post, amount, options) unless options[:full_refund].present?
         add_refund_reason(post, options)
-        commit('refund', post, options[:capture_id])
+
+        commit(:refund, post, options[:capture_id])
       end
 
       private
@@ -57,6 +61,7 @@ module ActiveMerchant
 
         response = parse(ssl_post(url, post_data(post), headers))
         success = success_from(response)
+
         Response.new(
           success_from(response),
           message_from(response),
@@ -156,13 +161,19 @@ module ActiveMerchant
       end
 
       def add_refund_amount(post, amount, options)
-        post[:amount] = {}
-        post[:ammount][:value] = amount
-        post[:ammount][:currency_code] = options[:currency_code]
+        post[:amount] = {
+          "value": amount,
+          "currency_code": options[:currency_code]
+        }
       end
 
-       def add_refund_reason(post, options)
+      def add_refund_reason(post, options)
         post[:note_to_payer] = options[:refund_reason]
+      end
+
+      def handle_response(response)
+        @response_http_code = response.code.to_i
+        super
       end
     end
   end
