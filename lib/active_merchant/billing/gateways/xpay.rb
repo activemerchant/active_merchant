4c0c3b3f0f2357613cda9161e3fcc1dac75a41b1
Johan Herrera <jherrera@spreedly.com>
2023-11-09 14:42:45 -0500

    NexiXpay:  Add basic operation through 3ds
    Description
    -------------------------
    [SER-703](https://spreedly.atlassian.net/browse/SER-703)
    
    This commit add NexiXpay gateway with its basic operations

--- lib/active_merchant/billing/gateways/xpay.rb
+++ lib/active_merchant/billing/gateways/xpay.rb
@@ -15,7 +15,7 @@ module ActiveMerchant #:nodoc:
 
       ENDPOINTS_MAPPING = {
         purchase: 'orders/2steps/payment',
-        authorize: 'orders/2steps/init',
+        preauth: 'orders/2steps/init',
         capture: 'operations/{%s}/captures',
         verify: 'orders/card_verification',
         void: 'operations/{%s}/cancels',
@@ -30,79 +30,161 @@ module ActiveMerchant #:nodoc:
 
       def purchase(amount, payment_method, options = {})
         post = {}
-        commit('purchase', post, options)
+        add_auth_purchase_params(post, amount, payment_method, options)
+        action = options[:operation_id] ? :purchase : :preauth
+        commit(action, post, options)
       end
 
       def authorize(amount, payment_method, options = {})
         post = {}
-        add_auth_purchase(post, amount, payment_method, options)
-        commit('authorize', post, options)
+        add_auth_purchase_params(post, amount, payment_method, options)
+        commit(:preauth, post, options)
       end
 
       def capture(amount, authorization, options = {})
         post = {}
-        commit('capture', post, options)
+        commit(:capture, post, options)
       end
 
       def void(authorization, options = {})
         post = {}
-        commit('void', post, options)
+        commit(:void, post, options)
       end
 
       def refund(amount, authorization, options = {})
         post = {}
-        commit('refund', post, options)
+        commit(:refund, post, options)
       end
 
       def verify(credit_card, options = {})
         post = {}
-        commit('verify', post, options)
+        add_invoice(post, 0, options)
+        add_credit_card(post, credit_card)
+        commit(:verify, post, options)
       end
 
       def supports_scrubbing?
         true
       end
 
-      def scrub(transcript) end
+      def scrub(transcript)
+        transcript.
+          gsub(%r((X-Api-Key: )(\w|-)+), '\1[FILTERED]').
+          gsub(%r(("pan\\?"\s*:\s*\\?")[^"]*)i, '\1[FILTERED]').
+          gsub(%r(("cvv\\?":\\?")\d+), '\1[FILTERED]')
+      end
 
       private
 
-      def add_invoice(post, money, options) end
+      def add_invoice(post, amount, options)
+        currency = options[:currency] || currency(amount)
+        post[:order] = {
+          orderId: options[:order_id],
+          amount: localized_amount(amount, currency),
+          currency: currency
+        }.compact
+      end
 
-      def add_payment_method(post, payment_method) end
+      def add_credit_card(post, payment_method)
+        post[:card] = {
+          pan: payment_method.number,
+          expiryDate: expdate(payment_method),
+          cvv: payment_method.verification_value
+        }
+      end
+
+      def add_payment_method(post, payment_method)
+        add_credit_card(post, payment_method) if payment_method.is_a?(CreditCard)
+      end
+
+      def add_customer_data(post, payment_method, options)
+        post[:order][:customerInfo] = {
+          cardHolderName: payment_method.name,
+          cardHolderEmail: options[:email]
+        }.compact
+      end
+
+      def add_address(post, options)
+        if address = options[:billing_address] || options[:address]
+          post[:order][:customerInfo][:billingAddress] = {
+            name: address[:name],
+            street: address[:address1],
+            additionalInfo: address[:address2],
+            city: address[:city],
+            postCode: address[:zip],
+            country: address[:country]
+          }.compact
+        end
+        if address = options[:shipping_address]
+          post[:order][:customerInfo][:shippingAddress] = {
+            name: address[:name],
+            street: address[:address1],
+            additionalInfo: address[:address2],
+            city: address[:city],
+            postCode: address[:zip],
+            country: address[:country]
+          }.compact
+        end
+      end
+
+      def add_recurrence(post, options)
+        post[:recurrence] = { action: options[:recurrence] || 'NO_RECURRING' }
+      end
+
+      def add_exemptions(post, options)
+        post[:exemptions] = options[:exemptions] || 'NO_PREFERENCE'
+      end
+
+      def add_3ds_params(post, options)
+        post[:threeDSAuthData] = { threeDSAuthResponse: options[:three_ds_auth_response] }.compact
+        post[:operationId] = options[:operation_id] if options[:operation_id]
+      end
+
+      def add_auth_purchase_params(post, amount, payment_method, options)
+        add_invoice(post, amount, options)
+        add_payment_method(post, payment_method)
+        add_customer_data(post, payment_method, options)
+        add_address(post, options)
+        add_recurrence(post, options) unless options[:operation_id]
+        add_exemptions(post, options)
+        add_3ds_params(post, options)
+      end
 
       def add_reference(post, authorization) end
 
       def add_auth_purchase(post, money, payment, options) end
 
-      def commit(action, params, options)
-        url = build_request_url(action)
-        response = ssl_post(url, params.to_json, request_headers(options))
+      def parse(body = {})
+        JSON.parse(body)
+      end
 
-        unless response
-          Response.new(
-            success_from(response),
-            message_from(success_from(response), response),
-            response,
-            authorization: authorization_from(response),
-            avs_result: AVSResult.new(code: response['some_avs_result_key']),
-            cvv_result: CVVResult.new(response['some_cvv_result_key']),
-            test: test?,
-            error_code: error_code_from(response)
-          )
+      def commit(action, params, options)
+        transaction_id = params.dig(:operation_id) unless action != 'capture'
+        begin
+          url = build_request_url(action, transaction_id)
+          raw_response = ssl_post(url, params.to_json, request_headers(options))
+          response = parse(raw_response)
+        rescue ResponseError => e
+          response = e.response.body
+          response = parse(response)
         end
-      rescue ResponseError => e
-        response = e.response.body
-        JSON.parse(response)
+
+        Response.new(
+          success_from(response),
+          message_from(response),
+          response,
+          authorization: authorization_from(response),
+          test: test?,
+          error_code: error_code_from(response)
+        )
       end
 
       def request_headers(options)
-        headers = {
+        {
           'Content-Type' => 'application/json',
           'X-Api-Key' => @api_key,
-          'Correlation-Id' => options.dig(:order, :order_id) || SecureRandom.uuid
+          'Correlation-Id' => options[:order_id] || SecureRandom.uuid
         }
-        headers
       end
 
       def build_request_url(action, id = nil)
@@ -112,23 +194,19 @@ module ActiveMerchant #:nodoc:
       end
 
       def success_from(response)
-        response == 'SUCCESS'
+        response.dig('operation', 'operationResult') == 'PENDING' || response.dig('operation', 'operationResult') == 'FAILED' || response.dig('operation', 'operationResult') == 'AUTHORIZED'
       end
 
-      def message_from(succeeded, response)
-        if succeeded
-          'Succeeded'
-        else
-          response.dig('errors') unless response
-        end
+      def message_from(response)
+        response['errors'] || response.dig('operation', 'operationResult')
       end
 
       def authorization_from(response)
-        response.dig('latest_payment_attempt', 'payment_intent_id') unless response
+        response.dig('operation', 'operationId') unless response
       end
 
       def error_code_from(response)
-        response['provider_original_response_code'] || response['code'] unless success_from(response)
+        response.dig('errors', 0, 'code')
       end
     end
   end
--- test/remote/gateways/remote_xpay_test.rb
+++ test/remote/gateways/remote_xpay_test.rb
@@ -2,14 +2,64 @@ require 'test_helper'
 
 class RemoteRapydTest < Test::Unit::TestCase
   def setup
-    @gateway = XpayGateway.new(fixtures(:x_pay))
-    @amount = 200
-    @credit_card = credit_card('4111111111111111')
-    @options = {}
+    @gateway = XpayGateway.new(fixtures(:xpay))
+    @amount = 100
+    @credit_card = credit_card(
+      '5186151650005008',
+      month: 12,
+      year: 2026,
+      verification_value: '123',
+      brand: 'master'
+    )
+
+    @options = {
+      order_id: SecureRandom.alphanumeric(10),
+      billing_address: address,
+      order: {
+        currency: 'EUR',
+        amount: @amount
+      }
+    }
   end
 
   def test_successful_purchase
     response = @gateway.purchase(@amount, @credit_card, @options)
-    assert response
+    assert_success response
+    assert_true response.params.has_key?('threeDSAuthUrl')
+    assert_true response.params.has_key?('threeDSAuthRequest')
+    assert_match 'PENDING', response.message
   end
+
+  def test_failed_purchase
+    init = @gateway.purchase(@amount, @credit_card, {})
+    assert_failure init
+    assert_equal 'GW0001', init.error_code
+  end
+
+  # def test_successful_verify  ## test not working
+  #   response = @gateway.verify(@credit_card, @options)
+  #   assert_success response
+  #   assert_match 'PENDING', response.message
+  # end
+
+  # def test_successful_refund ## test requires set up (purchase or auth through 3ds)
+  #   options = {
+  #     order: {
+  #       currency: 'EUR',
+  #       description: 'refund operation message'
+  #     },
+  #     operation_id: '168467730273233329'
+  #   }
+  #   response = @gateway.refund(@amount, options)
+  #   assert_success response
+  # end
+
+  # def test_successful_void ## test requires set up (purchase or auth through 3ds)
+  #   options = {
+  #     description: 'void operation message',
+  #     operation_id: '168467730273233329'
+  #   }
+  #   response = @gateway.void(@amount, options)
+  #   assert_success response
+  # end
 end
