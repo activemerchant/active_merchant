4043cdc6dc0dbd10b79185f07a4cfa4f823cf393
Bhanu <bhanu.prakash@bluebash.co>
2023-12-12 12:38:32 +0530

    Updated code for 3ds payments

--- lib/active_merchant/billing/gateways/axcessms.rb
+++ lib/active_merchant/billing/gateways/axcessms.rb
@@ -1,6 +1,8 @@
 module ActiveMerchant #:nodoc:
   module Billing #:nodoc:
     class AxcessmsGateway < Gateway
+      include Empty
+
       self.test_url = 'https://eu-test.oppwa.com/'
       self.live_url = 'https://eu-prod.oppwa.com/'
 
@@ -23,7 +25,7 @@ module ActiveMerchant #:nodoc:
       PAYMENT_CODE_REFUND = 'RF'
       PAYMENT_CODE_REBILL = 'RB'
 
-      SUCCESS_CODE = '000.000.100'
+      SUCCESS_CODES = %w[000.000.100 000.000.100 000.100.110]
       SOFT_DECLINE_CODES = %w{
         800.100.201 800.100.203 800.100.204 100.150.204 000.100.204
         000.100.205 800.100.205 000.100.221 000.100.223 000.100.225
@@ -31,21 +33,23 @@ module ActiveMerchant #:nodoc:
       }.freeze
 
       def initialize(options = {})
-        requires!(options, :token)
+        requires!(options, :token, :entityId)
         super
         @response_http_code = nil
       end
 
-      def authorize(money, credit_card, options = {})
+      def authorize(money, payment_method, options = {})
         post = {}
-        add_card_details(post, credit_card, options)
+        add_card_details(post, payment_method, options)
+        add_three_d_secure_data(post, money, options) if options[:three_d_secure_data].present?
         add_request_details(post, PAYMENT_CODE_PREAUTHORIZATION, money, options)
         commit(post)
       end
 
-      def purchase(money, credit_card, options = {})
+      def purchase(money, payment_method, options = {})
         post = {}
-        add_card_details(post, credit_card, options)
+        add_three_d_secure_data(post, money, options) if options[:three_d_secure_data].present?
+        add_card_details(post, payment_method, options)
         add_request_details(post, PAYMENT_CODE_DEBIT, money, options)
         commit(post)
       end
@@ -74,47 +78,60 @@ module ActiveMerchant #:nodoc:
         commit(post, transaction_id)
       end
 
+      def confirm(transaction_id)
+        post = {}
+        commit(post, transaction_id, 'confirm')
+      end
+
       private
 
-      def add_card_details(post, credit_card, options)
-        post["paymentBrand"] = options["paymentBrand"]
-        post["card.number"] = credit_card["number"]
-        post["card.holder"] = credit_card["holder"]
-        post["card.expiryMonth"] = credit_card["expiryMonth"]
-        post["card.expiryYear"] = credit_card["expiryYear"]
-        post["card.cvv"] = credit_card["cvv"]
-        if options["billing"].present?
-          post["billing.street1"] = options["billing"]["street1"]
-          post["billing.street2"] = options["billing"]["street2"]
-          post["billing.city"] = options["billing"]["city"]
-          post["billing.postcode"] = options["billing"]["postcode"]
-          post["billing.state"] = options["billing"]["state"]
-          post["billing.country"] = options["billing"]["country"]
+      def add_card_details(post, payment_method, options)
+        post["customer.email"] = options[:email]
+        post["customer.ip"] = options[:ip]
+        post["card.number"] = payment_method.number
+        post["card.holder"] = payment_method.name
+        post["card.expiryMonth"] = format(payment_method.month, :two_digits)
+        post["card.expiryYear"] = format(payment_method.year, :four_digits)
+        post["card.cvv"] = payment_method.verification_value unless empty?(payment_method.verification_value)
+        post["shopperResultUrl"] = options[:redirect_links][:success_url]
+        if options[:billing_address].present?
+          post["billing.street1"] = options[:billing_address][:address1]
+          post["billing.street2"] = options[:billing_address][:address2]
+          post["billing.city"] = options[:billing_address][:city]
+          post["billing.postcode"] = options[:billing_address][:zip]
+          post["billing.state"] = options[:billing_address][:state]
+          post["billing.country"] = options[:billing_address][:country]
+          post['customer.phone'] = options[:billing_address][:phone]
         end
-        if options["shipping"].present?
-          post["shipping.street1"] = options["shipping"]["street1"]
-          post["shipping.street2"] = options["shipping"]["street2"]
-          post["shipping.city"] = options["shipping"]["city"]
-          post["shipping.postcode"] = options["shipping"]["postcode"]
-          post["shipping.state"] = options["shipping"]["state"]
-          post["shipping.country"] = options["shipping"]["country"]
+        if options[:shipping_address].present?
+          post["shipping.street1"] = options[:shipping_address][:address1]
+          post["shipping.street2"] = options[:shipping_address][:address2]
+          post["shipping.city"] = options[:shipping_address][:city]
+          post["shipping.postcode"] = options[:shipping_address][:zip]
+          post["shipping.state"] = options[:shipping_address][:state]
+          post["shipping.country"] = options[:shipping_address][:country]
+          post['customer.phone'] = options[:shipping_address][:phone]
         end
-        if options["customer"].present?
-          post["customer.email"] = options["customer"]["email"]
-          post["customer.mobile"] = options["customer"]["mobile"]
-        end
+        post["customer.browser.userAgent"] = options[:browser_details][:identity]
       end
 
       def add_request_details(post, payment_code, money, options)
-        post["entityId"] = options["entityId"]
         post["amount"] = money
-        post["currency"] = options["currency"]
+        post["currency"] = options[:currency]
         post["paymentType"] = payment_code
       end
 
-      def commit(params, transaction_id = "")
-        path = "#{url}#{API_VERSION}/payments"
-        path = "#{path}/#{transaction_id}" if transaction_id.present?
+      def add_three_d_secure_data(post, money, options)
+        post["threeDSecure.challengeIndicator"]= options[:three_d_secure_data][:challengeIndicator]
+        post["threeDSecure.exemptionFlag"]= options[:three_d_secure_data][:exemptionFlag]
+        post["threeDSecure.verificationId"]= options[:three_d_secure_data][:verificationId]
+        post["threeDSecure.eci"]= options[:three_d_secure_data][:eci]
+        post["threeDSecure.xid"]= options[:three_d_secure_data][:xid]
+      end
+
+      def commit(params, transaction_id = "", action=nil)
+        params = params.merge("entityId" => @options[:entityId])
+        path = generate_path(action, transaction_id)
         uri = URI(path)
         http = Net::HTTP.new(uri.host, uri.port)
         http.use_ssl = true
@@ -123,14 +140,13 @@ module ActiveMerchant #:nodoc:
         req.set_form_data(params)
         res = http.request(req)
         response_data = JSON.parse(res.body)
-        succeeded = success_from(res.message)
+        succeeded = success_from(response_data)
         Response.new(
           succeeded,
           message_from(succeeded, response_data),
           response_data,
           authorization: authorization_from(response_data, params["paymentType"]),
           test: test?,
-          response_type: response_type(response.dig('result', 'code')),
           response_http_code: @response_http_code,
           request_endpoint: path,
           request_method: :post,
@@ -142,6 +158,14 @@ module ActiveMerchant #:nodoc:
         test? ? test_url : live_url
       end
 
+      def generate_path(action, transaction_id)
+        base_path = "#{url}#{API_VERSION}/"
+        path_suffix = action == 'confirm' ? 'threeDSecure' : 'payments'
+        path = "#{base_path}#{path_suffix}"
+        path << "/#{transaction_id}" if transaction_id.present?
+        path
+      end
+
       def authorization_from(response, payment_type)
         authorization = response["id"].present? ? response["id"] : "Failed"
         [authorization, payment_type].join('#')
@@ -151,12 +175,13 @@ module ActiveMerchant #:nodoc:
         if succeeded
           'Succeeded'
         else
-          response["result"]["description"]
+          response.dig('result', 'description') == 'transaction pending' ? '3DS_REQUIRED' : response.dig('result', 'description')
         end
       end
 
-      def success_from(resonse_message)
-        resonse_message == "OK"
+      def success_from(response)
+        code = response.dig('result', 'code')
+        SUCCESS_CODES.include?(code)
       end
 
 
