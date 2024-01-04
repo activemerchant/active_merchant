e0ecef25be2eeba650d8ef4327b0da719a3c1513
Gustavo Sanmartin <gasb_150@hotmail.com>
2024-01-04 11:51:33 -0500

    HiPay: Scrub/Refund/Void (#4995)
    Ser-720
    SER-721
    ----
    Summary:
    ----
    Adding to the HiPay gateway support for scrub, refund, and void.
    
    HiPay allows partial capture and partial refunds, this commit include the amount to use it.
    
    Tests
    ----
    Remote Test:
    Finished in 6.627757 seconds.
    15 tests, 62 assertions, 0 failures, 0 errors, 0 pendings, 0 omissions, 0 notifications
    100% passed
    
    Unit Tests:
    21 tests, 61 assertions, 0 failures, 0 errors, 0 pendings, 0 omissions, 0 notifications
    100% passed
    
    RuboCop:
    787 files inspected, no offenses detected
    
    Co-authored-by: Gustavo Sanmartin <gsanmartin@EN2010363.local>

--- lib/active_merchant/billing/gateways/hi_pay.rb
+++ lib/active_merchant/billing/gateways/hi_pay.rb
@@ -51,22 +51,41 @@ module ActiveMerchant #:nodoc:
       end
 
       def capture(money, authorization, options)
-        post = {}
-        post[:operation] = 'capture'
-        post[:currency] = (options[:currency] || currency(money))
-        transaction_ref, _card_token, _payment_product = authorization.split('|')
-        commit('capture', post, { transaction_reference: transaction_ref })
+        reference_operation(money, authorization, options.merge({ operation: 'capture' }))
       end
 
       def store(payment_method, options = {})
         tokenize(payment_method, options.merge({ multiuse: '1' }))
       end
 
-      def scrub(transcrip)
-        # code
+      def refund(money, authorization, options)
+        reference_operation(money, authorization, options.merge({ operation: 'refund' }))
+      end
+
+      def void(authorization, options)
+        reference_operation(nil, authorization, options.merge({ operation: 'cancel' }))
+      end
+
+      def supports_scrubbing?
+        true
+      end
+
+      def scrub(transcript)
+        transcript.
+          gsub(%r((Authorization: Basic )[\w =]+), '\1[FILTERED]').
+          gsub(%r((card_number=)\w+), '\1[FILTERED]\2').
+          gsub(%r((cvc=)\w+), '\1[FILTERED]\2')
       end
 
       private
+
+      def reference_operation(money, authorization, options)
+        post = {}
+        post[:operation] = options[:operation]
+        post[:currency] = (options[:currency] || currency(money))
+        post[:amount] = amount(money) if options[:operation] == 'refund' || options[:operation] == 'capture'
+        commit(options[:operation], post, { transaction_reference: authorization.split('|').first })
+      end
 
       def add_product_data(post, options)
         post[:orderid] = options[:order_id] if options[:order_id]
@@ -143,6 +162,10 @@ module ActiveMerchant #:nodoc:
           response['state'] == 'completed'
         when 'capture'
           response['status'] == '118' && response['message'] == 'Captured'
+        when 'refund'
+          response['status'] == '124' && response['message'] == 'Refund Requested'
+        when 'cancel'
+          response['status'] == '175' && response['message'] == 'Authorization Cancellation requested'
         when 'store'
           response.include? 'token'
         else
@@ -170,7 +193,7 @@ module ActiveMerchant #:nodoc:
         case action
         when 'store'
           "#{token_url}/create"
-        when 'capture'
+        when 'capture', 'refund', 'cancel'
           endpoint = "maintenance/transaction/#{options[:transaction_reference]}"
           base_url(endpoint)
         else
