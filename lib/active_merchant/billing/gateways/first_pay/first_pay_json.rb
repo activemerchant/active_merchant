650e70a84aa65085dd58989a2200d154ff184471
Luis Mario Urrea Murillo <sinourain@gmail.com>
2024-02-28 13:35:35 -0500

    FirstPay: Add REST JSON transaction methods (#5035)
    * FirstPay: Add REST JSON transaction methods
    
    Description
    -------------------------
    Spreedly reference:
    [SER-1092](https://spreedly.atlassian.net/browse/SER-1092)
    [SER-1094](https://spreedly.atlassian.net/browse/SER-1094)
    [SER-1093](https://spreedly.atlassian.net/browse/SER-1093)
    
    This commit contains:
    - Setup to support FirstPay XML gateway and FirstPay REST JSON
    - Add the transaction methods for REST JSON: purchase, authorize, capture, void and refund
    - Add scrub method
    - Add unit and remote tests
    
    Unit test
    -------------------------
    Finished in 40.920192 seconds.
    5824 tests, 79102 assertions, 2 failures, 0 errors, 0 pendings, 0 omissions, 0 notifications
    99.9657% passed
    142.33 tests/s, 1933.08 assertions/s
    
    Remote test
    -------------------------
    Finished in 65.655399 seconds.
    14 tests, 34 assertions, 0 failures, 0 errors, 0 pendings, 0 omissions, 0 notifications
    100% passed
    0.21 tests/s, 0.52 assertions/s
    
    Rubocop
    -------------------------
    792 files inspected, no offenses detected
    
    * Changelog entry
    
    ---------
    
    Co-authored-by: Luis Urrea <devluisurrea+endava@gmail.com>
    Co-authored-by: naashton <nashton@gmail.com>

---
+++ lib/active_merchant/billing/gateways/first_pay/first_pay_json.rb
@@ -0,0 +1,166 @@
+require 'active_merchant/billing/gateways/first_pay/first_pay_common'
+
+module ActiveMerchant #:nodoc:
+  module Billing #:nodoc:
+    class FirstPayJsonGateway < Gateway
+      include FirstPayCommon
+
+      ACTIONS = {
+        purchase: 'Sale',
+        authorize: 'Auth',
+        capture: 'Settle',
+        refund: 'Refund',
+        void: 'Void'
+      }.freeze
+
+      self.live_url = 'https://secure.1stpaygateway.net/secure/RestGW/Gateway/Transaction/'
+
+      # Creates a new FirstPayJsonGateway
+      #
+      # The gateway requires two values for connection to be passed
+      # in the +options+ hash.
+      #
+      # ==== Options
+      #
+      # * <tt>:merchant_key</tt>  -- FirstPay's merchant_key (REQUIRED)
+      # * <tt>:processor_id</tt>  -- FirstPay's processor_id or processorId (REQUIRED)
+      def initialize(options = {})
+        requires!(options, :merchant_key, :processor_id)
+        super
+      end
+
+      def purchase(money, payment, options = {})
+        post = {}
+        add_invoice(post, money, options)
+        add_payment(post, payment, options)
+        add_address(post, payment, options)
+
+        commit(:purchase, post)
+      end
+
+      def authorize(money, payment, options = {})
+        post = {}
+        add_invoice(post, money, options)
+        add_payment(post, payment, options)
+        add_address(post, payment, options)
+
+        commit(:authorize, post)
+      end
+
+      def capture(money, authorization, options = {})
+        post = {}
+        add_invoice(post, money, options)
+        add_reference(post, authorization)
+
+        commit(:capture, post)
+      end
+
+      def refund(money, authorization, options = {})
+        post = {}
+        add_invoice(post, money, options)
+        add_reference(post, authorization)
+
+        commit(:refund, post)
+      end
+
+      def void(authorization, options = {})
+        post = {}
+        add_reference(post, authorization)
+
+        commit(:void, post)
+      end
+
+      def scrub(transcript)
+        transcript.
+          gsub(%r(("processorId\\?"\s*:\s*\\?")[^"]*)i, '\1[FILTERED]').
+          gsub(%r(("merchantKey\\?"\s*:\s*\\?")[^"]*)i, '\1[FILTERED]').
+          gsub(%r(("cardNumber\\?"\s*:\s*\\?")[^"]*)i, '\1[FILTERED]').
+          gsub(%r(("cvv\\?"\s*:\s*\\?)[^,]*)i, '\1[FILTERED]')
+      end
+
+      private
+
+      def add_address(post, creditcard, options)
+        if address = options[:billing_address] || options[:address]
+          post[:ownerName] = address[:name]
+          post[:ownerStreet] = address[:address1]
+          post[:ownerCity] = address[:city]
+          post[:ownerState] = address[:state]
+          post[:ownerZip] = address[:zip]
+          post[:ownerCountry] = address[:country]
+        end
+      end
+
+      def add_invoice(post, money, options)
+        post[:orderId] = options[:order_id]
+        post[:transactionAmount] = amount(money)
+      end
+
+      def add_payment(post, payment, options)
+        post[:cardNumber] = payment.number
+        post[:cardExpMonth] = payment.month
+        post[:cardExpYear] = format(payment.year, :two_digits)
+        post[:cvv] = payment.verification_value
+      end
+
+      def add_reference(post, authorization)
+        post[:refNumber] = authorization
+      end
+
+      def commit(action, parameters)
+        response = parse(api_request(live_url + ACTIONS[action], post_data(parameters)))
+
+        Response.new(
+          success_from(response),
+          message_from(response),
+          response,
+          authorization: authorization_from(response),
+          error_code: error_code_from(response),
+          test: test?
+        )
+      end
+
+      def api_request(url, data)
+        ssl_post(url, data, headers)
+      rescue ResponseError => e
+        e.response.body
+      end
+
+      def parse(data)
+        JSON.parse data
+      end
+
+      def headers
+        { 'Content-Type' => 'application/json' }
+      end
+
+      def format_messages(messages)
+        return unless messages.present?
+
+        messages.map { |message| message['message'] || message }.join('; ')
+      end
+
+      def success_from(response)
+        response['isSuccess']
+      end
+
+      def message_from(response)
+        format_messages(response['errorMessages'] + response['validationFailures']) || response['data']['authResponse']
+      end
+
+      def error_code_from(response)
+        return 'isError' if response['isError']
+
+        return 'validationHasFailed' if response['validationHasFailed']
+      end
+
+      def authorization_from(response)
+        response.dig('data', 'referenceNumber') || ''
+      end
+
+      def post_data(params)
+        params.merge({ processorId: @options[:processor_id], merchantKey: @options[:merchant_key] }).to_json
+      end
+    end
+  end
+end
