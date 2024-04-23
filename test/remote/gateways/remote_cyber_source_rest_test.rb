2d7997e4a17d8cdfe3a8ba4a119fc8c396c9790e
aenand <aenand@spreedly.com>
2024-04-23 14:35:02 -0400

    Cybersource Rest: Support normalized 3DS data
    COMP-80
    
    Adds support for 3DS2 data to the Cybersource Rest gateway.
    This follows the standard pattern in AM to add three_d_secure data and
    takes inspriation from the legacy Cybersource integration for 3DS.
    
    Docs:https://developer.cybersource.com/api-reference-assets/index.html#payments
    
    Test Summary
    Local:
    5848 tests, 79233 assertions, 0 failures, 23 errors, 0 pendings, 0 omissions, 0 notifications
    99.6067% passed
    Tests failing are unrelated to this change
    Unit:
    32 tests, 165 assertions, 0 failures, 0 errors, 0 pendings, 0 omissions, 0 notifications
    100% passed
    Remote:
    49 tests, 150 assertions, 7 failures, 0 errors, 0 pendings, 0 omissions, 0 notifications
    85.7143% passed
    Failing on master too

--- test/remote/gateways/remote_cyber_source_rest_test.rb
+++ test/remote/gateways/remote_cyber_source_rest_test.rb
@@ -512,4 +512,30 @@ class RemoteCyberSourceRestTest < Test::Unit::TestCase
     assert_nil response.params['_links']['capture']
     assert_equal 'AUD', response.params['orderInformation']['amountDetails']['currency']
   end
+
+  def test_successful_authorize_with_3ds2_visa
+    @options[:three_d_secure] = {
+      version: '2.2.0',
+      cavv: '3q2+78r+ur7erb7vyv66vv\/\/\/\/8=',
+      eci: '05',
+      ds_transaction_id: 'ODUzNTYzOTcwODU5NzY3Qw==',
+      enrolled: 'true',
+      authentication_response_status: 'Y'
+    }
+    auth = @gateway.authorize(@amount, @visa_card, @options)
+    assert_success auth
+  end
+
+  def test_successful_authorize_with_3ds2_mastercard
+    @options[:three_d_secure] = {
+      version: '2.2.0',
+      cavv: '3q2+78r+ur7erb7vyv66vv\/\/\/\/8=',
+      eci: '05',
+      ds_transaction_id: 'ODUzNTYzOTcwODU5NzY3Qw==',
+      enrolled: 'true',
+      authentication_response_status: 'Y'
+    }
+    auth = @gateway.authorize(@amount, @master_card, @options)
+    assert_success auth
+  end
 end
