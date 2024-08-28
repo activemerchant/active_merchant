8797c6ad8978e421909efdbb39e0408edb5dcb92
Alma Malambo <amalambo@spreedly.com>
2024-08-28 16:16:08 -0500

    Decidir: Switch order of NT fields
    Switch order of expiration_month and expiration_year for
    NT in order to successfully scrub token.
    
    Remote:
    27 tests, 97 assertions, 0 failures, 0 errors, 0 pendings, 0 omissions, 0 notifications
    100% passed

--- lib/active_merchant/billing/gateways/decidir.rb
+++ lib/active_merchant/billing/gateways/decidir.rb
@@ -203,11 +203,11 @@ module ActiveMerchant #:nodoc:
         post[:card_data][:security_code] = payment_method.verification_value if payment_method.verification_value? && options[:pass_cvv_for_nt]
 
         post[:token_card_data] = {
-          expiration_month: format(payment_method.month, :two_digits),
-          expiration_year: format(payment_method.year, :two_digits),
           token: payment_method.number,
           eci: payment_method.eci,
-          cryptogram: payment_method.payment_cryptogram
+          cryptogram: payment_method.payment_cryptogram,
+          expiration_month: format(payment_method.month, :two_digits),
+          expiration_year: format(payment_method.year, :two_digits)
         }
       end
 
