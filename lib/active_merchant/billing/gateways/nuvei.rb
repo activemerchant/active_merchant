80f0f1d4d62cbf312fc5056c34c43c5ffe4c7c0f
Javier Pedroza <jpedroza@spreedly.com>
2024-09-04 17:20:04 -0500

    Nuvei: Add partial approval feature
    Description
    -------------------------
    [SER-1356](https://spreedly.atlassian.net/browse/SER-1356)
    
    This commit enable partial approval for payment sending a GSF flag
    
    Unit test
    -------------------------
    Finished in 0.010615 seconds.
    
    1 tests, 3 assertions, 0 failures, 0 errors, 0 pendings, 0 omissions, 0 notifications
    100% passed
    
    94.21 tests/s, 282.62 assertions/s
    
    Remote test
    -------------------------
    Finished in 37.737084 seconds.
    
    18 tests, 50 assertions, 0 failures, 0 errors, 0 pendings, 0 omissions, 0 notifications
    100% passed
    
    0.48 tests/s, 1.32 assertions/s
    
    Rubocop
    -------------------------
    801 files inspected, no offenses detected

--- lib/active_merchant/billing/gateways/nuvei.rb
+++ lib/active_merchant/billing/gateways/nuvei.rb
@@ -37,6 +37,7 @@ module ActiveMerchant
         add_customer_ip(post, options)
         add_stored_credentials(post, payment, options)
         post[:userTokenId] = options[:user_token_id] if options[:user_token_id]
+        post[:isPartialApproval] = options[:is_partial_approval] ? 1 : 0
 
         commit(:purchase, post)
       end
