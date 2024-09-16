5f83d5de1b1d02e46eef5ed5a8ba702a103a984e
Javier Pedroza <jpedroza@spreedly.com>
2024-09-16 10:16:25 -0500

    Nuvei: Add 3DS Global
    Description
    -------------------------
     This commit enable 3ds Global for Nuvei
    
    Unit test
    -------------------------
    Finished in 1.229216 seconds.
    
    19 tests, 98 assertions, 0 failures, 0 errors, 0 pendings, 0 omissions, 0 notifications
    100% passed
    
    15.46 tests/s, 79.73 assertions/s
    
    Remote test
    -------------------------
    Finished in 106.016738 seconds.
    
    30 tests, 99 assertions, 0 failures, 0 errors, 0 pendings, 0 omissions, 0 notifications
    100% passed
    
    0.28 tests/s, 0.93 assertions/s
    
    Rubocop
    -------------------------
    801 files inspected, no offenses detected

--- test/remote/gateways/remote_nuvei_test.rb
+++ test/remote/gateways/remote_nuvei_test.rb
@@ -11,6 +11,7 @@ class RemoteNuveiTest < Test::Unit::TestCase
     @challenge_credit_card = credit_card('2221008123677736', first_name: 'CL-BRW2', last_name: '')
     @three_ds_amount = 151 # for challenge = 151, for frictionless >= 150
     @frictionless_credit_card = credit_card('4000020951595032', first_name: 'FL-BRW2', last_name: '')
+    @credit_card_3ds = credit_card('4000020951595032')
 
     @options = {
       email: 'test@gmail.com',
@@ -38,6 +39,14 @@ class RemoteNuveiTest < Test::Unit::TestCase
         }
       }
     }
+
+    @three_d_secure_options = @options.merge({
+      three_d_secure: {
+        cavv: 'jJ81HADVRtXfCBATEp01CJUAAAA=',
+        ds_transaction_id: '97267598-FAE6-48F2-8083-C23433990FBC',
+        eci: '05'
+      }
+    })
   end
 
   def test_transcript_scrubbing
@@ -268,5 +277,29 @@ class RemoteNuveiTest < Test::Unit::TestCase
     recurring_response = @gateway.purchase(@amount, @credit_card, stored_credential_options)
     assert_success recurring_response
     assert_match 'SUCCESS', recurring_response.params['status']
+  end
+
+  def test_failing_purchase_three_d_secure
+    @three_d_secure_options[:three_d_secure][:cavv] = 'wrong_cavv_value'
+    assert response = @gateway.purchase(@amount, @credit_card_3ds, @three_d_secure_options)
+    assert_failure response
+    assert_equal 'UNEXPECTED SYSTEM ERROR - PLEASE RETRY LATER', response.message
+    assert_match 'ERROR', response.params['transactionStatus']
+  end
+
+  def test_successful_purchase_with_three_d_secure
+    assert response = @gateway.purchase(@amount, @credit_card_3ds, @three_d_secure_options)
+    assert_success response
+    assert response.authorization
+    assert_equal 'APPROVED', response.message
+    assert_match 'SUCCESS', response.params['status']
+  end
+
+  def test_successful_purchase_three_d_secure_challenge_preference
+    assert response = @gateway.purchase(@amount, @credit_card_3ds, @three_d_secure_options.merge(challenge_preference: 'ExemptionRequest', exemption_request_reason: 'AccountVerification'))
+    assert_success response
+    assert_equal 'APPROVED', response.message
+    assert_match 'SUCCESS', response.params['status']
+    assert response.authorization
   end
 end
