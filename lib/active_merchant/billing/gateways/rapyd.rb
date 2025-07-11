9daec4c7108bb4e6599fb58adab19f8796eaa1ac
ritesh-kapoor-spreedly <rkapoor@spreedly.com>
2025-07-11 22:09:28 +0530

    Normalize API  for Rapyd, SafeCharge and SecureNet (#5514)
    Remote Tests
    
    Rapyd
    56 tests, 144 assertions, 15 failures, 0 errors, 0 pendings, 0 omissions, 0 notifications
    73.2143% passed
    
    SafeCharge & SecureNet
    Credentials are invalid

--- lib/active_merchant/billing/gateways/rapyd.rb
+++ lib/active_merchant/billing/gateways/rapyd.rb
@@ -3,11 +3,13 @@ module ActiveMerchant # :nodoc:
     class RapydGateway < Gateway
       class_attribute :payment_redirect_test, :payment_redirect_live
 
-      self.test_url = 'https://sandboxapi.rapyd.net/v1/'
-      self.live_url = 'https://api.rapyd.net/v1/'
+      version 'v1'
 
-      self.payment_redirect_test = 'https://sandboxpayment-redirect.rapyd.net/v1/'
-      self.payment_redirect_live = 'https://payment-redirect.rapyd.net/v1/'
+      self.test_url = "https://sandboxapi.rapyd.net/#{fetch_version}/"
+      self.live_url = "https://api.rapyd.net/#{fetch_version}/"
+
+      self.payment_redirect_test = "https://sandboxpayment-redirect.rapyd.net/#{fetch_version}/"
+      self.payment_redirect_live = "https://payment-redirect.rapyd.net/#{fetch_version}/"
 
       self.supported_countries = %w(CA CL CO DO SV PE PT VI AU HK IN ID JP MY NZ PH SG KR TW TH VN AD AT BE BA BG HR CY CZ DK EE FI FR GE DE GI GR GL HU IS IE IL IT LV LI LT LU MK MT MD MC ME NL GB NO PL RO RU SM SK SI ZA ES SE CH TR VA)
       self.default_currency = 'USD'
@@ -335,7 +337,7 @@ module ActiveMerchant # :nodoc:
       end
 
       def commit(method, action, parameters)
-        rel_path = "#{method}/v1/#{action}"
+        rel_path = "#{method}/#{fetch_version}/#{action}"
         response = api_request(method, url(action, @options[:url_override]), rel_path, parameters)
 
         Response.new(
